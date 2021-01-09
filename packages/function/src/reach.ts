4ad8731bb5bf1b29a3f281c7cd46ae82d5882ea3
wtklbm <wtklbm@gmail.com>
2021-01-09 22:55:23 +0800

    📃docs: 增加文档说明

--- packages/function/src/reach.ts
+++ packages/function/src/reach.ts
@@ -4,12 +4,17 @@ import { isIntSafe } from '@curong/types';
  * 当函数执行到达指定次数后执行回调函数
  *
  * @param count 要等待的总次数 (必须大于 `0`)
- * @param callback 要执行的回调函数
+ * @param callback 当次数到达时所要执行的回调函数
  * @example
  * ```javascript
  * let r = reach(3, () => 10);
  * console.log(r()()()); // 10
  * ```
+ *
+ * # 柯里化函数
+ *
+ * 该函数是柯里化函数，柯里化函数就是把一个大函数拆分成很多的具体的功能的小函数。
+ * 高阶函数中包含柯里化，柯理化的好处是可以保留参数，它非常像 `bind` 方法。
  */
 export default function reach<T>(count: number, callback: () => T): () => any {
     if (!isIntSafe(count)) {
