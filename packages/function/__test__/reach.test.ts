03775a12783fd44cdf48a444fbe14ce8ee482422
wtklbm <wtklbm@gmail.com>
2021-01-09 22:14:34 +0800

    🧪test: 增加测试

--- packages/function/__test__/reach.test.ts
+++ packages/function/__test__/reach.test.ts
@@ -31,4 +31,13 @@ describe('@curong/function/reach', () => {
 
         expect(ret).toBe(10);
     });
+
+    test('测试5', () => {
+        expect(() => reach(3.1, () => 1)).toThrow();
+        expect(() => reach(-3.1, () => 1)).toThrow();
+    });
+
+    test('测试6', () => {
+        expect(reach(0, () => 1)()).toBe(1);
+    });
 });
