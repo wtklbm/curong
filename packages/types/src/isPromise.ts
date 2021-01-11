95c3f05b65e1fb8dbfdc3bc4dbdf74f6d0864e59
wtklbm <wtklbm@gmail.com>
2021-01-11 20:59:18 +0800

    📃docs: 添加 `polyfill`

--- packages/types/src/isPromise.ts
+++ packages/types/src/isPromise.ts
@@ -3,10 +3,21 @@ import getTag from './getTag';
 /**
  * 是不是一个 `Promise`
  *
- * `Promise` 是一个对象或函数，并包含 `then` 方法。
+ * `Promise` 可能是一个对象或者是一个函数，并且包含 `then` 方法。
  *
  * @param value 要验证的值
  * @returns 是则返回 `true`，否则为 `false`
+ *
+ * @polyfill
+ * ```javascript
+ * const isPromise = value => {
+ *     return (
+ *         ((typeof value === 'object' && value !== null) ||
+ *             typeof value === 'function') &&
+ *         typeof value.then === 'function'
+ *     );
+ * };
+ * ```
  */
 export default function isPromise(value: any): value is Promise<any> {
     return getTag(value) === 'Promise';
