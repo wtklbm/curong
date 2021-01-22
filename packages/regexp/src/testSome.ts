39ea02c72bdc7f7fbf2aa1596d174b0a2bec12a5
wtklbm <wtklbm@gmail.com>
2021-01-22 16:19:50 +0800

    🦄refactor: 删除冗余断言

--- packages/regexp/src/testSome.ts
+++ packages/regexp/src/testSome.ts
@@ -1,5 +1,3 @@
-import { isRegExp, isString, isArray } from '@curong/types';
-
 /**
  * 使用一组正则来验证一个字符串是否符合预期
  *
@@ -11,27 +9,13 @@ import { isRegExp, isString, isArray } from '@curong/types';
  * - 如果 `regexps` 不为空并且 `str` 为空，则返回 `false`
  * - 如果 `regexps` 为空并且 `str` 为空，则报错
  */
-export default function testSome(regexps: Array<RegExp>, str: string): boolean {
-    if (!isString(str)) {
-        throw new TypeError(`[testSome]: str不是一个字符串, "${str}"`);
-    }
-
-    if (!isArray(regexps)) {
-        throw new TypeError(
-            `[testSome]: regexps不是一个正则数组, "${regexps}"`
-        );
-    }
-
+export default function testSome(regexps: RegExp[], str: string): boolean {
     if (str.length === 0 && regexps.length === 0) {
         throw new TypeError('[testSome]: str和regexps不能同时为空');
     }
 
     for (let i = 0, len = regexps.length; i < len; i++) {
         const reg = regexps[i];
-
-        if (!isRegExp(reg)) {
-            throw new TypeError(`[testSome]: reg不是一个正则, "${reg}"`);
-        }
 
         if (reg.test(str)) {
             return true;
