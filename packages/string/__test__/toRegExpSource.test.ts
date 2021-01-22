b6b1eb7b295f06aff4be46aff0b19fe06acd40b1
wtklbm <wtklbm@gmail.com>
2021-01-22 17:37:58 +0800

    🦄refactor: 删除冗余测试

--- packages/string/__test__/toRegExpSource.test.ts
+++ packages/string/__test__/toRegExpSource.test.ts
@@ -1,12 +1,7 @@
-// @ts-nocheck
 import { toRegExpSource } from '../src';
 
 describe('@curong/string/toRegExpSource', () => {
     test('测试1', () => {
-        expect(toRegExpSource).toThrowError();
-    });
-
-    test('测试2', () => {
         expect(toRegExpSource('xxx')).toBe('xxx');
         expect(toRegExpSource('+')).toBe('\\+');
     });
