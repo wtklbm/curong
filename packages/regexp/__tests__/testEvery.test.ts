ec2d201e7896c2cfc3997e29f0442dc059260c18
wtklbm <wtklbm@gmail.com>
2021-01-22 16:50:03 +0800

    🦄refactor: 删除冗余测试

--- packages/regexp/__tests__/testEvery.test.ts
+++ packages/regexp/__tests__/testEvery.test.ts
@@ -1,4 +1,3 @@
-// @ts-nocheck
 import { testEvery } from '../src';
 
 describe('@curong/regexp/testEvery', () => {
@@ -7,8 +6,11 @@ describe('@curong/regexp/testEvery', () => {
     });
 
     test('测试2', () => {
-        // @ts-ignore
-        expect(() => testEvery([/\d+/])).toThrowError();
+        const regexps = [/\d+/, /^[\w ]+$/];
+        const str = 'hello 123 world';
+        const res = testEvery(regexps, str);
+
+        expect(res).toBe(true);
     });
 
     test('测试3', () => {
@@ -28,37 +30,14 @@ describe('@curong/regexp/testEvery', () => {
     });
 
     test('测试5', () => {
-        const regexps = [/\d+/, /^[\w ]+$/];
-        const str = 'hello 123 world';
-        const res = testEvery(regexps, str);
-
-        expect(res).toBe(true);
+        expect(() => testEvery([], '')).toThrowError();
     });
 
     test('测试6', () => {
-        // @ts-ignore
-        expect(() => testEvery(123, 'xxx')).toThrowError();
-    });
-
-    test('测试7', () => {
-        const regexps = [/\d+/, 0, /^[\w ]+$/];
-        const str = 'hello 123 world';
-
-        // @ts-ignore
-        expect(() => testEvery(regexps, str)).toThrowError();
-    });
-
-    test('测试8', () => {
         expect(testEvery([/\d+/], '')).toBe(false);
     });
 
-    test('测试9', () => {
+    test('测试7', () => {
         expect(testEvery([], 'xxx')).toBe(true);
-    });
-
-    test('测试10', () => {
-        expect(() => testEvery([], '')).toThrowError();
-        expect(() => testEvery([], null)).toThrowError();
-        expect(() => testEvery(null, null)).toThrowError();
     });
 });
