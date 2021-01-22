1511f8b2c29169edf5c21b59780fc0de2b6e751f
wtklbm <wtklbm@gmail.com>
2021-01-22 17:31:22 +0800

    🦄refactor: 删除冗余测试

--- packages/regexp/__tests__/inChkPunctuation.test.ts
+++ packages/regexp/__tests__/inChkPunctuation.test.ts
@@ -1,16 +1,7 @@
-// @ts-nocheck
 import { inChkPunctuation } from '../src';
 
 describe('@curong/regexp/inChkPunctuation', () => {
     test('测试1', () => {
-        expect(inChkPunctuation).toThrowError();
-    });
-
-    test('测试2', () => {
-        expect(() => inChkPunctuation(123)).toThrowError();
-    });
-
-    test('测试3', () => {
         expect(inChkPunctuation('我爱中国！')).toBe(true);
         expect(inChkPunctuation('xxx!')).toBe(false);
         expect(inChkPunctuation('我爱中国')).toBe(false);
