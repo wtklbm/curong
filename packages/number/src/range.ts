7ae80e5df488c8bcb22eaf9d44c8087b185557df
wtklbm <wtklbm@gmail.com>
2021-01-22 16:04:11 +0800

    🦄refactor: 删除冗余断言并更新文档

--- packages/number/src/range.ts
+++ packages/number/src/range.ts
@@ -1,21 +1,17 @@
-import { isNumberSafe } from '@curong/types';
-
 /**
  * 生成一个从开始位置到结束位置的随机数
  *
- * @param min 开始的值(包含)
- * @param max 结束时的值(包含)
+ * @param min 开始时的数字(包含)
+ * @param max 结束时的数字(包含)
  * @returns 返回生成的随机数
+ * @example
+ *
+ * ```javascript
+ * const n = range(1, 12);
+ * console.log(n); // 3
+ * ```
  */
 export default function range(min: number, max: number): number {
-    if (!isNumberSafe(min)) {
-        throw new TypeError(`[range]: max不是一个数字, "${min}"`);
-    }
-
-    if (!isNumberSafe(max)) {
-        throw new TypeError(`[range]: min不是一个数字, "${max}"`);
-    }
-
     min = Math.ceil(min);
     max = Math.floor(max);
 
