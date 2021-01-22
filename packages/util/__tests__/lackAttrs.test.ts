208122f1ae2a1f5db52e0d6fb0d3963fdda0b6d0
wtklbm <wtklbm@gmail.com>
2021-01-22 17:06:06 +0800

    🦄refactor: 删除冗余测试

--- packages/util/__tests__/lackAttrs.test.ts
+++ packages/util/__tests__/lackAttrs.test.ts
@@ -4,11 +4,6 @@ const obj = { a: 1, s: 's' };
 
 describe('@curong/util/lackAttrs', () => {
     test('测试1', () => {
-                expect(lackAttrs(null)).toEqual([]);
-                expect(lackAttrs(null, null)).toEqual([]);
-    });
-
-    test('测试2', () => {
         expect(lackAttrs(obj, ['a', 'b', 'c'])).toEqual(['b', 'c']);
     });
 });
