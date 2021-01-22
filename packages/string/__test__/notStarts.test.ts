b2106367e74df1eb3b61e35bca22d9c2c2ae4b19
wtklbm <wtklbm@gmail.com>
2021-01-22 16:53:32 +0800

    🦄refactor: 删除冗余测试

--- packages/string/__test__/notStarts.test.ts
+++ packages/string/__test__/notStarts.test.ts
@@ -3,17 +3,11 @@ import { notStarts } from '../src';
 describe('@curong/string/notStarts', () => {
     test('测试1', () => {
         expect(notStarts).toThrowError();
-        // @ts-ignore
-        expect(() => notStarts('xxx', 'x')).toThrowError();
-        // @ts-ignore
-        expect(() => notStarts('xxx', [12, 'x', /\d/])).toThrowError();
     });
 
     test('测试2', () => {
         expect(notStarts('', ['x'])).toBe(true);
         expect(notStarts('xxx', ['a', 'b', 'c'])).toBe(true);
-        //@ts-ignore
-        expect(notStarts('xxx', ['x', 12, /\d/])).toBe(false);
         expect(notStarts('xxx', ['x'])).toBe(false);
         expect(notStarts('x x x', ['x', 'a', 'b'])).toBe(false);
         expect(notStarts('x x x', ['x', 'a', 'b'], { position: 1 })).toBe(true);
