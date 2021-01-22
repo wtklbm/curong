9e7d3b4af498f5a9987ebfe84881a890ca6944b3
wtklbm <wtklbm@gmail.com>
2021-01-22 18:39:15 +0800

    🐞fix: 删除一个判断

--- packages/types/src/isTransform.ts
+++ packages/types/src/isTransform.ts
@@ -2,7 +2,6 @@ import { Transform } from 'stream';
 
 import isDuplex from './isDuplex';
 import isFunctionHave from './isFunctionHave';
-import isObject from './isObject';
 
 /**
  * 是不是一个 `Transform`
@@ -11,10 +10,5 @@ import isObject from './isObject';
  * @returns 是则返回 `true`，否则为 `false`
  */
 export default function isTransform(value: any): value is Transform {
-    return (
-        isDuplex(value) &&
-        isFunctionHave((value as Transform)._transform) &&
-        // @ts-ignore
-        isObject(value._transformState)
-    );
+    return isDuplex(value) && isFunctionHave((value as Transform)._transform);
 }
