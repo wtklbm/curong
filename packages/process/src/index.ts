181054cc7be984dc340da3aff73398ae4b96dde9
wtklbm <wtklbm@gmail.com>
2021-01-23 17:02:56 +0800

    ✨feat: 添加 `sleep` 方法

--- packages/process/src/index.ts
+++ packages/process/src/index.ts
@@ -1,7 +1,8 @@
-import spawn from './spawn';
 import exec from './exec';
+import sleep from './sleep';
+import spawn from './spawn';
 
 export * from './types/spawn';
 export * from './types/exec';
 
-export { spawn, exec };
+export { exec, sleep, spawn };
