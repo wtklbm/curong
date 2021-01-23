c9a9286106be0892b49d9573014d50c331481bd8
wtklbm <wtklbm@gmail.com>
2021-01-23 17:10:47 +0800

    ✨feat: 添加 `minLengthIndex` 方法

---
+++ packages/array/src/minLengthIndex.ts
@@ -0,0 +1,32 @@
+import { isZero, isEqual } from '@curong/types';
+
+/**
+ * 获取字符串数组中最小长度的项的索引位置
+ *
+ * @param lines 字符串数组
+ * @returns 如果数组不为空则返回找到的最小长度的索引，否则返回 `null`
+ */
+export default function minLengthIndex(lines: string[]): number | null {
+    const linesLength = lines.length;
+
+    if (isZero(linesLength)) {
+        return null;
+    }
+
+    let minIdx = 0;
+
+    if (isEqual(linesLength, 1)) {
+        return minIdx;
+    }
+
+    let minLen = lines[0].length;
+
+    for (let i = 1, len = lines.length, lineLen; i < len; i++) {
+        if ((lineLen = lines[i].length) < minLen) {
+            minLen = lineLen;
+            minIdx = i;
+        }
+    }
+
+    return minIdx;
+}
