9a9c2218e5c3edae1a39228dd4ad20d354fa2331
wtklbm <wtklbm@gmail.com>
2021-01-23 17:16:04 +0800

    ✨feat: 添加 `minLengthIndex` 方法

---
+++ packages/array/__tests__/minLengthIndex.test.ts
@@ -0,0 +1,13 @@
+import { minLengthIndex } from '../src';
+
+describe('@curong/array/maxLengthIndex', () => {
+    test('测试1', () => {
+        expect(minLengthIndex([])).toBe(null);
+        expect(minLengthIndex(['test'])).toBe(0);
+    });
+
+    test('测试2', () => {
+        const lines = ['hello.', 'this is a test.', 'very match.'];
+        expect(minLengthIndex(lines)).toBe(0);
+    });
+});
