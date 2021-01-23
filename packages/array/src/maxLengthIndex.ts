9f6f0b29b6be98e9d2cdd81e9a1528dee1192112
wtklbm <wtklbm@gmail.com>
2021-01-23 17:08:27 +0800

    ✨feat: 添加 `maxLengthIndex` 方法

---
+++ packages/array/src/maxLengthIndex.ts
@@ -0,0 +1,32 @@
+import { isZero, isEqual } from '@curong/types';
+
+/**
+ * 获取字符串数组中最大长度的项的索引位置
+ *
+ * @param lines 字符串数组
+ * @returns 如果数组不为空则返回找到的最大长度的索引，否则返回 `null`
+ */
+export default function maxLengthIndex(lines: string[]): number | null {
+    const linesLength = lines.length;
+
+    if (isZero(linesLength)) {
+        return null;
+    }
+
+    let maxIdx = 0;
+
+    if (isEqual(linesLength, 1)) {
+        return maxIdx;
+    }
+
+    let maxLen = lines[0].length;
+
+    for (let i = 1, len = lines.length, lineLen; i < len; i++) {
+        if ((lineLen = lines[i].length) > maxLen) {
+            maxLen = lineLen;
+            maxIdx = i;
+        }
+    }
+
+    return maxIdx;
+}
