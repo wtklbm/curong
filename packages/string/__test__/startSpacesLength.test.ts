7c53acb2446165754b58a3d470ad40a265bb8f14
wtklbm <wtklbm@gmail.com>
2021-01-23 17:49:03 +0800

    ✨feat: 添加 `startSpacesLength` 方法

---
+++ packages/string/__test__/startSpacesLength.test.ts
@@ -0,0 +1,8 @@
+import { startSpacesLength } from '../src';
+
+describe('@curong/string/startSpacesLength', () => {
+    test('测试1', () => {
+        expect(startSpacesLength('+')).toBe(0);
+        expect(startSpacesLength(' +')).toBe(1);
+    });
+});
