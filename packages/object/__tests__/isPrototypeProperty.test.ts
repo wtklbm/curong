6684cf7a50020cad45dbe186256d40c5707e2790
wtklbm <wtklbm@gmail.com>
2021-01-25 17:44:15 +0800

    🧪test: 添加 `isPrototypeProperty` 方法的测试

---
+++ packages/object/__tests__/isPrototypeProperty.test.ts
@@ -0,0 +1,20 @@
+import { isPrototypeProperty } from '../src';
+
+describe('@curong/object/isPrototypeProperty', () => {
+    test('测试1', () => {
+        class Arr extends Array {
+            constructor() {
+                super();
+            }
+
+            private sortBy() {}
+
+            static sortByKey() {}
+        }
+
+        const arr = new Arr();
+        expect(isPrototypeProperty(arr, 'slice')).toBe(true);
+        expect(isPrototypeProperty(arr, 'sortBy')).toBe(true);
+        expect(isPrototypeProperty(arr, 'sortByKey')).toBe(false);
+    });
+});
