757d3afd330d545cb00d4edd748ee4a3c7d77882
wtklbm <wtklbm@gmail.com>
2021-01-25 17:44:45 +0800

    📃docs: 添加示例文档

--- packages/object/src/createWithNull.ts
+++ packages/object/src/createWithNull.ts
@@ -1,10 +1,33 @@
+import { isObject } from '@curong/types';
+
 import { ObjectType } from './types/objectType';
 
 /**
  * 创建一个纯对象，该对象的原型是 `null`
  *
+ * @param properties 包含一个或多个属性描述符的 JavaScript 对象
  * @returns 返回一个纯对象
+ * @example
+ *
+ * ```javascript
+ * const obj = createWithNull({
+ *     a: {
+ *         get() {
+ *             return 1;
+ *         }
+ *     }
+ * });
+ *
+ * console.log(obj.__proto__); // undefined
+ * console.log(obj.a); // 1
+ * ```
  */
-export default function createWithNull(): ObjectType {
+export default function createWithNull(
+    properties?: PropertyDescriptorMap & ThisType<any>
+): ObjectType {
+    if (isObject(properties)) {
+        return Object.create(null, properties);
+    }
+
     return Object.create(null);
 }
