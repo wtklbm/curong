7ceee158dd5408a528444535019c9852fe836874
wtklbm <wtklbm@gmail.com>
2021-01-26 19:28:12 +0800

    📃docs: 添加示例文档

--- packages/string/src/rmControlLeft.ts
+++ packages/string/src/rmControlLeft.ts
@@ -5,6 +5,12 @@ import { controlLeftReg } from './characters';
  *
  * @param value 要删除字符的字符串
  * @returns 返回处理好的字符串
+ * @example
+ *
+ * ```javascript
+ * const ret = rmControlLeft(`\b\r\nxx\vx\t\n`);
+ * console.log(ret); // 'xx\vx\t\n'
+ * ```
  */
 export default function rmControlLeft(value: string): string {
     return value.replace(controlLeftReg, '');
