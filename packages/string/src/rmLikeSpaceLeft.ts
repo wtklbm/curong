9b611d4baf3a6c642cbbebd63a39a529e9f6ca3b
wtklbm <wtklbm@gmail.com>
2021-01-26 19:35:45 +0800

    📃docs: 添加示例文档

--- packages/string/src/rmLikeSpaceLeft.ts
+++ packages/string/src/rmLikeSpaceLeft.ts
@@ -5,6 +5,12 @@ import { likeSpaceLeftReg } from './characters';
  *
  * @param value 要删除字符的字符串
  * @returns 返回处理好的字符串
+ * @example
+ *
+ * ```javascript
+ * const ret = rmLikeSpaceLeft(`\u3000\u3000x\u3000xx\u3000\u3000`);
+ * console.log(ret); // 'x\u3000xx\u3000\u3000'
+ * ```
  */
 export default function rmLikeSpaceLeft(value: string): string {
     return value.replace(likeSpaceLeftReg, '');
