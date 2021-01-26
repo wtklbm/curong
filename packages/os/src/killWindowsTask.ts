2f9858409292a2042dded04cd36bb14357949768
wtklbm <wtklbm@gmail.com>
2021-01-26 15:59:42 +0800

    📃docs: 添加示例文档

--- packages/os/src/killWindowsTask.ts
+++ packages/os/src/killWindowsTask.ts
@@ -6,6 +6,12 @@ import { exec } from '@curong/process';
  * @param imageName 要结束的映像名称(包含 `.exe` 后缀扩展名，比如 `cmd.exe`)
  * @returns 如果进程正确退出则返回 `true`，否则为 `false`。
  * 返回 `false` 的情况还有可能是因为没有找到当前要结束的那个进程。
+ * @example
+ *
+ * ```javascript
+ * const imageName = 'notepad.exe';
+ * await killWindowsTask(imageName);
+ * ```
  */
 export default async function killWindowsTask(
     imageName: string
