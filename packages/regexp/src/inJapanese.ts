97c9a683863765f80b479d7385b76a1c242fa849
wtklbm <wtklbm@gmail.com>
2021-01-26 16:45:22 +0800

    📃docs: 添加示例文档

--- packages/regexp/src/inJapanese.ts
+++ packages/regexp/src/inJapanese.ts
@@ -6,6 +6,12 @@ import { japanese } from './source';
  * @param str 要验证的字符串
  * @returns 是则返回 `true`，否则为 `false`
  * @info 日文中包含汉字，所以用该方法验证汉字也返回 `true`
+ * @example
+ *
+ * ```javascript
+ * const ret = inJapanese('こんにちは中国');
+ * console.log(ret); // true
+ * ```
  */
 export default function inJapanese(str: string) {
     return new RegExp(`${japanese}+`).test(str);
