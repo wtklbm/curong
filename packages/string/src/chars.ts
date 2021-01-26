ae7c7799818fa60cc68809c5572d4ded2739c862
wtklbm <wtklbm@gmail.com>
2021-01-26 19:04:05 +0800

    📃docs: 添加示例文档

--- packages/string/src/chars.ts
+++ packages/string/src/chars.ts
@@ -5,6 +5,12 @@
  *
  * @param value 要转换的字符串
  * @returns 返回转换好的字符数组
+ * @example
+ *
+ * ```javascript
+ * const ret = chars('中国');
+ * console.log(ret); // [ '中', '国' ]
+ * ```
  */
 export default function chars(value: string): string[] {
     return value.split('');
