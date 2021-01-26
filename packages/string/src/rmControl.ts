84611327f232d0a1591e077d4caa34713fd22399
wtklbm <wtklbm@gmail.com>
2021-01-26 19:26:37 +0800

    📃docs: 添加示例文档

--- packages/string/src/rmControl.ts
+++ packages/string/src/rmControl.ts
@@ -5,6 +5,12 @@ import { controlReg } from './characters';
  *
  * @param value 要删除字符的字符串
  * @returns 返回处理好的字符串
+ * @example
+ *
+ * ```javascript
+ * const ret = rmControl(`\b\r\nxx\vx\t\n`);
+ * console.log(ret); // 'xxx'
+ * ```
  */
 export default function rmControl(value: string): string {
     return value.replace(controlReg, '');
