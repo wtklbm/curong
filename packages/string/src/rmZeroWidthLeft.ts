d41a296a5d27186779bd50a851f597a1afb19f92
wtklbm <wtklbm@gmail.com>
2021-01-26 19:39:31 +0800

    📃docs: 添加示例文档

--- packages/string/src/rmZeroWidthLeft.ts
+++ packages/string/src/rmZeroWidthLeft.ts
@@ -5,6 +5,12 @@ import { zeroWidthLeftReg } from './characters';
  *
  * @param value 要删除字符的字符串
  * @returns 返回处理好的字符串
+ * @example
+ *
+ * ```javascript
+ * const ret = rmZeroWidthLeft(`\u200B\u200Bx\u200Bxx\u200B\u200B`);
+ * console.log(ret); // 'x\u200Bxx\u200B\u200B'
+ * ```
  */
 export default function rmZeroWidthLeft(value: string): string {
     return value.replace(zeroWidthLeftReg, '');
