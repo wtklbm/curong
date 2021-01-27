ee926a765d3ce9736dd80115832255ecd892bf9a
wtklbm <wtklbm@gmail.com>
2021-01-27 19:10:17 +0800

    📃docs: 添加默认参数文档

--- packages/fs/src/types/readFile.ts
+++ packages/fs/src/types/readFile.ts
@@ -1,5 +1,10 @@
 export type ReadFileOptions = {
+    /** 文件的编码，默认 `utf8` */
     encoding?: BufferEncoding;
+
+    /** 标识，默认 `r+` */
     flag?: string;
+
+    /** 权限，默认为 `0o777` */
     mode?: number;
 };
