3e21fee7e128e5bf8a3637bc16ba1a1c5107a102
wtklbm <wtklbm@gmail.com>
2021-01-27 19:18:21 +0800

    📃docs: 添加默认参数文档

--- packages/regexp/src/inChinese.ts
+++ packages/regexp/src/inChinese.ts
@@ -4,7 +4,7 @@ import { unifiedIdeograph, hiragana, katakana } from './source';
  * 是否包含中文汉字
  *
  * @param str 要验证的字符串
- * @param vhk 是否在该字符串中不能包含日文(平假名和片假名)
+ * @param vhk 是否在该字符串中不能包含日文(平假名和片假名)，默认为 `false`
  * @returns 是则返回 `true`，否则为 `false`
  * @example
  *
