31123789b6ddcd01b4685693cb830db0b95e9a43
wtklbm <wtklbm@gmail.com>
2021-01-27 19:43:36 +0800

    📃docs: 添加默认参数文档

--- packages/term/src/types/progressBar.ts
+++ packages/term/src/types/progressBar.ts
@@ -1,16 +1,16 @@
 export type ProgressBarOptions = {
-    /** 总数 */
+    /** 总数，默认为 `NaN` */
     total?: number;
 
-    /** 是否显示百分比 */
+    /** 是否显示百分比，默认为 `true` */
     percentage?: boolean;
 
-    /** 是否显示计数 */
+    /** 是否显示计数，默认为 `true` */
     count?: boolean;
 
-    /** 是否显示动态进度条图像 */
+    /** 是否显示动态进度条图像，默认为 `true` */
     picture?: boolean;
 
-    /** 是否隐藏光标 */
+    /** 是否隐藏光标，默认为 `true` */
     hiddenCursor?: boolean;
 };
