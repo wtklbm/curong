1d87af970ba676bf89f3d6a2c29ac892d18980ca
wtklbm <wtklbm@gmail.com>
2021-01-27 19:10:32 +0800

    📃docs: 添加默认参数文档

--- packages/fs/src/types/readFragment.ts
+++ packages/fs/src/types/readFragment.ts
@@ -1,5 +1,10 @@
 export type ReadFragmentOptions = {
+    /** 开始索引，默认为 0 */
     start?: number;
+
+    /** 结束索引，默认为 0 */
     end?: number;
+
+    /** 是否只读取第一行，默认为 `false` */
     firstLine?: boolean;
 };
