21798f7d664a5adf5cbb95cfcd4b4c82d8ba74f1
wtklbm <wtklbm@gmail.com>
2021-01-27 19:33:31 +0800

    📃docs: 添加默认参数文档

--- packages/string/src/behindOf.ts
+++ packages/string/src/behindOf.ts
@@ -11,7 +11,7 @@ import { GenericOptions } from './types/generic';
  * @param options 配置参数
  *
  * - `position` 偏移位置索引，从该索引的前面找
- * - `caseSensitivity` 是否区分大小写
+ * - `caseSensitivity` 是否区分大小写，默认为 `true`
  *
  * @returns 如果在截取的从索引 `position` 到 `str.length` 的位置的字符串中找到的某个字符串，
  * 只要有一个结果返回 `true`，那么结果就是 `true`，否则返回 `false`。
