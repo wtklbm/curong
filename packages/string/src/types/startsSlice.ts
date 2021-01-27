bd9e72a6030a3222ae11d66272b908cd2b593e7e
wtklbm <wtklbm@gmail.com>
2021-01-27 19:35:48 +0800

    📃docs: 添加默认参数文档

--- packages/string/src/types/startsSlice.ts
+++ packages/string/src/types/startsSlice.ts
@@ -1,6 +1,6 @@
 import { GenericOptions } from './generic';
 
 export interface StartsSlice extends GenericOptions {
-    /** 是否保留从索引 `0` 到偏移索引之间的值 */
+    /** 是否保留从索引 `0` 到偏移索引之间的值，默认为 `false` */
     preserve?: boolean;
 }
