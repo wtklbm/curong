1b5dced34acba177294fa2ffdf6a066a2d66fc14
wtklbm <wtklbm@gmail.com>
2021-01-27 14:34:01 +0800

    📃docs: 添加示例文档

--- packages/term/src/colorCode8bit.ts
+++ packages/term/src/colorCode8bit.ts
@@ -7,6 +7,17 @@ import { ColorCodeResult } from './types/color';
  *
  * @param colorCode 从0到255的任意数字
  * @returns 返回生成好的前景色和背景色终端代码
+ * @example
+ *
+ * ```javascript
+ * const ret = colorCode8bit(23);
+ *
+ * // {
+ * //     foreground: '38;5;23',
+ * //     background: '48;5;23'
+ * // }
+ * console.log(ret);
+ * ```
  */
 export default function colorCode8bit(colorCode: number): ColorCodeResult {
     if (!isUintSafe(colorCode) || colorCode < 0 || colorCode > 255) {
