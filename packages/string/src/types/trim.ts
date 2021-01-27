18412bdbce0dab97423cd1303504dc22b1cdb33d
wtklbm <wtklbm@gmail.com>
2021-01-27 19:35:57 +0800

    📃docs: 添加默认参数文档

--- packages/string/src/types/trim.ts
+++ packages/string/src/types/trim.ts
@@ -1,13 +1,13 @@
 export type TrimOptions = {
-    /** 是否删除空格 */
+    /** 是否删除空格，默认为 `true` */
     space?: boolean;
 
-    /** 是否删除零宽字符 */
+    /** 是否删除零宽字符，默认为 `true` */
     zeroWidth?: boolean;
 
-    /** 是否删除像空格的字符 */
+    /** 是否删除像空格的字符，默认为 `true` */
     likeSpace?: boolean;
 
-    /** 是否删除控制字符(回车、换行、制表符等) */
+    /** 是否删除控制字符(回车、换行、制表符等)，默认为 `true` */
     control?: boolean;
 };
