e6a74d94270c650327fa6e462dcca5d8d03719be
wtklbm <wtklbm@gmail.com>
2021-02-04 14:57:01 +0800

    📃docs: 添加强引用

--- packages/types/src/isMap.ts
+++ packages/types/src/isMap.ts
@@ -5,6 +5,19 @@ import getTag from './getTag';
  *
  * @param value 要验证的值
  * @returns 是则返回 `true`，否则为 `false`
+ *
+ * # 强引用
+ *
+ * `Map` 使用的是强引用，当 `Map` 中引用的变量重新赋值时，`Map` 中的引用并不会被销毁。
+ *
+ * ```javascript
+ * let o = { name: 'wtklbm' };
+ * const map = new Map([[o, o.name]]);
+ *
+ * // 虽然 `o` 的引用已经改为了 `null`，但是因为 `map` 依然引用着 `{ name: 'wtklbm' }`，
+ * // 所以 `{ name: 'wtklbm' }` 所占用的内存并不会被销毁。
+ * o = null;
+ * ```
  */
 export default function isMap(value: any): value is Map<any, any> {
     return getTag(value) === 'Map';
