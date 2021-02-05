e854075f7cf508df1cd0777d3757253b1d89920e
wtklbm <wtklbm@gmail.com>
2021-02-05 13:47:51 +0800

    🦄refactor: 修改 `deleteAttrs` 方法

--- packages/object/src/deleteAttrs.ts
+++ packages/object/src/deleteAttrs.ts
@@ -1,12 +1,14 @@
 import { isUndefined } from '@curong/types';
 
+import derive from './derive';
+
 import { ObjectType } from './types/objectType';
 
 /**
  * 从一个对象中删除一些属性
  *
  * @param obj 对象
- * @param keys 要从对象中的删除的属性字符串数组
+ * @param deleteKeys 要从对象中的删除的属性字符串数组
  * @returns 返回处理好的对象
  * @example
  *
@@ -19,33 +21,17 @@ import { ObjectType } from './types/objectType';
  */
 export default function deleteAttrs<T extends ObjectType>(
     obj: T,
-    keys: Array<keyof T>
+    deleteKeys: Array<keyof T>
 ): T {
     let key: PropertyKey | undefined;
 
-    try {
-        while (!isUndefined((key = keys.shift()))) {
-            delete obj[key as keyof ObjectType];
-        }
+    while (!isUndefined((key = deleteKeys.pop()))) {
+        if (!Reflect.deleteProperty(obj, key)) {
+            deleteKeys.push(key as keyof T);
 
-        return obj;
-    } catch (e) {}
-
-    const ret = Object.create(Object.getPrototypeOf(obj));
-    const keysSet: Set<PropertyKey> = new Set(keys);
-    const objKeys = Object.getOwnPropertyNames(obj);
-
-    while (!isUndefined((key = objKeys.shift()))) {
-        // @ts-ignore
-        if (!keysSet.has(key)) {
-            Object.defineProperty(
-                ret,
-                key!,
-                // @ts-ignore
-                Object.getOwnPropertyDescriptor(obj, key)
-            );
+            return derive(obj, deleteKeys);
         }
     }
 
-    return ret;
+    return obj;
 }
