6fd27cbba85d9e2710b47060b67c248f6429a169
wtklbm <wtklbm@gmail.com>
2021-02-16 16:16:15 +0800

    ✨feat: 添加 `stripBOM` 方法

--- packages/fs/src/index.ts
+++ packages/fs/src/index.ts
@@ -14,6 +14,7 @@ import readline from './readline';
 import readlineFromCode from './readlineFromCode';
 import readlineStream from './readlineStream';
 import readLnk from './readLnk';
+import stripBOM from './stripBOM';
 import writeFile from './writeFile';
 
 export * from './types/destPath';
@@ -40,5 +41,6 @@ export {
     readlineFromCode,
     readlineStream,
     readLnk,
+    stripBOM,
     writeFile
 };
