676560930192a69087eac9132e804e1692fda21a
wtklbm <wtklbm@gmail.com>
2021-02-16 16:16:03 +0800

    ✨feat: 添加 `stripBOM` 方法

---
+++ packages/fs/src/stripBOM.ts
@@ -0,0 +1,9 @@
+/**
+ * 删除字节顺序标记 (BOM)，编码为 `0xFEFF`
+ *
+ * @param content 要使用的字符串
+ * @returns 返回处理好的内容
+ */
+export default function stripBOM(content: string): string {
+    return content.charCodeAt(0) === 0xfeff ? content.slice(1) : content;
+}
