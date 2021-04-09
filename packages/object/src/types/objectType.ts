ec47440b2db6d49b12274dcaad692282a4e47798
wtklbm <wtklbm@gmail.com>
2021-04-09 09:38:15 +0800

    🦄refactor: 修改对象类型的定义

--- packages/object/src/types/objectType.ts
+++ packages/object/src/types/objectType.ts
@@ -1,4 +1,11 @@
-export type PropertyKey = string | number;
+/** 属性名的类型，属性名是 `string`、`number` 和 `symbol` 类型中的某一个类型。  */
+export type PropertyKey = string | number | symbol;
 
-/** 对象的默认类型 */
-export type ObjectType = { [key in PropertyKey]: any };
+/**
+ * 对象的默认类型
+ *
+ * 对象的属性名是 `string` 或 `number` 或 `symbol` 类型，对象的值是 `any` 类型。
+ *
+ * @note `Record<PropertyKey, any>` 相当于 `{ [key in PropertyKey]: any }`
+ */
+export type ObjectType = Record<PropertyKey, any>;
