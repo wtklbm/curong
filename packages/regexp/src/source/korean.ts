d3ac2cbd324302413900aff5decb0ccdda5932bf
wtklbm <wtklbm@gmail.com>
2021-04-16 16:38:36 +0800

    📃docs: 更新文档

--- packages/regexp/src/source/korean.ts
+++ packages/regexp/src/source/korean.ts
@@ -1,7 +1,11 @@
 /**
  * 验证韩文的正则字符串
  *
- * @see http://memory.loc.gov/diglib/codetables/9.3.html
+ * @see
+ *  - http://memory.loc.gov/diglib/codetables/9.3.html
+ *  - [韩文拼音](http://www.unicode.org/charts/PDF/UAC00.pdf)
+ *  - [韩文字母](http://www.unicode.org/charts/PDF/U1100.pdf)
+ *  - [韩文兼容字母](http://www.unicode.org/charts/PDF/U3130.pdf)
  *
  * @info 该正则使用 `xregexp` 自动生成
  *
