82bfca473ba2bdd76e116858ba280802da820f61
wtklbm <wtklbm@gmail.com>
2021-04-16 16:35:53 +0800

    📃docs: 更新文档

--- packages/regexp/src/source/chkPunctuation.ts
+++ packages/regexp/src/source/chkPunctuation.ts
@@ -1,6 +1,11 @@
 /**
  * 验证中日韩的标点符号的正则字符串
  *
+ * @see
+ *  - [CJK标点符号](http://www.unicode.org/charts/PDF/U3000.pdf)
+ *  - [中文竖排标点](http://www.unicode.org/charts/PDF/UFE10.pdf)
+ *  - [CJK兼容符号（竖排变体、下划线、顿号）](http://www.unicode.org/charts/PDF/UFE30.pdf)
+ *
  * @info 该正则是自己写的，不是自动生成的。参考的原始代码部分：`\\p{InCJK_Symbols_and_Punctuation}`。
  */
 const chkPunctuation =
