a4b403cc252501a8e77b45d20aaa4095fadb0211
wtklbm <wtklbm@gmail.com>
2021-04-16 16:38:26 +0800

    ✨feat: 更新表达式和文档

--- packages/regexp/src/source/katakana.ts
+++ packages/regexp/src/source/katakana.ts
@@ -1,6 +1,10 @@
 /**
  * 验证片假名的正则字符串
  *
+ * @see
+ *  - [日文片假名](http://www.unicode.org/charts/PDF/U30A0.pdf)
+ *  - [日文片假名拼音扩展](http://www.unicode.org/charts/PDF/U31F0.pdf)
+ *
  * @example
  *
  * ```javascript
@@ -9,6 +13,6 @@
  * ```
  */
 const katakana =
-    '[\u30A1-\u30FA\u30FD-\u30FF\u31F0-\u31FF\u32D0-\u32FE\u3300-\u3357\uFF66-\uFF6F\uFF71-\uFF9D]';
+    '[\u30A0-\u30FF\u30FD-\u30FF\u31F0-\u31FF\u32D0-\u32FE\u3300-\u3357\uFF66-\uFF6F\uFF71-\uFF9D]';
 
 export default katakana;
