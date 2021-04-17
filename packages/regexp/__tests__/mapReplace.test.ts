cc8f6959a694c17e31cae464bea45373a23989fa
wtklbm <wtklbm@gmail.com>
2021-04-17 14:17:38 +0800

    🌈style: 格式化代码

--- packages/regexp/__tests__/mapReplace.test.ts
+++ packages/regexp/__tests__/mapReplace.test.ts
@@ -2,21 +2,14 @@ import { mapReplace } from '../src';
 
 describe('@curong/regexp/mapReplace', () => {
     test('测试1', () => {
-        const regexps = [
-            /b/g,
-            /a/g
-        ]
+        const regexps = [/b/g, /a/g];
         expect(mapReplace(regexps, 'Aa', 'x')).toBe('Ax');
         expect(mapReplace(regexps, 'ab', 'x')).toBe('xx');
     });
 
-
     test('测试2', () => {
-        const regexps = [
-            /b/g,
-            /a/g
-        ]
-        expect(mapReplace(regexps, 'Aa', (v) => v + 'x')).toBe('Aax');
-        expect(mapReplace(regexps, 'ab', (v) => v + 'x')).toBe('axbx');
+        const regexps = [/b/g, /a/g];
+        expect(mapReplace(regexps, 'Aa', v => v + 'x')).toBe('Aax');
+        expect(mapReplace(regexps, 'ab', v => v + 'x')).toBe('axbx');
     });
 });
