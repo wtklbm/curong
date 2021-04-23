8e2b15ca0679acb63f5cc4fd76b117e8878a464c
wtklbm <wtklbm@gmail.com>
2021-04-24 00:48:59 +0800

    ✨feat: 添加 `symbolicLink` 方法

--- packages/fs/src/symbolicLink.ts
+++ packages/fs/src/symbolicLink.ts
@@ -4,8 +4,10 @@ import { readlink } from 'fs/promises';
  * 获取符号链接所指向的原始地址
  *
  * @param pathString 符号链接地址
- * @returns 返回找到的符号链接的源路径
+ * @returns 如果找到符号链接，则返回符号链接的源路径，否则返回 `null`
  */
-export default async function symbolicLink(pathString: string) {
+export default async function symbolicLink(
+    pathString: string
+): Promise<string | null> {
     return await readlink(pathString).catch(() => null);
 }
