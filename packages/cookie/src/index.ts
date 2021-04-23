6154574c5629f3d842d7648ff753b87e49a5dc02
wtklbm <wtklbm@gmail.com>
2021-04-23 22:22:20 +0800

    🦄refactor: 重导出类型

--- packages/cookie/src/index.ts
+++ packages/cookie/src/index.ts
@@ -1,7 +1,6 @@
 import parse from './parse';
 import create from './create';
 
-export * from './types/parse';
-export * from './types/create';
+export * from './types';
 
 export { parse, create };
