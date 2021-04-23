c1eb6137d05c025ac3836752745c5118c3a23a74
wtklbm <wtklbm@gmail.com>
2021-04-24 01:11:01 +0800

    🦄refactor: 修改参数名

--- packages/fs/src/copySymbolicLink.ts
+++ packages/fs/src/copySymbolicLink.ts
@@ -13,24 +13,27 @@ const name = 'copySymbolicLink';
 /**
  * 将一个符号链接拷贝到另一个路径
  *
- * @param from 符号链接地址
- * @param to 目标路径
+ * @param fromPath 符号链接地址
+ * @param toPath 目标路径
  * @throws
  *
  * - 如果要拷贝的内容不是符号链接则抛出异常
  * - 如果拷贝符号链接失败则抛出异常
  */
-export default async function copySymbolicLink(from: string, to: string) {
+export default async function copySymbolicLink(
+    fromPath: string,
+    toPath: string
+): Promise<void> {
     // 如果要拷贝的内容不是符号链接
-    if (!(await isSymbolicLink(from))) {
+    if (!(await isSymbolicLink(fromPath))) {
         throw format({
             name,
-            message: 'from不是一个符号链接',
-            data: { from, to }
+            message: 'fromPath不是一个符号链接',
+            data: { fromPath, toPath }
         });
     }
 
-    const toDir = dirname(to);
+    const toDir = dirname(toPath);
     let type: string | null;
 
     // 先判断目标文件夹存不存在
@@ -39,20 +42,20 @@ export default async function copySymbolicLink(from: string, to: string) {
     }
 
     // 判断符号链接的类型
-    if (await isFile(from)) {
+    if (await isFile(fromPath)) {
         type = 'file';
-    } else if (await isDir(from)) {
+    } else if (await isDir(fromPath)) {
         type = 'dir';
     } else {
         type = null;
     }
 
     // 创建符号链接
-    await symlink(from, to, type).catch(error => {
+    await symlink(fromPath, toPath, type).catch(error => {
         throw format({
             name,
-            message: '拷贝符号链接失败，无法将from拷贝到to',
-            data: { from, to, error }
+            message: '拷贝符号链接失败，无法将fromPath拷贝到toPath',
+            data: { fromPath, toPath, error }
         });
     });
 }
