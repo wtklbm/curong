999531ee3ba54cb04fd71c183001416d4266b347
wtklbm <wtklbm@gmail.com>
2021-04-23 22:24:59 +0800

    🦄refactor: 重导出类型

--- packages/request/src/index.ts
+++ packages/request/src/index.ts
@@ -2,8 +2,6 @@ import publicIP from './publicIP';
 import queryDNS from './queryDNS';
 import request from './request';
 
-export * from './types/publicIP';
-export * from './types/queryDNS';
-export * from './types/request';
+export * from './types';
 
 export { publicIP, queryDNS, request };
