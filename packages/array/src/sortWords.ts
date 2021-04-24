de82e7137772d40afebec4fe9096489a25201cbb
wtklbm <wtklbm@gmail.com>
2021-04-24 11:32:47 +0800

    ✨feat: 添加 `reverseOrder` 参数

--- packages/array/src/sortWords.ts
+++ packages/array/src/sortWords.ts
@@ -2,15 +2,26 @@
  * 给单词数组排序，中文按照音序排序，英文按照整个单词排序 (全量排序)
  *
  * @param value 要排序的字符串数组
+ * @param reverseOrder 是否倒序排列
  * @returns 返回排序好的字符串数组
  * @example
  *
  * ```javascript
  * const value = ['中', 'this', '国', 'is', 'test', 'a'];
- * const ret = sortWords(value);
- * console.log(ret); // [ '国', '中', 'a', 'is', 'test', 'this' ]
+ * const ret1 = sortWords(value);
+ * console.log(ret1); // [ '国', '中', 'a', 'is', 'test', 'this' ]
+ *
+ * const ret2 = sortWords(value, true);
+ * console.log(ret2); // [ 'this', 'test', 'is', 'a', '中', '国' ]
  * ```
  */
-export default function sortWords(value: string[]): string[] {
-    return value.sort((a, b) => a.localeCompare(b));
+export default function sortWords(
+    value: string[],
+    reverseOrder = false
+): string[] {
+    if (reverseOrder) {
+        return value.sort((a, b) => b.localeCompare(a));
+    }
+
+    return value.sort((a: any, b: any) => a.localeCompare(b));
 }
