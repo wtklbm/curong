23ea4c6ad560bd17a2423077d66c0fdf7e4ec9a6
wtklbm <wtklbm@gmail.com>
2021-04-24 10:58:04 +0800

    ✨feat: 添加 `sortWords` 方法

--- packages/array/src/index.ts
+++ packages/array/src/index.ts
@@ -2,6 +2,7 @@ import maxLengthIndex from './maxLengthIndex';
 import minLengthIndex from './minLengthIndex';
 import shuffle from './shuffle';
 import sortToIndexes from './sortToIndexes';
+import sortWords from './sortWords';
 import uniqueFromPrimitive from './uniqueFromPrimitive';
 
 export * from './types';
@@ -11,5 +12,6 @@ export {
     minLengthIndex,
     shuffle,
     sortToIndexes,
+    sortWords,
     uniqueFromPrimitive
 };
