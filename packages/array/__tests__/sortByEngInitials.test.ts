f6dd1ff321c551807bd3a83abc0a60608dc0535f
wtklbm <wtklbm@gmail.com>
2021-04-24 11:41:54 +0800

    🧪test: 添加 `sortByEngInitials` 方法

---
+++ packages/array/__tests__/sortByEngInitials.test.ts
@@ -0,0 +1,12 @@
+import { sortByEngInitials } from '../src';
+
+describe('@curong/string/sortByEngInitials', () => {
+    test('测试1', () => {
+        const value = ['ae', 'aa', 'ac', 'cg', 'do', 'gj', 'bl'];
+        const ret1 = sortByEngInitials(value);
+        expect(ret1).toEqual(['ae', 'aa', 'ac', 'bl', 'cg', 'do', 'gj']);
+
+        const ret2 = sortByEngInitials(value, true);
+        expect(ret2).toEqual(['gj', 'do', 'cg', 'bl', 'ae', 'aa', 'ac']);
+    });
+});
