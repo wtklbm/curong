cf39625b47a7eebfa4d6ad6d11828ec021d286c7
wtklbm <wtklbm@gmail.com>
2021-04-24 11:33:12 +0800

    🧪test: 添加 `reverseOrder` 参数测试

--- packages/array/__tests__/sortWords.test.ts
+++ packages/array/__tests__/sortWords.test.ts
@@ -3,7 +3,10 @@ import { sortWords } from '../src';
 describe('@curong/string/sortWords', () => {
     test('测试1', () => {
         const value = ['中', 'this', '国', 'is', 'test', 'a'];
-        const ret = sortWords(value);
-        expect(ret).toEqual(['国', '中', 'a', 'is', 'test', 'this']);
+        const ret1 = sortWords(value);
+        expect(ret1).toEqual(['国', '中', 'a', 'is', 'test', 'this']);
+
+        const ret2 = sortWords(value, true);
+        expect(ret2).toEqual(['this', 'test', 'is', 'a', '中', '国']);
     });
 });
