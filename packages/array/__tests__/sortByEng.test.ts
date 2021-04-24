35afb2a14b7ae8ccd7182f3003276a5a2a850d5c
wtklbm <wtklbm@gmail.com>
2021-04-24 11:48:26 +0800

    🧪test: 添加 `sortByEng` 方法

---
+++ packages/array/__tests__/sortByEng.test.ts
@@ -0,0 +1,12 @@
+import { sortByEng } from '../src';
+
+describe('@curong/string/sortByEng', () => {
+    test('测试1', () => {
+        const value = ['ae', 'aa', 'ac', 'cg', 'do', 'gj', 'bl'];
+        const ret1 = sortByEng(value);
+        expect(ret1).toEqual(['aa', 'ac', 'ae', 'bl', 'cg', 'do', 'gj']);
+
+        const ret2 = sortByEng(value, true);
+        expect(ret2).toEqual(['gj', 'do', 'cg', 'bl', 'ae', 'ac', 'aa']);
+    });
+});
