f5f1b1a65a27944566ff4c9fc3c5301835c5daa8
wtklbm <wtklbm@gmail.com>
2021-06-03 02:43:05 +0800

    ✨ feat: 添加 `toLowerCaseKey` 方法

---
+++ packages/object/src/toLowerCaseKey.ts
@@ -0,0 +1,41 @@
+import { ObjectType } from './types';
+
+/**
+ * 将一个对象中的键全部转换为小写，并返回一个新的对象
+ *
+ * @param object 要处理的对象
+ * @param isCover 是否覆盖新对象中之前所存储的值，默认为 `true`
+ *
+ *  - 如果为 `true`，则会将新对象中之前所存储的属性中的值替换掉
+ *  - 如果为 `false`，则不会对新对象中的属性重复赋值
+ *
+ * @returns 返回处理好的新的对象
+ * @example
+ *
+ * ```javascript
+ * let ret = toLowerCaseKey({ name: 1, NAME: 2 });
+ * console.log(ret); // { name: 2 }
+ * ret = toLowerCaseKey({ name: 1, NAME: 2 }, false);
+ * console.log(ret); // { name: 1 }
+ * ```
+ */
+export default function toLowerCaseKey(
+    object: ObjectType,
+    isCover: boolean = true
+): ObjectType {
+    const set = new Set();
+
+    return Object.keys(object).reduce((memo, key) => {
+        const value = object[key];
+        key = key.toLocaleLowerCase();
+
+        if (isCover) {
+            memo[key] = value;
+        } else if (!set.has(key)) {
+            memo[key] = value;
+            set.add(key);
+        }
+
+        return memo;
+    }, {} as ObjectType);
+}
