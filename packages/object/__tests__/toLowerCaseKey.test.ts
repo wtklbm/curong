09692956769dfbf8ced15f694a983100957ec131
wtklbm <wtklbm@gmail.com>
2021-06-04 02:34:13 +0800

    🧪 test: 添加新的测试

--- packages/object/__tests__/toLowerCaseKey.test.ts
+++ packages/object/__tests__/toLowerCaseKey.test.ts
@@ -2,12 +2,21 @@ import { toLowerCaseKey } from '../src';
 
 describe('@curong/object/toLowerCaseKey', () => {
     test('测试1', () => {
-        let ret = toLowerCaseKey({ name: 1, NAME: 2 });
+        const ret = toLowerCaseKey({ name: 1, NAME: 2 });
         expect(ret).toEqual({ name: 2 });
     });
 
     test('测试2', () => {
-        let ret = toLowerCaseKey({ name: 1, NAME: 2 }, false);
+        const ret = toLowerCaseKey({ name: 1, NAME: 2 }, false);
         expect(ret).toEqual({ name: 1 });
+    });
+
+    test('测试3', () => {
+        const v = Symbol('v');
+        const ret = toLowerCaseKey(
+            { name: 1, NAME: 2, 1: true, [v]: 'v' },
+            false
+        );
+        expect(ret).toEqual({ name: 1, 1: true, [v]: 'v' });
     });
 });
