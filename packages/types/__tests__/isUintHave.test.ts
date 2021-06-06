22ea554c9bc6e7b5826de86b79c49b8a253a0fd9
wtklbm <wtklbm@gmail.com>
2021-06-07 02:33:44 +0800

    🧪 test: 添加 `isUintHave` 方法的测试

---
+++ packages/types/__tests__/isUintHave.test.ts
@@ -0,0 +1,13 @@
+import { isUintHave } from '../src';
+
+describe('@curong/types/isUintHave', () => {
+    test('测试1', () => {
+        expect(isUintHave(12.1)).toBe(false);
+        expect(isUintHave(-1)).toBe(false);
+    });
+
+    test('测试2', () => {
+        expect(isUintHave(0)).toBe(false);
+        expect(isUintHave(15)).toBe(true);
+    });
+});
