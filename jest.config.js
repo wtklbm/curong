d533b34e6b6a3223fd88e27c1060c15447f2a069
wtklbm <wtklbm@gmail.com>
2021-06-10 15:29:08 +0800

    🐳 chore: 修改 `jest` 配置

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,25 @@
 module.exports = {
+    // 详细模式
+    verbose: true,
+    // 项目根目录
     roots: ['packages'],
+    // 转换
     transform: {
         '^.+\\.tsx?$': 'ts-jest'
     },
+    // 模块名映射
     moduleNameMapper: {
-        '^@curong/(.*?)$': '<rootDir>/packages/$1/src'
+        '^@curong/(.+)$': '<rootDir>/packages/$1/src'
     },
-    testRegex: '((__tests?__|tests?)|[.](test|spec))[.][jt]sx?$',
-    testPathIgnorePatterns: ['(packages/.*)?/(node_modules|dist)'],
-    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node']
+    // 用 `glob` 模式匹配测试文件
+    testMatch: ['**/@(__test?(s)__|test?(s))/*.@(test|spec).*'],
+    // 忽略的文件或文件夹
+    testPathIgnorePatterns: [
+        '/node_modules/',
+        '/dist/',
+        '/.vscode/',
+        '/coverage/'
+    ],
+    // 模块文件扩展名
+    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'mjs', 'node']
 };
