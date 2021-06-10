7141d9e4a7b01681abdefbd00537b92890add936
wtklbm <wtklbm@gmail.com>
2021-06-10 15:29:36 +0800

    ✨ feat: 添加 `tryCatch` 方法

---
+++ packages/function/src/tryCatch.ts
@@ -0,0 +1,15 @@
+/**
+ * `try...catch...` 语法的 `Promise` 包装
+ *
+ * @param fn 要执行的操作
+ * @returns 如果函数执行成功则将结果返回，否则抛出异常
+ */
+export default function tryCatch(fn: any) {
+    return new Promise((resolve, reject) => {
+        try {
+            resolve(fn());
+        } catch (e) {
+            reject(e);
+        }
+    });
+}
