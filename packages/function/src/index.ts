8fe1245b74fe9aba0644b65e868af4af5d8c1a39
wtklbm <wtklbm@gmail.com>
2021-06-10 15:29:45 +0800

    ✨ feat: 添加 `tryCatch` 方法

--- packages/function/src/index.ts
+++ packages/function/src/index.ts
@@ -2,7 +2,8 @@ import before from './before';
 import curring from './curring';
 import reach from './reach';
 import sleepRun from './sleepRun';
+import tryCatch from './tryCatch';
 
 export * from './types';
 
-export { before, curring, reach, sleepRun };
+export { before, curring, reach, sleepRun, tryCatch };
