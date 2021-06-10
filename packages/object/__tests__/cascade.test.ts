804b5d30269f7fa05d061c30cc6754170dfbbdc7
wtklbm <wtklbm@gmail.com>
2021-06-10 18:08:08 +0800

    🧪 test: 添加 `cascade` 方法的测试

---
+++ packages/object/__tests__/cascade.test.ts
@@ -0,0 +1,32 @@
+import { cascade } from '../src';
+
+describe('@curong/object/cascade', () => {
+    const o = { key: [{ value: 'this is a test.' }] };
+
+    test('测试1', () => {
+        let ret = cascade(o, 'key[0].value');
+        expect(ret).toBe('this is a test.');
+
+        ret = cascade(o, '.key[0].value');
+        expect(ret).toBe('this is a test.');
+
+        ret = cascade(o, '.key[0]["value"]');
+        expect(ret).toBe('this is a test.');
+
+        ret = cascade(o, '.key[0].["value"]');
+        expect(ret).toBe('this is a test.');
+
+        ret = cascade(o, 'key[0].["value"]');
+        expect(ret).toBe('this is a test.');
+    });
+
+    test('测试2', () => {
+        expect(cascade(o, 'key[1].value1')).toBe(undefined);
+
+        try {
+            cascade(o, 'key[1].value');
+        } catch (e) {
+            expect(e).toThrowError();
+        }
+    });
+});
