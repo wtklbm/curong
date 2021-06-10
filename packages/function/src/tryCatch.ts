4cf6877d63303dff40671b24e3876211dfb93a5e
wtklbm <wtklbm@gmail.com>
2021-06-10 16:01:06 +0800

    📃 docs: 添加示例

--- packages/function/src/tryCatch.ts
+++ packages/function/src/tryCatch.ts
@@ -3,6 +3,12 @@
  *
  * @param fn 要执行的操作
  * @returns 如果函数执行成功则将结果返回，否则抛出异常
+ * @example
+ *
+ * ```javascript
+ * const ret = await tryCatch(async () => Promise.resolve(10));
+ * console.log(ret); // 10
+ * ```
  */
 export default function tryCatch(fn: any) {
     return new Promise((resolve, reject) => {
