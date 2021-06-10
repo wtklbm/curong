0236bd151c4fc4291879a2979ebcc9bc7a0c06c8
wtklbm <wtklbm@gmail.com>
2021-06-10 17:19:08 +0800

    ✨ feat: 添加 `fromCascadeKeys` 方法

---
+++ packages/object/src/fromCascadeKeys.ts
@@ -0,0 +1,73 @@
+import { isArrayHave, isNumber } from '@curong/types';
+
+import { FromCascadeKeysOptions } from './types';
+
+const addPack = (
+    key: string | number,
+    optionalChar: string,
+    isAppendWen: boolean = true
+) => {
+    // 处理数字
+    if (isNumber(key)) {
+        // 数字后面不能带 `?`
+        return `[${key}]`;
+    }
+
+    //# 处理字符串
+
+    if (/^[^0-9][\w$]*$/.test(key)) {
+        return isAppendWen ? `${optionalChar}.${key}` : `.${key}`;
+    }
+
+    if (/^('[^']+'|"[^"]+")$/.test(key)) {
+        return `[${key}]`;
+    }
+
+    return `['${key}']`;
+};
+
+/**
+ * 将级联属性数组转换为一个属性字符串
+ *
+ * @param keys 属性数组
+ * @param options 配置选项
+ * @returns 返回级联属性字符串
+ * @example
+ *
+ * ```javascript
+ * const ret = fromCascadeKeys(['a', 'b', 10, 'c']);
+ * console.log(ret); // 'a.b[10].c'
+ * ```
+ */
+export default function fromCascadeKeys(
+    keys: Array<string | number>,
+    options?: FromCascadeKeysOptions
+): string {
+    const { optional = false, startDot = false } = options ?? {};
+    let oc = '';
+
+    if (optional) {
+        oc = '?';
+    }
+
+    // 如果属性数组中没有内容
+    if (!isArrayHave(keys)) {
+        return '';
+    }
+
+    // 获取到第一项
+    let first = addPack(keys.shift()!, oc, false);
+
+    // 如果不需要给第一项的开头加 `.`
+    if (!startDot && first.startsWith('.')) {
+        first = first.slice(1);
+    }
+
+    // 如果属性数组中只有一项，则直接返回
+    if (keys.length === 1) {
+        return first;
+    }
+
+    // 遍历剩余项
+    return keys.reduce((m, k) => (m.push(addPack(k, oc)), m), [first]).join('');
+}
