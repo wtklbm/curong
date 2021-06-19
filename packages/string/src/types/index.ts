c3989da255b2bf84051810a61ecb2fc640b7160d
wtklbm <wtklbm@gmail.com>
2021-06-19 19:17:47 +0800

    ✨ feat: 添加类型声明

--- packages/string/src/types/index.ts
+++ packages/string/src/types/index.ts
@@ -2,5 +2,6 @@ export * from './bindOutside';
 export * from './characters';
 export * from './generic';
 export * from './startsSlice';
+export * from './toSentences';
 export * from './trim';
 export * from './word';
