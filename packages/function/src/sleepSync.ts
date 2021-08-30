5c571ef36e11ecb759aff49382cb10168a6124de
wtklbm <wtklbm@gmail.com>
2021-08-30 15:37:10 +0800

    📃 docs: 更新文档

--- packages/function/src/sleepSync.ts
+++ packages/function/src/sleepSync.ts
@@ -4,6 +4,14 @@ import { isUint } from '@curong/types';
  * 同步阻塞一段时间
  *
  * @param duration 要阻塞多长时间，以毫秒为单位
+ * @throw 如果 duration 不是一个无符号整数，则会抛出异常
+ * @example
+ *
+ * ```javascript
+ * console.time('delay');
+ * sleepSync(20);
+ * console.timeEnd('delay'); // 21.0ms
+ * ```
  */
 export default function sleepSync(duration: number): void {
     if (!isUint(duration)) {
