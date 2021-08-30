bbfcd17b03eec95d94c03655c0025f34ccd06738
wtklbm <wtklbm@gmail.com>
2021-08-30 15:31:10 +0800

    ✨ feat: 添加 `sleepSync` 方法

--- packages/function/src/index.ts
+++ packages/function/src/index.ts
@@ -2,8 +2,9 @@ import before from './before';
 import curring from './curring';
 import reach from './reach';
 import sleepRun from './sleepRun';
+import sleepSync from './sleepSync';
 import tryCatch from './tryCatch';
 
 export * from './types';
 
-export { before, curring, reach, sleepRun, tryCatch };
+export { before, curring, reach, sleepRun, sleepSync, tryCatch };
