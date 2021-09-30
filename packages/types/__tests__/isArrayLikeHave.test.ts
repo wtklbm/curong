526a65f0c37e38f7ceb1d173cae37ccb825c0ae3
wtklbm <wtklbm@gmail.com>
2021-09-30 22:42:09 +0800

    🧪 test: 添加 `isArrayLikeHave` 方法的测试

---
+++ packages/types/__tests__/isArrayLikeHave.test.ts
@@ -0,0 +1,21 @@
+import { isArrayLikeHave } from '../src';
+
+describe('@curong/types/isArrayLikeHave', () => {
+    test('测试1', () => {
+        expect(isArrayLikeHave(12)).toBe(false);
+    });
+
+    test('测试2', () => {
+        expect(isArrayLikeHave([])).toBe(false);
+        expect(isArrayLikeHave([1])).toBe(false);
+        const fn1 = (function () {
+            expect(isArrayLikeHave(arguments)).toBe(false);
+        })();
+    });
+
+    test('测试3', () => {
+        const fn2 = (function (a: any) {
+            expect(isArrayLikeHave(arguments)).toBe(true);
+        })(0);
+    });
+});
