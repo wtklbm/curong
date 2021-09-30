cae8a61cddfa5de52d4c85b36239be62c61e9541
wtklbm <wtklbm@gmail.com>
2021-09-30 22:40:59 +0800

    ✨ feat: 添加 `isArrayLikeHave` 方法

---
+++ packages/types/src/isArrayLikeHave.ts
@@ -0,0 +1,13 @@
+import isArrayLike from './isArrayLike';
+
+/**
+ * 是不是一个长度大于 `0` 的类数组
+ *
+ * @param value 要验证的值
+ * @returns 是则返回 `true`，否则为 `false`
+ */
+export default function isArrayLikeHave<T = any>(
+    value: any
+): value is ArrayLike<T> {
+    return isArrayLike(value) && value.length > 0;
+}
