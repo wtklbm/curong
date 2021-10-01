c2b9b28842e86c6270a8ec09a075773b4bb3ae5b
wtklbm <wtklbm@gmail.com>
2021-10-01 16:28:41 +0800

    ✨ feat: 添加 `fromArrayLike` 方法

---
+++ packages/array/src/fromArrayLike.ts
@@ -0,0 +1,20 @@
+/**
+ * 将一个类数组转换为数组
+ *
+ * @param value 类数组
+ * @returns 返回转换后的数组
+ * @example
+ *
+ * ```js
+ * const print = function () {
+ *     const args = fromArrayLike(arguments);
+ *
+ *     console.log(args); // [ 1, 32, 43, 54, 2, 32 ]
+ * };
+ *
+ * print(1, 32, 43, 54, 2, 32);
+ * ```
+ */
+export default function fromArrayLike<T = any>(value: ArrayLike<T>): T[] {
+    return Array.prototype.slice.call(value);
+}
