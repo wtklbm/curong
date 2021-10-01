fd873fbcc26dddb1ff5c59c9f840906bff0f24e1
wtklbm <wtklbm@gmail.com>
2021-10-01 15:26:10 +0800

    ✨ feat: 添加 `similarity` 参数

--- packages/types/src/isArrayLike.ts
+++ packages/types/src/isArrayLike.ts
@@ -1,13 +1,18 @@
 import isArray from './isArray';
 import isArrayIndex from './isArrayIndex';
-import isFunction from './isFunction';
-import isNullOrUndefined from './isNullOrUndefined';
+import isTypeofObject from './isTypeofObject';
 import isWindow from './isWindow';
 
 /**
- * 是不是一个类数组
+ * 是不是一个类数组 (不包含数组)
  *
  * @param value 要验证的值
+ * @param similarity 类数组的相似程度，默认值为 `0`
+ *
+ *  - `0`: 粗略判断，只要 `length` 属性符合要求即可
+ *  - `1`: 在保证 `length` 符合要求的情况下，判断对象中可枚举的属性是否存在
+ *  - `2`: 不仅属性存在，还有保证对象的可枚举的属性的个数是 `length` 的个数加一
+ *
  * @returns 是则返回 `true`，否则为 `false`
  * @example
  *
@@ -20,18 +25,37 @@ import isWindow from './isWindow';
  * ```
  */
 export default function isArrayLike<T = any>(
-    value: any
+    value: any,
+    similarity: 0 | 1 | 2 = 0
 ): value is ArrayLike<T> {
-    if (
-        isNullOrUndefined(value) ||
-        isArray(value) ||
-        isFunction(value) ||
-        isWindow(value)
-    ) {
-        return false;
+    let ret = false;
+
+    if (!isTypeofObject(value) || isArray(value) || isWindow(value)) {
+        return ret;
     }
 
+    // Note: 数组/类数组、字符串、函数、对象、`Window`、`Buffer`/`ArrayBuffer` 等都有 `length` 属性
     const l = value.length;
 
-    return l === 0 || (isArrayIndex(l) && value[l - 1] in value);
+    if (!isArrayIndex(l)) {
+        return ret;
+    }
+
+    ret = true;
+
+    if (similarity <= 0) {
+        return ret;
+    }
+
+    for (let i = 0; i < l; i++) {
+        if (!(i in value)) {
+            ret = false;
+        }
+    }
+
+    if (similarity === 1) {
+        return ret;
+    }
+
+    return ret && Object.keys(value).length === l + 1;
 }
