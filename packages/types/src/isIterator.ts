7422b8a5e367a16afe5b0f287a85009205dd781d
wtklbm <wtklbm@gmail.com>
2021-10-02 21:13:27 +0800

    ✨ feat: 添加 `isIterator` 方法

---
+++ packages/types/src/isIterator.ts
@@ -0,0 +1,14 @@
+import { isFunction } from '.';
+
+/**
+ * 是不是一个 `Iterator`
+ *
+ * @param value 要验证的值
+ * @returns 是则返回 `true`，否则为 `false`
+ * @info 在这里，只要迭代器有 `next` 方法，则返回 `true`
+ */
+export default function isIterator<T = any, R = any, N = undefined>(
+    value: any
+): value is Iterator<T, R, N> {
+    return value && isFunction(value.next);
+}
