79e2e0eef40ee9d113b96ff5d3ce10ff6067c79f
wtklbm <wtklbm@gmail.com>
2021-10-02 21:09:46 +0800

    ✨ feat: 添加 `isGenerator` 方法

---
+++ packages/types/src/isGenerator.ts
@@ -0,0 +1,18 @@
+import getTag from './getTag';
+
+/**
+ * 是不是一个 `Generator`
+ *
+ * 每一个 `Generator` 上，都会有三个方法:
+ *  - `next`
+ *  - `throw`
+ *  - `return`
+ *
+ * @param value 要验证的值
+ * @returns 是则返回 `true`，否则为 `false`
+ */
+export default function isGenerator<T = unknown, R = any, N = unknown>(
+    value: any
+): value is Generator<T, R, N> {
+    return getTag(value) === 'Generator';
+}
