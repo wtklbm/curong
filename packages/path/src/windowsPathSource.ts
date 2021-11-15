08a34d532072640e4cffd0f8325b4fefc80f7778
wtklbm <wtklbm@gmail.com>
2021-11-15 20:51:18 +0800

    🦄 refactor: 移动到新的依赖

--- packages/path/src/windowsPathSource.ts
+++ packages/path/src/windowsPathSource.ts
@@ -1,7 +1,7 @@
 /**
  * 验证 `Windows` 路径的正则字符串
  *
- * 如果需要验证是否为合法的 `Windows` 路径字符串，请使用 `isPathWin` 方法。
+ * 如果需要验证是否为合法的 `Windows` 路径字符串，请使用 `isWindowsPath` 方法。
  *
  * ```javascript
  * const pathWin =
@@ -12,7 +12,7 @@
  * ```
  * @link [docs.microsoft](https://docs.microsoft.com/zh-cn/windows/win32/fileio/naming-a-file?redirectedfrom=MSDN)
  */
-const windowsPath =
+const windowsPathSource =
     '(((\\\\\\\\\\?\\\\)?[a-zA-Z]:\\\\?)|\\.{1,2})(\\\\((?![<>:"/\\\\|?*]|CON|PRN|AUX|NUL|COM[1-9]|LPT[1-9])(.*[^.\\s])*)+)+';
 
-export default windowsPath;
+export default windowsPathSource;
