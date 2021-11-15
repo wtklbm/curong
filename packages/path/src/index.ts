1637df3a74484951170e1b04ea95895efdf11c53
wtklbm <wtklbm@gmail.com>
2021-11-15 20:58:16 +0800

    ✨ feat: 添加 `isWindowsExtendedLengthPath` 方法

--- packages/path/src/index.ts
+++ packages/path/src/index.ts
@@ -1,3 +1,5 @@
-export { default as unifiedSeparator } from './unifiedSeparator';
+export { default as isWindowsExtendedLengthPath } from './isWindowsExtendedLengthPath';
 export { default as isWindowsPath } from './isWindowsPath';
+export { default as unifiedSeparator } from './unifiedSeparator';
+
 export { default as windowsPathSource } from './windowsPathSource';
