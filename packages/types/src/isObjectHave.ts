b9c3e8f63c0c25411b926f70e10e04e6ddc891fc
wtklbm <wtklbm@gmail.com>
2021-12-01 14:23:12 +0800

    ✨ feat: 添加 `methodLevel` 参数

--- packages/types/src/isObjectHave.ts
+++ packages/types/src/isObjectHave.ts
@@ -4,10 +4,26 @@ import isObject from './isObject';
  * 是不是一个属性个数大于 `0` 的对象
  *
  * @param value 要验证的值
+ * @param methodLevel 通过什么方法来判断属性个数
+ *  - `0`: `Object.keys` (默认值)
+ *  - `1`: `Object.getOwnPropertyNames`
+ *  - `2`: `Object.getOwnPropertySymbols`
+ *  - `3`: `Reflect.ownKeys`
+ *
  * @returns 是则返回 `true`，否则为 `false`
  */
 export default function isObjectHave<K extends keyof any, T = any>(
-    value: any
+    value: any,
+    methodLevel: 0 | 1 | 2 | 3 = 0
 ): value is Record<K, T> {
-    return isObject(value) && Object.keys(value).length > 0;
+    const f =
+        methodLevel === 0
+            ? Object.keys
+            : methodLevel === 1
+            ? Object.getOwnPropertyNames
+            : methodLevel === 2
+            ? Object.getOwnPropertySymbols
+            : Reflect.ownKeys;
+
+    return isObject(value) && f(value).length > 0;
 }
