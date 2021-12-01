31ee7e8bf9f530990bfc29dbd911a5d759cd1929
wtklbm <wtklbm@gmail.com>
2021-12-01 14:23:06 +0800

    🧪 test: 添加新的测试

--- packages/types/__tests__/isPlainObjectHave.test.ts
+++ packages/types/__tests__/isPlainObjectHave.test.ts
@@ -7,5 +7,16 @@ describe('@curong/types/isPlainObjectHave', () => {
 
     test('测试2', () => {
         expect(isPlainObjectHave({ key: 'value' })).toBe(true);
+
+        const obj = { a: 0, [Symbol.iterator]: () => {} };
+        Object.defineProperties(obj, {
+            property1: { enumerable: true, value: 1 },
+            property2: { enumerable: false, value: 2 }
+        });
+        expect(isPlainObjectHave(obj)).toBe(true);
+        expect(isPlainObjectHave(obj, 0)).toBe(true);
+        expect(isPlainObjectHave(obj, 1)).toBe(true);
+        expect(isPlainObjectHave(obj, 2)).toBe(true);
+        expect(isPlainObjectHave(obj, 3)).toBe(true);
     });
 });
