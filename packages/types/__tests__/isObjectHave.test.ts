9c0b37bbd7d6e9176bd6b8f4b8d4bcfb5dfaf1f0
wtklbm <wtklbm@gmail.com>
2021-12-01 14:23:16 +0800

    🧪 test: 添加新的测试

--- packages/types/__tests__/isObjectHave.test.ts
+++ packages/types/__tests__/isObjectHave.test.ts
@@ -8,6 +8,20 @@ describe('@curong/types/isObjectHave', () => {
     test('测试2', () => {
         expect(isObjectHave({})).toBe(false);
         expect(isObjectHave({ a: 1 })).toBe(true);
+
+        const obj = Object.assign(Object.create(null), {
+            a: 0,
+            [Symbol.iterator]: () => {}
+        });
+        Object.defineProperties(obj, {
+            property1: { enumerable: true, value: 1 },
+            property2: { enumerable: false, value: 2 }
+        });
+        expect(isObjectHave(obj)).toBe(true);
+        expect(isObjectHave(obj, 0)).toBe(true);
+        expect(isObjectHave(obj, 1)).toBe(true);
+        expect(isObjectHave(obj, 2)).toBe(true);
+        expect(isObjectHave(obj, 3)).toBe(true);
     });
 
     test('测试3', () => {
