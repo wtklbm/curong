7477f3d1cf5602a8fa069333bede7504a3446d45
wtklbm <wtklbm@gmail.com>
2021-12-02 17:53:26 +0800

    📃 docs: 更新文档

--- packages/types/src/isObject.ts
+++ packages/types/src/isObject.ts
@@ -5,9 +5,11 @@ import getTag from './getTag';
  *
  * @param value 要验证的值
  * @returns 是则返回 `true`，否则为 `false`
- * @info 该方法通过 `Object.prototype.toString.call(value)` 来验证是否等于 `[object Object]`。
- * 如果想验证普通对象，请使用 `isPlainObject` 方法。
- * 如果想使用 `typeof` 验证一个对象，请使用 `isTypeofObject` 方法。
+ * @info
+ *  - 该方法通过 `Object.prototype.toString.call(value)` 来验证是否等于 `[object Object]`
+ *  - 如果想验证普通对象，请使用 `isPlainObject` 方法
+ *  - 如果想使用 `typeof` 验证一个对象，请使用 `isTypeofObject` 方法
+ *  - 判断一个类的实例 (`class xxx {}`)，所得到的结果是 `true`
  */
 export default function isObject<K extends keyof any, T = any>(
     value: any
