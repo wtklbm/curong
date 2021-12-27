b16ab8624c16cee8343cc21846b767c007751d0a
wtklbm <wtklbm@gmail.com>
2021-12-27 15:16:22 +0800

    🧪 test: 更新测试

--- packages/types/__tests__/isNullOrUndefined.test.ts
+++ packages/types/__tests__/isNullOrUndefined.test.ts
@@ -3,14 +3,14 @@ import { isNullOrUndefined } from '../src';
 describe('@curong/types/isNullOrUndefined', () => {
     test('测试1', () => {
         expect(isNullOrUndefined(12)).toBe(false);
+        expect(isNullOrUndefined(0)).toBe(false);
+        expect(isNullOrUndefined('')).toBe(false);
+        expect(isNullOrUndefined(true)).toBe(false);
+        expect(isNullOrUndefined(false)).toBe(false);
     });
 
     test('测试2', () => {
         expect(isNullOrUndefined(null)).toBe(true);
         expect(isNullOrUndefined(undefined)).toBe(true);
-    });
-
-    test('测试3', () => {
-        expect(isNullOrUndefined([1])).toBe(false);
     });
 });
