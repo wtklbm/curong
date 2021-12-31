2700e7d10ed8d046fb2fcebac2acba52489a01ab
wtklbm <wtklbm@gmail.com>
2021-12-31 18:31:56 +0800

    ✨ feat: 添加 `isUrlSearchParams` 方法

---
+++ packages/types/src/isURLSearchParams.ts
@@ -0,0 +1,11 @@
+/**
+ * 是不是一个 `URLSearchParams`
+ *
+ * @param value 要验证的值
+ * @returns 是则返回 `true`，否则为 `false`
+ */
+export default function isUrlSearchParams(
+    value: unknown
+): value is URLSearchParams {
+    return value instanceof URLSearchParams;
+}
