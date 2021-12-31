28910224499c921b29f32c886aa19e01a125e784
wtklbm <wtklbm@gmail.com>
2021-12-31 17:33:57 +0800

    🧪 test: 添加 `isSizey` 方法的测试

---
+++ packages/types/__tests__/isSizey.test.ts
@@ -0,0 +1,17 @@
+import { isSizey } from '../src';
+
+describe('@curong/types/isSizey', () => {
+    test('测试1', () => {
+        expect(isSizey(null)).toBe(false);
+        expect(isSizey(undefined)).toBe(false);
+        expect(isSizey([])).toBe(false);
+        expect(isSizey('')).toBe(false);
+        expect(isSizey(new Function())).toBe(false);
+    });
+
+    test('测试2', () => {
+        expect(isSizey(new Map())).toBe(true);
+        expect(isSizey(new Set())).toBe(true);
+        expect(isSizey({ size: 0 })).toBe(true);
+    });
+});
