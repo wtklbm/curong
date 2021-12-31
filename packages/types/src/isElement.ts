f344cebb40c16ee61189e9a7c1ffd25e4b4660d6
wtklbm <wtklbm@gmail.com>
2021-12-31 15:46:37 +0800

    ✨ feat: 添加 `isElement` 方法

---
+++ packages/types/src/isElement.ts
@@ -0,0 +1,13 @@
+/**
+ * 是不是一个 `Element`
+ *
+ * @param value 要验证的值
+ * @returns 是则返回 `true`，否则为 `false`
+ * @info
+ * `Element` 是最通用的基类，`Document` 中的所有对象都继承自该基类。
+ * `Element` 里面定义了一些公共的方法和属性。
+ *
+ */
+export default function isElement(value: unknown): value is Element {
+    return value instanceof Element;
+}
