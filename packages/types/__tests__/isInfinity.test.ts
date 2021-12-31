d311e7d955f72da945ffd922a42e1aa0bb8960aa
wtklbm <wtklbm@gmail.com>
2021-12-31 16:59:31 +0800

    🧪 test: 添加 `isInfinity` 方法的测试

---
+++ packages/types/__tests__/isInfinity.test.ts
@@ -0,0 +1,9 @@
+import { isInfinity } from '../src';
+
+describe('@curong/types/isInfinity', () => {
+    test('测试1', () => {
+        expect(isInfinity(1.7976931348623157e103088)).toBe(true);
+        expect(isInfinity(1.7976931348623157e103089)).toBe(true);
+        expect(isInfinity(Infinity)).toBe(true);
+    });
+});
