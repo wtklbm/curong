3d650ee0d18b00e63eb9d7bbd2dc631eaddac84c
wtklbm <wtklbm@gmail.com>
2021-12-31 19:03:17 +0800

    ✨ feat: 添加 `isNumeric` 方法

---
+++ packages/types/src/isNumeric.ts
@@ -0,0 +1,17 @@
+import isNaN from './isNaN';
+import isNumber from './isNumber';
+import isString from './isString';
+
+/**
+ * 是不是一个不是 `NaN` 的数字或数字字符串
+ *
+ * @param value 要验证的值
+ * @returns 是则返回 `true`，否则为 `false`
+ * @info 字符串 `Infinity` 和 `-Infinity` 的结果为 `true`
+ */
+export default function isNumeric(value: unknown): value is string | number {
+    return (
+        (isNumber(value) && !isNaN(value)) ||
+        (isString(value) && !isNaN(Number(value)))
+    );
+}
