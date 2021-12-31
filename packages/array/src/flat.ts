c5b70d22ed5b33daab2dfe8496d15ecf8eae6ecf
wtklbm <wtklbm@gmail.com>
2021-12-31 20:53:39 +0800

    ✨ feat: 添加 `flat` 方法

---
+++ packages/array/src/flat.ts
@@ -0,0 +1,17 @@
+import { isArray } from '@curong/types';
+
+/**
+ * 展平一个多维数组
+ *
+ * @param value 多维数组
+ * @returns 返回一个一位数组
+ * @example
+ *
+ * ```javascript
+ * const ret = flat([1, [[2], 3], 4]);
+ * console.log(ret); // [ 1, 2, 3, 4 ]
+ * ```
+ */
+export default function flat<T>(value: T[]): T[] {
+    return value.reduce<T[]>((a, b) => a.concat(isArray(b) ? flat(b) : b), []);
+}
