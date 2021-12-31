b1e8d4e28a8a5b4866da532c24ffff038aa7c915
wtklbm <wtklbm@gmail.com>
2021-12-31 17:42:45 +0800

    ✨ feat: 添加 `isTruthy` 方法

---
+++ packages/types/src/isTruthy.ts
@@ -0,0 +1,25 @@
+import { Falsy } from './isFalsy';
+
+export type Truthy<T> = Exclude<T, Falsy>;
+
+/**
+ * 是不是一个非虚值 (强制转换为 `Boolean` 后为 `true` 的值)
+ *
+ * @param value 要验证的值
+ * @returns 是则返回 `true`，否则为 `false`
+ * @info
+ * 在判断语句时，值都会被强制类型转换为布尔值，例如条件语句和循环语句。
+ * 在 `JavaScript` 中只有 `8` 个 `falsy` 值，对他们取反，就是 `truthy` 值。
+ *
+ *  - `false`     `false` 关键字
+ *  - `0`         数值 0
+ *  - `-0`         数值负 0
+ *  - `0n`        数值 0n
+ *  - `""`        空字符串 (字符串的长度为零)
+ *  - `null`      null - 缺少值
+ *  - `undefined` undefined - 原始值
+ *  - `NaN`       NaN - 非数值
+ */
+export default function isTruthy<T>(value: T): value is Truthy<T> {
+    return !!value;
+}
