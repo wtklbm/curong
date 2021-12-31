16cc3b70f7fcda49dcaa854c1253889e05d383b2
wtklbm <wtklbm@gmail.com>
2021-12-31 15:13:08 +0800

    🧪 test: 添加测试

--- packages/types/__tests__/isClass.test.ts
+++ packages/types/__tests__/isClass.test.ts
@@ -5,10 +5,17 @@ describe('@curong/types/isClass', () => {
         function test1() {}
         expect(isClass(new Function(''))).toBe(false);
         expect(isClass(test1)).toBe(false);
+        expect(isClass(123)).toBe(false);
+        expect(isClass(() => {})).toBe(false);
+        expect(isClass(function () {})).toBe(false);
+        expect(isClass(function hello() {})).toBe(false);
+        expect(isClass(new Function())).toBe(false);
+        expect(isClass(new (class Hello {})())).toBe(false);
     });
 
     test('测试2', () => {
         class a {}
         expect(isClass(a)).toBe(true);
+        expect(isClass(class Hello {})).toBe(true);
     });
 });
