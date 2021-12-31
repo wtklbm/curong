75b79c0be57cecbf87194ea5836ea35a0e88632d
wtklbm <wtklbm@gmail.com>
2021-12-31 20:41:13 +0800

    ✨ feat: 添加 `isSameError` 方法

---
+++ packages/types/src/isSameError.ts
@@ -0,0 +1,17 @@
+import { isAnyError } from '.';
+
+/**
+ * 是不是一个 `name` 和 `message` 相同的 `Error` 对象
+ *
+ * @param value 要验证的值
+ * @returns 是则返回 `true`，否则为 `false`
+ */
+export default function isSameError(
+    value: unknown,
+    name: string,
+    message: string
+): value is Error {
+    return (
+        isAnyError(value) && value.name === name && value.message === message
+    );
+}
