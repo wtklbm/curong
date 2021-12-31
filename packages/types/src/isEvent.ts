5cc36effc14a2d58b3a408b67faf8c50140675d6
wtklbm <wtklbm@gmail.com>
2021-12-31 15:54:08 +0800

    ✨ feat: 添加 `isEvent` 方法

---
+++ packages/types/src/isEvent.ts
@@ -0,0 +1,9 @@
+/**
+ * 是不是一个 `Event` 事件
+ *
+ * @param value 要验证的值
+ * @returns 是则返回 `true`，否则为 `false`
+ */
+export default function isEvent(value: unknown): value is Event {
+    return value instanceof Event;
+}
