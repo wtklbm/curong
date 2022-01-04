72b801afd3aedd21a13875a7bb9274733ab922cf
wtklbm <wtklbm@gmail.com>
2022-01-04 15:12:33 +0800

    ✨ feat: 添加 `isLengthyHave` 方法

---
+++ packages/types/src/isLengthyHave.ts
@@ -0,0 +1,11 @@
+import isLengthy, { Lengthy } from './isLengthy';
+
+/**
+ * 是不是一个具有 `length` 属性的类型，其 `length` 的值是一个大于 0 的整数
+ *
+ * @param value 要验证的值
+ * @returns 是则返回 `true`，否则为 `false`
+ */
+export default function isLengthyHave<T>(value: unknown): value is Lengthy<T> {
+    return isLengthy(value) && value.length > 0;
+}
