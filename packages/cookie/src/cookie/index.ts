c8b51d08d197c1edea1dcdb527329484bb750489
wtklbm <wtklbm@gmail.com>
2022-01-13 21:13:21 +0800

    🦄 refactor: 更改导出方式

--- packages/cookie/src/cookie/index.ts
+++ packages/cookie/src/cookie/index.ts
@@ -1,6 +1,4 @@
-import createCookie from './create';
-import parseCookie from './parse';
+export { default as createCookie } from './create';
+export { default as parseCookie } from './parse';
 
 export * from './types';
-
-export { parseCookie, createCookie };
