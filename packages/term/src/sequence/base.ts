76d3208b9d54fe46f4617a0a81c216cc4d421121
wtklbm <wtklbm@gmail.com>
2022-01-13 21:41:29 +0800

    🌈 style: 更新代码样式

--- packages/term/src/sequence/base.ts
+++ packages/term/src/sequence/base.ts
@@ -6,22 +6,22 @@ export const ESC = '\u001B';
  *
  * `[` 表示引入带有参数的控制序列
  */
-export const CSI = ESC + '[';
+export const CSI = `${ESC}[`;
 
 /** 保存当前的光标位置和属性 */
-export const SaveCursorAndAttr = ESC + '7';
+export const SaveCursorAndAttr = `${ESC}7`;
 
 /** 恢复之前保存的光标位置和属性 */
-export const restoreCursorAndAttr = ESC + '8';
+export const restoreCursorAndAttr = `${ESC}8`;
 
 /** 将所有终端设置重置为默认值 */
-export const ResetDevice = ESC + 'c';
+export const ResetDevice = `${ESC}c`;
 
 /** 如果文本长于显示区域的长度，则文本将自动换行到下一行 */
-export const lineWrapOpen = CSI + '7h';
+export const lineWrapOpen = `${CSI}7h`;
 
 /** 禁用自动换行 */
-export const lineWrapClose = CSI + '7l';
+export const lineWrapClose = `${CSI}7l`;
 
 /** 恢复为默认字体 */
-export const restoreFont = ESC + '(';
+export const restoreFont = `${ESC}(`;
