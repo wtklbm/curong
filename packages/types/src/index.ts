9252a1e0a3afdf0e9f738985826d4cd567d8af53
wtklbm <wtklbm@gmail.com>
2022-01-13 21:44:48 +0800

    🦄 refactor: 更改导出方式

--- packages/types/src/index.ts
+++ packages/types/src/index.ts
@@ -1,240 +1,120 @@
-import getTag from './getTag';
-import isAnyError from './isAnyError';
-import isArguments from './isArguments';
-import isArray from './isArray';
-import isArrayBuffer from './isArrayBuffer';
-import isArrayBufferView from './isArrayBufferView';
-import isArrayHave from './isArrayHave';
-import isArrayIndex from './isArrayIndex';
-import isArrayLike from './isArrayLike';
-import isArrayLikeHave from './isArrayLikeHave';
-import isAsyncFunction from './isAsyncFunction';
-import isBigInt from './isBigInt';
-import isBigInt64Array from './isBigInt64Array';
-import isBigIntHave from './isBigIntHave';
-import isBigUint64Array from './isBigUint64Array';
-import isBlob from './isBlob';
-import isBoolean from './isBoolean';
-import isBooleanObject from './isBooleanObject';
-import isBooleanPrimitive from './isBooleanPrimitive';
-import isBuffer from './isBuffer';
-import isBufferHave from './isBufferHave';
-import isClass from './isClass';
-import isDataView from './isDataView';
-import isDate from './isDate';
-import isDouble from './isDouble';
-import isDuplex from './isDuplex';
-import isElement from './isElement';
-import isEqual from './isEqual';
-import isError from './isError';
-import isEvalError from './isEvalError';
-import isEvent from './isEvent';
-import isFalse from './isFalse';
-import isFalsy from './isFalsy';
-import isFile from './isFile';
-import isFileReader from './isFileReader';
-import isFloat32Array from './isFloat32Array';
-import isFloat64Array from './isFloat64Array';
-import isFormData from './isFormData';
-import isFunction from './isFunction';
-import isFunctionHave from './isFunctionHave';
-import isGenerator from './isGenerator';
-import isGeneratorFunction from './isGeneratorFunction';
-import isInfinity from './isInfinity';
-import isInt from './isInt';
-import isInt16Array from './isInt16Array';
-import isInt32Array from './isInt32Array';
-import isInt8Array from './isInt8Array';
-import isIntHave from './isIntHave';
-import isIntSafe from './isIntSafe';
-import isIterable from './isIterable';
-import isIterator from './isIterator';
-import isLengthy from './isLengthy';
-import isLengthyHave from './isLengthyHave';
-import isMap from './isMap';
-import isMapHave from './isMapHave';
-import isNaN from './isNaN';
-import isNegativeInfinity from './isNegativeInfinity';
-import isNodeList from './isNodeList';
-import isNotEqual from './isNotEqual';
-import isNotZero from './isNotZero';
-import isNull from './isNull';
-import isNullOrUndefined from './isNullOrUndefined';
-import isNumber from './isNumber';
-import isNumberHave from './isNumberHave';
-import isNumberObject from './isNumberObject';
-import isNumberPrimitive from './isNumberPrimitive';
-import isNumberSafe from './isNumberSafe';
-import isNumeric from './isNumeric';
-import isObject from './isObject';
-import isObjectHave from './isObjectHave';
-import isPlainObject from './isPlainObject';
-import isPlainObjectHave from './isPlainObjectHave';
-import isPrimitive from './isPrimitive';
-import isPromise from './isPromise';
-import isRangeError from './isRangeError';
-import isReactElement from './isReactElement';
-import isReadable from './isReadable';
-import isReferenceError from './isReferenceError';
-import isRegExp from './isRegExp';
-import isSameError from './isSameError';
-import isSet from './isSet';
-import isSetHave from './isSetHave';
-import isSizey from './isSizey';
-import isSizeyHave from './isSizeyHave';
-import isStream from './isStream';
-import isString from './isString';
-import isStringHave from './isStringHave';
-import isStringObject from './isStringObject';
-import isStringPrimitive from './isStringPrimitive';
-import isStringTrim from './isStringTrim';
-import isSymbol from './isSymbol';
-import isSyncFunction from './isSyncFunction';
-import isSyntaxError from './isSyntaxError';
-import isTextNode from './isTextNode';
-import isTransform from './isTransform';
-import isTrue from './isTrue';
-import isTruthy from './isTruthy';
-import isTypeError from './isTypeError';
-import isTypeofObject from './isTypeofObject';
-import isUDouble from './isUDouble';
-import isUint from './isUint';
-import isUint16Array from './isUint16Array';
-import isUint32Array from './isUint32Array';
-import isUint8Array from './isUint8Array';
-import isUint8ClampedArray from './isUint8ClampedArray';
-import isUintHave from './isUintHave';
-import isUintSafe from './isUintSafe';
-import isUintSafeHave from './isUintSafeHave';
-import isUndefined from './isUndefined';
-import isUrlSearchParams from './isURLSearchParams';
-import isVarName from './isVarName';
-import isWeakMap from './isWeakMap';
-import isWeakSet from './isWeakSet';
-import isWindow from './isWindow';
-import isWritable from './isWritable';
-import isZero from './isZero';
-import MAX_ARRAY_LENGTH from './MAX_ARRAY_LENGTH';
+export { default as getTag } from './getTag';
+export { default as isAnyError } from './isAnyError';
+export { default as isArguments } from './isArguments';
+export { default as isArray } from './isArray';
+export { default as isArrayBuffer } from './isArrayBuffer';
+export { default as isArrayBufferView } from './isArrayBufferView';
+export { default as isArrayHave } from './isArrayHave';
+export { default as isArrayIndex } from './isArrayIndex';
+export { default as isArrayLike } from './isArrayLike';
+export { default as isArrayLikeHave } from './isArrayLikeHave';
+export { default as isAsyncFunction } from './isAsyncFunction';
+export { default as isBigInt } from './isBigInt';
+export { default as isBigInt64Array } from './isBigInt64Array';
+export { default as isBigIntHave } from './isBigIntHave';
+export { default as isBigUint64Array } from './isBigUint64Array';
+export { default as isBlob } from './isBlob';
+export { default as isBoolean } from './isBoolean';
+export { default as isBooleanObject } from './isBooleanObject';
+export { default as isBooleanPrimitive } from './isBooleanPrimitive';
+export { default as isBuffer } from './isBuffer';
+export { default as isBufferHave } from './isBufferHave';
+export { default as isClass } from './isClass';
+export { default as isDataView } from './isDataView';
+export { default as isDate } from './isDate';
+export { default as isDouble } from './isDouble';
+export { default as isDuplex } from './isDuplex';
+export { default as isElement } from './isElement';
+export { default as isEqual } from './isEqual';
+export { default as isError } from './isError';
+export { default as isEvalError } from './isEvalError';
+export { default as isEvent } from './isEvent';
+export { default as isFalse } from './isFalse';
+export { default as isFalsy } from './isFalsy';
+export { default as isFile } from './isFile';
+export { default as isFileReader } from './isFileReader';
+export { default as isFloat32Array } from './isFloat32Array';
+export { default as isFloat64Array } from './isFloat64Array';
+export { default as isFormData } from './isFormData';
+export { default as isFunction } from './isFunction';
+export { default as isFunctionHave } from './isFunctionHave';
+export { default as isGenerator } from './isGenerator';
+export { default as isGeneratorFunction } from './isGeneratorFunction';
+export { default as isInfinity } from './isInfinity';
+export { default as isInt } from './isInt';
+export { default as isInt16Array } from './isInt16Array';
+export { default as isInt32Array } from './isInt32Array';
+export { default as isInt8Array } from './isInt8Array';
+export { default as isIntHave } from './isIntHave';
+export { default as isIntSafe } from './isIntSafe';
+export { default as isIterable } from './isIterable';
+export { default as isIterator } from './isIterator';
+export { default as isLengthy } from './isLengthy';
+export { default as isLengthyHave } from './isLengthyHave';
+export { default as isMap } from './isMap';
+export { default as isMapHave } from './isMapHave';
+export { default as isNaN } from './isNaN';
+export { default as isNegativeInfinity } from './isNegativeInfinity';
+export { default as isNodeList } from './isNodeList';
+export { default as isNotEqual } from './isNotEqual';
+export { default as isNotZero } from './isNotZero';
+export { default as isNull } from './isNull';
+export { default as isNullOrUndefined } from './isNullOrUndefined';
+export { default as isNumber } from './isNumber';
+export { default as isNumberHave } from './isNumberHave';
+export { default as isNumberObject } from './isNumberObject';
+export { default as isNumberPrimitive } from './isNumberPrimitive';
+export { default as isNumberSafe } from './isNumberSafe';
+export { default as isNumeric } from './isNumeric';
+export { default as isObject } from './isObject';
+export { default as isObjectHave } from './isObjectHave';
+export { default as isPlainObject } from './isPlainObject';
+export { default as isPlainObjectHave } from './isPlainObjectHave';
+export { default as isPrimitive } from './isPrimitive';
+export { default as isPromise } from './isPromise';
+export { default as isRangeError } from './isRangeError';
+export { default as isReactElement } from './isReactElement';
+export { default as isReadable } from './isReadable';
+export { default as isReferenceError } from './isReferenceError';
+export { default as isRegExp } from './isRegExp';
+export { default as isSameError } from './isSameError';
+export { default as isSet } from './isSet';
+export { default as isSetHave } from './isSetHave';
+export { default as isSizey } from './isSizey';
+export { default as isSizeyHave } from './isSizeyHave';
+export { default as isStream } from './isStream';
+export { default as isString } from './isString';
+export { default as isStringHave } from './isStringHave';
+export { default as isStringObject } from './isStringObject';
+export { default as isStringPrimitive } from './isStringPrimitive';
+export { default as isStringTrim } from './isStringTrim';
+export { default as isSymbol } from './isSymbol';
+export { default as isSyncFunction } from './isSyncFunction';
+export { default as isSyntaxError } from './isSyntaxError';
+export { default as isTextNode } from './isTextNode';
+export { default as isTransform } from './isTransform';
+export { default as isTrue } from './isTrue';
+export { default as isTruthy } from './isTruthy';
+export { default as isTypeError } from './isTypeError';
+export { default as isTypeofObject } from './isTypeofObject';
+export { default as isUDouble } from './isUDouble';
+export { default as isUint } from './isUint';
+export { default as isUint16Array } from './isUint16Array';
+export { default as isUint32Array } from './isUint32Array';
+export { default as isUint8Array } from './isUint8Array';
+export { default as isUint8ClampedArray } from './isUint8ClampedArray';
+export { default as isUintHave } from './isUintHave';
+export { default as isUintSafe } from './isUintSafe';
+export { default as isUintSafeHave } from './isUintSafeHave';
+export { default as isUndefined } from './isUndefined';
+export { default as isUrlSearchParams } from './isURLSearchParams';
+export { default as isVarName } from './isVarName';
+export { default as isWeakMap } from './isWeakMap';
+export { default as isWeakSet } from './isWeakSet';
+export { default as isWindow } from './isWindow';
+export { default as isWritable } from './isWritable';
+export { default as isZero } from './isZero';
+export { default as MAX_ARRAY_LENGTH } from './MAX_ARRAY_LENGTH';
 
 export type { Lengthy } from './isLengthy';
 export type { Sizey } from './isSizey';
-
-export {
-    getTag,
-    isAnyError,
-    isArguments,
-    isArray,
-    isArrayBuffer,
-    isArrayBufferView,
-    isArrayHave,
-    isArrayIndex,
-    isArrayLike,
-    isArrayLikeHave,
-    isAsyncFunction,
-    isBigInt,
-    isBigInt64Array,
-    isBigIntHave,
-    isBigUint64Array,
-    isBlob,
-    isBoolean,
-    isBooleanObject,
-    isBooleanPrimitive,
-    isBuffer,
-    isBufferHave,
-    isClass,
-    isDataView,
-    isDate,
-    isDouble,
-    isDuplex,
-    isElement,
-    isEqual,
-    isError,
-    isEvalError,
-    isEvent,
-    isFalse,
-    isFalsy,
-    isFile,
-    isFileReader,
-    isFloat32Array,
-    isFloat64Array,
-    isFormData,
-    isFunction,
-    isFunctionHave,
-    isGenerator,
-    isGeneratorFunction,
-    isInfinity,
-    isInt,
-    isInt16Array,
-    isInt32Array,
-    isInt8Array,
-    isIntHave,
-    isIntSafe,
-    isIterable,
-    isIterator,
-    isLengthy,
-    isLengthyHave,
-    isMap,
-    isMapHave,
-    isNaN,
-    isNegativeInfinity,
-    isNodeList,
-    isNotEqual,
-    isNotZero,
-    isNull,
-    isNullOrUndefined,
-    isNumber,
-    isNumberHave,
-    isNumberObject,
-    isNumberPrimitive,
-    isNumberSafe,
-    isNumeric,
-    isObject,
-    isObjectHave,
-    isPlainObject,
-    isPlainObjectHave,
-    isPrimitive,
-    isPromise,
-    isRangeError,
-    isReactElement,
-    isReadable,
-    isReferenceError,
-    isRegExp,
-    isSameError,
-    isSet,
-    isSetHave,
-    isSizey,
-    isSizeyHave,
-    isStream,
-    isString,
-    isStringHave,
-    isStringObject,
-    isStringPrimitive,
-    isStringTrim,
-    isSymbol,
-    isSyncFunction,
-    isSyntaxError,
-    isTextNode,
-    isTransform,
-    isTrue,
-    isTruthy,
-    isTypeError,
-    isTypeofObject,
-    isUDouble,
-    isUint,
-    isUint16Array,
-    isUint32Array,
-    isUint8Array,
-    isUint8ClampedArray,
-    isUintHave,
-    isUintSafe,
-    isUintSafeHave,
-    isUndefined,
-    isUrlSearchParams,
-    isVarName,
-    isWeakMap,
-    isWeakSet,
-    isWindow,
-    isWritable,
-    isZero,
-    MAX_ARRAY_LENGTH
-};
