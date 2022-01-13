873a9f21fa98fbcb2454a590a16cd9cf1ce33d95
wtklbm <wtklbm@gmail.com>
2022-01-13 21:46:27 +0800

    🦄 refactor: 更改导出方式

--- packages/util/src/index.ts
+++ packages/util/src/index.ts
@@ -1,15 +1,6 @@
-import allAttrs from './allAttrs';
-import copy from './copy';
-import copyAttrs from './copyAttrs';
-import lackAttrs from './lackAttrs';
-import maxArgsLength from './maxArgsLength';
-import maxCallStackSize from './maxCallStackSize';
-
-export {
-    allAttrs,
-    copy,
-    copyAttrs,
-    lackAttrs,
-    maxArgsLength,
-    maxCallStackSize
-};
+export { default as allAttrs } from './allAttrs';
+export { default as copy } from './copy';
+export { default as copyAttrs } from './copyAttrs';
+export { default as lackAttrs } from './lackAttrs';
+export { default as maxArgsLength } from './maxArgsLength';
+export { default as maxCallStackSize } from './maxCallStackSize';
