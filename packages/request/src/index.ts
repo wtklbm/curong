af71d472608a3e5c4952e469be52dc24a3e14242
wtklbm <wtklbm@gmail.com>
2022-01-13 21:31:59 +0800

    🦄 refactor: 更改导出方式

--- packages/request/src/index.ts
+++ packages/request/src/index.ts
@@ -1,7 +1,5 @@
-import publicIP from './publicIP';
-import queryDNS from './queryDNS';
+export { default as publicIP } from './publicIP';
+export { default as queryDNS } from './queryDNS';
 
 export * from './request';
 export * from './types';
-
-export { publicIP, queryDNS };
