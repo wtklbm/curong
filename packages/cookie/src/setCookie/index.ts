2a3f51cfddd7c61070f4d0afd250e7ed702c6cd3
wtklbm <wtklbm@gmail.com>
2022-01-13 21:13:27 +0800

    🦄 refactor: 更改导出方式

--- packages/cookie/src/setCookie/index.ts
+++ packages/cookie/src/setCookie/index.ts
@@ -1,7 +1,5 @@
-import joinSetCookie from './join';
-import parseSetCookie from './parse';
-import splitSetCookie from './split';
+export { default as joinSetCookie } from './join';
+export { default as parseSetCookie } from './parse';
+export { default as splitSetCookie } from './split';
 
 export * from './types';
-
-export { joinSetCookie, parseSetCookie, splitSetCookie };
