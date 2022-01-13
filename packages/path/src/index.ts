1a6e02e057e7f8d59a4d928796f57fb0c415d481
wtklbm <wtklbm@gmail.com>
2022-01-13 21:23:40 +0800

    🌈 style: 更新代码样式

--- packages/path/src/index.ts
+++ packages/path/src/index.ts
@@ -1,5 +1,4 @@
 export { default as isWindowsExtendedLengthPath } from './isWindowsExtendedLengthPath';
 export { default as isWindowsPath } from './isWindowsPath';
 export { default as unifiedSeparator } from './unifiedSeparator';
-
 export { default as windowsPathSource } from './windowsPathSource';
