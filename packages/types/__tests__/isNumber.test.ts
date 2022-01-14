5c44e43bb06556ef5afb5cbb6569ba1aacbc64f1
wtklbm <wtklbm@gmail.com>
2022-01-14 15:58:21 +0800

    🧪 test: 添加测试

--- packages/types/__tests__/isNumber.test.ts
+++ packages/types/__tests__/isNumber.test.ts
@@ -7,9 +7,16 @@ describe('@curong/types/isNumber', () => {
 
     test('测试2', () => {
         expect(isNumber(12)).toBe(true);
-    });
-
-    test('测试3', () => {
         expect(isNumber([1])).toBe(false);
+        expect(isNumber(0)).toBe(true);
+        expect(isNumber(1)).toBe(true);
+        expect(isNumber(1.1)).toBe(true);
+        expect(isNumber(0xff)).toBe(true);
+
+        // @ts-ignore
+        expect(isNumber(0644)).toBe(true);
+        expect(isNumber(6.2e5)).toBe(true);
+        expect(isNumber(NaN)).toBe(false);
+        expect(isNumber(Infinity)).toBe(true);
     });
 });
