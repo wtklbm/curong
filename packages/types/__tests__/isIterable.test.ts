549410e49ddf83bd2c9f100695eb828f6b495c4c
wtklbm <wtklbm@gmail.com>
2022-01-14 15:58:15 +0800

    🧪 test: 添加测试

--- packages/types/__tests__/isIterable.test.ts
+++ packages/types/__tests__/isIterable.test.ts
@@ -5,20 +5,28 @@ describe('@curong/types/isIterable', () => {
         expect(isIterable(new Function())).toBe(false);
         expect(isIterable(0)).toBe(false);
         expect(isIterable(false)).toBe(false);
+        expect(isIterable(NaN)).toBe(false);
+        expect(isIterable(Infinity)).toBe(false);
         expect(isIterable(undefined)).toBe(false);
         expect(isIterable(null)).toBe(false);
+        expect(isIterable(Promise)).toBe(false);
+        expect(isIterable(Promise.resolve())).toBe(false);
+        expect(isIterable({})).toBe(false);
+        expect(isIterable({ then() {} })).toBe(false);
+        expect(isIterable((() => {})())).toBe(false);
     });
 
     test('测试2', () => {
         function* test() {}
         expect(isIterable(test())).toBe(true);
         expect(isIterable('')).toBe(true);
+
         expect(isIterable([])).toBe(true);
         expect(isIterable('')).toBe(true);
-        expect(isIterable({})).toBe(false);
         expect(isIterable(new Set())).toBe(true);
         expect(isIterable(new WeakSet())).toBe(false);
         expect(isIterable(new Map())).toBe(true);
         expect(isIterable(new WeakMap())).toBe(false);
+        expect(isIterable((function* () {})())).toBe(true);
     });
 });
