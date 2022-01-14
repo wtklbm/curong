09fab9beeefa67f08c3282c6e7204430b51876ab
wtklbm <wtklbm@gmail.com>
2022-01-14 13:37:51 +0800

    🧪 test: 更新测试

--- packages/function/__tests__/tryCatch.test.ts
+++ packages/function/__tests__/tryCatch.test.ts
@@ -9,17 +9,47 @@ describe('@curong/function/tryCatch', () => {
         expect(ret).toBe(10);
     });
 
-    test('测试2', () => {
-        tryCatch(() => {
+    test('测试2', async () => {
+        await tryCatch(() => {
             throw new Error('报错了');
         }).catch(e => {
             expect(e.message).toBe('报错了');
         });
 
-        tryCatch(async () => {
+        await tryCatch(async () => {
             return Promise.resolve(Promise.reject('报错了'));
         }).catch(e => {
             expect(e).toBe('报错了');
+        });
+    });
+
+    test('测试3', async () => {
+        let ret = await tryCatch(() => 1);
+        expect(ret).toBe(1);
+
+        ret = await tryCatch(async () => Promise.resolve(10));
+        expect(ret).toBe(10);
+    });
+
+    test('测试5', async () => {
+        const fn1 = () => new Promise(r => setTimeout(() => r('完成了'), 1e3));
+        const fn2 = () =>
+            new Promise((_, r) => setTimeout(() => r('出错了'), 1e3));
+
+        expect(await tryCatch(fn1)).toBe('完成了');
+        await tryCatch(fn2).catch(e => {
+            expect(e).toBe('出错了');
+        });
+    });
+
+    test('测试6', async () => {
+        const c = {};
+
+        // @ts-ignore
+        await tryCatch(() => c.xx.xx.xx).catch(e => {
+            expect(e.message).toBe(
+                `Cannot read properties of undefined (reading 'xx')`
+            );
         });
     });
 });
