a8d02bf942eb7d711c0c7585cb433667c1bac860
wtklbm <wtklbm@gmail.com>
2022-01-14 15:00:02 +0800

    ✨ feat: 添加类型定义

--- packages/types/src/isClass.ts
+++ packages/types/src/isClass.ts
@@ -1,12 +1,17 @@
 import isFunction from './isFunction';
 
+/** 类的类型定义 */
+export interface Class<T = any> {
+    new (...args: any[]): T;
+}
+
 /**
  * 是不是一个类
  *
  * @param value 要验证的值
  * @returns 是则返回 `true`，否则为 `false`
  */
-export default function isClass(value: unknown): value is Function {
+export default function isClass<T = any>(value: unknown): value is Class<T> {
     // 类不能直接执行，必须使用 `new` 关键字，
     // 类中无法访问 `caller`，`callee` 和 `arguments` 属性
     return (
