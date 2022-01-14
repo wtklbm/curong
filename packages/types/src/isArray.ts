48b7b3775b1c8bd0b51917286329bfc33a49dea0
wtklbm <wtklbm@gmail.com>
2022-01-14 14:11:51 +0800

    🦄 refactor: 更新类型定义

--- packages/types/src/isArray.ts
+++ packages/types/src/isArray.ts
@@ -13,7 +13,7 @@
  * }
  * ```
  */
-export default function isArray(value: unknown): value is Array<any> {
+export default function isArray<T extends any[]>(value: unknown): value is T {
     // Note: 数组/类数组、字符串、函数、对象、`Window`、`Buffer`/`ArrayBuffer` 等都有 `length` 属性
     return Array.isArray(value);
 }
