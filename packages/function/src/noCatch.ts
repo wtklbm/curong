2a1fac8d7791ed21828a6339e16adb0d15533591
wtklbm <wtklbm@gmail.com>
2022-01-14 13:15:17 +0800

    🦄 refactor: 更新代码逻辑

--- packages/function/src/noCatch.ts
+++ packages/function/src/noCatch.ts
@@ -1,10 +1,8 @@
-import { isFunction, isIterator } from '@curong/types';
-
 /**
- * 获取值，但忽略抛出的错误
+ * 获取函数返回的值，并忽略抛出的错误
  *
- * @param value 要获取的值 (包括普通值、生成器、迭代器、同步函数和异步函数)
- * @returns 返回获取到的结果
+ * @param fn 要执行的函数 (包括同步函数和异步函数，不包括 `Generator` 函数)
+ * @returns 返回函数的执行结果
  * @example
  *
  * ```javascript
@@ -13,16 +11,11 @@ import { isFunction, isIterator } from '@curong/types';
  * console.log(ret); // 完成了
  * ```
  */
-export default async function noCatch(value: any) {
+export default async function noCatch<T = any>(
+    fn: (...args: any[]) => Promise<T> | T,
+    ...args: any[]
+) {
     try {
-        if (isFunction(value)) {
-            value = value();
-        }
-
-        if (isIterator(value)) {
-            return value.next().value;
-        }
-
-        return Promise.resolve(value).catch(() => {});
+        return Promise.resolve(fn(...args)).catch(() => {});
     } catch (_) {}
 }
