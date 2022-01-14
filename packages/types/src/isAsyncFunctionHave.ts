4147614aeb0dac04f12d576c4cd453a38aafb8ad
wtklbm <wtklbm@gmail.com>
2022-01-14 15:03:05 +0800

    ✨ feat: 添加 `isAsyncFunctionHave` 方法

---
+++ packages/types/src/isAsyncFunctionHave.ts
@@ -0,0 +1,13 @@
+import isAsyncFunction, { AsyncFunction } from './isAsyncFunction';
+
+/**
+ * 是不是一个参数个数大于 `0` 的异步函数
+ *
+ * @param value 要验证的值
+ * @returns 是则返回 `true`，否则为 `false`
+ */
+export default function isAsyncFunctionHave<T = any>(
+    value: unknown
+): value is AsyncFunction<T> {
+    return isAsyncFunction(value) && value.length > 0;
+}
