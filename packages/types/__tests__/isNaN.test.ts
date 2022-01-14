ce34796bb9d790784e82f5a9f60441d9475c9df1
wtklbm <wtklbm@gmail.com>
2022-01-14 15:38:38 +0800

    🧪 test: 添加测试

--- packages/types/__tests__/isNaN.test.ts
+++ packages/types/__tests__/isNaN.test.ts
@@ -3,13 +3,22 @@ import { isNaN } from '../src';
 describe('@curong/types/isNaN', () => {
     test('测试1', () => {
         expect(isNaN(12)).toBe(false);
+        expect(isNaN(12, false)).toBe(false);
     });
 
     test('测试2', () => {
         expect(isNaN(NaN)).toBe(true);
+        expect(isNaN(NaN, false)).toBe(true);
     });
 
     test('测试3', () => {
         expect(isNaN([1])).toBe(false);
+        expect(isNaN([1], false)).toBe(false);
+
+        expect(isNaN([1, 2])).toBe(true);
+        expect(isNaN([1, 2], false)).toBe(false);
+
+        expect(isNaN('中')).toBe(true);
+        expect(isNaN('中', false)).toBe(false);
     });
 });
