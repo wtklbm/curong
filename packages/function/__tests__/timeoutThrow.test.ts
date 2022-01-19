ed1055693d61a642e709f58ae013e916346dda6a
wtklbm <wtklbm@gmail.com>
2022-01-19 15:59:08 +0800

    🧪 test: 添加 `timeoutThrow` 方法的测试

---
+++ packages/function/__tests__/timeoutThrow.test.ts
@@ -0,0 +1,27 @@
+import { timeoutThrow } from '../src';
+
+describe('@curong/function/timeoutThrow', () => {
+    test('测试1', async () => {
+        const fn = (a: number, b: string) => a + +b * 2;
+        const ret = await timeoutThrow(122, fn, 1, '2');
+
+        expect(ret).toBe(5);
+    });
+
+    test('测试2', async () => {
+        const fn = (a: number, b: string, bool: boolean) =>
+            new Promise(resolve => {
+                const timer = setTimeout(() => {
+                    clearTimeout(timer);
+                    resolve(a + +b * 2 - +bool);
+                }, 999);
+            });
+
+        const ret = await timeoutThrow(1e3, fn, 1, '2', false);
+        expect(ret).toBe(5);
+
+        await timeoutThrow(123, fn, 1, '2', true).catch(e =>
+            expect(e).toBeTruthy()
+        );
+    });
+});
