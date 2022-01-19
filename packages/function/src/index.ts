0261e7f0a7e0013f5478c3f5dd619eb113181861
wtklbm <wtklbm@gmail.com>
2022-01-19 15:56:48 +0800

    ✨ feat: 添加 `timeoutThrow` 方法

--- packages/function/src/index.ts
+++ packages/function/src/index.ts
@@ -2,8 +2,10 @@ export { default as before } from './before';
 export { default as curring } from './curring';
 export { default as noCatch } from './noCatch';
 export { default as reach } from './reach';
+export { default as sleepAsync } from './sleepAsync';
 export { default as sleepRun } from './sleepRun';
 export { default as sleepSync } from './sleepSync';
+export { default as timeoutThrow } from './timeoutThrow';
 export { default as tryCatch } from './tryCatch';
 
 export * from './types';
