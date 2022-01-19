d6ad494ca3d2727d9d4cc62420207438d56b19ba
wtklbm <wtklbm@gmail.com>
2022-01-19 22:16:38 +0800

    📃 docs: 添加文档

--- packages/types/src/isSizey.ts
+++ packages/types/src/isSizey.ts
@@ -1,6 +1,7 @@
 import isUint from './isUint';
 import isNullOrUndefined from './isNullOrUndefined';
 
+/** 具有 `size` 属性的值的类型定义 */
 export type Sizey<T extends {} = {}> = T & { size: number };
 
 /**
