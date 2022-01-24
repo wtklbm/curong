a406bcc6e1eeabddd75764e9494d643c5f2a589d
wtklbm <wtklbm@gmail.com>
2022-01-25 01:09:16 +0800

    ✨ feat: 添加 `cancelExec` 方法

---
+++ packages/function/src/cancelExec.ts
@@ -0,0 +1,52 @@
+import { isFunction } from '@curong/types';
+
+/**
+ * 随时取消对 `Promise` 的执行
+ *
+ * @param promise 要执行的 `Promise`
+ * @param isThrow 是否在取消 `Promise` 后抛出一个错误，默认为 `false`
+ * @returns 返回一个数组，数组的第一项是一个新的 `Promise`，第二项是中止新的 `Promise` 的函数
+ * @example
+ *
+ * ```javascript
+ * const fn = (a: number, b: string, bool: boolean) => {
+ *     return new Promise(resolve => {
+ *         const timer = setTimeout(() => {
+ *             clearTimeout(timer);
+ *             resolve(a + +b * 2 - +bool);
+ *         }, 3e3);
+ *     });
+ * };
+ *
+ * const [promise, abort] = cancelExec(fn(1, '2', false));
+ *
+ * promise.then(
+ *     data => console.log(data),
+ *     err => console.log(err) // 在这里打印 "超过 2s 了"
+ * );
+ *
+ * setTimeout(() => {
+ *     abort('超过 2s 了');
+ * }, 2e3);
+ * ```
+ */
+export default function cancelExec<T extends unknown>(
+    promise: Promise<T>,
+    isThrow: boolean = false
+): [Promise<T>, (payload?: unknown) => void] {
+    let abort: (payload?: unknown) => void;
+
+    return [
+        Promise.race([
+            promise,
+            new Promise(
+                (resolve, reject) =>
+                    (abort = payload => {
+                        payload = isFunction(payload) ? payload() : payload;
+                        return isThrow ? reject(payload) : resolve(payload);
+                    })
+            )
+        ]) as Promise<T>,
+        abort!
+    ];
+}
