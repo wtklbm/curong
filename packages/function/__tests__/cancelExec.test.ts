6af6a16e22bcec24d2ea8ababc399370c6dd3c93
wtklbm <wtklbm@gmail.com>
2022-01-25 01:09:25 +0800

    🧪 test: 添加 `cancelExec` 方法的测试

---
+++ packages/function/__tests__/cancelExec.test.ts
@@ -0,0 +1,148 @@
+import { cancelExec } from '../src';
+
+const fn = (a: number, b: string, bool: boolean) => {
+    return new Promise(resolve => {
+        const timer = setTimeout(() => {
+            clearTimeout(timer);
+            resolve(a + +b * 2 - +bool);
+        }, 50);
+    });
+};
+
+describe('@curong/function/cancelExec', () => {
+    test('测试1', () => {
+        const [promise, abort] = cancelExec(fn(1, '2', false));
+
+        promise.then(
+            data => {
+                expect(data).toBe('超过 2s 了');
+            },
+            err => {
+                expect(err).toBe(undefined);
+            }
+        );
+
+        setTimeout(() => {
+            abort('超过 2s 了');
+        }, 10);
+    });
+
+    test('测试2', () => {
+        const [promise, abort] = cancelExec(fn(1, '2', false), true);
+
+        promise.then(
+            data => {
+                expect(data).toBe(undefined);
+            },
+            err => {
+                expect(err).toBe('超过 2s 了');
+            }
+        );
+
+        setTimeout(() => {
+            abort('超过 2s 了');
+        }, 10);
+    });
+
+    test('测试3', () => {
+        const [promise, abort] = cancelExec(fn(1, '2', false));
+
+        promise.then(
+            data => {
+                expect(data).toBe(undefined);
+            },
+            err => {
+                expect(err).toBe(undefined);
+            }
+        );
+
+        setTimeout(() => {
+            abort();
+        }, 10);
+    });
+
+    test('测试4', () => {
+        const [promise, abort] = cancelExec(fn(1, '2', false));
+
+        promise.then(
+            data => {
+                expect(data).toBe(undefined);
+            },
+            err => {
+                expect(err).toBe(undefined);
+            }
+        );
+
+        setTimeout(() => {
+            abort();
+        }, 10);
+    });
+
+    test('测试5', () => {
+        const [promise, abort] = cancelExec(fn(1, '2', false));
+
+        promise.then(
+            data => {
+                expect(data).toBe(5);
+            },
+            err => {
+                expect(err).toBe(undefined);
+            }
+        );
+
+        setTimeout(() => {
+            abort('超过 2s 了');
+        }, 60);
+    });
+
+    test('测试5', () => {
+        const [promise, abort] = cancelExec(fn(1, '2', false), true);
+
+        promise.then(
+            data => {
+                expect(data).toBe(5);
+            },
+            err => {
+                expect(err).toBe(undefined);
+            }
+        );
+
+        setTimeout(() => {
+            abort('超过 2s 了');
+        }, 60);
+    });
+
+    test('测试6', () => {
+        const [promise, abort] = cancelExec(fn(1, '2', false));
+
+        promise.then(
+            data => {
+                expect(data).toBe('超过 2s 了');
+            },
+            err => {
+                expect(err).toBe(undefined);
+            }
+        );
+
+        setTimeout(() => {
+            abort(() => '超过 2s 了');
+        }, 10);
+    });
+
+    test('测试7', () => {
+        const [promise, abort] = cancelExec(fn(1, '2', false), true);
+
+        promise.then(
+            data => {
+                expect(data).toBe(undefined);
+            },
+            err => {
+                expect(err).toBe('超过 2s 了');
+            }
+        );
+
+        setTimeout(() => {
+            abort(() => '超过 2s 了');
+        }, 10);
+    });
+});
