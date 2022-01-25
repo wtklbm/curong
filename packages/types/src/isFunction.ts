a1fdc74d6f8e3d5382e3e8ab0edaa800b7252337
wtklbm <wtklbm@gmail.com>
2022-01-25 19:30:54 +0800

    🦄 refactor: 更新类型定义

--- packages/types/src/isFunction.ts
+++ packages/types/src/isFunction.ts
@@ -1,5 +1,5 @@
 /** 函数的类型定义 */
-export type Function<T = any> = (...args: any[]) => T;
+export type Function<T = unknown> = (...args: any[]) => T;
 
 /**
  * 是不是一个函数(包含同步函数、异步函数和 `Generator` 函数)
@@ -7,7 +7,7 @@ export type Function<T = any> = (...args: any[]) => T;
  * @param value 要验证的值
  * @returns 是则返回 `true`，否则为 `false`
  */
-export default function isFunction<T = any>(
+export default function isFunction<T = unknown>(
     value: unknown
 ): value is Function<T> {
     return typeof value === 'function';
