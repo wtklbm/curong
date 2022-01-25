0b3b3ec993d2f1c3e8097378ce7b68a8e461d178
wtklbm <wtklbm@gmail.com>
2022-01-25 19:30:45 +0800

    🦄 refactor: 更新类型定义

--- packages/types/src/isClass.ts
+++ packages/types/src/isClass.ts
@@ -1,7 +1,7 @@
 import isFunction from './isFunction';
 
 /** 类的类型定义 */
-export interface Class<T = any> {
+export interface Class<T = unknown> {
     new (...args: any[]): T;
 }
 
@@ -11,7 +11,7 @@ export interface Class<T = any> {
  * @param value 要验证的值
  * @returns 是则返回 `true`，否则为 `false`
  */
-export default function isClass<T = any>(value: unknown): value is Class<T> {
+export default function isClass<T = unknown>(value: unknown): value is Class<T> {
     // 类不能直接执行，必须使用 `new` 关键字，
     // 类中无法访问 `caller`，`callee` 和 `arguments` 属性
     return (
