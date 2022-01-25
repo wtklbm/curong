a7d0255a359f562a3a87b11278124a67941a1d06
wtklbm <wtklbm@gmail.com>
2022-01-25 19:32:37 +0800

    🦄 refactor: 更新类型定义

--- packages/types/src/isSet.ts
+++ packages/types/src/isSet.ts
@@ -6,6 +6,6 @@ import getTag from './getTag';
  * @param value 要验证的值
  * @returns 是则返回 `true`，否则为 `false`
  */
-export default function isSet<T = any>(value: unknown): value is Set<T> {
+export default function isSet<T = unknown>(value: unknown): value is Set<T> {
     return getTag(value) === 'Set';
 }
