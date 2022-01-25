91b4e61aa327dd5fae85d43dde97c1c3f699144d
wtklbm <wtklbm@gmail.com>
2022-01-25 19:31:13 +0800

    🦄 refactor: 更新类型定义

--- packages/types/src/isIterator.ts
+++ packages/types/src/isIterator.ts
@@ -8,7 +8,7 @@ import isNullOrUndefined from './isNullOrUndefined';
  * @returns 是则返回 `true`，否则为 `false`
  * @info 在这里，只要迭代器有 `next` 方法，则返回 `true`
  */
-export default function isIterator<T = any, R = any, N = undefined>(
+export default function isIterator<T = unknown, R = unknown, N = undefined>(
     value: any
 ): value is Iterator<T, R, N> {
     return !isNullOrUndefined(value) && isFunction(value.next);
