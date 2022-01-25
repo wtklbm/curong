17092e049ed012f4f40e7980badfd37bb06833b7
wtklbm <wtklbm@gmail.com>
2022-01-25 19:30:31 +0800

    🦄 refactor: 更新类型定义

--- packages/types/src/isAsyncFunction.ts
+++ packages/types/src/isAsyncFunction.ts
@@ -1,7 +1,7 @@
 import getTag from './getTag';
 
 /** 异步函数的类型定义 */
-export type AsyncFunction<T = any> = (...args: any[]) => Promise<T>;
+export type AsyncFunction<T = unknown> = (...args: any[]) => Promise<T>;
 
 /**
  * 是不是一个异步函数
@@ -9,7 +9,7 @@ export type AsyncFunction<T = any> = (...args: any[]) => Promise<T>;
  * @param value 要验证的值
  * @returns 是则返回 `true`，否则为 `false`
  */
-export default function isAsyncFunction<T = any>(
+export default function isAsyncFunction<T = unknown>(
     value: unknown
 ): value is AsyncFunction<T> {
     return getTag(value) === 'AsyncFunction';
