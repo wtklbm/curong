5188fe9a738a83ba21afdaa3ddf5bbfbcbcf866a
wtklbm <wtklbm@gmail.com>
2022-02-02 13:48:50 +0800

    🌈 style: 格式化代码

--- packages/string/__tests__/subSpaces.test.ts
+++ packages/string/__tests__/subSpaces.test.ts
@@ -2,10 +2,12 @@ import { subSpaces } from '../src';
 
 describe('@curong/string/subSpaces', () => {
     test('测试1', () => {
-        expect(subSpaces('有100本书。', /\d+/)).toBe('有 100 本书。')
+        expect(subSpaces('有100本书。', /\d+/)).toBe('有 100 本书。');
     });
 
     test('测试1', () => {
-        expect(subSpaces('有100本书。', /\d+/, v => '`' + v + '`')).toBe('有 `100` 本书。')
+        expect(subSpaces('有100本书。', /\d+/, v => '`' + v + '`')).toBe(
+            '有 `100` 本书。'
+        );
     });
 });
