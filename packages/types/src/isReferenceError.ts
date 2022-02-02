371845b16d3c1c4d4a3d5b1efd10d7e3a1992f9d
wtklbm <wtklbm@gmail.com>
2022-02-02 13:50:52 +0800

    🌈 style: 格式化代码

--- packages/types/src/isReferenceError.ts
+++ packages/types/src/isReferenceError.ts
@@ -6,7 +6,9 @@ import isAnyError from './isAnyError';
  * @param value 要验证的值
  * @returns 是则返回 `true`，否则为 `false`
  */
-export default function isReferenceError(value: unknown): value is ReferenceError {
+export default function isReferenceError(
+    value: unknown
+): value is ReferenceError {
     return (
         isAnyError(value) &&
         Object.getPrototypeOf(value).name === 'ReferenceError'
