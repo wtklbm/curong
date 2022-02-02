c40824ab71ed5a3d51f76e6a24b59b9f7ba260ba
wtklbm <wtklbm@gmail.com>
2022-02-02 13:52:26 +0800

    🦄 refactor: 重新处理 `timer` 逻辑

--- packages/function/src/timeoutThrow.ts
+++ packages/function/src/timeoutThrow.ts
@@ -1,5 +1,3 @@
-import { isUint } from '@curong/types';
-
 /** 超时时错误 */
 export class TimeoutThrowError extends Error {
     constructor(message: string) {
@@ -12,9 +10,12 @@ export class TimeoutThrowError extends Error {
  * 执行一个函数，并获取函数的返回值。如果函数的执行时间超过 `duration` 时，就会抛出异常
  *
  * @param duration 一个以毫秒为单位的超时时间
+ *  - 如果 `duration` 为小于或等于 0 的数字，则表示定时器应尽快执行
+ *  - 如果 `duration` 为一个大于 0 的数字，则表示至少应等待 `duration` 毫秒后执行
  * @param fn 要执行的同步或异步函数
  * @param args 要传递给函数的参数
  * @returns 如果函数执行的时间没有超时，则返回函数的返回值，否则返回一个超时错误
+ * @throw 如果方法已超时，则会抛出 `TimeoutThrowError` 异常
  * @example
  *
  * ```javascript
@@ -27,17 +28,13 @@ export default async function timeoutThrow<
     A extends readonly unknown[],
     R = unknown
 >(duration: number, fn: (...args: A) => R, ...args: A): Promise<R> {
-    if (!isUint(duration)) {
-        throw new TypeError(
-            `[timeoutThrow]: duration不是一个有效的超时毫秒数: "${duration}"`
-        );
-    }
-
     return Promise.race([
         Promise.resolve(fn(...args)),
         new Promise((_, reject) => {
-            const timer = setTimeout(() => {
+            let timer: any = setTimeout(() => {
                 clearTimeout(timer);
+                timer = null;
+
                 reject(
                     new TimeoutThrowError(
                         `[timeoutThrow]: 该方法执行的时间已超时: "${duration}ms"`
