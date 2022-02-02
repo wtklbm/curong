58b4f833dee8b4d2dea8914122f5a4d965ca7131
wtklbm <wtklbm@gmail.com>
2022-02-02 13:49:05 +0800

    🌈 style: 格式化代码

--- packages/types/__tests__/isIterator.test.ts
+++ packages/types/__tests__/isIterator.test.ts
@@ -3,7 +3,7 @@ import { isIterator } from '../src';
 describe('@curong/types/isIterator', () => {
     test('测试1', () => {
         expect(isIterator(new Function())).toBe(false);
-        expect(isIterator("")).toBe(false);
+        expect(isIterator('')).toBe(false);
         expect(isIterator(0)).toBe(false);
         expect(isIterator(false)).toBe(false);
         expect(isIterator(undefined)).toBe(false);
