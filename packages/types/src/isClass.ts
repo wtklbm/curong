dc55a0b12cd9df98022989774df2bfaebb448ed7
wtklbm <wtklbm@gmail.com>
2022-02-02 13:50:42 +0800

    🌈 style: 格式化代码

--- packages/types/src/isClass.ts
+++ packages/types/src/isClass.ts
@@ -11,7 +11,9 @@ export interface Class<T = unknown> {
  * @param value 要验证的值
  * @returns 是则返回 `true`，否则为 `false`
  */
-export default function isClass<T = unknown>(value: unknown): value is Class<T> {
+export default function isClass<T = unknown>(
+    value: unknown
+): value is Class<T> {
     // 类不能直接执行，必须使用 `new` 关键字，
     // 类中无法访问 `caller`，`callee` 和 `arguments` 属性
     return (
