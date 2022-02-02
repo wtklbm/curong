5ef73709b7b3bfb83956f05bed388a1bef89913e
wtklbm <wtklbm@gmail.com>
2022-02-02 13:48:59 +0800

    🌈 style: 格式化代码

--- packages/string/__tests__/trimEnd.test.ts
+++ packages/string/__tests__/trimEnd.test.ts
@@ -4,12 +4,8 @@ describe('@curong/string/trimEnd', () => {
     test('测试1', () => {
         expect(trimEnd(`xxx \u3000`)).toBe('xxx');
         expect(trimEnd(`xxx \u3000`, { space: false })).toBe('xxx ');
-        expect(trimEnd(`xxx \u200B`, { zeroWidth: false })).toBe(
-            'xxx \u200B'
-        );
-        expect(trimEnd(`xxx \u3000`, { likeSpace: false })).toBe(
-            'xxx \u3000'
-        );
+        expect(trimEnd(`xxx \u200B`, { zeroWidth: false })).toBe('xxx \u200B');
+        expect(trimEnd(`xxx \u3000`, { likeSpace: false })).toBe('xxx \u3000');
         expect(trimEnd(`xxx\t \u3000`)).toBe('xxx');
         expect(trimEnd(`xxx\t \u3000`, { control: false })).toBe('xxx\t');
     });
