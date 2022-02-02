5e3a15ba13ccb2ada732733c8817d4284b3107b8
wtklbm <wtklbm@gmail.com>
2022-02-02 13:52:10 +0800

    🦄 refactor: 重新处理 `timer` 逻辑

--- packages/function/src/sleepAsync.ts
+++ packages/function/src/sleepAsync.ts
@@ -1,10 +1,9 @@
-import { isUint } from '@curong/types';
-
 /**
  * 异步阻塞一段时间
  *
  * @param duration 要阻塞多长时间，以毫秒为单位
- * @throw 如果 duration 不是一个无符号整数，则会抛出异常
+ *  - 如果 `duration` 为小于或等于 0 的数字，则表示定时器应尽快执行
+ *  - 如果 `duration` 为一个大于 0 的数字，则表示至少应等待 `duration` 毫秒后执行
  * @example
  *
  * ```javascript
@@ -14,15 +13,10 @@ import { isUint } from '@curong/types';
  * ```
  */
 export default async function sleepAsync(duration: number): Promise<void> {
-    if (!isUint(duration)) {
-        throw new TypeError(
-            `[sleepAsync]: duration不是一个有效的超时毫秒数: "${duration}"`
-        );
-    }
-
     return new Promise(resolve => {
-        const timer = setTimeout(() => {
+        let timer: any = setTimeout(() => {
             clearTimeout(timer);
+            timer = null;
             resolve();
         }, duration);
     });
