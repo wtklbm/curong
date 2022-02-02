bb5a6e592789d943e2c1333af9be2fc653a108d4
wtklbm <wtklbm@gmail.com>
2022-02-02 13:50:37 +0800

    🌈 style: 格式化代码

--- packages/types/src/isBigUint64Array.ts
+++ packages/types/src/isBigUint64Array.ts
@@ -6,6 +6,8 @@ import getTag from './getTag';
  * @param value 要验证的值
  * @returns 是则返回 `true`，否则为 `false`
  */
-export default function isBigUint64Array(value: unknown): value is BigUint64Array {
+export default function isBigUint64Array(
+    value: unknown
+): value is BigUint64Array {
     return getTag(value) === 'BigUint64Array';
 }
