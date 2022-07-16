0082076b7fc797801ca0acaa161084ea42c81c05
wtklbm <wtklbm@gmail.com>
2022-07-17 01:09:49 +0800

    📃 docs: 更新文档

--- packages/types/src/isBigInt.ts
+++ packages/types/src/isBigInt.ts
@@ -3,6 +3,8 @@
  *
  * @param value 要验证的值
  * @returns 是则返回 `true`，否则为 `false`
+ * @note
+ *  - `BigInt` 和 `Number` 是不兼容的，不能相互赋值，也不能直接做加法运算
  */
 export default function isBigInt(value: unknown): value is BigInt {
     return typeof value === 'bigint';
