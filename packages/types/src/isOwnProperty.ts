f022eb9947665cd7cec7132c9192c8c2b8bb9024
wtklbm <wtklbm@gmail.com>
2022-07-19 14:46:29 +0800

    ✨ feat: 添加 `isOwnProperty` 方法

---
+++ packages/types/src/isOwnProperty.ts
@@ -0,0 +1,12 @@
+import isNullOrUndefined from './isNullOrUndefined';
+
+/**
+ * 确定一个值上是否具有具有指定名称的私有属性
+ *
+ * @param value 值
+ * @param key 属性名称
+ * @returns 如果存在私有属性，则返回 `true`，否则为 `false`
+ */
+export default function isOwnProperty(value: unknown, key: PropertyKey): boolean {
+    return !isNullOrUndefined(value) && Object.hasOwnProperty.call(value, key);
+}
