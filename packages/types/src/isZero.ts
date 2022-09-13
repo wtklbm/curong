9b364d71bb28375d80b9c792b0431b46bf89e96a
wtklbm <wtklbm@gmail.com>
2022-09-13 18:10:06 +0800

    🦄 refactor: 使用 `unknown` 代替 `any`

--- packages/types/src/isZero.ts
+++ packages/types/src/isZero.ts
@@ -4,6 +4,6 @@
  * @param value 要验证的值
  * @returns 是则返回 `true`，否则为 `false`
  */
-export default function isZero(value: any): boolean {
+export default function isZero(value: unknown): boolean {
     return value === 0;
 }
