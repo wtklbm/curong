d788a8f99cca1be84162e66dd728c6919875e6f0
wtklbm <wtklbm@gmail.com>
2022-09-13 18:09:38 +0800

    🦄 refactor: 使用 `unknown` 代替 `any`

--- packages/types/src/isArguments.ts
+++ packages/types/src/isArguments.ts
@@ -6,6 +6,6 @@ import getTag from './getTag';
  * @param value 要验证的值
  * @returns 是则返回 `true`，否则为 `false`
  */
-export default function isArguments(value: any): value is IArguments {
+export default function isArguments(value: unknown): value is IArguments {
     return getTag(value) === 'Arguments';
 }
