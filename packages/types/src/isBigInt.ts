3e105f31591e2aa202a77fe7a5e12c0af58ef533
wtklbm <wtklbm@gmail.com>
2023-01-20 22:28:07 +0800

    🦄 refactor: 优化调用逻辑

--- packages/types/src/isBigInt.ts
+++ packages/types/src/isBigInt.ts
@@ -1,5 +1,8 @@
+import isBigIntObject from './isBigIntObject';
+import isBigIntPrimitive from './isBigIntPrimitive';
+
 /**
- * 是不是一个 `BigInt` (大数)
+ * 是不是一个 `BigInt` (大数) 或被包装后的 `BigInt` 对象
  *
  * @param value 要验证的值
  * @returns 是则返回 `true`，否则为 `false`
@@ -7,5 +10,5 @@
  *  - `BigInt` 和 `Number` 是不兼容的，不能相互赋值，也不能直接做加法运算
  */
 export default function isBigInt(value: unknown): value is BigInt {
-    return typeof value === 'bigint';
+    return isBigIntPrimitive(value) || isBigIntObject(value);
 }
