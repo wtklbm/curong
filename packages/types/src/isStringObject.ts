e13532ae2ce14bfc2651e755a4434fc3f1956b3e
wtklbm <wtklbm@gmail.com>
2023-01-20 22:06:45 +0800

    📃 docs: 更新文档

--- packages/types/src/isStringObject.ts
+++ packages/types/src/isStringObject.ts
@@ -1,7 +1,7 @@
 import getTag from './getTag';
 
 /**
- * 是不是一个被包装后的字符串对象，即 `new String()`
+ * 是不是一个被包装后的字符串对象，即 `new String()` || `Object('')`
  *
  * @param value 要验证的值
  * @returns 是则返回 `true`，否则为 `false`
