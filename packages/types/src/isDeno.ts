d9075b3de173d083f2d187878372b33c09c6fde6
wtklbm <wtklbm@gmail.com>
2023-01-20 23:03:52 +0800

    🎈 perf: 添加判断条件

--- packages/types/src/isDeno.ts
+++ packages/types/src/isDeno.ts
@@ -8,5 +8,9 @@ declare global {
  * @returns 是则返回 `true`，否则为 `false`
  */
 export default function isDeno(): boolean {
-    return typeof Deno !== 'undefined' && typeof Deno.version !== 'undefined';
+    return (
+        typeof Deno !== 'undefined' &&
+        typeof Deno.version !== 'undefined' &&
+        typeof Deno.core !== 'undefined'
+    );
 }
