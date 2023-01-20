ed999f386692080f40ea44a5f6f3099fbe198bab
wtklbm <wtklbm@gmail.com>
2023-01-20 22:06:40 +0800

    📃 docs: 更新文档

--- packages/types/src/isNumberObject.ts
+++ packages/types/src/isNumberObject.ts
@@ -1,7 +1,7 @@
 import getTag from './getTag';
 
 /**
- * 是不是一个被包装后的数字对象，即 `new Number()`
+ * 是不是一个被包装后的数字对象，即 `new Number()` || `Object(0)`
  *
  * @param value 要验证的值
  * @returns 是则返回 `true`，否则为 `false`
