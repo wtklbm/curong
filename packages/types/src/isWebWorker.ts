f58eba502f1c84fc54c9a03787be32c8f09a40a6
wtklbm <wtklbm@gmail.com>
2023-01-20 23:17:19 +0800

    ✨ feat: 添加 `isWebWorker` 方法

---
+++ packages/types/src/isWebWorker.ts
@@ -0,0 +1,12 @@
+/**
+ * 当前的执行环境是不是 `Web Worker`
+ *
+ * @returns 是则返回 `true`，否则为 `false`
+ */
+export default function isWebWorker(): boolean {
+    return (
+        typeof self === 'object' &&
+        self !== null &&
+        self.constructor?.name === 'DedicatedWorkerGlobalScope'
+    );
+}
