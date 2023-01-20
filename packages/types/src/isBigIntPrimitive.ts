bac382f95ae8e38e63803f2be3f32df5c926d93b
wtklbm <wtklbm@gmail.com>
2023-01-20 22:24:23 +0800

    ✨ feat: 添加 `isBigIntPrimitive` 方法

---
+++ packages/types/src/isBigIntPrimitive.ts
@@ -0,0 +1,9 @@
+/**
+ * 是不是一个基本的大数，即 `bigint`
+ *
+ * @param value 要验证的值
+ * @returns 是则返回 `true`，否则为 `false`
+ */
+export default function isBigIntPrimitive(value: unknown): value is bigint {
+    return typeof value === 'bigint';
+}
