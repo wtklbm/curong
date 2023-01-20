c5e37b4d408346d68a63a63d171b112e786d24b6
wtklbm <wtklbm@gmail.com>
2023-01-20 22:16:02 +0800

    🧪 test: 添加 `isSymbolObject` 方法的测试

---
+++ packages/types/__tests__/isSymbolObject.test.ts
@@ -0,0 +1,13 @@
+import { isSymbolObject } from '../src';
+
+describe('@curong/types/isSymbolObject', () => {
+    test('测试1', () => {
+        expect(isSymbolObject(12)).toBe(false);
+        expect(isSymbolObject(null)).toBe(false);
+        expect(isSymbolObject(Symbol('x'))).toBe(false);
+    });
+
+    test('测试2', () => {
+        expect(isSymbolObject(Object(Symbol('x')))).toBe(true);
+    });
+});
