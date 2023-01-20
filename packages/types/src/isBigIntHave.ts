b57ceb9efbc89cbb0f38ec64b2f85cf056a3f3ac
wtklbm <wtklbm@gmail.com>
2023-01-20 22:37:33 +0800

    🐞 fix: 更新返回值类型

--- packages/types/src/isBigIntHave.ts
+++ packages/types/src/isBigIntHave.ts
@@ -1,13 +1,13 @@
 import isBigInt from './isBigInt';
 
 /**
- * 是不是一个大于 `0n` 的 `BigInt` (大数)
+ * 是不是一个大于 `0n` 的 `bigint` (大数)
  *
  * @param value 要验证的值
  * @returns 是则返回 `true`，否则为 `false`
  * @note
  *  - `BigInt` 和 `Number` 是不兼容的，不能相互赋值，也不能直接做加法运算
  */
-export default function isBigIntHave(value: unknown): value is BigInt {
+export default function isBigIntHave(value: unknown): value is bigint {
     return isBigInt(value) && (value as BigInt) > 0n;
 }
