1a537d4e74c0d90b6ef7c1752ea5ed7528dddda3
wtklbm <wtklbm@gmail.com>
2023-01-20 22:15:34 +0800

    ✨ feat: 添加 `isSymbolObject` 方法

---
+++ packages/types/src/isSymbolObject.ts
@@ -0,0 +1,11 @@
+import getTag from './getTag';
+
+/**
+ * 是不是一个被包装后的 `Symbol` 对象，即 `Object(Symbol())`
+ *
+ * @param value 要验证的值
+ * @returns 是则返回 `true`，否则为 `false`
+ */
+export default function isSymbolObject(value: unknown): value is Symbol {
+    return typeof value === 'object' && getTag(value) === 'Symbol';
+}
