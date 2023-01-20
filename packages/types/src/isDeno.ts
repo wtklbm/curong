fabb5fd7cf250905980f180156f346db20ff5eaa
wtklbm <wtklbm@gmail.com>
2023-01-20 20:55:18 +0800

    ✨ feat: 添加 `isDeno` 方法

---
+++ packages/types/src/isDeno.ts
@@ -0,0 +1,12 @@
+declare global {
+    export const Deno: any;
+}
+
+/**
+ * 当前的执行环境是不是 `Deno`
+ *
+ * @returns 是则返回 `true`，否则为 `false`
+ */
+export default function isDeno(): boolean {
+    return typeof Deno !== 'undefined' && typeof Deno.version !== 'undefined';
+}
