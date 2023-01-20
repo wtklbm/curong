82ee12b30bb6aa8c70bf4ad87f301532a8f9873c
wtklbm <wtklbm@gmail.com>
2023-01-20 22:10:50 +0800

    ✨ feat: 添加 `isBigIntObject` 方法

---
+++ packages/types/src/isBigIntObject.ts
@@ -0,0 +1,11 @@
+import getTag from './getTag';
+
+/**
+ * 是不是一个被包装后的 `BigInt` 对象，即 `Object(0n)`
+ *
+ * @param value 要验证的值
+ * @returns 是则返回 `true`，否则为 `false`
+ */
+export default function isBigIntObject(value: unknown): value is BigInt {
+    return typeof value === 'object' && getTag(value) === 'BigInt';
+}
