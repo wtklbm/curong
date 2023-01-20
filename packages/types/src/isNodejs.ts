e718de822104d80ead71302f32c4834ae40227a0
wtklbm <wtklbm@gmail.com>
2023-01-20 20:27:27 +0800

    ✨ feat: 添加 `isNodejs` 方法

---
+++ packages/types/src/isNodejs.ts
@@ -0,0 +1,22 @@
+import getTag from './getTag';
+
+/**
+ * 当前的执行环境是不是 `Node.js`
+ *
+ * @returns 是则返回 `true`，否则为 `false`
+ */
+export default function isNodejs(): boolean {
+    return (
+        typeof global === 'object' &&
+        typeof setImmediate === 'function' &&
+        typeof clearImmediate === 'function' &&
+        typeof Buffer === 'function' &&
+        typeof Buffer.isBuffer === 'function' &&
+        typeof Buffer.byteLength === 'function' &&
+        typeof process === 'object' &&
+        getTag(process) === 'process' &&
+        typeof process.nextTick === 'function' &&
+        typeof process.exit === 'function' &&
+        typeof process.cwd === 'function'
+    );
+}
