57758793d3ac402a113b6bfd043d58328a8d4bea
wtklbm <wtklbm@gmail.com>
2023-03-07 22:49:31 +0800

    ✨ feat: 添加 `IsUnknown` 类型

---
+++ packages/types/src/IsUnknown.ts
@@ -0,0 +1,25 @@
+import type { IsAny } from './IsAny';
+import { IsNever } from './IsNever';
+
+/**
+ * 是不是一个 `unknown` 类型的值
+ *
+ * @typeParam T 要验证的值
+ * @example
+ *
+ * ```typescript
+ * IsUnknown<unknown> = true
+ * IsUnknown<1> = false
+ * IsUnknown<string> = false
+ * ```
+ * @returns 是则返回 `true`，否则为 `false`
+ */
+export type IsUnknown<T> = IsNever<T> extends false
+    ? T extends unknown
+        ? unknown extends T
+            ? IsAny<T> extends false
+                ? true
+                : false
+            : false
+        : false
+    : false;
