227bc091a463403d3d290ae32d611fcd0019e50a
wtklbm <wtklbm@gmail.com>
2023-04-07 16:55:13 +0800

    🧪 test: 添加新的测试

--- packages/string/__tests__/bindInside.test.ts
+++ packages/string/__tests__/bindInside.test.ts
@@ -1,6 +1,6 @@
 import { bindInside } from '../src';
 
-describe('@curong/string/bindOutside', () => {
+describe('@curong/string/bindInside', () => {
     test('测试1', () => {
         const v = 'the [`crate`] is <[`crate`]> very {`crate`} good.';
         const ret = bindInside(v, v => v.replace('crate', 'pkg'));
@@ -8,11 +8,20 @@ describe('@curong/string/bindOutside', () => {
     });
 
     test('测试2', () => {
-        const v = 'the [`\\[crate`] is very good.';
-        const ret = bindInside(v, { escape: true }, v =>
+        const v = 'the [`\\[crate\\]`] is very good.';
+        let ret = bindInside(v, { escape: true }, v =>
             v.replace('crate', 'pkg')
         );
-        expect(ret).toBe('the [`\\[pkg`] is very good.');
+        expect(ret).toBe('the [`\\[pkg\\]`] is very good.');
+
+        ret = bindInside(v, { escape: true }, v => v.replace('the', 'pkg'));
+        expect(ret).toBe(v);
+
+        ret = bindInside(v, { escape: true }, v => v.replace('good', 'crate'));
+        expect(ret).toBe(v);
+
+        ret = bindInside(v, { escape: true }, v => v.replace('very', 'crate'));
+        expect(ret).toBe(v);
     });
 
     test('测试3', () => {
@@ -31,17 +40,44 @@ describe('@curong/string/bindOutside', () => {
             v.replace('value', 'pkg')
         );
         expect(ret).toBe(`th's is 'pkg \\'test. xxx'. yes. xx'.`);
-        ret = bindInside(v, { escape: true }, v =>
-            v.replace('th', 'pkg')
-        );
+        ret = bindInside(v, { escape: true }, v => v.replace('th', 'pkg'));
         expect(ret).toBe(`th's is 'value \\'test. xxx'. yes. xx'.`);
-        ret = bindInside(v, { escape: true }, v =>
-            v.replace('xxx', 'pkg')
-        );
+        ret = bindInside(v, { escape: true }, v => v.replace('xxx', 'pkg'));
         expect(ret).toBe(`th's is 'value \\'test. pkg'. yes. xx'.`);
-        ret = bindInside(v, { escape: true }, v =>
-            v.replace('yes', 'pkg')
-        );
+        ret = bindInside(v, { escape: true }, v => v.replace('yes', 'pkg'));
         expect(ret).toBe(`th's is 'value \\'test. xxx'. yes. xx'.`);
+    });
+
+    test('测试5', () => {
+        const v = `th's is 'value \\'test. xxx'. yes. xx'.`;
+
+        let ret = bindInside(v, { escape: true }, v =>
+            v.replace('is', 'very very')
+        );
+        expect(ret).toBe(v);
+
+        ret = bindInside(v, { escape: true }, v => v.replace('value', 'pkg'));
+        expect(ret).toBe(`th's is 'pkg \\'test. xxx'. yes. xx'.`);
+        ret = bindInside(v, { escape: true }, v => v.replace('xxx', 'pkg'));
+        expect(ret).toBe(`th's is 'value \\'test. pkg'. yes. xx'.`);
+        ret = bindInside(v, { escape: true }, v => v.replace('test', 'pkg'));
+        expect(ret).toBe(`th's is 'value \\'pkg. xxx'. yes. xx'.`);
+        ret = bindInside(v, { escape: true }, v => v.replace('xx', 'pkg'));
+        expect(ret).toBe(`th's is 'value \\'test. pkgx'. yes. xx'.`);
+    });
+
+    test('测试6', () => {
+        const v = '[`crate`](http://good/crate) is very good.';
+        let ret = bindInside(v, v => v.replace('crate', 'xxx'));
+        expect(ret).toBe('[`xxx`](http://good/crate) is very good.');
+
+        ret = bindInside(v, v => v.replace('is', 'xxx'));
+        expect(ret).toBe('[`crate`](http://good/crate) is very good.');
+
+        ret = bindInside(v, v => v.replace('very', 'xxx'));
+        expect(ret).toBe('[`crate`](http://good/crate) is very good.');
+
+        ret = bindInside(v, v => v.replace('good', 'xxx'));
+        expect(ret).toBe('[`crate`](http://xxx/crate) is very good.');
     });
 });
