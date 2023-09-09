5aabc2772a81ac3391c272d918e642c2ad4d7532
wtklbm <wtklbm@gmail.com>
2023-09-09 21:46:24 +0800

    🎈 perf: 修改请求方法的逻辑

--- packages/request/src/request/request.ts
+++ packages/request/src/request/request.ts
@@ -1,26 +1,12 @@
-import { request as HttpsRequest } from 'https';
-import { IncomingMessage, request as HttpRequest } from 'http';
-import { stringify, ParsedUrlQueryInput } from 'querystring';
+import { IncomingMessage } from 'http';
 
-import { format } from '@curong/term';
 import { sleepRun } from '@curong/function';
-import { toLowerCaseKey } from '@curong/object';
-import {
-    isFunctionHave,
-    isStringHave,
-    isObjectHave,
-    isNull,
-    isZero
-} from '@curong/types';
+import { isStringHave, isObjectHave, isArrayBuffer } from '@curong/types';
 
-import { commonHeaders } from './headers';
-import { isGzipContent, unzipContent, contentTypeCallback } from './content';
+import { deleteOptionsAttr, optionsHandler } from './options';
+import { contentTypeCallback, pipeDecompressStream } from './content';
 
-import {
-    RequestOptions,
-    RequestHandler,
-    RequestResult
-} from '../types/request';
+import { RequestOptions, RequestHandler, RequestResult } from '../types';
 
 /**
  * 从远程连接获取响应的内容并返回 `Buffer`
@@ -100,52 +86,107 @@ import {
  */
 export default function request(
     options: RequestOptions,
-    handlers: RequestHandler = {}
+    handlers?: RequestHandler
+): Promise<RequestResult>;
+
+/**
+ * 从远程连接获取响应的内容并返回 `Buffer`
+ *
+ * @param url 要请求的 URL 字符串地址或 URL 对象
+ * @param options 参数列表，默认为 `{}`
+ *
+ * - `method` 请求方式
+ * - `query` 当前请求的查询字符串对象，会被转换为 `a=b&b=c` 的格式
+ * - `body` 当前请求的请求体对象
+ * - `delay` 延迟请求时间，单位 `毫秒`
+ * - `timeout` 响应超时时间，单位 `毫秒`
+ * - `headers` 请求头对象
+ *    - `port` 端口号
+ *    - `timeout` 响应头超时时间
+ *    - `headers` 请求头
+ *    - `...` 更多参数
+ *
+ * @param handlers 回调函数对象
+ *
+ * - `header` 接收到响应头之后的回调函数，方法签名为:
+ *   `(res: IncomingMessage, options: RequestOptions) => true | void;`
+ * - `data` 接收到响应体之后的回调函数，方法签名为:
+ *   `( chunk: any, res: IncomingMessage, options: RequestOptions ) => true | void;`
+ *
+ * @example
+ *
+ * ```javascript
+ * const requestOpts: RequestOptions = {
+ *     method: 'GET',
+ *     // 更多参数会拼接到 URL 的末尾
+ *     query: {
+ *         s: 'node.js'
+ *     },
+ *     headers: {
+ *         'Cache-Control': 'no-cache'
+ *     },
+ *     timeout: 1e3
+ * };
+ * const ret = await request('https://baidu.com/s?wd=1', requestOpts, {
+ *     header(res, _options) {
+ *         // 打开网页错误了
+ *         if (!/^(2\d{2})|(30[24])$/.test(res.statusCode!.toString())) {
+ *             // 返回 `true` 时会关闭连接
+ *             return true;
+ *         }
+ *     },
+ *     data(chunk, _res, _options) {
+ *         console.log(chunk);
+ *     }
+ * });
+ *
+ * // {
+ * //     config: {},
+ * //     data: Buffer,
+ * //     error: null,
+ * //     response: IncomingMessage
+ * // }
+ * console.log(ret);
+ * ```
+ *
+ * @todo
+ *
+ * - 支持跨域
+ * - 支持 `Ajax` 和 `fetch`
+ * - 删除 `querystring`，支持 `body` 参数嵌套
+ * - 处理响应头，避免连接的状态挂起
+ */
+export default function request(
+    url: string | URL,
+    options?: RequestOptions,
+    handlers?: RequestHandler
+): Promise<RequestResult>;
+
+export default function request(
+    url: any,
+    options: any = {},
+    handlers: any = {}
 ): Promise<RequestResult> {
-    const { https = true, hostname, delay } = options ?? {};
-
-    if (!isStringHave(hostname)) {
-        throw format({
-            name: 'request',
-            message: '主机名错误',
-            data: { options, handlers }
-        });
+    if (isObjectHave(url)) {
+        handlers = options;
+        options = url;
+        url = undefined;
     }
 
-    // 获取请求地址
-    const address = `${https ? 'https' : 'http'}://${hostname}`;
+    const requestFn = optionsHandler(url, options);
+    const { body, delay, timeout } = options;
+    deleteOptionsAttr(options, ['body', 'delay']);
 
-    /** 配置参数 */
-    options = {
-        // @ts-ignore
-        hostname,
-        path: '/',
-        method: 'GET',
-        timeout: 2147483647,
-        // 如果 `https` 的安全证书不是合法的，则忽略证书验证
-        rejectUnauthorized: false,
-        ...options,
-        headers: toLowerCaseKey({
-            Host: hostname,
-            Referer: address,
-            Origin: address,
-            ...commonHeaders,
-            ...options.headers
-        })
-    };
-
-    if (isObjectHave(options.query)) {
-        options.path += `?` + stringify(options.query);
-    }
-
-    let buffers: Buffer[] = [];
-    const isHeaderFn = isFunctionHave(handlers.header);
-    const isDataFn = isFunctionHave(handlers.data);
-    const requestFn = https ? HttpsRequest : HttpRequest;
-    const ht = contentTypeCallback(options);
     const getF = (resolve: any, reject: any) => {
         /** 发起网络请求 */
         const req = requestFn(options, (res: IncomingMessage) => {
+            if (req.destroyed) {
+                return;
+            }
+
+            const buffers: Buffer[] = [];
+            const serialStream = pipeDecompressStream(res);
+
             // 关闭当前当前响应
             const removeListeners = () => {
                 req.emit('close');
@@ -158,25 +199,12 @@ export default function request(
             const returns = async (error?: Error) => {
                 removeListeners();
 
-                let data = Buffer.concat(buffers);
-
-                if (isGzipContent(res)) {
-                    data = await unzipContent(data).catch(error => {
-                        throw format({
-                            name: 'request',
-                            message: 'gzip 解压失败',
-                            data: { data, error }
-                        });
-                    });
-
-                    try {
-                        // 删除内容编码以避免混淆下游操作
-                        delete res.headers['content-encoding'];
-                    } catch {}
-                }
-
+                const data = Buffer.concat(buffers);
                 const responseData: RequestResult = {
                     data,
+                    status: res.statusCode,
+                    statusText: res.statusMessage,
+                    request: req,
                     response: res,
                     config: options,
                     error: error ?? null
@@ -189,57 +217,67 @@ export default function request(
             };
 
             /** 处理响应头 */
-            if (isHeaderFn && handlers.header!(res, options)) {
+            if (handlers.header && handlers.header!(res, options)) {
                 return resolve(returns());
             }
 
             /** 接收响应体 */
-            res.on('data', (chunk: Buffer) => {
-                if (isDataFn && handlers.data!(chunk, res, options)) {
+            serialStream.on('data', (chunk: Buffer) => {
+                if (handlers.data && handlers.data!(chunk, res, options)) {
                     return resolve(returns());
                 }
 
                 buffers.push(chunk);
             });
 
-            res.on('end', () => resolve(returns()));
-            res.on('error', e => reject(returns(e)));
+            serialStream.on('end', () => resolve(returns()));
+            serialStream.on('error', e => reject(returns(e)));
         });
 
-        const rejectF = (error: Error) => {
+        req.setTimeout(timeout, () => {
+            req.abort();
+
+            reject({
+                message: 'request timeout abort.',
+                request: req,
+                config: options
+            });
+        });
+
+        req.on('error', error => {
             req.emit('close');
             req.removeAllListeners();
 
-            return resolve({ config: options, error });
-        };
+            reject({ request: req, config: options, error });
+        });
 
-        req.on('error', e => reject(rejectF(e)));
+        //# 发送请求体
+        let bodyBuffer;
 
-        // 发送请求体
-        do {
-            const { body } = options;
-            let buffer = null;
+        if (isObjectHave(body)) {
+            bodyBuffer = Buffer.from(contentTypeCallback(options)(body));
+        } else if (isStringHave(body)) {
+            bodyBuffer = Buffer.from(body);
+        } else if (isArrayBuffer(body)) {
+            bodyBuffer = Buffer.from(new Uint8Array(body));
+        } else {
+            return reject({
+                message: '[request]: 当前传递的 body 的格式是不受支持的',
+                request: req,
+                config: options
+            });
+        }
 
-            if (isObjectHave(body)) {
-                buffer = Buffer.from(ht(body as ParsedUrlQueryInput));
-            } else if (isStringHave(body)) {
-                buffer = Buffer.from(body);
-            }
-
-            if (isNull(buffer) || isZero(buffer.length)) {
-                break;
-            }
-
-            if (!options.headers) {
-                options.headers = {};
-            }
-
-            options.headers['content-length'] = buffer.length;
-            req.write(buffer);
-        } while (false);
+        // 默认情况下，`Content-Length` 的值的计算必须正确
+        // 如果不想计算，可以使用 `Transfer-Encoding: “chunk”` 并修改请求逻辑来创建分块压缩的请求
+        options.headers['content-length'] = bodyBuffer.length;
 
         // 结束请求
-        req.end();
+        //
+        // @note 这里不要用 `req.write()` 来发送请求体
+        //  `req.write()` 异步的，有可能请求体还没有发送完，就走了 `req.end()`
+        //  从而造成请求失败，或返回的结果非预期的值
+        req.end(bodyBuffer);
     };
 
     return new Promise(async (resolve, reject) => {
