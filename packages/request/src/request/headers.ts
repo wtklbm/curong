9167438cd053172daa42d687ef27d22c242469d6
wtklbm <wtklbm@gmail.com>
2023-09-09 22:51:38 +0800

    📃 docs: 添加说明

--- packages/request/src/request/headers.ts
+++ packages/request/src/request/headers.ts
@@ -5,7 +5,7 @@ export const commonHeaders = {
     'Cache-Control': 'no-store',
     'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
     Accept: 'application/json,text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
-    'Accept-Encoding': 'gzip, compress, deflate, br',
+    'Accept-Encoding': 'gzip, compress, deflate, br', // 仅 `Node.js`
     'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8',
     'X-Requested-With': 'XMLHttpRequest',
     'User-Agent':
