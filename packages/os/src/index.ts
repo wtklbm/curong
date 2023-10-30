c43bfecca4254976682487898a3197fe86c06ec4
wtklbm <wtklbm@gmail.com>
2023-10-30 23:56:51 +0800

    ✨ feat: 添加 `windowsMakeShortcut` 方法

--- packages/os/src/index.ts
+++ packages/os/src/index.ts
@@ -2,6 +2,7 @@ export { default as appendCmd } from './appendCmd';
 export { default as findWindowsTask } from './findWindowsTask';
 export { default as killWindowsTask } from './killWindowsTask';
 export { default as windowsLocal } from './windowsLocal';
+export { default as windowsMakeShortcut } from './windowsMakeShortcut';
 export { default as windowsTaskList } from './windowsTaskList';
 
 export * from './platform';
