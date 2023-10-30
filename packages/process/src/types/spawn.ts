6c5876c8df69810339d47ea0f0fc7542629e7893
wtklbm <wtklbm@gmail.com>
2023-10-30 23:48:29 +0800

    🦄 refactor: 更新类型声明

--- packages/process/src/types/spawn.ts
+++ packages/process/src/types/spawn.ts
@@ -1,10 +1,34 @@
-import { ChildProcess } from 'child_process';
+import { Writable } from 'stream';
+import { ChildProcess, SpawnOptions as _SpawnOptions } from 'child_process';
 
-export type SpawnResult = {
-    pid: number;
-    output: Array<string>;
-    stdout: string;
-    stderr: string;
+export interface SpawnProcess {
+    /** 当前所执行的命令 */
+    cmd: string;
+
+    /** 当前命令的所执行的参数 */
+    args: string[];
+
+    /** 执行命令后返回的状态码 */
     status: number | null;
-    child: ChildProcess;
+
+    /** 信号量 */
+    signal: string | null;
+
+    /** 标准输出 */
+    stdout: string | Buffer | null;
+
+    /** 标准错误 */
+    stderr: string | Buffer | null;
+}
+
+export type SpawnOptions = Partial<
+    _SpawnOptions & {
+        /** 是否要将结果转换为字符串，默认为 `true` */
+        isToString: boolean;
+    }
+>;
+
+export type SpawnResult = Promise<SpawnProcess> & {
+    stdin: Writable | null;
+    process: ChildProcess | null;
 };
