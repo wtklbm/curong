5c24fc6c71ad8d36316cc143f8708a810ea45682
wtklbm <wtklbm@gmail.com>
2023-11-10 13:31:03 +0800

    🦄 refactor: 整理转义方法

--- packages/process/src/spawn.ts
+++ packages/process/src/spawn.ts
@@ -15,41 +15,42 @@ const escapeCmd = (input: string, doubleEscape: boolean): string => {
         return '""';
     }
 
-    let result;
+    let ret;
+
     if (!/[ \v\t\n"]/.test(input)) {
-        result = input;
+        ret = input;
     } else {
-        result = '"';
-        for (let i = 0; i <= input.length; ++i) {
-            let slashCount = 0;
+        ret = '"';
+
+        for (let i = 0, j; i <= input.length; ++i) {
+            j = 0;
+
             while (input[i] === '\\') {
                 ++i;
-                ++slashCount;
+                ++j;
             }
 
             if (i === input.length) {
-                result += '\\'.repeat(slashCount * 2);
+                ret += '\\'.repeat(j * 2);
                 break;
             }
 
-            if (input[i] === '"') {
-                result += '\\'.repeat(slashCount * 2 + 1);
-                result += input[i];
-            } else {
-                result += '\\'.repeat(slashCount);
-                result += input[i];
-            }
+            ret += '\\'.repeat(input[i] === '"' ? j * 2 + 1 : j) + input[i];
         }
-        result += '"';
+
+        ret += '"';
     }
 
-    // 在 shell 元字符前加上前缀 ^
-    result = result.replace(/[ |!%^&()<>"]/g, '^$&');
-    if (doubleEscape) {
-        result = result.replace(/[ |!%^&()<>"]/g, '^$&');
-    }
+    /** 在 shell 元字符前加上 ^ 前缀 */
+    const addPrefix = (ret: string, len: number) => {
+        for (let i = 0; i < len; i++) {
+            ret = ret.replace(/[ |!%^&()<>"]/g, '^$&');
+        }
 
-    return result;
+        return ret;
+    };
+
+    return doubleEscape ? addPrefix(ret, 2) : addPrefix(ret, 1);
 };
 
 const escapeSh = (input: string): string => {
@@ -57,7 +58,7 @@ const escapeSh = (input: string): string => {
         return `''`;
     }
 
-    if (!/[\t\n\r "#$&'*;?`|~()<>\\]/.test(input)) {
+    if (!/[\r\n\t '"`#$&~*;?|()<>\\]/.test(input)) {
         return input;
     }
 
