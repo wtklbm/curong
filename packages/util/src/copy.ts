1925040f0757485a5787ada56916147b49542aea
wtklbm <wtklbm@gmail.com>
2024-04-23 01:38:03 +0800

    🐞 fix: 在赋值时进行容错处理

--- packages/util/src/copy.ts
+++ packages/util/src/copy.ts
@@ -49,7 +49,11 @@ const copyAttrs = <T extends object>(
             }
         }
 
-        memo[key] = recursiveCopy(src[key], hash);
+        // 能赋值的就赋值，赋值不了的就算了
+        try {
+            memo[key] = recursiveCopy(src[key], hash);
+        } catch {}
+
         return memo;
     }, dest);
 };
