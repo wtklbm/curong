df4bc34f7f8d36034735f1975f0120741eaada07
wtklbm <wtklbm@gmail.com>
2024-04-26 00:11:29 +0800

    📃 docs: 更新文档

--- packages/types/src/isPlainObject.ts
+++ packages/types/src/isPlainObject.ts
@@ -5,8 +5,10 @@ import isObject from './isObject';
  *
  * @param value 要验证的值
  * @returns 是则返回 `true`，否则为 `false`
- * @info 如果想验证任意的对象，请使用 `isObject` 方法。
- * 如果想使用 `typeof` 验证一个对象，请使用 `isTypeofObject` 方法。
+ * @info
+ * - 如果想验证任意的对象，请使用 `isObject` 方法
+ * - 如果想使用 `typeof` 验证一个对象，请使用 `isTypeofObject` 方法
+ * - 如果想验证纯对象 (通过 `Object.create(null)` 创建的对象)，请使用 `isPureObject` 方法
  */
 export default function isPlainObject<K extends keyof any, T = unknown>(
     value: any
