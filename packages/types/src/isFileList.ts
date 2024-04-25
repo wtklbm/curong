f149c1f2252fdeca01bc8c1b4e71cf3f9d53b59f
wtklbm <wtklbm@gmail.com>
2024-04-26 01:28:19 +0800

    ✨ feat: 添加 `isFileList` 方法

---
+++ packages/types/src/isFileList.ts
@@ -0,0 +1,11 @@
+import getTag from './getTag';
+
+/**
+ * 是不是一个 `FileList` 对象
+ *
+ * @param value 要验证的值
+ * @returns 是则返回 `true`，否则为 `false`
+ */
+export default function isFileList(value: unknown): value is FileList {
+    return getTag(value) === 'FileList';
+}
