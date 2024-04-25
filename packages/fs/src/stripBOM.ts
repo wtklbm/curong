3f77a0a3fc7814307f552a130c6be98e63b0b646
wtklbm <wtklbm@gmail.com>
2024-04-26 00:35:29 +0800

    📃 docs: 更新文档

--- packages/fs/src/stripBOM.ts
+++ packages/fs/src/stripBOM.ts
@@ -1,9 +1,9 @@
 /**
- * 删除字节顺序标记 (BOM)，编码为 `0xFEFF`
+ * 删除字节顺序标记 (UTF-8 BOM)，编码为 `0xFEFF` (EF BB BF)
  *
  * @param content 要使用的字符串
  * @returns 返回处理好的内容
  */
 export default function stripBOM(content: string): string {
-    return content.charCodeAt(0) === 0xfeff ? content.slice(1) : content;
+    return content.charCodeAt(0) === 0XFEFF ? content.slice(1) : content;
 }
