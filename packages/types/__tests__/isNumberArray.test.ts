bf6925b3df696a1b128024489b436303bc704c0a
wtklbm <wtklbm@gmail.com>
2024-05-01 21:54:48 +0800

    🧪 test: 添加 `isNumberArray` 的测试

---
+++ packages/types/__tests__/isNumberArray.test.ts
@@ -0,0 +1,16 @@
+import { isNumberArray } from '../src';
+
+describe('@curong/types/isNumberArray', () => {
+    test('测试1', () => {
+        expect(isNumberArray(0)).toBe(false);
+        expect(isNumberArray('')).toBe(false);
+        expect(isNumberArray(2)).toBe(false);
+        expect(isNumberArray([2, '1'])).toBe(false);
+        expect(isNumberArray([2, true])).toBe(false);
+    });
+
+    test('测试2', () => {
+        expect(isNumberArray([2])).toBe(true);
+        expect(isNumberArray([2, 2])).toBe(true);
+    });
+});
