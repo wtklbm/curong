92f5cb1dc2cc88f425a3b33ddd75f55875837cf4
wtklbm <wtklbm@gmail.com>
2024-05-01 21:52:49 +0800

    ✨ feat: 添加 `isNullArray` 方法

---
+++ packages/types/src/isNullArray.ts
@@ -0,0 +1,12 @@
+import isArray from './isArray';
+import isNull from './isNull';
+
+/**
+ * 是不是一个数组，且每一项的值都是 `null`
+ *
+ * @param value 要验证的值
+ * @returns 是则返回 `true`，否则为 `false`
+ */
+export default function isNullArray(value: unknown):value is null[] {
+    return isArray(value) && value.every(isNull);
+}
