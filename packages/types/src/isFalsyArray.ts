ea5fb7c9b8a48efd9da184e9bd168634212bf1a8
wtklbm <wtklbm@gmail.com>
2024-05-01 22:19:02 +0800

    ✨ feat: 添加 `isFalsyArray` 方法

---
+++ packages/types/src/isFalsyArray.ts
@@ -0,0 +1,14 @@
+import isArray from './isArray';
+import isFalsy from './isFalsy';
+
+import { type Falsy } from './types';
+
+/**
+ * 是不是一个数组，且每一项的值都是虚值 (强制转换为 `Boolean` 后为 `false` 的值)
+ *
+ * @param value 要验证的值
+ * @returns 是则返回 `true`，否则为 `false`
+ */
+export default function isFalsyArray(value: unknown): value is Falsy[] {
+    return isArray(value) && value.every(isFalsy);
+}
