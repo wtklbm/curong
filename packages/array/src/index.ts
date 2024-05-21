619ee3abf68b18f1ccbafd19f1902a875b5df5b8
wtklbm <wtklbm@gmail.com>
2024-05-21 14:05:41 +0800

    ✨ feat: 添加 `unique` 方法

--- packages/array/src/index.ts
+++ packages/array/src/index.ts
@@ -10,6 +10,7 @@ export { default as sortByEngInitials } from './sortByEngInitials';
 export { default as sortToIndexes } from './sortToIndexes';
 export { default as sortWords } from './sortWords';
 export { default as splitByStep } from './splitByStep';
+export { default as unique } from './unique';
 export { default as uniqueFromPrimitive } from './uniqueFromPrimitive';
 
 export * from './types';
