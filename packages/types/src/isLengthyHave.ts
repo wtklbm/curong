b17f1d414423f41f6e144f45cc083f3b378a2da8
wtklbm <wtklbm@gmail.com>
2024-05-22 20:52:49 +0800

    📃 docs: 更新文档

--- packages/types/src/isLengthyHave.ts
+++ packages/types/src/isLengthyHave.ts
@@ -2,7 +2,7 @@ import isLengthy from './isLengthy';
 import type { Lengthy } from './types';
 
 /**
- * 是不是一个具有 `length` 属性的类型，其 `length` 的值是一个大于 0 的整数
+ * 是不是一个具有 `length` 属性的类型，其 `length` 的值是一个大于 0 的安全的无符号整数
  *
  * @param value 要验证的值
  * @returns 是则返回 `true`，否则为 `false`
