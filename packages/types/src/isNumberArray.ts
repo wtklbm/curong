54809965ace560c425311f6fb8e2496755571a5f
wtklbm <wtklbm@gmail.com>
2024-05-22 22:39:34 +0800

    🎈 perf: 改为非空数组

--- packages/types/src/isNumberArray.ts
+++ packages/types/src/isNumberArray.ts
@@ -1,12 +1,12 @@
-import isArray from './isArray';
+import isArrayHave from './isArrayHave';
 import isNumber from './isNumber';
 
 /**
- * 是不是一个数组，且每一项的值都是数字或被包装后的数字对象
+ * 是不是一个长度大于 0 的数组，且每一项的值都是数字或被包装后的数字对象
  *
  * @param value 要验证的值
  * @returns 是则返回 `true`，否则为 `false`
  */
 export default function isNumberArray(value: unknown): value is number[] {
-    return isArray(value) && value.every(isNumber);
+    return isArrayHave(value) && value.every(isNumber);
 }
