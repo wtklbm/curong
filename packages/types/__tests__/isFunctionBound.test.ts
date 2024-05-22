7842a2dbf70742402cedb99e0d5ac25b0e1ad397
wtklbm <wtklbm@gmail.com>
2024-05-22 23:06:24 +0800

    🧪 test: 添加 `isFunctionBound` 方法的测试

---
+++ packages/types/__tests__/isFunctionBound.test.ts
@@ -0,0 +1,28 @@
+import { isFunctionBound } from '../src';
+
+describe('@curong/types/isFunctionBound', () => {
+    test('测试1', () => {
+        expect(isFunctionBound(12)).toBe(false);
+        expect(isFunctionBound([1])).toBe(false);
+        expect(isFunctionBound(new Function())).toBe(false);
+        expect(isFunctionBound(function a() {})).toBe(false);
+        expect(isFunctionBound(async function a() {})).toBe(false);
+        expect(isFunctionBound(function A() {})).toBe(false);
+        expect(isFunctionBound(async function A() {})).toBe(false);
+        expect(isFunctionBound(() => {})).toBe(false);
+        expect(isFunctionBound(async () => {})).toBe(false);
+    });
+
+    test('测试2', () => {
+        const fn1 = () => {};
+        const fn2 = fn1.bind(null);
+        expect(isFunctionBound(fn1)).toBe(false);
+        expect(isFunctionBound(fn2)).toBe(true);
+
+        function greet(name) {
+            console.log(`Hello, ${name}!`);
+        }
+        const boundGreet = greet.bind(null, 'Alice');
+        expect(isFunctionBound(boundGreet)).toBe(true);
+    });
+});
