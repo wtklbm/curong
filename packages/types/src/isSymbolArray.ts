5357aabf4e8570e12188597c9e37c6b96fe64a75
wtklbm <wtklbm@gmail.com>
2024-05-22 22:41:08 +0800

    🎈 perf: 改为非空数组

--- packages/types/src/isSymbolArray.ts
+++ packages/types/src/isSymbolArray.ts
@@ -1,12 +1,12 @@
-import isArray from './isArray';
+import isArrayHave from './isArrayHave';
 import isSymbol from './isSymbol';
 
 /**
- * 是不是一个数组，且每一项的值都是 `symbol` 或被包装后的 `Symbol` 对象
+ * 是不是一个长度大于 0 的数组，且每一项的值都是 `symbol` 或被包装后的 `Symbol` 对象
  *
  * @param value 要验证的值
  * @returns 是则返回 `true`，否则为 `false`
  */
 export default function isSymbolArray(value: unknown): value is symbol[] {
-    return isArray(value) && value.every(isSymbol);
+    return isArrayHave(value) && value.every(isSymbol);
 }
