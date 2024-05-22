ed0d22accb757ab88ba372b1b64b76d0b429fa6a
wtklbm <wtklbm@gmail.com>
2024-05-22 22:40:42 +0800

    🎈 perf: 改为非空数组

--- packages/types/src/isStringArray.ts
+++ packages/types/src/isStringArray.ts
@@ -1,12 +1,12 @@
-import isArray from './isArray';
+import isArrayHave from './isArrayHave';
 import isString from './isString';
 
 /**
- * 是不是一个数组，且每一项的值都是字符串或被包装后的字符串对象
+ * 是不是一个长度大于 0 的数组，且每一项的值都是字符串或被包装后的字符串对象
  *
  * @param value 要验证的值
  * @returns 是则返回 `true`，否则为 `false`
  */
 export default function isStringArray(value: unknown): value is string[] {
-    return isArray(value) && value.every(isString);
+    return isArrayHave(value) && value.every(isString);
 }
