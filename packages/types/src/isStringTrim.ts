e0afe5dff47ea26146a3adf6a41cec0aa83dd45a
wtklbm <wtklbm@gmail.com>
2024-05-22 20:47:09 +0800

    📃 docs: 更新文档

--- packages/types/src/isStringTrim.ts
+++ packages/types/src/isStringTrim.ts
@@ -1,7 +1,7 @@
 import isString from './isString';
 
 /**
- * 是不是一个经过 `trim` 后长度大于 `0` 的字符串
+ * 是不是一个经过 `trim` 后长度大于指定长度的字符串
  *
  * @param value 要验证的值
  * @param length 当字符串被 `trim` 之后，剩余的长度要大于 `length` 才返回 `true`，默认 `length` 为 0
