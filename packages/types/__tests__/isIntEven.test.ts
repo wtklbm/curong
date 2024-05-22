c32369cee256816157243a240e228761ac961355
wtklbm <wtklbm@gmail.com>
2024-05-22 23:32:36 +0800

    🧪 test: 添加 `isIntEven` 方法的测试

---
+++ packages/types/__tests__/isIntEven.test.ts
@@ -0,0 +1,21 @@
+import { isIntEven } from '../src';
+
+describe('@curong/types/isIntEven', () => {
+    test('测试1', () => {
+        expect(isIntEven([1])).toBe(false);
+        expect(isIntEven(12.1)).toBe(false);
+        expect(isIntEven(NaN)).toBe(false);
+        expect(isIntEven(Number.NaN)).toBe(false);
+        expect(isIntEven(Infinity)).toBe(false);
+        expect(isIntEven(-Infinity)).toBe(false);
+        expect(isIntEven(Number.MAX_VALUE)).toBe(false);
+        expect(isIntEven(Number.MIN_VALUE)).toBe(false);
+    });
+
+    test('测试2', () => {
+        expect(isIntEven(1)).toBe(false);
+        expect(isIntEven(-1)).toBe(false);
+        expect(isIntEven(2)).toBe(true);
+        expect(isIntEven(-2)).toBe(true);
+    });
+});
