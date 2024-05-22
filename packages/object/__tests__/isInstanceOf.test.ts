5b12aa993ce0a5b2b712d0409e3ab42266497e13
wtklbm <wtklbm@gmail.com>
2024-05-23 02:16:40 +0800

    🧪 test: 添加 `isInstanceOf` 方法的测试

---
+++ packages/object/__tests__/isInstanceOf.test.ts
@@ -0,0 +1,44 @@
+import { isInstanceOf } from '../src';
+
+class MyClass {}
+class AnotherClass {}
+
+describe('@curong/object/isInstanceOf', () => {
+    test('测试1', () => {
+        const myInstance = new MyClass();
+        expect(isInstanceOf(myInstance, MyClass)).toBe(true);
+    });
+
+    test('测试2', () => {
+        const notMyInstance = {};
+        expect(isInstanceOf(notMyInstance, MyClass)).toBe(false);
+    });
+
+    test('测试3', () => {
+        expect(isInstanceOf(null, MyClass)).toBe(false);
+        expect(isInstanceOf(undefined, MyClass)).toBe(false);
+    });
+
+    test('测试4', () => {
+        const anotherInstance = new AnotherClass();
+        expect(isInstanceOf(anotherInstance, MyClass)).toBe(false);
+    });
+
+    test('测试5', () => {
+        class SubClass extends MyClass {}
+        const subInstance = new SubClass();
+        expect(isInstanceOf(subInstance, MyClass)).toBe(true);
+    });
+
+    test('测试6', () => {
+        expect(isInstanceOf(42, MyClass)).toBe(false);
+        expect(isInstanceOf('测试', MyClass)).toBe(false);
+        expect(isInstanceOf(true, MyClass)).toBe(false);
+        expect(isInstanceOf(Symbol('测试'), MyClass)).toBe(false);
+    });
+
+    test('测试7', () => {
+        const func = () => {};
+        expect(isInstanceOf(func, MyClass)).toBe(false);
+    });
+});
