f700584ebf0cfe11679e1795fffad1f80629b146
wtklbm <wtklbm@gmail.com>
2024-05-23 02:16:18 +0800

    ✨ feat: 添加 `isInstanceOf` 方法

---
+++ packages/object/src/isInstanceOf.ts
@@ -0,0 +1,30 @@
+import { isNullOrUndefined, type Class } from '@curong/types';
+
+/**
+ * 判断给定的值是否是指定类的实例
+ *
+ * 该函数通过检查值的原型链来判断其是否为指定类的实例。如果值为 `null` 或 `undefined`，则直接返回 `false`。
+ *
+ * @param {unknown} value 要检查的值
+ * @param {Class<T>} class_ 要检查的类
+ * @returns 如果值是指定类的实例则返回 `true`，否则为 `false`
+ * @example
+ *
+ * ```javascript
+ * class MyClass {}
+ * const myInstance = new MyClass();
+ *
+ * console.log(isInstanceOf(myInstance, MyClass)); // true
+ * console.log(isInstanceOf({}, MyClass)); // false
+ * console.log(isInstanceOf(null, MyClass)); // false
+ * ```
+ */
+export default function isInstanceOf<T extends unknown>(
+    value: unknown,
+    class_: Class<T>
+): value is T {
+    return (
+        !isNullOrUndefined(value) &&
+        Object.getPrototypeOf(value) === class_.prototype
+    );
+}
