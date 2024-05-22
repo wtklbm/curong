713b9379472bcd8b0ca02bbf622b1d6048acdff8
wtklbm <wtklbm@gmail.com>
2024-05-22 22:40:51 +0800

    🎈 perf: 改为非空数组

--- packages/types/src/isNullOrUndefinedArray.ts
+++ packages/types/src/isNullOrUndefinedArray.ts
@@ -1,8 +1,8 @@
-import isArray from './isArray';
+import isArrayHave from './isArrayHave';
 import isNullOrUndefined from './isNullOrUndefined';
 
 /**
- * 是不是一个数组，且每一项的值都是 `null` 或 `undefined`
+ * 是不是一个长度大于 0 的数组，且每一项的值都是 `null` 或 `undefined`
  *
  * @param value 要验证的值
  * @returns 是则返回 `true`，否则为 `false`
@@ -10,5 +10,5 @@ import isNullOrUndefined from './isNullOrUndefined';
 export default function isNullOrUndefinedArray(
     value: unknown
 ): value is Array<null | undefined> {
-    return isArray(value) && value.every(isNullOrUndefined);
+    return isArrayHave(value) && value.every(isNullOrUndefined);
 }
