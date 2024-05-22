76cbf9dbbd04c47cfc00fec788bbce747803e7b2
wtklbm <wtklbm@gmail.com>
2024-05-22 23:06:06 +0800

    ✨ feat: 添加 `isFunctionBound` 方法

---
+++ packages/types/src/isFunctionBound.ts
@@ -0,0 +1,17 @@
+import isFunction from './isFunction';
+import type { Function } from './types';
+
+/**
+ * 是不是一个经过 `.bind()` 所绑定过的函数
+ *
+ * @param value 要验证的值
+ * @returns 是则返回 `true`，否则为 `false`
+ */
+export function isFunctionBound<T = unknown>(
+    value: unknown
+): value is Function<T> {
+    return (
+        isFunction(value) &&
+        !Object.prototype.hasOwnProperty.call(value, 'prototype')
+    );
+}
