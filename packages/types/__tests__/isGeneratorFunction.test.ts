e878c3fef02c42cf1381631c4ff0b15a0e42f746
wtklbm <wtklbm@gmail.com>
2024-05-22 21:44:10 +0800

    🧪 test: 支持同步和异步的判断

--- packages/types/__tests__/isGeneratorFunction.test.ts
+++ packages/types/__tests__/isGeneratorFunction.test.ts
@@ -3,10 +3,19 @@ import { isGeneratorFunction } from '../src';
 describe('@curong/types/isGeneratorFunction', () => {
     test('测试1', () => {
         expect(isGeneratorFunction(new Function())).toBe(false);
+
+        function fn1() { }
+        expect(isGeneratorFunction(fn1())).toBe(false);
+
+        async function fn2() {}
+        expect(isGeneratorFunction(fn2())).toBe(false);
     });
 
     test('测试2', () => {
-        function* test() {}
-        expect(isGeneratorFunction(test)).toBe(true);
+        function* test1() {}
+        expect(isGeneratorFunction(test1())).toBe(true);
+
+        async function* test2() { }
+        expect(isGeneratorFunction(test2())).toBe(true);
     });
 });
