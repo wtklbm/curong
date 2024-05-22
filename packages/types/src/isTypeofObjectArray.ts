c2c2f9ba188ddba293d922e927a47b3bef95a0a2
wtklbm <wtklbm@gmail.com>
2024-05-22 22:57:38 +0800

    ✨ feat: 添加 `isTypeofObjectArray` 方法

---
+++ packages/types/src/isTypeofObjectArray.ts
@@ -0,0 +1,15 @@
+import isArrayHave from './isArrayHave';
+import isTypeofObject from './isTypeofObject';
+import type { Function } from './types';
+
+/**
+ * 是不是一个长度大于 0 的数组，且每一项的值都是对象 (通过 `typeof` 判断且不为 `null`)
+ *
+ * @param value 要验证的值
+ * @returns 是则返回 `true`，否则为 `false`
+ */
+export default function isTypeofObjectArray<T = unknown>(
+    value: unknown
+): value is Function<T>[] {
+    return isArrayHave(value) && value.every(isTypeofObject);
+}
