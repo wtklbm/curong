2ddb322bf614ee2bf3ecd00693367c046fd3baa9
wtklbm <wtklbm@gmail.com>
2024-05-22 21:40:21 +0800

    ✨ feat: 添加 `isAsyncGeneratorFunction` 方法

---
+++ packages/types/src/isAsyncGeneratorFunction.ts
@@ -0,0 +1,13 @@
+import getTag from './getTag';
+
+/**
+ * 是不是一个异步的 `Generator` 函数
+ *
+ * @param value 要验证的值
+ * @returns 是则返回 `true`，否则为 `false`
+ */
+export default function isAsyncGeneratorFunction(
+    value: unknown
+): value is AsyncGeneratorFunction {
+    return getTag(value) === 'AsyncGeneratorFunction';
+}
