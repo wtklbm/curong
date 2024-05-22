fb736fa56a1345b3882ccc7afacc790ec7fb7408
wtklbm <wtklbm@gmail.com>
2024-05-22 21:10:48 +0800

    ✨ feat: 添加新的类型

--- packages/types/src/isArrayBufferView.ts
+++ packages/types/src/isArrayBufferView.ts
@@ -1,3 +1,5 @@
+import isBigInt64Array from './isBigInt64Array';
+import isBigUint64Array from './isBigUint64Array';
 import isBuffer from './isBuffer';
 import isDataView from './isDataView';
 import isFloat32Array from './isFloat32Array';
@@ -10,7 +12,11 @@ import isUint8ClampedArray from './isUint8ClampedArray';
 import isUint16Array from './isUint16Array';
 import isUint32Array from './isUint32Array';
 
-/** `ArrayBufferView` 是任何 `TypedArray` 类型(二进制缓冲区)的帮助程序类型 */
+/**
+ * `ArrayBufferView` 是任何 `TypedArray` 类型(二进制缓冲区)的帮助程序类型
+ *
+ * @see [TypedArray 对象](https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/Global_Objects/TypedArray#typedarray_对象)
+ */
 export type ArrayBufferView =
     | Int8Array
     | Uint8Array
@@ -21,6 +27,8 @@ export type ArrayBufferView =
     | Uint32Array
     | Float32Array
     | Float64Array
+    | BigInt64Array
+    | BigUint64Array
     | DataView;
 
 /**
@@ -31,7 +39,7 @@ export type ArrayBufferView =
  * @info 需要注意的是：虽然 `Buffer` 是基于 `ArrayBuffer` 的，但是 `isArrayBufferView(Buffer.from(''))` 的结果为 `false`。
  * 如果需要验证 `Buffer`，请使用 `isBuffer` 方法。
  *
- * `ArrayBufferView` 是代表以下任何 `TypedArray` 类型(二进制缓冲区)的帮助程序类型：
+ * `ArrayBufferView` 是代表以下任何 [`TypedArray`](https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/Global_Objects/TypedArray) 类型(二进制缓冲区)的帮助程序类型：
  *
  * - `Int8Array`
  * - `Uint8Array`
@@ -42,6 +50,8 @@ export type ArrayBufferView =
  * - `Uint32Array`
  * - `Float32Array`
  * - `Float64Array`
+ * - `BigInt64Array`
+ * - `BigUint64Array`
  * - `DataView`
  */
 export default function isArrayBufferView(
@@ -57,6 +67,8 @@ export default function isArrayBufferView(
         isUint32Array(value) ||
         isFloat32Array(value) ||
         isFloat64Array(value) ||
+        isBigInt64Array(value) ||
+        isBigUint64Array(value) ||
         isDataView(value);
 
     return isBufferView && !isBuffer(value);
