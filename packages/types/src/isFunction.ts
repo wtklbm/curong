8ab6b2fd9276a857a827c726a60ae51eecc87052
wtklbm <wtklbm@gmail.com>
2024-05-22 22:42:03 +0800

    📃 docs: 更新文档

--- packages/types/src/isFunction.ts
+++ packages/types/src/isFunction.ts
@@ -1,7 +1,9 @@
 import type { Function } from './types';
 
 /**
- * 是不是一个函数(包含同步函数、异步函数和 `Generator` 函数)
+ * 是不是一个函数 (包含同步函数、异步函数、`Generator` 函数 ...)
+ *
+ * 该函数可以验证任何类型的函数，只要 `typeof value` 为 `function` 就会返回 `true`
  *
  * @param value 要验证的值
  * @returns 是则返回 `true`，否则为 `false`
