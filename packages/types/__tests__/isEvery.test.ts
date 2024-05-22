4ef8008ba9f970d868dcfbbc1a9a5cd612ddacef
wtklbm <wtklbm@gmail.com>
2024-05-23 00:51:11 +0800

    🧪 test: 添加 `isEvery` 方法的测试

---
+++ packages/types/__tests__/isEvery.test.ts
@@ -0,0 +1,33 @@
+import { isEvery, isNumber, isNumberHave, isNumberSafe } from '../src';
+
+describe('@curong/types/isEvery', () => {
+    test('测试1', () => {
+        expect(isEvery(5, v => v > 0)).toBe(true);
+        expect(isEvery(0, v => v > 0)).toBe(false);
+        expect(isEvery([1, 2, 3], v => v > 0)).toBe(true);
+        expect(isEvery([1, 2, -1], v => v > 0)).toBe(false);
+        expect(isEvery(5, [v => v > 0, v => v < 10])).toBe(true);
+        expect(isEvery(15, [v => v > 0, v => v < 10])).toBe(false);
+        expect(isEvery([1, 2, 3], [v => v > 0, v => v < 4])).toBe(true);
+        expect(isEvery([1, 2, 15], [v => v > 0, v => v < 10])).toBe(false);
+    });
+
+    test('测试2', () => {
+        const v: any[] = [1];
+        const fns: any[] = [isNumber, isNumberSafe, isNumberHave];
+        expect(isEvery(1, fns)).toBeTruthy();
+        expect(isEvery(1, isNumber)).toBeTruthy();
+        expect(isEvery(v, fns)).toBeTruthy();
+        expect(isEvery(v, isNumberSafe)).toBeTruthy();
+
+        v.push(2);
+        expect(isEvery(v, fns)).toBeTruthy();
+
+        v.push('12');
+        expect(isEvery(v, fns)).toBeFalsy();
+        v.pop();
+
+        fns.push((v: any) => typeof v === 'string');
+        expect(isEvery(v, fns)).toBeFalsy();
+    });
+});
