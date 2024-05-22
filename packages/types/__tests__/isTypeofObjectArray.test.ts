80dfaa3e649ce44257510af8b4fb4a9362278e13
wtklbm <wtklbm@gmail.com>
2024-05-22 22:58:32 +0800

    🧪 test: 添加 `isTypeofObjectArray` 方法的测试

---
+++ packages/types/__tests__/isTypeofObjectArray.test.ts
@@ -0,0 +1,19 @@
+import { isTypeofObjectArray } from '../src';
+
+describe('@curong/types/isTypeofObjectArray', () => {
+    test('测试1', () => {
+        expect(isTypeofObjectArray([])).toBe(false);
+        expect(isTypeofObjectArray(0)).toBe(false);
+        expect(isTypeofObjectArray('')).toBe(false);
+        expect(isTypeofObjectArray(2)).toBe(false);
+        expect(isTypeofObjectArray([2, '1'])).toBe(false);
+        expect(isTypeofObjectArray([2, true])).toBe(false);
+        expect(isTypeofObjectArray([() => 1])).toBe(false);
+    });
+
+    test('测试2', () => {
+        expect(isTypeofObjectArray([{}])).toBe(false);
+        expect(isTypeofObjectArray([{}, true])).toBe(false);
+        expect(isTypeofObjectArray([{}, Object.create(null)])).toBe(true);
+    });
+});
