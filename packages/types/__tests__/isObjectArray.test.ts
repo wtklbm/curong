b2708ef871fbd648a63b83d5aeb3366100d7418d
wtklbm <wtklbm@gmail.com>
2024-05-22 22:57:00 +0800

    🧪 test: 添加 `isObjectArray` 方法的测试

---
+++ packages/types/__tests__/isObjectArray.test.ts
@@ -0,0 +1,19 @@
+import { isObjectArray } from '../src';
+
+describe('@curong/types/isObjectArray', () => {
+    test('测试1', () => {
+        expect(isObjectArray([])).toBe(false);
+        expect(isObjectArray(0)).toBe(false);
+        expect(isObjectArray('')).toBe(false);
+        expect(isObjectArray(2)).toBe(false);
+        expect(isObjectArray([2, '1'])).toBe(false);
+        expect(isObjectArray([2, true])).toBe(false);
+        expect(isObjectArray([() => 1])).toBe(false);
+    });
+
+    test('测试2', () => {
+        expect(isObjectArray([{}])).toBe(false);
+        expect(isObjectArray([{}, true])).toBe(false);
+        expect(isObjectArray([{}, Object(1)])).toBe(true);
+    });
+});
