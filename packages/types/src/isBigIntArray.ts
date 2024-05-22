c85fe21dc7778e204d749b185b5397094b0fd1fb
wtklbm <wtklbm@gmail.com>
2024-05-22 22:39:43 +0800

    🎈 perf: 改为非空数组

--- packages/types/src/isBigIntArray.ts
+++ packages/types/src/isBigIntArray.ts
@@ -1,12 +1,12 @@
-import isArray from './isArray';
+import isArrayHave from './isArrayHave';
 import isBigInt from './isBigInt';
 
 /**
- * 是不是一个数组，且每一项的值都是 `bigInt` (大数) 或被包装后的 `BigInt` 对象
+ * 是不是一个长度大于 0 的数组，且每一项的值都是 `bigInt` (大数) 或被包装后的 `BigInt` 对象
  *
  * @param value 要验证的值
  * @returns 是则返回 `true`，否则为 `false`
  */
 export default function isBigIntArray(value: unknown): value is bigint[] {
-    return isArray(value) && value.every(isBigInt);
+    return isArrayHave(value) && value.every(isBigInt);
 }
