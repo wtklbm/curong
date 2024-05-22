c569d284993c21f5a02992c8f5ec0bf88bb4a3e0
wtklbm <wtklbm@gmail.com>
2024-05-22 21:43:21 +0800

    🎈 perf: 支持同步和异步的判断

--- packages/types/src/isGeneratorFunction.ts
+++ packages/types/src/isGeneratorFunction.ts
@@ -1,11 +1,12 @@
-import getTag from './getTag';
+import isAsyncGeneratorFunction from './isAsyncGeneratorFunction';
+import isSyncGeneratorFunction from './isSyncGeneratorFunction';
 
 /**
- * 是不是一个 `Generator` 函数
+ * 是不是一个同步或异步的 `Generator` 函数
  *
  * @param value 要验证的值
  * @returns 是则返回 `true`，否则为 `false`
  */
 export default function isGeneratorFunction(value: unknown): value is Function {
-    return getTag(value) === 'GeneratorFunction';
+    return isSyncGeneratorFunction(value) || isAsyncGeneratorFunction(value);
 }
