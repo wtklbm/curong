5fb86fc10e3f3f443623e6b1bf54e78fbfb98f86
wtklbm <wtklbm@gmail.com>
2024-05-22 22:41:22 +0800

    🎈 perf: 改为非空数组

--- packages/types/src/isTruthyArray.ts
+++ packages/types/src/isTruthyArray.ts
@@ -1,13 +1,13 @@
-import isArray from './isArray';
+import isArrayHave from './isArrayHave';
 import isTruthy from './isTruthy';
 import type { Truthy } from './types';
 
 /**
- * 是不是一个数组，且每一项的值都是非虚值 (强制转换为 `Boolean` 后为 `true` 的值)
+ * 是不是一个长度大于 0 的数组，且每一项的值都是非虚值 (强制转换为 `Boolean` 后为 `true` 的值)
  *
  * @param value 要验证的值
  * @returns 是则返回 `true`，否则为 `false`
  */
 export default function isTruthyArray<T>(value: unknown): value is Truthy<T>[] {
-    return isArray(value) && value.every(isTruthy);
+    return isArrayHave(value) && value.every(isTruthy);
 }
