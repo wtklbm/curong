0345e221532d7481519b6aec3423a25292e4397f
wtklbm <wtklbm@gmail.com>
2024-05-23 01:16:18 +0800

    ✨ feat: 添加 `isHtmlElement` 方法

---
+++ packages/types/src/isHtmlElement.ts
@@ -0,0 +1,36 @@
+import isFunction from './isFunction';
+import isObject from './isObject';
+import isPlainObject from './isPlainObject';
+import isString from './isString';
+
+const DOM_PROPERTIES_TO_CHECK: Array<keyof HTMLElement> = [
+    'attributes',
+    'innerHTML',
+    'nodeValue',
+    'ownerDocument',
+    'style'
+];
+
+/**
+ * 是不是一个 `HTMLElement`
+ *
+ * @param value 要验证的值
+ * @returns 是则返回 `true`，否则为 `false`
+ */
+export default function isHtmlElement(value: unknown): value is HTMLElement {
+    try {
+        return (
+            isObject(value) &&
+            !isPlainObject(value) &&
+            (value as HTMLElement).nodeType === 1 &&
+            isString((value as HTMLElement).nodeName) &&
+            DOM_PROPERTIES_TO_CHECK.every(p => p in value) &&
+            isFunction(
+                // @ts-ignore
+                (value as HTMLElement).ownerDocument.defaultView.HTMLElement
+            )
+        );
+    } catch {}
+
+    return false;
+}
