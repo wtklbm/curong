5a0b0ea723d380046fc8f3e3d23889857ab15e41
wtklbm <wtklbm@gmail.com>
2024-05-22 21:53:42 +0800

    ✨ feat: 添加 `isSyncGeneratorFunction` 方法

---
+++ packages/types/src/isSyncGeneratorFunction.ts
@@ -0,0 +1,13 @@
+import getTag from './getTag';
+
+/**
+ * 是不是一个同步的 `Generator` 函数
+ *
+ * @param value 要验证的值
+ * @returns 是则返回 `true`，否则为 `false`
+ */
+export default function isSyncGeneratorFunction(
+    value: unknown
+): value is GeneratorFunction {
+    return getTag(value) === 'GeneratorFunction';
+}
