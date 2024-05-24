e7395a54ee653ab518c1b8148301c0961449bfe8
wtklbm <wtklbm@gmail.com>
2024-05-24 23:17:42 +0800

    🧪 test: 添加 `dimensionLevel` 方法的测试

---
+++ packages/array/__tests__/dimensionLevel.test.ts
@@ -0,0 +1,58 @@
+import { dimensionLevel } from '../src';
+
+describe('@curong/array/dimensionLevel', () => {
+    test('测试1', () => {
+        expect(dimensionLevel([])).toEqual(1);
+        expect(dimensionLevel([1, 2, 3])).toEqual(1);
+        expect(dimensionLevel([1, [], 3])).toEqual(1);
+        expect(
+            dimensionLevel([1, [[[[[[[2]]]]]]], [3, [4], [[[[5]]]], 6]])
+        ).toEqual(1);
+    });
+
+    test('测试2', () => {
+        expect(dimensionLevel([[]])).toEqual(2);
+        expect(
+            dimensionLevel([
+                [1, 2],
+                [3, 4]
+            ])
+        ).toEqual(2);
+        expect(
+            dimensionLevel([
+                [[], 1],
+                [3, 4]
+            ])
+        ).toEqual(2);
+        expect(
+            dimensionLevel([
+                [[], 1],
+                [[], 4]
+            ])
+        ).toEqual(2);
+        expect(
+            dimensionLevel([
+                [1, []],
+                [3, 4]
+            ])
+        ).toEqual(2);
+        expect(
+            dimensionLevel([
+                [[], []],
+                [[], 4]
+            ])
+        ).toEqual(2);
+        expect(
+            dimensionLevel([
+                [[], []],
+                [[], []]
+            ])
+        ).toEqual(3);
+
+        expect(dimensionLevel([[[]]])).toEqual(3);
+        expect(dimensionLevel([[[1, 2]], [[3, 4]]])).toEqual(3);
+
+        expect(dimensionLevel([[[[]]]])).toEqual(4);
+        expect(dimensionLevel([[[1, []]], [[[], 4]]])).toEqual(3);
+    });
+});
