6af67bf49b8afb8ac0a1f6a9201a9c5f251462ea
wtklbm <wtklbm@gmail.com>
2024-05-24 23:17:31 +0800

    ✨ feat: 添加 `dimensionLevel` 方法

---
+++ packages/array/src/dimensionLevel.ts
@@ -0,0 +1,30 @@
+import { isArrayHave } from '@curong/types';
+
+/**
+ * 判断一个数组的维度
+ *
+ * @param value 要检查的数组
+ * @returns 返回数组的维度数字
+ * @example
+ *
+ * ```javascript
+ * console.log(dimensionLevel([1, 2, 3])); // 1
+ * console.log(dimensionLevel([[1, 2], [3, 4]])); // 2
+ * console.log(dimensionLevel([[[1, 2]], [[3, 4]]])); // 3
+ * ```
+ */
+export default function dimensionLevel<T extends unknown[]>(value: T): number {
+    let level = 1;
+    let internal: unknown[] = value;
+
+    while (isArrayHave(internal)) {
+        if (!internal.every(Array.isArray)) {
+            return level;
+        }
+
+        internal = internal[0];
+        level++;
+    }
+
+    return level;
+}
