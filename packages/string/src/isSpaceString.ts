695c92645b1e8dac3bd47d465d33bf34b452a315
wtklbm <wtklbm@gmail.com>
2024-05-26 13:39:05 +0800

    ✨ feat: 将 `chars` 参数改为 `extras`

--- packages/string/src/isSpaceString.ts
+++ packages/string/src/isSpaceString.ts
@@ -1,5 +1,5 @@
 import { trimRegSource } from './trimRegSource';
-import type { SpecialCharNames, TrimOptions } from './types';
+import type { TrimOptions } from './types';
 
 /**
  * 验证一个非空字符串是不是一个空字符字符串
@@ -10,7 +10,7 @@ import type { SpecialCharNames, TrimOptions } from './types';
  *   - `zeroWidth`: 是否验证零宽字符，默认为 `true`
  *   - `likeSpace`: 是否验证像空格的字符，默认为 `true`
  *   - `control`: 是否验证控制字符(回车、换行、制表符等)，默认为 `true`
- * @param chars 自定义要验证的额外字符
+ * @param extras 要验证的额外的自定义字符
  * @returns 是则返回 `true`，否则为 `false` (空字符串的结果为 `false`)
  * @example
  *
@@ -22,7 +22,7 @@ import type { SpecialCharNames, TrimOptions } from './types';
 export default function isSpaceString(
     value: string,
     options?: TrimOptions,
-    chars?: SpecialCharNames[]
+    extras?: string | string[]
 ): boolean {
-    return new RegExp(`^${trimRegSource(options, chars)}$`).test(value);
+    return new RegExp(`^${trimRegSource(options, extras)}$`).test(value);
 }
