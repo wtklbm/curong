99e9fc2f95582b856dda73edfe1e84436c803a4f
wtklbm <wtklbm@gmail.com>
2024-05-26 20:28:24 +0800

    🦄 refactor: 使用 `getTagEqual` 替代 `getTag`

--- packages/types/src/isInt32Array.ts
+++ packages/types/src/isInt32Array.ts
@@ -1,4 +1,4 @@
-import getTag from './getTag';
+import getTagEqual from './getTagEqual';
 
 /**
  * 是不是一个 `Int32Array`，每一项的值为 `-2147483648 - 2147483647`
@@ -7,5 +7,5 @@ import getTag from './getTag';
  * @returns 是则返回 `true`，否则为 `false`
  */
 export default function isInt32Array(value: unknown): value is Int32Array {
-    return getTag(value) === 'Int32Array';
+    return getTagEqual(value, 'Int32Array');
 }
