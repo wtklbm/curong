61a071ae061f896c5a6a71e4cae64bbd42f7f35f
wtklbm <wtklbm@gmail.com>
2024-05-27 02:54:21 +0800

    ✨ feat: 添加 `isTwoDimensionalArray` 方法

---
+++ packages/types/src/isTwoDimensionalArray.ts
@@ -0,0 +1,18 @@
+import isArray from './isArray';
+import isArrayHave from './isArrayHave';
+
+/**
+ * 是不是一个长度大于 0 的数组，且每一项的值都是数组 (二维数组)
+ *
+ * 需要注意的时，判断三维以上的数组时该方法也会返回 `true`，这是因为三维数组包含二维数组。
+ * 如果想严格判断是否为指定维度的数组，请使用 `@curong/array` 中的 `isMultiDimensional` 方法。
+ * 如果想具体判断数组的维数，请使用 `@curong/array` 中的 `dimensionLevel` 方法。
+ *
+ * @param value 要验证的值
+ * @returns 是则返回 `true`，否则为 `false`
+ */
+export default function isTwoDimensionalArray<T extends unknown[][]>(
+    value: unknown
+): value is T {
+    return isArrayHave(value) && value.every(isArray);
+}
