e066cec76bfe4aa575e3d2c02944bf44042b674e
wtklbm <wtklbm@gmail.com>
2024-05-26 13:19:51 +0800

    ✨ feat: 添加 `extras` 参数

--- packages/string/src/trimRegSource.ts
+++ packages/string/src/trimRegSource.ts
@@ -1,4 +1,4 @@
-import { isArrayHave, isTrue } from '@curong/types';
+import { isStringArray, isStringHave, isTrue } from '@curong/types';
 
 import {
     controlRegSource,
@@ -12,17 +12,18 @@ import type { SpecialCharNames, TrimOptions } from './types';
  * 根据选项生成可以删除字符串特定字符串的正则表达式字符串
  *
  * @param options 配置选项
- *   - `space`: 是否删除空格，默认为 `true`
- *   - `zeroWidth`: 是否删除零宽字符，默认为 `true`
- *   - `likeSpace`: 是否删除像空格的字符，默认为 `true`
- *   - `control`: 是否删除控制字符(回车、换行、制表符等)，默认为 `true`
- *
- * @param chars 自定义要删除的字符
+ *  - `space`: 是否删除空格，默认为 `true`
+ *  - `zeroWidth`: 是否删除零宽字符，默认为 `true`
+ *  - `likeSpace`: 是否删除像空格的字符，默认为 `true`
+ *  - `control`: 是否删除控制字符(回车、换行、制表符等)，默认为 `true`
+ * @param chars 特殊字符集的名称数组
+ * @param extras 自定义的额外的字符
  * @returns 返回处理好的正则表达式字符串
  */
 export const trimRegSource = (
     options: TrimOptions = {},
-    chars?: SpecialCharNames[]
+    chars?: SpecialCharNames[],
+    extras?: string | string[]
 ): string => {
     const {
         space = true,
@@ -31,27 +32,23 @@ export const trimRegSource = (
         control = true
     } = options;
 
-    const regexpSources = [];
+    const conditions: { is: () => boolean; ret: () => string }[] = [
+        { is: () => isTrue(space), ret: () => ' ' },
+        { is: () => isTrue(zeroWidth), ret: () => zeroWidthRegSource },
+        { is: () => isTrue(likeSpace), ret: () => likeSpaceRegSource },
+        { is: () => isTrue(control), ret: () => controlRegSource },
+        {
+            is: () => isStringArray(chars),
+            ret: () => `[${chars!.map(v => specialChars[v]).join('')}]`
+        },
+        {
+            is: () => isStringHave(extras) || isStringArray(extras),
+            ret: () => `[${[extras].flat().join('')}]`
+        }
+    ];
 
-    if (isTrue(space)) {
-        regexpSources.push(' ');
-    }
-
-    if (isTrue(zeroWidth)) {
-        regexpSources.push(zeroWidthRegSource);
-    }
-
-    if (isTrue(likeSpace)) {
-        regexpSources.push(likeSpaceRegSource);
-    }
-
-    if (isTrue(control)) {
-        regexpSources.push(controlRegSource);
-    }
-
-    if (isArrayHave(chars)) {
-        regexpSources.push(`[${chars.map(v => specialChars[v]).join('')}]`);
-    }
-
-    return `(?:${regexpSources.join('|')})+`;
+    return `(?:${conditions
+        .filter(v => v.is())
+        .map(v => v.ret())
+        .join('|')})+`;
 };
