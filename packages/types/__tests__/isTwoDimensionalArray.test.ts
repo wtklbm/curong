7a6c96f44da44c287ddf51d81b44f42fc3f7b2c8
wtklbm <wtklbm@gmail.com>
2024-05-27 03:20:25 +0800

    🧪 test: 更新测试

--- packages/types/__tests__/isTwoDimensionalArray.test.ts
+++ packages/types/__tests__/isTwoDimensionalArray.test.ts
@@ -4,11 +4,11 @@ describe('@curong/types/isTwoDimensionalArray', () => {
     it('测试1', () => {
         expect(isTwoDimensionalArray(123)).toBe(false);
         expect(isTwoDimensionalArray([1, [2, 3], 'not an array'])).toBe(false);
+        expect(isTwoDimensionalArray([])).toBe(false);
         expect(isTwoDimensionalArray([[1, 2], 3])).toBe(false);
     });
 
     it('测试2', () => {
-        expect(isTwoDimensionalArray([])).toBe(true);
         expect(isTwoDimensionalArray([[]])).toBe(true);
         expect(isTwoDimensionalArray([[[]]])).toBe(true);
         expect(isTwoDimensionalArray([[[[]]]])).toBe(true);
