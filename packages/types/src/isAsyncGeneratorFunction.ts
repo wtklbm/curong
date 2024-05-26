a0dee9956bc3c7734eb2f3d9a3225dec1f69ecd9
wtklbm <wtklbm@gmail.com>
2024-05-26 20:26:19 +0800

    🦄 refactor: 使用 `getTagEqual` 替代 `getTag`

--- packages/types/src/isAsyncGeneratorFunction.ts
+++ packages/types/src/isAsyncGeneratorFunction.ts
@@ -1,4 +1,4 @@
-import getTag from './getTag';
+import getTagEqual from './getTagEqual';
 
 /**
  * 是不是一个异步的 `Generator` 函数
@@ -9,5 +9,5 @@ import getTag from './getTag';
 export default function isAsyncGeneratorFunction(
     value: unknown
 ): value is AsyncGeneratorFunction {
-    return getTag(value) === 'AsyncGeneratorFunction';
+    return getTagEqual(value, 'AsyncGeneratorFunction');
 }
