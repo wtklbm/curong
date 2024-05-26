0ced2a427b8682c860baac64027267b4d450a311
wtklbm <wtklbm@gmail.com>
2024-05-26 18:54:34 +0800

    🧪 test: 添加 `keys` 方法的测试

---
+++ packages/object/__tests__/keys.test.ts
@@ -0,0 +1,49 @@
+import { keys } from '../src';
+
+// 用于测试的对象
+const b = Symbol('yes');
+const obj = { a: 1, [b]: 'yes' };
+
+Object.defineProperty(obj, 'c', {
+    value: 4,
+    enumerable: false // 将属性设置为不可枚举
+});
+
+describe('keys function', () => {
+    test('should throw a TypeError for invalid methodLevel', () => {
+        expect(() => keys(obj, 4)).toThrowError();
+        expect(() => keys(obj, -1)).toThrowError();
+    });
+
+    test('should handle an empty object', () => {
+        expect(keys({})).toEqual([]);
+        expect(keys([])).toEqual([]);
+    });
+
+    test('should use methodLevel 0 by default and call the corresponding function', () => {
+        expect(keys(obj)).toEqual(['a']);
+        expect(Object.keys(obj)).toEqual(['a']);
+    });
+
+    test('should call the function corresponding to methodLevel 1', () => {
+        expect(keys(obj, 1)).toEqual(['a', 'c']);
+        expect(Object.getOwnPropertyNames(obj)).toEqual(['a', 'c']);
+    });
+
+    test('should call the function corresponding to methodLevel 2', () => {
+        expect(keys(obj, 2)).toEqual([b]);
+        expect(Object.getOwnPropertySymbols(obj)).toEqual([b]);
+    });
+
+    test('should call the function corresponding to methodLevel 2', () => {
+        expect(keys(obj, 3)).toEqual(['a', 'c', b]);
+        expect(Reflect.ownKeys(obj)).toEqual(['a', 'c', b]);
+    });
+
+    test('should handle array-like objects', () => {
+        const arrayLike = { 0: 'a', 1: 'b', length: 2 };
+        const result = keys(arrayLike);
+        expect(result).toEqual(['0', '1', 'length']);
+        expect(Object.keys(arrayLike)).toEqual(['0', '1', 'length']);
+    });
+});
