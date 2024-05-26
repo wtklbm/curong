21ac06019d6f00f3454feda870e7b6c9ef370f74
wtklbm <wtklbm@gmail.com>
2024-05-26 20:31:01 +0800

    🦄 refactor: 使用 `getTagEqual` 替代 `getTag`

--- packages/types/src/isUint8Array.ts
+++ packages/types/src/isUint8Array.ts
@@ -1,4 +1,4 @@
-import getTag from './getTag';
+import getTagEqual from './getTagEqual';
 
 /**
  * 是不是一个 `Uint8Array`，每一项的值为 `0 - 255`
@@ -7,5 +7,5 @@ import getTag from './getTag';
  * @returns 是则返回 `true`，否则为 `false`
  */
 export default function isUint8Array(value: unknown): value is Uint8Array {
-    return getTag(value) === 'Uint8Array';
+    return getTagEqual(value, 'Uint8Array');
 }
