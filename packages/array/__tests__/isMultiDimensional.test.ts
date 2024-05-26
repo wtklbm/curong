eca96722b9df56d1d433f83500ea9b1a57038892
wtklbm <wtklbm@gmail.com>
2024-05-27 01:27:14 +0800

    🧪 test: 添加新的测试

--- packages/array/__tests__/isMultiDimensional.test.ts
+++ packages/array/__tests__/isMultiDimensional.test.ts
@@ -13,7 +13,7 @@ describe('@curong/types/isMultiDimensional', () => {
         expect(isMultiDimensional([[2, 3]])).toBe(true);
     });
 
-    test('should return true for 2D array', () => {
+    test('测试3', () => {
         const arr: number[][] = [
             [1, 2],
             [3, 4]
@@ -22,14 +22,14 @@ describe('@curong/types/isMultiDimensional', () => {
         expect(isMultiDimensional(arr, 2)).toBe(true);
     });
 
-    test('should return false for 1D array', () => {
+    test('测试4', () => {
         const arr: number[] = [1, 2, 3, 4];
         expect(isMultiDimensional(arr)).toBe(false);
-        expect(isMultiDimensional(arr, 1)).toBe(false);
+        expect(() => isMultiDimensional(arr, 1)).toThrow();
         expect(isMultiDimensional(arr, 2)).toBe(false);
     });
 
-    test('should return true for 3D array when depth is set to 3', () => {
+    test('测试5', () => {
         const arr: number[][][] = [
             [[1], [2]],
             [[3], [4]]
@@ -38,16 +38,17 @@ describe('@curong/types/isMultiDimensional', () => {
         expect(isMultiDimensional(arr, 3)).toBe(true);
     });
 
-    test('should return false for 3D array when depth is set to 2', () => {
+    test('测试6', () => {
         const arr: number[][][] = [
             [[1], [2]],
             [[3], [4]]
         ];
         expect(isMultiDimensional(arr)).toBe(true);
-        expect(isMultiDimensional(arr, 2)).toBe(false);
+        expect(isMultiDimensional(arr, 2)).toBe(true);
+        expect(isMultiDimensional(arr, 2, true)).toBe(false);
     });
 
-    test('should throw an error for negative depth', () => {
+    test('测试7', () => {
         const arr: number[][] = [
             [1, 2],
             [3, 4]
@@ -57,25 +58,40 @@ describe('@curong/types/isMultiDimensional', () => {
         }).toThrow();
     });
 
-    describe('isMultiDimensional', () => {
-        it('should return false for arr1', () => {
-            const arr1 = [1, 2, 3];
-            expect(isMultiDimensional(arr1)).toBe(false);
-            expect(isMultiDimensional(arr1, 1)).toBe(false);
-        });
+    test('测试8', () => {
+        const arr1 = [1, 2, 3];
+        expect(isMultiDimensional(arr1)).toBe(false);
+        expect(() => isMultiDimensional(() => arr1, 1)).toThrow();
+    });
 
-        it('should return true for arr2', () => {
-            const arr2 = [
+    test('测试9', () => {
+        const arr2 = [
+            [1, 2],
+            [3, 4],
+            [5, 6]
+        ];
+        expect(isMultiDimensional(arr2)).toBe(true);
+        expect(isMultiDimensional(arr2, 2)).toBe(true);
+    });
+
+    test('测试10', () => {
+        const arr3 = [
+            [
                 [1, 2],
-                [3, 4],
-                [5, 6]
-            ];
-            expect(isMultiDimensional(arr2)).toBe(true);
-            expect(isMultiDimensional(arr2, 2)).toBe(true);
-        });
+                [3, 4]
+            ],
+            [
+                [5, 6],
+                [7, 8]
+            ]
+        ];
+        expect(isMultiDimensional(arr3)).toBe(true);
+        expect(isMultiDimensional(arr3, 3)).toBe(true);
+    });
 
-        it('should return true for arr3', () => {
-            const arr3 = [
+    test('测试11', () => {
+        const arr4 = [
+            [
                 [
                     [1, 2],
                     [3, 4]
@@ -84,68 +100,69 @@ describe('@curong/types/isMultiDimensional', () => {
                     [5, 6],
                     [7, 8]
                 ]
-            ];
-            expect(isMultiDimensional(arr3)).toBe(true);
-            expect(isMultiDimensional(arr3, 3)).toBe(true);
-        });
+            ]
+        ];
+        expect(isMultiDimensional(arr4)).toBe(true);
+        expect(isMultiDimensional(arr4, 4)).toBe(true);
+    });
 
-        it('should return true for arr4 with dimension 4', () => {
-            const arr4 = [
-                [
-                    [
-                        [1, 2],
-                        [3, 4]
-                    ],
-                    [
-                        [5, 6],
-                        [7, 8]
-                    ]
-                ]
-            ];
-            expect(isMultiDimensional(arr4)).toBe(true);
-            expect(isMultiDimensional(arr4, 4)).toBe(true);
-        });
+    test('测试12', () => {
+        const arr5 = [[1, 2], 3, [4, 5], [], [6, 7, 8]];
+        expect(isMultiDimensional(arr5)).toBe(false);
+        expect(isMultiDimensional(arr5, 2)).toBe(false);
+    });
 
-        it('should return true for arr5', () => {
-            const arr5 = [[1, 2], 3, [4, 5], [], [6, 7, 8]];
-            expect(isMultiDimensional(arr5)).toBe(false);
-            expect(isMultiDimensional(arr5, 2)).toBe(false);
-        });
+    test('测试13', () => {
+        let arr: any = [[]];
+        expect(isMultiDimensional(arr)).toBe(true);
+        expect(isMultiDimensional(arr, 2)).toBe(true);
 
-        it('should return true for [[]]', () => {
-            let arr: any = [[]];
-            expect(isMultiDimensional(arr)).toBe(true);
-            expect(isMultiDimensional(arr, 2)).toBe(true);
+        arr = [[[]]];
+        expect(isMultiDimensional(arr)).toBe(true);
+        expect(isMultiDimensional(arr, 3)).toBe(true);
 
-            arr = [[[]]];
-            expect(isMultiDimensional(arr)).toBe(true);
-            expect(isMultiDimensional(arr, 3)).toBe(true);
+        arr = [[[[]]]];
+        expect(isMultiDimensional(arr)).toBe(true);
+        expect(isMultiDimensional(arr, 4)).toBe(true);
 
-            arr = [[[[]]]];
-            expect(isMultiDimensional(arr)).toBe(true);
-            expect(isMultiDimensional(arr, 4)).toBe(true);
+        arr = [[[[[]]]]];
+        expect(isMultiDimensional(arr)).toBe(true);
+        expect(isMultiDimensional(arr, 5)).toBe(true);
+    });
 
-            arr = [[[[[]]]]];
-            expect(isMultiDimensional(arr)).toBe(true);
-            expect(isMultiDimensional(arr, 5)).toBe(true);
-        });
+    test('测试14', () => {
+        let arr: any = [[], 1];
+        expect(isMultiDimensional(arr)).toBe(false);
+        expect(isMultiDimensional(arr, 2)).toBe(false);
 
-        it('should return true for [[], 1]', () => {
-            let arr: any = [[], 1];
-            expect(isMultiDimensional(arr)).toBe(false);
-            expect(isMultiDimensional(arr, 2)).toBe(false);
+        arr = [[[], 1]];
+        expect(isMultiDimensional(arr)).toBe(true);
+        expect(isMultiDimensional(arr, 2)).toBe(true);
 
-            arr = [[[], 1]];
-            expect(isMultiDimensional(arr)).toBe(true);
-            expect(isMultiDimensional(arr, 2)).toBe(true);
+        arr = [[[[], 1]]];
+        expect(isMultiDimensional(arr)).toBe(true);
+        expect(isMultiDimensional(arr, 3)).toBe(true);
 
-            arr = [[[[], 1]]];
-            expect(isMultiDimensional(arr)).toBe(true);
-            expect(isMultiDimensional(arr, 3)).toBe(true);
+        arr = [[[[[], 1]]]];
+        expect(isMultiDimensional(arr)).toBe(true);
+        expect(isMultiDimensional(arr, 4)).toBe(true);
+    });
 
-            arr = [[[[[], 1]]]];
-            expect(isMultiDimensional(arr)).toBe(true);
-            expect(isMultiDimensional(arr, 4)).toBe(true);
-        });
+    test('测试15', () => {
+        expect(isMultiDimensional([])).toBe(false);
+        expect(isMultiDimensional([[]])).toBe(true);
+        expect(() => isMultiDimensional([[]], 1)).toThrow();
+        expect(isMultiDimensional([1, 2, 3, 4])).toBe(false);
+        expect(isMultiDimensional([[1, 2, 3, 4]], 2)).toBe(true);
+        expect(isMultiDimensional([[1, 2, 3, 4]], 2, true)).toBe(true);
+        expect(() => isMultiDimensional([[1, 2, 3, 4]], 1)).toThrow();
+        expect(() => isMultiDimensional([[1, 2, 3, 4]], 1, true)).toThrow();
+        expect(
+            isMultiDimensional([
+                [1, 2],
+                [3, 4]
+            ])
+        ).toBe(true);
+        expect(isMultiDimensional([[[1, 2]], [[3, 4]]], 3)).toBe(true);
     });
 });
