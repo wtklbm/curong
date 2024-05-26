a6fdbd030d518a74f765fab7a9a96ca087ef1e79
wtklbm <wtklbm@gmail.com>
2024-05-27 03:40:42 +0800

    🧪 test: 更新测试

--- packages/types/__tests__/isSyncGeneratorFunction.test.ts
+++ packages/types/__tests__/isSyncGeneratorFunction.test.ts
@@ -5,17 +5,21 @@ describe('@curong/types/isSyncGeneratorFunction', () => {
         expect(isSyncGeneratorFunction(new Function())).toBe(false);
 
         function fn1() { }
+        expect(isSyncGeneratorFunction(fn1)).toBe(false);
         expect(isSyncGeneratorFunction(fn1())).toBe(false);
 
         async function fn2() {}
+        expect(isSyncGeneratorFunction(fn2)).toBe(false);
         expect(isSyncGeneratorFunction(fn2())).toBe(false);
     });
 
     test('测试2', () => {
         function* test1() {}
-        expect(isSyncGeneratorFunction(test1())).toBe(true);
+        expect(isSyncGeneratorFunction(test1)).toBe(true); // GeneratorFunction
+        expect(isSyncGeneratorFunction(test1())).toBe(false); // Generator
 
         async function* test2() { }
-        expect(isSyncGeneratorFunction(test2())).toBe(false);
+        expect(isSyncGeneratorFunction(test2)).toBe(false); // AsyncGeneratorFunction
+        expect(isSyncGeneratorFunction(test2())).toBe(false); // AsyncGenerator
     });
 });
