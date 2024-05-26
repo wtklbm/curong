3b9d18907ce6c59484e34f03529e9da5eba25184
wtklbm <wtklbm@gmail.com>
2024-05-26 13:38:16 +0800

    ✨ feat: 删除 `chars` 参数

--- packages/string/src/trimRegSource.ts
+++ packages/string/src/trimRegSource.ts
@@ -3,10 +3,9 @@ import { isStringArray, isStringHave, isTrue } from '@curong/types';
 import {
     controlRegSource,
     likeSpaceRegSource,
-    specialChars,
     zeroWidthRegSource
 } from './characters';
-import type { SpecialCharNames, TrimOptions } from './types';
+import type { TrimOptions } from './types';
 
 /**
  * 根据选项生成可以删除字符串特定字符串的正则表达式字符串
@@ -16,13 +15,11 @@ import type { SpecialCharNames, TrimOptions } from './types';
  *  - `zeroWidth`: 是否删除零宽字符，默认为 `true`
  *  - `likeSpace`: 是否删除像空格的字符，默认为 `true`
  *  - `control`: 是否删除控制字符(回车、换行、制表符等)，默认为 `true`
- * @param chars 特殊字符集的名称数组
- * @param extras 自定义的额外的字符
+ * @param extras 要验证的额外的自定义字符
  * @returns 返回处理好的正则表达式字符串
  */
 export const trimRegSource = (
     options: TrimOptions = {},
-    chars?: SpecialCharNames[],
     extras?: string | string[]
 ): string => {
     const {
@@ -37,10 +34,6 @@ export const trimRegSource = (
         { is: () => isTrue(zeroWidth), ret: () => zeroWidthRegSource },
         { is: () => isTrue(likeSpace), ret: () => likeSpaceRegSource },
         { is: () => isTrue(control), ret: () => controlRegSource },
-        {
-            is: () => isStringArray(chars),
-            ret: () => `[${chars!.map(v => specialChars[v]).join('')}]`
-        },
         {
             is: () => isStringHave(extras) || isStringArray(extras),
             ret: () => `[${[extras].flat().join('')}]`
