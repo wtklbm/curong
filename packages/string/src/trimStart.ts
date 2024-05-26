1b71a55b05257319497c81c5fd6467cba656dddc
wtklbm <wtklbm@gmail.com>
2024-05-26 13:38:56 +0800

    ✨ feat: 将 `chars` 参数改为 `extras`

--- packages/string/src/trimStart.ts
+++ packages/string/src/trimStart.ts
@@ -1,7 +1,7 @@
 import { isZero } from '@curong/types';
 
 import { trimRegSource } from './trimRegSource';
-import type { SpecialCharNames, TrimOptions } from './types';
+import type { TrimOptions } from './types';
 
 /**
  * 删除字符串开头的特定字符
@@ -12,8 +12,7 @@ import type { SpecialCharNames, TrimOptions } from './types';
  *   - `zeroWidth`: 是否删除零宽字符，默认为 `true`
  *   - `likeSpace`: 是否删除像空格的字符，默认为 `true`
  *   - `control`: 是否删除控制字符(回车、换行、制表符等)，默认为 `true`
- *
- * @param chars 自定义要删除的字符
+ * @param extras 要验证的额外的自定义字符
  * @returns 返回处理好的字符串
  * @example
  *
@@ -25,11 +24,11 @@ import type { SpecialCharNames, TrimOptions } from './types';
 export default function trimStart(
     value: string,
     options?: TrimOptions,
-    chars?: SpecialCharNames[]
+    extras?: string | string[]
 ): string {
     if (isZero(value.length)) {
         return value;
     }
 
-    return value.replace(new RegExp(`^${trimRegSource(options, chars)}`), '');
+    return value.replace(new RegExp(`^${trimRegSource(options, extras)}`), '');
 }
