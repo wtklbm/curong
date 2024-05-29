825fce58f136f4655bb7595fba1f61386bebbd8f
wtklbm <wtklbm@gmail.com>
2024-05-30 00:24:50 +0800

    🧪 test: 添加新的测试

--- packages/types/__tests__/isNumberValid.test.ts
+++ packages/types/__tests__/isNumberValid.test.ts
@@ -6,12 +6,29 @@ describe('@curong/types/isNumberValid', () => {
         expect(isNumberValid('abc')).toBe(false);
         expect(isNumberValid('123abc')).toBe(false);
         expect(isNumberValid('NaN')).toBe(false);
-        expect(isNumberValid('123 ')).toBe(true);
-        expect(isNumberValid('', true)).toBe(true);
-        expect(isNumberValid(' ', true)).toBe(true);
+        expect(isNumberValid('')).toBe(false);
+        expect(isNumberValid('11 23')).toBe(false);
+        expect(isNumberValid('MIN_VALUE')).toBe(false);
+        expect(isNumberValid('Number.MIN_VALUE')).toBe(false);
+        expect(isNumberValid('POSITIVE_INFINITY')).toBe(false);
+        expect(isNumberValid('xxx')).toBe(false);
+        expect(isNumberValid(NaN)).toBe(false);
+        expect(isNumberValid(Number.NaN)).toBe(false);
+        expect(isNumberValid(Number.NaN + '')).toBe(false);
+        expect(isNumberValid(true + '')).toBe(false);
     });
 
     test('测试2', () => {
+        expect(isNumberValid('123 ')).toBe(true);
+        expect(isNumberValid('', true)).toBe(true);
+        expect(isNumberValid(' ', true)).toBe(true);
+        expect(isNumberValid(true)).toBe(true);
+        expect(isNumberValid(1)).toBe(true);
+        expect(isNumberValid(Number(1))).toBe(true);
+        expect(isNumberValid(new Number(1))).toBe(true);
+        expect(isNumberValid(Object(1))).toBe(true);
+        expect(isNumberValid(0)).toBe(true);
+        expect(isNumberValid(11.23)).toBe(true);
         expect(isNumberValid(null)).toBe(true);
         expect(isNumberValid('123')).toBe(true);
         expect(isNumberValid(' 123 ')).toBe(true);
@@ -25,5 +42,17 @@ describe('@curong/types/isNumberValid', () => {
         expect(isNumberValid('-Infinity')).toBe(true); // -Infinity 是有效数字
         expect(isNumberValid('', false)).toBe(false);
         expect(isNumberValid(' ', false)).toBe(false);
+
+        expect(isNumberValid('Infinity')).toBe(true);
+        expect(isNumberValid('-Infinity')).toBe(true);
+        expect(isNumberValid(`${Number.POSITIVE_INFINITY}`)).toBe(true);
+        expect(isNumberValid('1')).toBe(true);
+        expect(isNumberValid(`${Number(1)}`)).toBe(true);
+        expect(isNumberValid(new Number(1) + '')).toBe(true);
+        expect(isNumberValid(Object(1) + '')).toBe(true);
+        expect(isNumberValid(Number(1) + '')).toBe(true);
+        expect(isNumberValid(0 + '')).toBe(true);
+        expect(isNumberValid(11.23 + '')).toBe(true);
+        expect(isNumberValid('11.23')).toBe(true);
     });
 });
