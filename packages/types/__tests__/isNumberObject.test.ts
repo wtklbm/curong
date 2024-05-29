74e3e9c4f3714edc48ef2f10257c7c150557ecfd
wtklbm <wtklbm@gmail.com>
2024-05-29 19:02:27 +0800

    🧪 test: 添加新的测试

--- packages/types/__tests__/isNumberObject.test.ts
+++ packages/types/__tests__/isNumberObject.test.ts
@@ -3,9 +3,19 @@ import { isNumberObject } from '../src';
 describe('@curong/types/isNumberObject', () => {
     test('测试1', () => {
         expect(isNumberObject(0)).toBe(false);
+
+        expect(isNumberObject(NaN)).toBe(false);
+        expect(isNumberObject(Number.NaN)).toBe(false);
+        expect(isNumberObject(Number(NaN))).toBe(false);
+        expect(isNumberObject(Number(Number.NaN))).toBe(false);
     });
 
     test('测试2', () => {
         expect(isNumberObject(new Number(1))).toBe(true);
+
+        expect(isNumberObject(new Number(NaN))).toBe(true);
+        expect(isNumberObject(new Number(Number.NaN))).toBe(true);
+        expect(isNumberObject(Object(NaN))).toBe(true);
+        expect(isNumberObject(Object(Number.NaN))).toBe(true);
     });
 });
