089d2b8222c8a6759ee49196047cb251a44c2916
wtklbm <wtklbm@gmail.com>
2024-05-29 18:13:03 +0800

    🧪 test: 更新测试

--- packages/types/__tests__/isNumeric.test.ts
+++ packages/types/__tests__/isNumeric.test.ts
@@ -2,16 +2,28 @@ import { isNumeric } from '../src';
 
 describe('@curong/types/isNumeric', () => {
     test('测试1', () => {
-        expect(isNumeric('Infinity')).toBe(true);
-        expect(isNumeric('-Infinity')).toBe(true);
-        expect(isNumeric('POSITIVE_INFINITY')).toBe(false);
+        expect(isNumeric('')).toBe(false);
+        expect(isNumeric('11 23')).toBe(false);
         expect(isNumeric('MIN_VALUE')).toBe(false);
         expect(isNumeric('Number.MIN_VALUE')).toBe(false);
-        expect(isNumeric(`${Number.POSITIVE_INFINITY}`)).toBe(true);
-        expect(isNumeric('11 23')).toBe(false);
+        expect(isNumeric('POSITIVE_INFINITY')).toBe(false);
         expect(isNumeric('xxx')).toBe(false);
+        expect(isNumeric(NaN)).toBe(false);
+        expect(isNumeric(Number.NaN)).toBe(false);
         expect(isNumeric(true)).toBe(false);
+    });
+
+    test('测试2', () => {
+        expect(isNumeric('Infinity')).toBe(true);
+        expect(isNumeric('-Infinity')).toBe(true);
+        expect(isNumeric(`${Number.POSITIVE_INFINITY}`)).toBe(true);
         expect(isNumeric(1)).toBe(true);
+        expect(isNumeric(Number(1))).toBe(true);
+        expect(isNumeric(new Number(1))).toBe(true);
+        expect(isNumeric(Object(1))).toBe(true);
+        expect(isNumeric(Number(1) + '')).toBe(true);
+        expect(isNumeric(new Number(1) + '')).toBe(true);
+        expect(isNumeric(Object(1) + '')).toBe(true);
         expect(isNumeric(0)).toBe(true);
         expect(isNumeric(11.23)).toBe(true);
         expect(isNumeric('1')).toBe(true);
