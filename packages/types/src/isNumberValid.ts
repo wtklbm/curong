ec46717b494fc2c2b93ac95bac889a1d842f2296
wtklbm <wtklbm@gmail.com>
2024-05-30 00:23:50 +0800

    📃 docs: 更新文档

--- packages/types/src/isNumberValid.ts
+++ packages/types/src/isNumberValid.ts
@@ -1,11 +1,11 @@
 import isUndefined from './isUndefined';
 
 /**
- * 是不是一个有效的数字字符串
+ * 是不是一个有效的数字或数字字符串
  *
  * @param value 要验证的值
  * @param isSpace 是否将空字符串也认为是有效的，默认为 `false`
- * @returns 是则返回 `true`，否则为 `false`
+ * @returns 是则返回 `true`，否则为 `false`。布尔值 (`true` 和 `false`) 也可以转换为数字，所以结果为 `true`
  * @example
  *
  * ```javascript
