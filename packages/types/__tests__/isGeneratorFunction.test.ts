6110cf1ee216a5821f0506f971d3b705e2585bfa
wtklbm <wtklbm@gmail.com>
2024-05-29 18:22:01 +0800

    🧪 test: 添加新的测试

--- packages/types/__tests__/isGeneratorFunction.test.ts
+++ packages/types/__tests__/isGeneratorFunction.test.ts
@@ -13,9 +13,11 @@ describe('@curong/types/isGeneratorFunction', () => {
 
     test('测试2', () => {
         function* test1() {}
-        expect(isGeneratorFunction(test1())).toBe(true);
+        expect(isGeneratorFunction(test1)).toBe(true);
+        expect(isGeneratorFunction(test1())).toBe(false);
 
         async function* test2() { }
-        expect(isGeneratorFunction(test2())).toBe(true);
+        expect(isGeneratorFunction(test2)).toBe(true);
+        expect(isGeneratorFunction(test2())).toBe(false);
     });
 });
