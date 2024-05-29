0895be1c123897ef7a3d8ab960b1bae92316d438
wtklbm <wtklbm@gmail.com>
2024-05-29 18:16:53 +0800

    ✨ feat: 添加 `isNumericString` 方法

---
+++ packages/types/src/isNumericString.ts
@@ -0,0 +1,12 @@
+import isStringHave from './isStringHave';
+
+/**
+ * 是不是一个转换为 `Number` 后不是 `NaN` 的数字字符串
+ *
+ * @param value 要验证的值
+ * @returns 是则返回 `true`，否则为 `false`
+ * @info 字符串 `Infinity` 和 `-Infinity` 的结果为 `true`
+ */
+export default function isNumericString(value: unknown): value is `${number}` {
+    return isStringHave(value) && !isNaN(value as any);
+}
