e5fb3c42ea32f525b54871f450a681ae101f4b43
wtklbm <wtklbm@gmail.com>
2024-05-30 00:23:22 +0800

    ✨ feat: 添加 `isNaN` 参数

--- packages/types/src/isNumberPrimitive.ts
+++ packages/types/src/isNumberPrimitive.ts
@@ -2,8 +2,12 @@
  * 是不是一个基本的数字，即 `number` || `Number()`
  *
  * @param value 要验证的值
+ * @param isNaN 当值为 `NaN` 时是否返回 `true`，默认为 `false`
  * @returns 是则返回 `true`，否则为 `false`
  */
-export default function isNumberPrimitive(value: unknown): value is number {
-    return typeof value === 'number';
+export default function isNumberPrimitive(
+    value: unknown,
+    isNaN: boolean = false
+): value is number {
+    return typeof value === 'number' && (isNaN || !Number.isNaN(value));
 }
