87b821d404b687c3e37d783f1722d38eb6b435f7
wtklbm <wtklbm@gmail.com>
2024-05-30 20:02:44 +0800

    ✨ feat: 添加更多参数

--- packages/types/src/isNumberValid.ts
+++ packages/types/src/isNumberValid.ts
@@ -1,25 +1,37 @@
+import isNumber from './isNumber';
+import isString from './isString';
 import isUndefined from './isUndefined';
 
 /**
- * 是不是一个有效的数字或数字字符串
+ * 是不是一个可以转换为有效的数字的值
  *
  * @param value 要验证的值
- * @param isSpace 是否将空字符串也认为是有效的，默认为 `false`
- * @returns 是则返回 `true`，否则为 `false`。布尔值 (`true` 和 `false`) 也可以转换为数字，所以结果为 `true`
+ * @param isImplicit 在进行数字转换时，是否可以进行隐式转换。默认为 `true`。
+ *  如果允许隐式转换，则布尔值、空字符串 (包括多个空格字符的字符串)、空数组、`null`、`BigInt`、`NaN` 等均可以转换为数字。
+ *  否则，则只判断数字 (非 `BigInt`) 和数字字符串是否可以转换为有效的数字。
+ * @param isAllowNaN 当 `isImplicit` 为 `false` 且 `value` 为 `NaN` 时是否返回 `true`。默认为 `false`
+ * @param isAllowEmptyString 当 `isImplicit` 为 `false` 且 `value` 为经过 `trim` 后长度为 0 的空字符串时是否返回 `true`。默认为 `false`
+ * @returns 是则返回 `true`，否则为 `false`
  * @example
  *
  * ```javascript
- * isNumberValid('0'); // true
- * isNumberValid(' 0 '); // true
+ * isNumberValid(' '); // true
+ * isNumberValid(' ', false); // false
+ * isNumberValid(' ', false, false, true); // true
  * ```
  */
 export default function isNumberValid(
     value: unknown,
-    isSpace: boolean = false
+    isImplicit: boolean = true,
+    isAllowNaN: boolean = false,
+    isAllowEmptyString: boolean = false
 ): boolean {
     return (
-        !isUndefined(value) &&
-        !isNaN(Number(value).valueOf()) &&
-        (isSpace || `${value}`.trim() !== '')
+        (isImplicit
+            ? !isUndefined(value)
+            : isNumber(value, isAllowNaN) ||
+              (isString(value) &&
+                  (isAllowEmptyString || value.trim() !== ''))) &&
+        (isAllowNaN || !isNaN(Number(value).valueOf()))
     );
 }
