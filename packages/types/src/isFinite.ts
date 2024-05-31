15e5d397f47530dc32092a84b0aefb13d6757c56
wtklbm <wtklbm@gmail.com>
2024-06-01 01:23:49 +0800

    🦄 refactor: 删除多余参数和逻辑

--- packages/types/src/isFinite.ts
+++ packages/types/src/isFinite.ts
@@ -1,17 +1,17 @@
 /**
- * 是不是一个不是 `Infinity`、`-Infinity` 或 `NaN`有限数
+ * 是不是一个不是 `Infinity`、`-Infinity` 或 `NaN` 的有限数
  *
+ * `isFinite` 方法在判断时会先将 `value` 隐式转换为 `Number` (`Number(value)`)。
  * 该方法与全局的 `isFinite` 最大的区别就是在尝试进行隐式转换时，如果当前值转换失败，全局的 `isFinite` 方法会抛出异常；
  * 而此方法不会抛出异常，而是直接返回一个布尔值。例如 `isFinite(0n)`，全局的 `isFinite` 会抛出
  * `Uncaught TypeError: Cannot convert a BigInt value to a number` 错误；而此方法会直接返回 `false`。
  *
  * @param value 要验证的值
- * @param isImplicit 是否进行隐式转换，默认为 `true`
  * @returns 是则返回 `true`，否则为 `false`
  */
-export default function (value: unknown, isImplicit: boolean = true): boolean {
+export default function (value: unknown): boolean {
     try {
-        return (isImplicit ? isFinite : Number.isFinite)(value as any);
+        return isFinite(value as any);
     } catch {}
 
     return false;
