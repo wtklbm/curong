68e295ba7ec757661bbde85f5b1b9a8f4030839a
wtklbm <wtklbm@gmail.com>
2024-05-31 19:37:52 +0800

    🧪 test: 添加更多的测试

--- packages/types/__tests__/isIntSafe.test.ts
+++ packages/types/__tests__/isIntSafe.test.ts
@@ -3,13 +3,14 @@ import { isIntSafe } from '../src';
 describe('@curong/types/isIntSafe', () => {
     test('测试1', () => {
         expect(isIntSafe(12.8)).toBe(false);
+        expect(isIntSafe([1])).toBe(false);
+        expect(isIntSafe(Number.MIN_SAFE_INTEGER - 2)).toBe(false);
+        expect(isIntSafe(Number.MAX_SAFE_INTEGER + 2)).toBe(false);
     });
 
     test('测试2', () => {
         expect(isIntSafe(22123412412434)).toBe(true);
-    });
-
-    test('测试3', () => {
-        expect(isIntSafe([1])).toBe(false);
+        expect(isIntSafe(Number.MIN_SAFE_INTEGER)).toBe(true);
+        expect(isIntSafe(Number.MAX_SAFE_INTEGER)).toBe(true);
     });
 });
