172d340a016e7d310f62575187fe625df58fb11d
wtklbm <wtklbm@gmail.com>
2024-06-01 02:12:54 +0800

    📃 docs: 更新文档

--- packages/types/src/isFinite.ts
+++ packages/types/src/isFinite.ts
@@ -1,7 +1,7 @@
 /**
  * 是不是一个不是 `Infinity`、`-Infinity` 或 `NaN` 的有限数
  *
- * `isFinite` 方法在判断时会先将 `value` 隐式转换为 `Number` (`Number(value)`)。
+ * `isFinite` 方法可以判断任意的值，并存在类型隐式转换的行为，它会将 `value` 隐式转换为 `Number` (`Number(value)`)。
  * 该方法与全局的 `isFinite` 最大的区别就是在尝试进行隐式转换时，如果当前值转换失败，全局的 `isFinite` 方法会抛出异常；
  * 而此方法不会抛出异常，而是直接返回一个布尔值。例如 `isFinite(0n)`，全局的 `isFinite` 会抛出
  * `Uncaught TypeError: Cannot convert a BigInt value to a number` 错误；而此方法会直接返回 `false`。
