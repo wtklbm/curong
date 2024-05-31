b41b89f50d9dc6e86cd919974b3f7566111ccbec
wtklbm <wtklbm@gmail.com>
2024-06-01 01:52:50 +0800

    🌈 style: 移动常量

--- packages/types/src/constants/object.ts
+++ packages/types/src/constants/object.ts
@@ -1,9 +1,7 @@
 /** 获取对象的 `key` 的函数数组 */
-const GET_KEYS_FUNCTIONS = [
+export const keysFns = [
     Object.keys,
     Object.getOwnPropertyNames,
     Object.getOwnPropertySymbols,
     Reflect.ownKeys
 ];
-
-export default GET_KEYS_FUNCTIONS;
