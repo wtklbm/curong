3c24deb9eb42d5ad26e99eaf32f39353b55adc6a
wtklbm <wtklbm@gmail.com>
2024-06-01 01:24:03 +0800

    🧪 test: 删除老的测试

--- packages/types/__tests__/isFinite.test.ts
+++ packages/types/__tests__/isFinite.test.ts
@@ -2,25 +2,15 @@ import { isFinite } from '../src';
 
 describe('@curong/types/isFinite', () => {
     test('测试1', () => {
-        // @ts-ignore
-        expect(isFinite(1n)).toBe(false); // `BigInt` 不是 `Number`
+        expect(isFinite(1n)).toBe(false);
         expect(isFinite('中')).toBe(false);
-        expect(isFinite('中', false)).toBe(false);
     });
 
     test('测试2', () => {
         expect(isFinite(NaN)).toBe(false);
-        expect(isFinite(NaN, false)).toBe(false);
-
         expect(isFinite(Number.NaN)).toBe(false);
-        expect(isFinite(Number.NaN, false)).toBe(false);
-        expect(isFinite(Number.NaN, false)).toBe(false);
-
         expect(isFinite(Infinity)).toBe(false);
-        expect(isFinite(Infinity, false)).toBe(false);
-
         expect(isFinite(-Infinity)).toBe(false);
-        expect(isFinite(-Infinity, false)).toBe(false);
     });
 
     test('测试3', () => {
@@ -34,16 +24,9 @@ describe('@curong/types/isFinite', () => {
     test('测试3', () => {
         expect(isFinite(0)).toBe(true);
         expect(isFinite('0')).toBe(true);
-        expect(isFinite('0', false)).toBe(false);
-
         expect(isFinite(-1)).toBe(true);
         expect(isFinite('-1')).toBe(true);
-        expect(isFinite('-1', false)).toBe(false);
-
-        expect(isFinite([1])).toBe(true); // 隐式转换为 1
-        expect(isFinite([1], false)).toBe(false);
-
+        expect(isFinite([1])).toBe(true);
         expect(isFinite([1, 2])).toBe(false);
-        expect(isFinite([1, 2], false)).toBe(false);
     });
 });
