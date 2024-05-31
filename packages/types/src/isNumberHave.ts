d5db0801761b866ccadcda9d30bffb758200c463
wtklbm <wtklbm@gmail.com>
2024-05-31 19:06:35 +0800

    ✨ feat: 更改为新的语义

--- packages/types/src/isNumberHave.ts
+++ packages/types/src/isNumberHave.ts
@@ -1,11 +1,11 @@
-import isNumber from './isNumber';
-
 /**
- * 是不是一个大于 `0` 并且小于或等于 `Number.MAX_VALUE` 的数字
+ * 是不是一个不是 `0` 、`Infinity`、`-Infinity` 或 `NaN` 的数字
+ *
+ * 该方法是 `isNumberPositive` 的别名。
  *
  * @param value 要验证的值
  * @returns 是则返回 `true`，否则为 `false`
  */
 export default function isNumberHave(value: unknown): value is number {
-    return isNumber(value) && value > 0 && value <= Number.MAX_VALUE;
+    return Number.isFinite(value) && value !== 0;
 }
