de73ecc3472cf2ee015524cdecffc32385aa9bdd
wtklbm <wtklbm@gmail.com>
2024-05-31 20:26:50 +0800

    🧪 test: 添加新的测试

--- packages/types/__tests__/isNumberSafe.test.ts
+++ packages/types/__tests__/isNumberSafe.test.ts
@@ -4,7 +4,10 @@ describe('@curong/types/isNumberSafe', () => {
     test('测试1', () => {
         expect(isNumberSafe(Infinity)).toBe(false);
         expect(isNumberSafe(-Infinity)).toBe(false);
-        expect(isNumberSafe(Number.POSITIVE_INFINITY)).toBe(false);
+        expect(isNumberSafe(Number.POSITIVE_INFINITY)).toBe(false); // Infinity
+        expect(isNumberSafe(Number.NEGATIVE_INFINITY)).toBe(false); // -Infinity
+        expect(isNumberSafe(NaN)).toBe(false);
+        expect(isNumberSafe(Number.NaN)).toBe(false);
     });
 
     test('测试2', () => {
@@ -18,7 +21,18 @@ describe('@curong/types/isNumberSafe', () => {
     });
 
     test('测试3', () => {
-        expect(isNumberSafe(NaN)).toBe(false);
-        expect(isNumberSafe(Number.NaN)).toBe(false);
+        expect(isNumberSafe(Number.MAX_VALUE)).toBe(true); // 1.7976931348623157e+308
+        expect(isNumberSafe(Number.MAX_VALUE + 1e300)).toBe(false); // Infinity
+        expect(isNumberSafe(Number.MAX_VALUE - 1e300)).toBe(true); // 1.7976931248623157e+308
+        expect(isNumberSafe(-Number.MAX_VALUE)).toBe(true); // -1.7976931348623157e+308
+        expect(isNumberSafe(-Number.MAX_VALUE - 1e300)).toBe(false); // -Infinity
+        expect(isNumberSafe(-Number.MAX_VALUE + 1e300)).toBe(true); // -1.7976931248623157e+308
+
+        expect(isNumberSafe(Number.MIN_VALUE)).toBe(true); // 5e-324
+        expect(isNumberSafe(Number.MIN_VALUE + 1e300)).toBe(true); // 1e+300
+        expect(isNumberSafe(Number.MIN_VALUE - 1e300)).toBe(true); // -1e+300
+        expect(isNumberSafe(-Number.MIN_VALUE)).toBe(true); // -5e-324
+        expect(isNumberSafe(-Number.MIN_VALUE - 1e300)).toBe(true); // -1e+300
+        expect(isNumberSafe(-Number.MIN_VALUE + 1e300)).toBe(true); // -1e+300
     });
 });
