d8be1f531407e49a6548def24970f49cd251b624
wtklbm <wtklbm@gmail.com>
2024-05-31 22:33:30 +0800

    📃 docs: 更新文档

--- packages/types/src/isNumberArray.ts
+++ packages/types/src/isNumberArray.ts
@@ -5,7 +5,7 @@ import isNumber from './isNumber';
  * 是不是一个长度大于 0 的数组，且每一项的值都是数字或被包装后的数字对象
  *
  * @param value 要验证的值
- * @param isNaN 当某一项的值为 `NaN` 时是否返回 `true`，默认为 `false`
+ * @param isNaN 当某一项或所有项的值为 `NaN` 时是否返回 `true`，默认为 `false`
  * @returns 是则返回 `true`，否则为 `false`
  */
 export default function isNumberArray(
