6b5a5cd47460d5eeb6b24dfc667a0995fdad3acf
wtklbm <wtklbm@gmail.com>
2024-05-31 18:38:53 +0800

    📃 docs: 更新文档

--- packages/types/src/isNumberValid.ts
+++ packages/types/src/isNumberValid.ts
@@ -11,7 +11,9 @@ import isUndefined from './isUndefined';
  *  否则，则只判断数字 (非 `BigInt`) 和数字字符串是否可以转换为有效的数字。
  * @param isAllowNaN 当 `isImplicit` 为 `false` 且 `value` 为 `NaN` 时是否返回 `true`。默认为 `false`
  * @param isAllowEmptyString 当 `isImplicit` 为 `false` 且 `value` 为经过 `trim` 后长度为 0 的空字符串时是否返回 `true`。默认为 `false`
- * @returns 是则返回 `true`，否则为 `false`
+ * @returns 是则返回 `true`，否则为 `false`。
+ *  因为 `Infinity` 和 `-Infinity` 是有效数字，所以在使用该方法时，可能还需要进行额外判断。
+ *  例如在进行算术运算时，需要判断 `isNumberValid(value) && isNumberSafe(value)`。
  * @example
  *
  * ```javascript
