abe23714d6d25ea728afec0cac5458ff17d5cab5
wtklbm <wtklbm@gmail.com>
2024-06-01 21:57:35 +0800

    📃 docs: 更新文档

--- packages/types/src/isInfinityPositive.ts
+++ packages/types/src/isInfinityPositive.ts
@@ -5,6 +5,22 @@
  *
  * @param value 要验证的值
  * @returns 是则返回 `true`，否则为 `false`
+ * @note
+ *
+ * - `Number.POSITIVE_INFINITY` 是一个不可写、不可枚举、不可配置的值
+ *
+ * ### 与数学上的无穷大的区别
+ *
+ * - 任何正值，包括 [`POSITIVE_INFINITY`](https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/Global_Objects/Number/POSITIVE_INFINITY)，乘以 `POSITIVE_INFINITY` 等于 `POSITIVE_INFINITY`
+ * - 任何负值，包括 [`NEGATIVE_INFINITY`](https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/Global_Objects/Number/NEGATIVE_INFINITY)，除以 `POSITIVE_INFINITY` 等于 `NEGATIVE_INFINITY`
+ * - 任何正数除以 `POSITIVE_INFINITY` 都是[正零](https://zh.wikipedia.org/wiki/0) (正如 [IEEE 754](https://zh.wikipedia.org/wiki/IEEE_754) 中所定义的)
+ * - 任何负数除以 `POSITIVE_INFINITY` 都是[负零](https://zh.wikipedia.org/wiki/-0) (正如 IEEE 754 中所定义的)
+ * - 零乘以 `POSITIVE_INFINITY` 等于 [`NaN`](https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/Global_Objects/NaN)
+ * - `NaN` 乘以 `POSITIVE_INFINITY` 等于 `NaN`
+ * - `POSITIVE_INFINITY`，除以任何正值 (除了 `POSITIVE_INFINITY`)都等于 `POSITIVE_INFINITY`
+ * - `POSITIVE_INFINITY`，除以任何负值 (除了 `NEGATIVE_INFINITY`)都等于 `NEGATIVE_INFINITY`
+ * - `POSITIVE_INFINITY`，除以 `NEGATIVE_INFINITY` 或 `POSITIVE_INFINITY`，都等于 `NaN`
+ * - `Number.POSITIVE_INFINITY > x` 对于任何不是 `POSITIVE_INFINITY` 的数字 x 都为真
  */
 export default function isInfinityPositive(value: unknown): value is number {
     return value === Infinity;
