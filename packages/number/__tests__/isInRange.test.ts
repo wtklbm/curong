9e584828fac08798caad9730a35c01b188bb8e33
wtklbm <wtklbm@gmail.com>
2024-06-01 19:27:48 +0800

    🧪 test: 添加新的测试

--- packages/number/__tests__/isInRange.test.ts
+++ packages/number/__tests__/isInRange.test.ts
@@ -1,7 +1,7 @@
 import { isInRange } from '../src';
 
 describe('isInRange', () => {
-    test('value is within the range when range is a single positive number', () => {
+    test('测试1', () => {
         expect(isInRange(5, 10)).toBe(true);
         expect(isInRange(10, 10)).toBe(true);
         expect(isInRange(0, 10)).toBe(true);
@@ -9,7 +9,7 @@ describe('isInRange', () => {
         expect(isInRange(11, 10)).toBe(false);
     });
 
-    test('value is within the range when range is a single negative number', () => {
+    test('测试2', () => {
         expect(isInRange(-5, -10)).toBe(true);
         expect(isInRange(-10, -10)).toBe(true);
         expect(isInRange(0, -10)).toBe(true);
@@ -17,7 +17,7 @@ describe('isInRange', () => {
         expect(isInRange(-11, -10)).toBe(false);
     });
 
-    test('value is within the range when range is an array of two numbers', () => {
+    test('测试3', () => {
         expect(isInRange(5, [1, 10])).toBe(true);
         expect(isInRange(1, [1, 10])).toBe(true);
         expect(isInRange(10, [1, 10])).toBe(true);
@@ -25,7 +25,7 @@ describe('isInRange', () => {
         expect(isInRange(11, [1, 10])).toBe(false);
     });
 
-    test('value is within the range when range is an array with negative numbers', () => {
+    test('测试4', () => {
         expect(isInRange(-5, [-10, -1])).toBe(true);
         expect(isInRange(-10, [-10, -1])).toBe(true);
         expect(isInRange(-1, [-10, -1])).toBe(true);
@@ -33,7 +33,7 @@ describe('isInRange', () => {
         expect(isInRange(-11, [-10, -1])).toBe(false);
     });
 
-    test('range is an invalid array', () => {
+    test('测试5', () => {
         // @ts-ignore
         expect(() => isInRange(5, [10])).toThrow(TypeError);
         // @ts-ignore
@@ -42,9 +42,47 @@ describe('isInRange', () => {
         expect(() => isInRange(5, [])).toThrow(TypeError);
     });
 
-    test('range is an invalid type', () => {
+    test('测试6', () => {
         expect(() => isInRange(5, '10' as any)).toThrow(TypeError);
         expect(() => isInRange(5, null as any)).toThrow(TypeError);
         expect(() => isInRange(5, undefined as any)).toThrow(TypeError);
+    });
+
+    test('测试7', () => {
+        expect(() => isInRange(5, NaN)).toThrow(TypeError);
+        expect(isInRange(5, Infinity)).toBe(true);
+        expect(isInRange(5, -Infinity)).toBe(false);
+
+        expect(() => isInRange(NaN, 10)).toThrow(TypeError);
+        expect(isInRange(Infinity, 10)).toBe(false);
+        expect(isInRange(-Infinity, 10)).toBe(false);
+
+        expect(() => isInRange(NaN, NaN)).toThrow(TypeError);
+        expect(isInRange(Infinity, Infinity)).toBe(true);
+        expect(isInRange(-Infinity, -Infinity)).toBe(true);
+
+        expect(() => isInRange(NaN, [0, 10])).toThrow(TypeError);
+        expect(isInRange(Infinity, [0, 10])).toBe(false);
+        expect(isInRange(-Infinity, [0, 10])).toBe(false);
+
+        expect(() => isInRange(5, [0, NaN])).toThrow(TypeError);
+        expect(isInRange(5, [0, Infinity])).toBe(true);
+        expect(isInRange(5, [0, -Infinity])).toBe(false);
+
+        expect(() => isInRange(NaN, [0, NaN])).toThrow(TypeError);
+        expect(isInRange(Infinity, [0, Infinity])).toBe(true);
+        expect(isInRange(-Infinity, [0, -Infinity])).toBe(true);
+
+        expect(() => isInRange(5, [NaN, 10])).toThrow(TypeError);
+        expect(isInRange(5, [Infinity, 10])).toBe(false);
+        expect(isInRange(5, [-Infinity, 10])).toBe(true);
+
+        expect(() => isInRange(5, [NaN, NaN])).toThrow(TypeError);
+        expect(isInRange(5, [Infinity, Infinity])).toBe(false);
+        expect(isInRange(5, [-Infinity, -Infinity])).toBe(false);
+
+        expect(() => isInRange(NaN, [NaN, NaN])).toThrow(TypeError);
+        expect(isInRange(Infinity, [Infinity, Infinity])).toBe(true);
+        expect(isInRange(-Infinity, [-Infinity, -Infinity])).toBe(true);
     });
 });
