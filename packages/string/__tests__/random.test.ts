4ff62e81c29009213b99210584c0a477525bd722
wtklbm <wtklbm@gmail.com>
2024-06-01 15:55:49 +0800

    🧪 test: 添加新的测试

--- packages/string/__tests__/random.test.ts
+++ packages/string/__tests__/random.test.ts
@@ -3,5 +3,19 @@ import { random } from '../src';
 describe('@curong/string/random', () => {
     test('测试1', () => {
         expect(random()?.length).toBe(15);
+
+        [
+            1,
+            15,
+            65536,
+            65537,
+            65537 * 1.5,
+            65537 * 1.9,
+            65537 * 2,
+            65537 * 2.1
+        ].forEach(v => {
+            v = Math.floor(v);
+            expect(random(v)?.length).toBe(v);
+        });
     });
 });
