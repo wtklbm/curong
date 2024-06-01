03eefa43dc2d3cf36dd4b853914984b826a3f5c6
wtklbm <wtklbm@gmail.com>
2024-06-01 15:55:45 +0800

    ✨ feat: 生成任意长度的随机数

--- packages/string/src/random.ts
+++ packages/string/src/random.ts
@@ -1,3 +1,4 @@
+import { format } from '@curong/term';
 import { isBrowser, isNodejs } from '@curong/types';
 
 /**
@@ -6,17 +7,17 @@ import { isBrowser, isNodejs } from '@curong/types';
  * @param length 要返回的随机数的长度，默认为 15 位 (可安全的转换为无符号整数)
  * @returns 返回一个随机数字符串
  *  - 该方法严重依赖高版本的 `Node.js` (>= v15.0.0)，在低版本上无法生成随机数
- * @throws
- *  - 如果 `length` 的长度大于 `65536`，则会抛出异常
  *  - 如果将随机数字符串转换为无符号整数，则需保证值不能超过 `Number.MAX_SAFE_INTEGER`
+ * @throws 如果 `crypto.getRandomValues` 方法不存在，则会抛出异常
  * @example
  *
  * ```JavaScript
  * // 生成一个长度为 10 的随机数字符串
  * const n = random(10);
  *
- * // 如果想使用随机数数字，可以调用 `parseInt(n)`
- * // NOTE 如果转换为无符号整数，则不能超过 `Number.MAX_SAFE_INTEGER` 的值，即：`9007199254740991`
+ * // 如果想使用随机数数字，可以调用 `parseInt(n)` 或 `Number(n)`
+ * // 如果要转换的数字的值超过了 `Number.MAX_SAFE_INTEGER`，则可能会导出不安全
+ * // 为了实现安全的存储和计算，可以使用 `BigInt` 进行转换 (`BigInt(n)`)
  * console.log(n);
  * ```
  *
@@ -34,26 +35,36 @@ import { isBrowser, isNodejs } from '@curong/types';
  * JS 中的 `Math.random()` 是一个伪随机数生成器函数，它是不安全的。为了尽可能的保证安全，
  * 就需要 `Crypto` 对象，并调用其 `getRandomValues` 方法来获得一个相对安全的随机数。
  */
-export default function random(length: number = 15): string | null {
-    let crypto: Crypto | null = null;
+export default function random(length: number = 15): string {
+    let crypto: Crypto;
 
-    try {
-        if (isBrowser()) {
-            crypto =
-                window.crypto ??
-                (<any>window).webkitCrypto ??
-                (<any>window).mozCrypto ??
-                (<any>window).oCrypto ??
-                (<any>window).msCrypto;
-        } else if (isNodejs()) {
-            crypto = globalThis.crypto ?? require('crypto').webcrypto;
-        }
+    if (isBrowser()) {
+        crypto =
+            window.crypto ??
+            (window as any).webkitCrypto ??
+            (window as any).mozCrypto ??
+            (window as any).oCrypto ??
+            (window as any).msCrypto;
+    } else if (isNodejs()) {
+        crypto = globalThis.crypto ?? require('crypto').webcrypto;
+    } else {
+        throw format({
+            name: 'random',
+            message: '在浏览器或 Node.js 环境下没有检测到 crypto 对象'
+        });
+    }
 
-        return crypto!
-            .getRandomValues(new Uint8Array(length))
-            .join('')
-            .slice(-length);
-    } catch {}
+    const creator = (length: number) =>
+        crypto.getRandomValues(new Uint8Array(length)).join('');
 
-    return null;
+    // `getRandomValues` 方法所支持的最大长度，超过此长度就会报错
+    const MAX_LENGTH = 65536;
+
+    return (
+        length < MAX_LENGTH
+            ? creator(length)
+            : Array.from({ length: Math.ceil(length / MAX_LENGTH) }, () =>
+                  creator(MAX_LENGTH)
+              ).join('')
+    ).slice(-length);
 }
