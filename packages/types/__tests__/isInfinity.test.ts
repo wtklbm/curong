2c6b4e61967a4a0e7d671749c2a2a184e2db75a4
wtklbm <wtklbm@gmail.com>
2024-06-01 21:24:49 +0800

    🧪 test: 添加新的测试

--- packages/types/__tests__/isInfinity.test.ts
+++ packages/types/__tests__/isInfinity.test.ts
@@ -3,7 +3,10 @@ import { isInfinity } from '../src';
 describe('@curong/types/isInfinity', () => {
     test('测试1', () => {
         expect(isInfinity(1.7976931348623157e103088)).toBe(true);
-        expect(isInfinity(1.7976931348623157e103089)).toBe(true);
+        expect(isInfinity(-1.7976931348623157e103089)).toBe(true);
         expect(isInfinity(Infinity)).toBe(true);
+        expect(isInfinity(-Infinity)).toBe(true);
+        expect(isInfinity(5/0)).toBe(true);
+        expect(isInfinity(-5/0)).toBe(true);
     });
 });
