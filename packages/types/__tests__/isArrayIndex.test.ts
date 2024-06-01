73f9a1375d9ae1b83beb8fd55cd84fdb5d08b5d2
wtklbm <wtklbm@gmail.com>
2024-06-01 17:19:25 +0800

    🧪 test: 添加新的测试

--- packages/types/__tests__/isArrayIndex.test.ts
+++ packages/types/__tests__/isArrayIndex.test.ts
@@ -1,4 +1,4 @@
-import { isArrayIndex, MAX_ARRAY_LENGTH } from '../src';
+import { isArrayIndex } from '../src';
 
 describe('@curong/types/isArrayIndex', () => {
     test('测试1', () => {
@@ -7,11 +7,12 @@ describe('@curong/types/isArrayIndex', () => {
 
     test('测试2', () => {
         expect(isArrayIndex(0)).toBe(true);
-        expect(isArrayIndex(MAX_ARRAY_LENGTH)).toBe(true);
+        expect(isArrayIndex(2 ** 32 - 1)).toBe(true);
     });
 
     test('测试3', () => {
-        expect(isArrayIndex(MAX_ARRAY_LENGTH + 1)).toBe(false);
+        expect(isArrayIndex(2 ** 32)).toBe(false);
+        expect(isArrayIndex(2 ** 32 + 1)).toBe(false);
         expect(isArrayIndex([1])).toBe(false);
     });
 });
