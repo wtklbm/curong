6b52685c1c0022c254a63c6444967a1d40fea225
wtklbm <wtklbm@gmail.com>
2024-06-19 21:41:08 +0800

    📃 docs: 更新文档

--- packages/types/src/buffer/isFloat64Array.ts
+++ packages/types/src/buffer/isFloat64Array.ts
@@ -5,7 +5,7 @@ import { getTagEqual } from '..';
  *
  * - 使用 `IEEE 754` 浮点格式存储数字，`Float64Array` 将 `52` 位用于尾数，以及 `11` 位用于指数
  * - `JavaScript` 数字默认使用双精度浮点格式，这与 `Float64Array` 相同
- * - 在 `Javascript` 中能够表示的最大值是 `1.7976931348623157e308`，通常简写为 `1.8e308`
+ * - 在 `Javascript` 中能够表示的最大值是 `1.7976931348623157e308`，网络上通常称为 `1.8e308`
  *
  * @param value 要验证的值
  * @returns 是则返回 `true`，否则为 `false`
