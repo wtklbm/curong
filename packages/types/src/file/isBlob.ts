322c56d88251a7ff04906c64af05ca9798a313a5
wtklbm <wtklbm@gmail.com>
2024-06-20 00:08:48 +0800

    📃 docs: 更新文档

--- packages/types/src/file/isBlob.ts
+++ packages/types/src/file/isBlob.ts
@@ -5,7 +5,8 @@ import { getTagEqual } from '..';
  *
  * @param value 要验证的值
  * @returns 是则返回 `true`，否则为 `false`
- * @info `Blob` 是不可变的原始数据的类似文件的对象，
+ *
+ * `Blob` 是不可变的原始数据的类似文件的对象，
  * 可以被读取为文本或二进制数据，也可以转换为 `ReadableStream`
  */
 export default function isBlob(value: unknown): value is Blob {
