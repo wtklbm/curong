f188791d918a278c053cff4ecebd7a9e7980d87a
wtklbm <wtklbm@gmail.com>
2024-06-19 21:40:49 +0800

    📃 docs: 更新文档

--- packages/types/src/buffer/isArrayBufferView.ts
+++ packages/types/src/buffer/isArrayBufferView.ts
@@ -20,7 +20,8 @@ import isTypedArray from './isTypedArray';
  *
  * @note
  *
- * `ArrayBuffer` 是一个字节数组，通常在其他语言中称为 `byte array` (字节数组)。`ArrayBuffer` 中的内容不能直接被操作，而是要通过 [TypedArray](https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/Global_Objects/TypedArray) (类型化数组对象) 或 [`DataView`](https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/Global_Objects/DataView) 对象来操作，它们会将缓冲区中的数据表示为特定的格式，并通过这些格式来读写缓冲区的内容。
+ * `ArrayBuffer` 是一个字节数组，通常在其他语言中称为 `byte array` (字节数组)。
+ * `ArrayBuffer` 中的内容不能直接被操作，而是要通过 [TypedArray](https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/Global_Objects/TypedArray) (类型化数组对象) 或 [`DataView`](https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/Global_Objects/DataView) 对象来操作，它们会将缓冲区中的数据表示为特定的格式，并通过这些格式来读写缓冲区的内容。
  */
 export default function isArrayBufferView(
     value: any
