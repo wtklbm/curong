b9ee966930623589b30fd46d5317ebe87af02bf2
wtklbm <wtklbm@gmail.com>
2024-06-20 00:16:39 +0800

    📃 docs: 更新文档

--- packages/types/src/function/isClass.ts
+++ packages/types/src/function/isClass.ts
@@ -6,12 +6,13 @@ import type { Class } from './types';
  *
  * @param value 要验证的值
  * @returns 是则返回 `true`，否则为 `false`
+ *
+ * - 类不能直接执行，必须使用 `new` 关键字
+ * - 类中无法访问 `caller`，`callee` 和 `arguments` 属性
  */
 export default function isClass<T = unknown>(
     value: unknown
 ): value is Class<T> {
-    // 类不能直接执行，必须使用 `new` 关键字，
-    // 类中无法访问 `caller`，`callee` 和 `arguments` 属性
     return (
         isFunction(value) &&
         Function.prototype.toString.call(value).startsWith('class ')
