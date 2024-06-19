fd7f734b06f814e93e0c02fc64c17f51d83d78db
wtklbm <wtklbm@gmail.com>
2024-06-20 00:19:11 +0800

    📃 docs: 更新文档

--- packages/types/src/generator/isGenerator.ts
+++ packages/types/src/generator/isGenerator.ts
@@ -4,13 +4,10 @@ import isSyncGenerator from './isSyncGenerator';
 /**
  * 是不是一个同步或异步的 `Generator`
  *
- * 每一个 `Generator` 上，都会有三个方法:
- *  - `next`
- *  - `throw`
- *  - `return`
- *
  * @param value 要验证的值
  * @returns 是则返回 `true`，否则为 `false`
+ *
+ * - 每一个 `Generator` 上，都会有三个方法: `next`、`throw`、`return`
  */
 export default function isGenerator<T = unknown, R = unknown, N = unknown>(
     value: any
