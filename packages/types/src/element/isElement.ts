881dd545ecd1c10ed4b183518ba3eba23c62d47c
wtklbm <wtklbm@gmail.com>
2024-06-19 21:55:57 +0800

    📃 docs: 更新文档

--- packages/types/src/element/isElement.ts
+++ packages/types/src/element/isElement.ts
@@ -3,9 +3,9 @@
  *
  * @param value 要验证的值
  * @returns 是则返回 `true`，否则为 `false`
- * @info
- * `Element` 是最通用的基类，`Document` 中的所有对象都继承自该基类。
- * `Element` 里面定义了一些公共的方法和属性。
+ *
+ * - `Element` 是最通用的基类，`Document` 中的所有对象都继承自该基类
+ * - `Element` 里面定义了一些公共的方法和属性
  *
  */
 export default function isElement(value: unknown): value is Element {
