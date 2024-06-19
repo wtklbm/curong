9c7b4fe6bd7d9b484574169df1c1a7c9298f90de
wtklbm <wtklbm@gmail.com>
2024-06-19 21:56:04 +0800

    📃 docs: 更新文档

--- packages/types/src/element/isHTMLElement.ts
+++ packages/types/src/element/isHTMLElement.ts
@@ -1,20 +1,20 @@
 /**
  * 是不是一个 `HTMLElement`
  *
+ * @param value 要验证的值
+ * @returns 是则返回 `true`，否则为 `false`
+ *
  * 每一个 `HTMLElement` 具有以下特征：
  *  - `nodeType` 等于 1
  *  - `nodeName` 是一个字符串
  *  - 包含 `attributes`、`innerHTML`、`nodeValue`、`ownerDocument`、`style` 等属性
  *  - 继承自`<value>.ownerDocument.defaultView.HTMLElement`
- *
- * @param value 要验证的值
- * @returns 是则返回 `true`，否则为 `false`
  */
 export default function isHTMLElement(value: unknown): value is HTMLElement {
     try {
         return (
             value instanceof
-            ((value as HTMLElement).ownerDocument.defaultView || window)
+            ((value as HTMLElement).ownerDocument.defaultView ?? window)
                 .HTMLElement
         );
     } catch {}
