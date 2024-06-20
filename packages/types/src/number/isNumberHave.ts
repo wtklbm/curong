fe93e78c070ba6a8b6d54739f42018a15ca18872
wtklbm <wtklbm@gmail.com>
2024-06-20 11:41:02 +0800

    📃 docs: 更新文档

--- packages/types/src/number/isNumberHave.ts
+++ packages/types/src/number/isNumberHave.ts
@@ -1,7 +1,7 @@
 import isFinite from './isFinite';
 
 /**
- * 是不是一个不是 `0` 、`Infinity`、`-Infinity` 或 `NaN` 的数字
+ * 是不是一个不是 `0`、`Infinity`、`-Infinity` 或 `NaN` 的数字
  *
  * @param value 要验证的值
  * @returns 是则返回 `true`，否则为 `false`
