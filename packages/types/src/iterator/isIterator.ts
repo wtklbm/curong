e39f642e4373f7e175540d4e7c8c98ab295b9f7d
wtklbm <wtklbm@gmail.com>
2024-06-20 16:07:33 +0800

    📃 docs: 更新文档

--- packages/types/src/iterator/isIterator.ts
+++ packages/types/src/iterator/isIterator.ts
@@ -5,7 +5,7 @@ import { isFunction, isNullOrUndefined } from '..';
  *
  * @param value 要验证的值
  * @returns 是则返回 `true`，否则为 `false`
- * @info 在这里，只要迭代器有 `next` 方法，则返回 `true`
+ * @note 在这里，只要迭代器有 `next` 方法，则返回 `true`
  */
 export default function isIterator<T = unknown, R = unknown, N = undefined>(
     value: any
