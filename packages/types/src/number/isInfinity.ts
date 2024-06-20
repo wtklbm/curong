7c932529aec2249b11c78b054c68dc6832773337
wtklbm <wtklbm@gmail.com>
2024-06-20 11:22:42 +0800

    📃 docs: 更新文档

--- packages/types/src/number/isInfinity.ts
+++ packages/types/src/number/isInfinity.ts
@@ -4,11 +4,12 @@ import isInfinityPositive from './isInfinityPositive';
 /**
  * 是不是一个无穷大的数
  *
- * - `Infinity`: 等价于  `Number.POSITIVE_INFINITY`，是超出 `1.7976931348623157E+103088` 的数值
- * - `-Infinity`: 等价于  `Number.NEGATIVE_INFINITY`，是超出 `-1.7976931348623157E+103088` 的数值
- *
  * @param value 要验证的值
  * @returns 是则返回 `true`，否则为 `false`
+ * @note
+ *
+ * - `Infinity`: 等价于  `Number.POSITIVE_INFINITY`，是超出 `1.7976931348623157e308` 的数值
+ * - `-Infinity`: 等价于  `Number.NEGATIVE_INFINITY`，是超出 `-1.7976931348623157e308` 的数值
  */
 export default function isInfinity(value: unknown): value is number {
     return isInfinityPositive(value) || isInfinityNegative(value);
