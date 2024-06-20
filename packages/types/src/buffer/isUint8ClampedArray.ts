5c1958b5cbe4f98f3f8aa99b6c955b2e9d9a1d26
wtklbm <wtklbm@gmail.com>
2024-06-20 16:06:08 +0800

    📃 docs: 更新文档

--- packages/types/src/buffer/isUint8ClampedArray.ts
+++ packages/types/src/buffer/isUint8ClampedArray.ts
@@ -3,10 +3,12 @@ import { getTagEqual } from '..';
 /**
  * 是不是一个 `Uint8ClampedArray`，每一项占一个字节，值为 `0 - 2^8-1`，即 `0 - 255` (一定在 `0` 到 `255` 之间)
  *
- * `Uint8ClampedArray` 像 `Uint8Array` 一样以二进制形式存储数字，但是当存储超出范围的数字时，它会将数字钳制 (`clamp`) 到 `0` 到 `255` 的范围内，而不是截断最高有效位。
- *
  * @param value 要验证的值
  * @returns 是则返回 `true`，否则为 `false`
+ * @note
+ *
+ * `Uint8ClampedArray` 像 `Uint8Array` 一样以二进制形式存储数字，但是当存储超出范围的数字时，
+ * 它会将数字钳制 (`clamp`) 到 `0` 到 `255` 的范围内，而不是截断最高有效位。
  */
 export default function isUint8ClampedArray(
     value: any
