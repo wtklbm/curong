2ca888e6da75c57c863456bef71e782ab25be1b5
wtklbm <wtklbm@gmail.com>
2024-06-20 15:12:06 +0800

    🧪 test: 添加 `isNativeFunction` 方法的测试

---
+++ packages/types/src/function/__tests__/isNativeFunction.test.ts
@@ -0,0 +1,41 @@
+import { isNativeFunction } from '..';
+
+describe('@curong/types/isNativeFunction', () => {
+    test('测试1', () => {
+        expect(isNativeFunction(null)).toBe(false);
+        expect(isNativeFunction(undefined)).toBe(false);
+        expect(isNativeFunction([])).toBe(false);
+        expect(isNativeFunction(1)).toBe(false);
+        expect(isNativeFunction('1')).toBe(false);
+        expect(isNativeFunction(true)).toBe(false);
+    });
+
+    test('测试2', () => {
+        expect(isNativeFunction(new Function('xxx'))).toBe(false);
+        expect(isNativeFunction(function a() {})).toBe(false);
+        expect(isNativeFunction(async function a() {})).toBe(false);
+        expect(isNativeFunction(() => {})).toBe(false);
+        expect(isNativeFunction(async () => {})).toBe(false);
+        expect(isNativeFunction(function (a: any) {})).toBe(false);
+        expect(isNativeFunction(async function fn() {})).toBe(false);
+        expect(isNativeFunction(function* () {})).toBe(false);
+        expect(isNativeFunction(async function* () {})).toBe(false);
+    });
+
+    test('测试3', () => {
+        expect(isNativeFunction(parseInt)).toBe(true);
+        expect(isNativeFunction(Promise.all)).toBe(true);
+        expect(isNativeFunction(''.valueOf)).toBe(true);
+        expect(isNativeFunction(''.toString)).toBe(true);
+    });
+
+    test('测试4', () => {
+        expect(isNativeFunction({}.toString)).toBe(true);
+        expect(isNativeFunction(Object.prototype.toString)).toBe(true);
+        expect(isNativeFunction(Object.prototype.toString.call)).toBe(true);
+
+        // @ts-ignore
+        Object.prototype.toString = null;
+        expect(isNativeFunction({})).toBe(false);
+    });
+});
