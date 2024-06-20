35f7a75ce2fba0aca11f9e4c503564112063e134
wtklbm <wtklbm@gmail.com>
2024-06-20 16:12:03 +0800

    📃 docs: 更新文档

--- packages/regexp/src/source/chkPunctuation.ts
+++ packages/regexp/src/source/chkPunctuation.ts
@@ -6,7 +6,7 @@
  *  - [中文竖排标点](http://www.unicode.org/charts/PDF/UFE10.pdf)
  *  - [CJK兼容符号（竖排变体、下划线、顿号）](http://www.unicode.org/charts/PDF/UFE30.pdf)
  *
- * @info 该正则是自己写的，不是自动生成的。参考的原始代码部分：`\\p{InCJK_Symbols_and_Punctuation}`。
+ * @note 该正则是自己写的，不是自动生成的。参考的原始代码部分：`\\p{InCJK_Symbols_and_Punctuation}`。
  */
 const chkPunctuation =
     '[\u2000-\u206F\u2E00-\u2E7F\u3000-\u303F\uFF00-\uFFEF\uFE30-\uFE4F\uFE10-\uFE1F]';
