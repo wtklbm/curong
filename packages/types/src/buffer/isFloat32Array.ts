aab1f945693fdd6d810e0b9939831bc141b03556
wtklbm <wtklbm@gmail.com>
2024-06-20 16:05:54 +0800

    📃 docs: 更新文档

--- packages/types/src/buffer/isFloat32Array.ts
+++ packages/types/src/buffer/isFloat32Array.ts
@@ -3,11 +3,12 @@ import { getTagEqual } from '..';
 /**
  * 是不是一个 `Float32Array`，每一项占四个字节，值为 `-3.4028234663852886e38 - 3.4028234663852886e38`，并且 `1.2e-38` 是最小的正数
  *
- * - 使用 `IEEE 754` 浮点格式存储数字，`Float32Array` 将 `23` 位用于尾数，以及 `8` 位用于指数
- * - 在 `Javascript` 中能够表示的最大值是 `3.4028234663852886e38`，网络上通常称为 `3.4e38`
- *
  * @param value 要验证的值
  * @returns 是则返回 `true`，否则为 `false`
+ * @note
+ *
+ * - 使用 `IEEE 754` 浮点格式存储数字，`Float32Array` 将 `23` 位用于尾数，以及 `8` 位用于指数
+ * - 在 `Javascript` 中能够表示的最大值是 `3.4028234663852886e38`，网络上通常称为 `3.4e38`
  */
 export default function isFloat32Array(value: unknown): value is Float32Array {
     return getTagEqual(value, 'Float32Array');
