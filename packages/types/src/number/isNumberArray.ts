1143017c38cb2fbe21e9d1591fe85e3d3e05efa7
wtklbm <wtklbm@gmail.com>
2024-06-20 11:40:56 +0800

    📃 docs: 更新文档

--- packages/types/src/number/isNumberArray.ts
+++ packages/types/src/number/isNumberArray.ts
@@ -3,7 +3,7 @@ import { isArrayHave } from '..';
 import isNumber from './isNumber';
 
 /**
- * 是不是一个长度大于 0 的数组，且每一项的值都是数字或被包装后的数字对象
+ * 是不是一个长度大于 `0` 的数组，且每一项的值都是数字或被包装后的数字对象
  *
  * @param value 要验证的值
  * @param isAllowNaN 当某一项或所有项的值为 `NaN` 时是否返回 `true`，默认为 `false`
