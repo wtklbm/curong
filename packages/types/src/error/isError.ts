5d5a7e6b9ad7f88602724858473a1af5f83505d9
wtklbm <wtklbm@gmail.com>
2024-06-20 16:06:54 +0800

    📃 docs: 更新文档

--- packages/types/src/error/isError.ts
+++ packages/types/src/error/isError.ts
@@ -3,9 +3,9 @@ import { getTagEqual } from '..';
 /**
  * 是不是一个 `Error` 对象
  *
- * @info 如果想验证任意的错误对象，请使用 `isAnyError` 方法
  * @param value 要验证的值
  * @returns 是则返回 `true`，否则为 `false`
+ * @note 如果想验证任意的错误对象，请使用 `isAnyError` 方法
  */
 export default function isError(value: unknown): value is Error {
     return (
