12a7883813a62098bfd54e72cfd0bb955f9bbb1f
wtklbm <wtklbm@gmail.com>
2024-06-20 11:26:35 +0800

    📃 docs: 更新文档

--- packages/types/src/number/isNumberSafe.ts
+++ packages/types/src/number/isNumberSafe.ts
@@ -12,7 +12,7 @@ import isFinite from './isFinite';
  * @note
  *
  *  - 不能是 `Infinity`、`-Infinity` 或 `NaN`
- *  - 必须是 `JavaScript` 中能表示的数字，即 `Number.MAX_VALUE ~ -Number.MAX_VALUE` (`1.7976931348623157e+308 ~ -1.7976931348623157e+308`)
+ *  - 必须是 `JavaScript` 中能表示的数字，即 `Number.MAX_VALUE ~ -Number.MAX_VALUE` (`1.7976931348623157e308 ~ -1.7976931348623157e308`)
  *  - 需要注意的是：`Number.MIN_VALUE` 是一个最小正数，是一个大于 0 的数
  *
  * 一个数值可以容纳的最大值是 `2^1024- 1` (指数为 `1023`，尾数为基于二进制的 `0.1111…`)，可以通过 [`Number.MAX_VALUE`](https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/Global_Objects/Number/MAX_VALUE) 获得。超过这个值的数会被替换为特殊的数值常量 [`Infinity`](https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/Global_Objects/Infinity)。
