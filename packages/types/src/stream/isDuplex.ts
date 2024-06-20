8e416e0039a2afdaf2922feda226203cb65a0671
wtklbm <wtklbm@gmail.com>
2024-06-20 12:22:40 +0800

    📃 docs: 更新文档

--- packages/types/src/stream/isDuplex.ts
+++ packages/types/src/stream/isDuplex.ts
@@ -4,11 +4,11 @@ import isReadable from './isReadable';
 import isWritable from './isWritable';
 
 /**
- * 是不是一个 `Duplex`
+ * 是不是一个双工流对象
  *
  * @param value 要验证的值
- * @returns 是则返回 `true`，否则为 `false`
- * @info `Transform` 也是双工流，所以 `new Transform()` 返回 `true`
+ * @returns 是则返回 `true`，否则为 `false`。
+ * 因为 `Transform` 也是双工流，所以 `new Transform()` 返回 `true`。
  */
 export default function isDuplex(value: unknown): value is Duplex {
     return isWritable(value) && isReadable(value);
