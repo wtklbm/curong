e0c82c436ef423f7c6e72f8234e8866283720ae0
wtklbm <wtklbm@gmail.com>
2024-06-20 21:43:06 +0800

    🦄 refactor: 提取变量

--- packages/os/src/platform.ts
+++ packages/os/src/platform.ts
@@ -1,13 +1,15 @@
 import { release, type } from 'os';
 
+const osType = type();
+
 /** 是不是 `Windows` 系统平台 */
-export const isWindows = type() === 'Windows_NT';
+export const isWindows = osType === 'Windows_NT';
 
 /** 是不是 `macOS` 系统平台 */
-export const isMacOS = type() === 'Darwin';
+export const isMacOS = osType === 'Darwin';
 
 /** 是不是 `Linux` 系统平台 */
-export const isLinux = type() === 'Linux';
+export const isLinux = osType === 'Linux';
 
 /** 是不是 `Windows` 系统平台上的 `Linux` 子系统 */
-export const isWSL = type() === 'Linux' && release().endsWith('Microsoft');
+export const isWSL = isLinux && release().endsWith('Microsoft');
