67d645cf0e6f2a90f9289307ec5449ab3ff104dc
wtklbm <wtklbm@gmail.com>
2024-06-20 11:41:07 +0800

    📃 docs: 更新文档

--- packages/types/src/number/int/isInt.ts
+++ packages/types/src/number/int/isInt.ts
@@ -1,11 +1,6 @@
 /**
  * 是不是一个整数
  *
- * 该方法是 {@link Number.isInteger} 的别名。
- *
- * 由于 `JavaScript` 是弱类型语言，所以数字 `1.0` 当做参数传递给函数后会变为 `1`，
- * 并且 `Number.isInteger(1.0)` 的值为 `true`，所以 `1.0` 也可以当做是数组的下标索引。
- *
  * @param value 要验证的值。包含正整数和负整数，也包括安全的整数和不安全的整数。但不包含 {@link NaN} 和正负 {@link Infinity}
  * @returns 是则返回 `true`，否则为 `false`。
  * @polyfill
@@ -17,6 +12,13 @@
  *       Math.floor(value) === value;
  * };
  * ```
+ *
+ * @note
+ *
+ * 该方法是 {@link Number.isInteger} 的别名。
+ *
+ * 由于 `JavaScript` 是弱类型语言，所以数字 `1.0` 当做参数传递给函数后会变为 `1`，
+ * 并且 `Number.isInteger(1.0)` 的值为 `true`，所以 `1.0` 也可以当做是数组的下标索引。
  */
 export default function isInt(value: unknown): value is number {
     return Number.isInteger(value);
