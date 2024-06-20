fa159ffde7aa50ad006499b053e9bab48162d0c8
wtklbm <wtklbm@gmail.com>
2024-06-20 11:41:11 +0800

    📃 docs: 更新文档

--- packages/types/src/number/int/isIntEven.ts
+++ packages/types/src/number/int/isIntEven.ts
@@ -1,7 +1,7 @@
 import isInt from './isInt';
 
 /**
- * 是不是一个偶数，即取模后等于 0 的整数
+ * 是不是一个偶数，即取模后等于 `0` 的整数
  *
  * @param value 要验证的值。包含正整数和负整数，也包括安全的整数和不安全的整数，但 `NaN` 和正负 `Infinity` 不是整数
  * @returns 是则返回 `true`，否则为 `false`
