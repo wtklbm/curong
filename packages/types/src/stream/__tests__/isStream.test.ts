fa9eb5b61392108df4d4ba1e2b1d262eb4125dab
wtklbm <wtklbm@gmail.com>
2024-06-20 12:26:13 +0800

    🧪 test: 添加新的测试

--- packages/types/src/stream/__tests__/isStream.test.ts
+++ packages/types/src/stream/__tests__/isStream.test.ts
@@ -1,4 +1,4 @@
-import { Stream } from 'stream';
+import { Duplex, Readable, Stream, Transform, Writable } from 'stream';
 
 import { isStream } from '..';
 
@@ -9,5 +9,9 @@ describe('@curong/types/isStream', () => {
 
     test('测试2', () => {
         expect(isStream(new Stream())).toBe(true);
+        expect(isStream(new Duplex())).toBe(true);
+        expect(isStream(new Transform())).toBe(true);
+        expect(isStream(new Writable())).toBe(true);
+        expect(isStream(new Readable())).toBe(true);
     });
 });
