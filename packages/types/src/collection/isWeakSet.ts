57eb8e30aac28312359b1275ca1c40bf94beebbb
wtklbm <wtklbm@gmail.com>
2024-06-20 16:06:28 +0800

    📃 docs: 更新文档

--- packages/types/src/collection/isWeakSet.ts
+++ packages/types/src/collection/isWeakSet.ts
@@ -5,6 +5,7 @@ import { getTagEqual } from '..';
  *
  * @param value 要验证的值
  * @returns 是则返回 `true`，否则为 `false`
+ * @note
  *
  * `WeakSet` 对象是一些只保存对象值的集合，集合中对象的引用为弱引用(在没有其他引用存在时垃圾回收能正确进行)，并且其中的每个对象值都只能出现一次。
  * 如果传入一个可迭代对象作为参数, 则该对象的所有迭代值都会被自动添加进生成的 `WeakSet` 对象中。其中 `null` 被认为是 `undefined`。
