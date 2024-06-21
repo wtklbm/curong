fc3e703f4705274bd7b3b4d853fbf8ecb0ce15d1
wtklbm <wtklbm@gmail.com>
2024-06-21 15:05:50 +0800

    📃 docs: 更新文档

--- packages/types/src/type/getTagEqual.ts
+++ packages/types/src/type/getTagEqual.ts
@@ -3,8 +3,8 @@ import getTag from './getTag';
 /**
  * 判断原型上的类型标记是否等于某个值
  *
- * @param value 要获取类型的数据
- * @param tag 要验证的类型标记
+ * @param value 要验证的值
+ * @param tag 要比较的类型标记
  * @returns 是则返回 `true`，否则为 `false`
  */
 export default function getTagEqual<T>(
