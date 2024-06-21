a2aec40c26680be776b4abf0d77c7bb9c8c894ca
wtklbm <wtklbm@gmail.com>
2024-06-21 15:15:50 +0800

    🦄 refactor: 使用公共函数

--- packages/types/src/bigint/isBigIntPrimitive.ts
+++ packages/types/src/bigint/isBigIntPrimitive.ts
@@ -1,3 +1,5 @@
+import typeofEqual from '../type/typeofEqual';
+
 /**
  * 是不是一个基本的大数，即 `bigint`
  *
@@ -5,5 +7,5 @@
  * @returns 是则返回 `true`，否则为 `false`
  */
 export default function isBigIntPrimitive(value: unknown): value is bigint {
-    return typeof value === 'bigint';
+    return typeofEqual(value, 'bigint');
 }
