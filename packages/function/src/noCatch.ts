2203e08476f3f4626d98f863ca069579262c8684
wtklbm <wtklbm@gmail.com>
2024-06-25 21:18:30 +0800

    🦄 refactor: 使用 `unknown` 代替 `any`

--- packages/function/src/noCatch.ts
+++ packages/function/src/noCatch.ts
@@ -12,8 +12,8 @@
  * ```
  */
 export default async function noCatch<T = unknown>(
-    fn: (...args: any[]) => Promise<T> | T,
-    ...args: any[]
+    fn: (...args: unknown[]) => Promise<T> | T,
+    ...args: unknown[]
 ): Promise<void | T> {
     try {
         return Promise.resolve(fn(...args)).catch(() => {});
