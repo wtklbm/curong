6956e9dd2345140b30faed8271268d3ac073c47e
wtklbm <wtklbm@gmail.com>
2024-06-25 19:20:05 +0800

    🦄 refactor: 更新类型声明

--- packages/object/src/types/constructor.ts
+++ packages/object/src/types/constructor.ts
@@ -1,10 +1,14 @@
 /** 未知的构造函数的类型 */
-export type UnknownConstructor<T extends {} = {}> = new (...args: any[]) => T;
+export type UnknownConstructor<
+    T extends {} = {},
+    A extends unknown[] = unknown[]
+> = new (...args: A) => T;
 
 /** 未知的构造函数的类型 */
-export type UnknownConstructorFunction<T extends {} = {}> = (
-    ...args: any[]
-) => T;
+export type UnknownConstructorFunction<
+    T extends {} = {},
+    A extends unknown[] = unknown[]
+> = (...args: A) => T;
 
 /** 类构造函数的类型 */
 export type ConstructorLike<T = unknown> = { readonly prototype: T };
