5b60ba55cdd52ae504936c0066d6031c9412bf6e
wtklbm <wtklbm@gmail.com>
2024-06-25 21:19:49 +0800

    🦄 refactor: 使用 `unknown` 代替 `any`

--- packages/types/src/array/isArrayFilled.ts
+++ packages/types/src/array/isArrayFilled.ts
@@ -6,7 +6,7 @@ import isArray from './isArray';
  * @param value 要验证的值
  * @returns 是则返回 `true`，否则为 `false`
  */
-export default function isArrayFilled<T extends any[]>(
+export default function isArrayFilled<T extends unknown[]>(
     value: unknown
 ): value is T {
     return isArray(value) && value.length > 0;
