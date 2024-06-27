2b59d6d892744410220a4afac004731975d4606a
wtklbm <wtklbm@gmail.com>
2024-06-27 20:54:51 +0800

    🦄 refactor: 更新类型声明

--- packages/types/src/function/isFunctionFilled.ts
+++ packages/types/src/function/isFunctionFilled.ts
@@ -7,8 +7,9 @@ import type { Function } from './types';
  * @param value 要验证的值
  * @returns 是则返回 `true`，否则为 `false`
  */
-export default function isFunctionFilled<T = unknown>(
-    value: unknown
-): value is Function<T> {
+export default function isFunctionFilled<
+    R = unknown,
+    A extends unknown[] = unknown[]
+>(value: unknown): value is Function<R, A> {
     return isFunction(value) && value.length > 0;
 }
