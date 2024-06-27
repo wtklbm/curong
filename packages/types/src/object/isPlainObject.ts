33df4bef9bc4f037f23cd1c15f2d5760f903501a
wtklbm <wtklbm@gmail.com>
2024-06-27 20:55:59 +0800

    🦄 refactor: 更新类型声明

--- packages/types/src/object/isPlainObject.ts
+++ packages/types/src/object/isPlainObject.ts
@@ -11,9 +11,9 @@ import isObject from './isObject';
  * - 如果想使用 `typeof` 验证一个对象，请使用 `isTypeofObject` 方法
  * - 如果想验证纯对象 (通过 `Object.create(null)` 创建的对象)，请使用 `isPureObject` 方法
  */
-export default function isPlainObject<K extends keyof any, T = unknown>(
+export default function isPlainObject<K extends PropertyKey, V = unknown>(
     value: any
-): value is Record<K, T> {
+): value is Record<K, V> {
     return (
         isObject(value) &&
         value.constructor === Object &&
