3bacae8699dc842df76499d26820c3f1f756f8f5
wtklbm <wtklbm@gmail.com>
2024-06-27 20:56:38 +0800

    🦄 refactor: 更新类型声明

--- packages/types/src/object/isTypeofObjectArray.ts
+++ packages/types/src/object/isTypeofObjectArray.ts
@@ -1,5 +1,4 @@
 import isArrayFilled from '../array/isArrayFilled';
-import type { Function } from '../function';
 
 import isTypeofObject from './isTypeofObject';
 
@@ -9,8 +8,8 @@ import isTypeofObject from './isTypeofObject';
  * @param value 要验证的值
  * @returns 是则返回 `true`，否则为 `false`
  */
-export default function isTypeofObjectArray<T = unknown>(
+export default function isTypeofObjectArray<K extends PropertyKey, V = unknown>(
     value: unknown
-): value is Function<T>[] {
+): value is Record<K, V>[] {
     return isArrayFilled(value) && value.every(isTypeofObject);
 }
