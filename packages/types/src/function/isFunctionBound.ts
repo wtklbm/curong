edc7e184255cb4b2f891afaeb68468764d2ef423
wtklbm <wtklbm@gmail.com>
2024-06-27 20:54:45 +0800

    🦄 refactor: 更新类型声明

--- packages/types/src/function/isFunctionBound.ts
+++ packages/types/src/function/isFunctionBound.ts
@@ -7,9 +7,10 @@ import type { Function } from './types';
  * @param value 要验证的值
  * @returns 是则返回 `true`，否则为 `false`
  */
-export default function isFunctionBound<T = unknown>(
-    value: unknown
-): value is Function<T> {
+export default function isFunctionBound<
+    R = unknown,
+    A extends unknown[] = unknown[]
+>(value: unknown): value is Function<R, A> {
     return (
         isFunction(value) &&
         value.name.startsWith('bound ') &&
