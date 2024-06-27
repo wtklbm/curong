d9426826060c3a5ecb001c2e7e613008a9434746
wtklbm <wtklbm@gmail.com>
2024-06-27 20:53:43 +0800

    🦄 refactor: 更新类型声明

--- packages/types/src/function/isAsyncFunctionFilled.ts
+++ packages/types/src/function/isAsyncFunctionFilled.ts
@@ -7,8 +7,9 @@ import type { AsyncFunction } from './types';
  * @param value 要验证的值
  * @returns 是则返回 `true`，否则为 `false`
  */
-export default function isAsyncFunctionFilled<T = unknown>(
-    value: unknown
-): value is AsyncFunction<T> {
+export default function isAsyncFunctionFilled<
+    R = unknown,
+    A extends unknown[] = unknown[]
+>(value: unknown): value is AsyncFunction<R, A> {
     return isAsyncFunction(value) && value.length > 0;
 }
