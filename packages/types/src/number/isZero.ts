e3bd2120979dd206fec33942a40c6336774c88fb
wtklbm <wtklbm@gmail.com>
2024-06-27 20:52:01 +0800

    🦄 refactor: 更新类型声明

--- packages/types/src/number/isZero.ts
+++ packages/types/src/number/isZero.ts
@@ -4,6 +4,6 @@
  * @param value 要验证的值
  * @returns 是则返回 `true`，否则为 `false`
  */
-export default function isZero(value: unknown): boolean {
+export default function isZero(value: unknown): value is 0 {
     return value === 0;
 }
