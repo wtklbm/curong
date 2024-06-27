a3f60552cae5288d9c8350c949a19cf392471f1b
wtklbm <wtklbm@gmail.com>
2024-06-28 01:28:29 +0800

    📃 docs: 更新文档

--- packages/types/src/number/isNumberPositive.ts
+++ packages/types/src/number/isNumberPositive.ts
@@ -8,8 +8,8 @@ import isFinite from './isFinite';
  * @note
  *
  * - 该数是一个可表示的安全的正数，即大于等于 `Number.MIN_VALUE` 且小于等于 `Number.MAX_VALUE` 的数
- * - `Number.MAX_VALUE` 的值为 `1.7976931348623157e308` (`(2**53 - 1) * (2**971)`)，大于 `MAX_VALUE` 的值表示为 `Infinity` 并将丢失其实际值
- * - `Number.MIN_VALUE` 的值为 `5e-324` (`2**(-1074)`)，是可表示的最小正数 (一个接近于 `0` 的很小的正数)。小于 `MIN_VALUE` 的值将转换为 `0`
+ * - `Number.MAX_VALUE` 的值为 `1.7976931348623157e308` (`(2^53 - 1) * (2^971)`)，大于 `MAX_VALUE` 的值表示为 `Infinity` 并将丢失其实际值
+ * - `Number.MIN_VALUE` 的值为 `5e-324` (`2^(-1074)`)，是可表示的最小正数 (一个接近于 `0` 的很小的正数)。小于 `MIN_VALUE` 的值将转换为 `0`
  */
 export default function isNumberPositive(value: unknown): value is number {
     return isFinite(value) && value > 0;
