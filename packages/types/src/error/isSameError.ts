2bea0906ce6ad03414df68bf874b94444c32eca3
wtklbm <wtklbm@gmail.com>
2024-06-28 01:28:11 +0800

    📃 docs: 更新文档

--- packages/types/src/error/isSameError.ts
+++ packages/types/src/error/isSameError.ts
@@ -4,6 +4,8 @@ import isAnyError from './isAnyError';
  * 是不是一个 `name` 和 `message` 相同的 `Error` 对象
  *
  * @param value 要验证的值
+ * @param name 错误的类型名
+ * @param message 错误的消息
  * @returns 是则返回 `true`，否则为 `false`
  */
 export default function isSameError(
