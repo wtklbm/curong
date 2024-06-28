4698041b93632cc4666d01992724a6d18c912792
wtklbm <wtklbm@gmail.com>
2024-06-29 01:04:04 +0800

    🦄 refactor: 更新类型声明

--- packages/types/src/function/types/Class.ts
+++ packages/types/src/function/types/Class.ts
@@ -1,14 +1,6 @@
-/**
- * 类的类型定义
- *
- * `interface` 的写法：
- *
- * ```typescript
- * export interface Class<R = unknown, A extends unknown[] = unknown[]> {
- *    new (...args: A): R;
- * }
- * ```
- */
-export type Class<R = unknown, A extends unknown[] = unknown[]> = new (
+/** 类的类型定义 */
+export type Class<R = unknown, A extends unknown[] = unknown[]> = (new (
     ...args: A
-) => R;
+) => R) & {
+    readonly prototype: R;
+};
