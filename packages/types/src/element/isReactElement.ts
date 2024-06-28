4c2308545d5b36436f33acbd6bcfd1888120b5f6
wtklbm <wtklbm@gmail.com>
2024-06-28 22:45:58 +0800

    🦄 refactor: 更新类型声明

--- packages/types/src/element/isReactElement.ts
+++ packages/types/src/element/isReactElement.ts
@@ -8,10 +8,10 @@ import isNullOrUndefined from '../nullable/isNullOrUndefined';
  * @param value 要验证的值
  * @returns 是则返回 `true`，否则为 `false`
  */
-export default function isReactElement(value: any): boolean {
+export default function isReactElement(value: unknown): boolean {
     // https://github.com/facebook/react/blob/b5ac963fb791d1298e7f396236383bc955f916c1/src/isomorphic/classic/element/ReactElement.js#L21-L25
     const forF = isFunction(Symbol) && isFunctionFilled(Symbol.for);
     const symbol = forF ? Symbol.for('react.element') : 60103;
 
-    return !isNullOrUndefined(value) && value['$$typeof'] === symbol;
+    return !isNullOrUndefined(value) && (value as any)['$$typeof'] === symbol;
 }
