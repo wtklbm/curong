78aaa3ca8304288142f55e98ef06c6c4d870c078
wtklbm <wtklbm@gmail.com>
2024-06-28 22:38:48 +0800

    🦄 refactor: 删除 `isNotZero`

--- packages/types/src/number/__tests__/isNotZero.test.ts
+++
@@ -1,8 +0,0 @@
-import { isNotZero } from '..';
-
-describe('@curong/types/isNotZero', () => {
-    test('测试1', () => {
-        expect(isNotZero('')).toBe(true);
-        expect(isNotZero(0)).toBe(false);
-    });
-});
