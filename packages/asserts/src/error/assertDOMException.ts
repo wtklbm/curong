780bb3da9b386a1f9bf7070b65f889418446db76
wtklbm <wtklbm@gmail.com>
2024-06-30 05:22:48 +0800

    ✨ feat: 添加 `assertDOMException` 方法

---
+++ packages/asserts/src/error/assertDOMException.ts
@@ -0,0 +1,19 @@
+import { isDOMException } from '@curong/types';
+
+import typeGuard from '../constants/typeGuard';
+
+/**
+ * 是不是一个 `DOMException` 对象
+ *
+ * @param value 要验证的值
+ * @param variableName 该值的变量名
+ * @param name 检查异常名称是否与指定的名称匹配
+ * @throws 如果不是则会抛出类型异常
+ */
+export default function assertDOMException(
+    value: unknown,
+    variableName: string,
+    name?: string
+): asserts value is Error {
+    return typeGuard(value, variableName, isDOMException, name);
+}
