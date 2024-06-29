0c79c3c9f37501cb867ebb787bfe3d47e5f0b606
wtklbm <wtklbm@gmail.com>
2024-06-30 04:59:32 +0800

    🧪 test: 更新测试中的参数样式

--- packages/number/__tests__/isInRange.test.ts
+++ packages/number/__tests__/isInRange.test.ts
@@ -18,37 +18,28 @@ describe('inRange', () => {
     });
 
     test('测试3', () => {
-        expect(inRange(5, [1, 10])).toBe(true);
-        expect(inRange(1, [1, 10])).toBe(true);
-        expect(inRange(10, [1, 10])).toBe(true);
-        expect(inRange(0, [1, 10])).toBe(false);
-        expect(inRange(11, [1, 10])).toBe(false);
+        expect(inRange(5, 1, 10)).toBe(true);
+        expect(inRange(1, 1, 10)).toBe(true);
+        expect(inRange(10, 1, 10)).toBe(true);
+        expect(inRange(0, 1, 10)).toBe(false);
+        expect(inRange(11, 1, 10)).toBe(false);
     });
 
     test('测试4', () => {
-        expect(inRange(-5, [-10, -1])).toBe(true);
-        expect(inRange(-10, [-10, -1])).toBe(true);
-        expect(inRange(-1, [-10, -1])).toBe(true);
-        expect(inRange(0, [-10, -1])).toBe(false);
-        expect(inRange(-11, [-10, -1])).toBe(false);
+        expect(inRange(-5, -10, -1)).toBe(true);
+        expect(inRange(-10, -10, -1)).toBe(true);
+        expect(inRange(-1, -10, -1)).toBe(true);
+        expect(inRange(0, -10, -1)).toBe(false);
+        expect(inRange(-11, -10, -1)).toBe(false);
     });
 
     test('测试5', () => {
-        // @ts-ignore
-        expect(() => inRange(5, [10])).toThrow(TypeError);
-        // @ts-ignore
-        expect(() => inRange(5, [10, 20, 30])).toThrow(TypeError);
-        // @ts-ignore
-        expect(() => inRange(5, [])).toThrow(TypeError);
-    });
-
-    test('测试6', () => {
         expect(() => inRange(5, '10' as any)).toThrow(TypeError);
         expect(() => inRange(5, null as any)).toThrow(TypeError);
         expect(() => inRange(5, undefined as any)).toThrow(TypeError);
     });
 
-    test('测试7', () => {
+    test('测试6', () => {
         expect(() => inRange(5, NaN)).toThrow(TypeError);
         expect(inRange(5, Infinity)).toBe(true);
         expect(inRange(5, -Infinity)).toBe(false);
@@ -61,28 +52,28 @@ describe('inRange', () => {
         expect(inRange(Infinity, Infinity)).toBe(true);
         expect(inRange(-Infinity, -Infinity)).toBe(true);
 
-        expect(() => inRange(NaN, [0, 10])).toThrow(TypeError);
-        expect(inRange(Infinity, [0, 10])).toBe(false);
-        expect(inRange(-Infinity, [0, 10])).toBe(false);
+        expect(() => inRange(NaN, 0, 10)).toThrow(TypeError);
+        expect(inRange(Infinity, 0, 10)).toBe(false);
+        expect(inRange(-Infinity, 0, 10)).toBe(false);
 
-        expect(() => inRange(5, [0, NaN])).toThrow(TypeError);
-        expect(inRange(5, [0, Infinity])).toBe(true);
-        expect(inRange(5, [0, -Infinity])).toBe(false);
+        expect(() => inRange(5, 0, NaN)).toThrow(TypeError);
+        expect(inRange(5, 0, Infinity)).toBe(true);
+        expect(inRange(5, 0, -Infinity)).toBe(false);
 
-        expect(() => inRange(NaN, [0, NaN])).toThrow(TypeError);
-        expect(inRange(Infinity, [0, Infinity])).toBe(true);
-        expect(inRange(-Infinity, [0, -Infinity])).toBe(true);
+        expect(() => inRange(NaN, 0, NaN)).toThrow(TypeError);
+        expect(inRange(Infinity, 0, Infinity)).toBe(true);
+        expect(inRange(-Infinity, 0, -Infinity)).toBe(true);
 
-        expect(() => inRange(5, [NaN, 10])).toThrow(TypeError);
-        expect(inRange(5, [Infinity, 10])).toBe(false);
-        expect(inRange(5, [-Infinity, 10])).toBe(true);
+        expect(() => inRange(5, NaN, 10)).toThrow(TypeError);
+        expect(inRange(5, Infinity, 10)).toBe(false);
+        expect(inRange(5, -Infinity, 10)).toBe(true);
 
-        expect(() => inRange(5, [NaN, NaN])).toThrow(TypeError);
-        expect(inRange(5, [Infinity, Infinity])).toBe(false);
-        expect(inRange(5, [-Infinity, -Infinity])).toBe(false);
+        expect(() => inRange(5, NaN, NaN)).toThrow(TypeError);
+        expect(inRange(5, Infinity, Infinity)).toBe(false);
+        expect(inRange(5, -Infinity, -Infinity)).toBe(false);
 
-        expect(() => inRange(NaN, [NaN, NaN])).toThrow(TypeError);
-        expect(inRange(Infinity, [Infinity, Infinity])).toBe(true);
-        expect(inRange(-Infinity, [-Infinity, -Infinity])).toBe(true);
+        expect(() => inRange(NaN, NaN, NaN)).toThrow(TypeError);
+        expect(inRange(Infinity, Infinity, Infinity)).toBe(true);
+        expect(inRange(-Infinity, -Infinity, -Infinity)).toBe(true);
     });
 });
