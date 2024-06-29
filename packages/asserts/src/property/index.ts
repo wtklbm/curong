2dfc44806116b55987a0fc8b6bc174be28e2ca75
wtklbm <wtklbm@gmail.com>
2024-06-30 05:23:50 +0800

    ✨ feat: 添加 `assertPrototype` 方法

--- packages/asserts/src/property/index.ts
+++ packages/asserts/src/property/index.ts
@@ -4,5 +4,6 @@ export { default as assertOwnProperty } from './assertOwnProperty';
 export { default as assertProperty } from './assertProperty';
 export { default as assertPropertyDescriptor } from './assertPropertyDescriptor';
 export { default as assertPropertyKey } from './assertPropertyKey';
+export { default as assertPrototype } from './assertPrototype';
 export { default as assertSizey } from './assertSizey';
 export { default as assertSizeyFilled } from './assertSizeyFilled';
