74ebcace0930c63faf58d266be3d0f94884b469b
wtklbm <wtklbm@gmail.com>
2024-06-30 04:58:01 +0800

    ✨ feat: 添加 `toLocaleString` 方法

--- packages/number/src/index.ts
+++ packages/number/src/index.ts
@@ -1,3 +1,6 @@
 export { default as inRange } from './inRange';
 export { default as range } from './range';
 export { default as toFiniteNumber } from './toFiniteNumber';
+export { default as toLocaleString } from './toLocaleString';
+
+export * from './types';
