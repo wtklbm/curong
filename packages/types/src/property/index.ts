4d23684371ce50a13dfd7d816eb0189fb7f061c1
wtklbm <wtklbm@gmail.com>
2024-06-30 05:18:38 +0800

    ✨ feat: 添加 `isPrototype` 方法

--- packages/types/src/property/index.ts
+++ packages/types/src/property/index.ts
@@ -4,6 +4,7 @@ export { default as isOwnProperty } from './isOwnProperty';
 export { default as isProperty } from './isProperty';
 export { default as isPropertyDescriptor } from './isPropertyDescriptor';
 export { default as isPropertyKey } from './isPropertyKey';
+export { default as isPrototype } from './isPrototype';
 export { default as isSizey } from './isSizey';
 export { default as isSizeyFilled } from './isSizeyFilled';
 
