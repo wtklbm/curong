2b53b915646f3c325878de0274f84e91b33ab766
wtklbm <wtklbm@gmail.com>
2024-06-30 05:23:07 +0800

    ✨ feat: 添加 `assertDOMException` 方法

--- packages/asserts/src/error/index.ts
+++ packages/asserts/src/error/index.ts
@@ -1,4 +1,5 @@
 export { default as assertAnyError } from './assertAnyError';
+export { default as assertDOMException } from './assertDOMException';
 export { default as assertError } from './assertError';
 export { default as assertEvalError } from './assertEvalError';
 export { default as assertRangeError } from './assertRangeError';
