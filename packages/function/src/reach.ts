0af5b9924e906255f67d20d6aa67e473d7c774ce
wtklbm <wtklbm@gmail.com>
2024-07-01 12:28:12 +0800

    ✨ feat: 添加 `this` 绑定并增加 `args` 选项

--- packages/function/src/reach.ts
+++ packages/function/src/reach.ts
@@ -5,7 +5,8 @@ import { isUintSafe } from '@curong/types';
  *
  * @param count 要等待的总次数 (必须大于 `0`)
  * @param callback 当次数到达时所要执行的回调函数
- * @throws 如果 `count` 不是大于或等于 `0` 的安全整数，则会抛出异常
+ * @param args 要传递给回调函数的参数
+ * @throws 如果 `count` 不是大于或等于 `0` 的安全整数，则会抛出类型错误异常
  * @example ````
  *
  * ### 常规示例
@@ -19,7 +20,7 @@ import { isUintSafe } from '@curong/types';
  *
  * ```javascript
  * const data = {};
- * const ready = after(2, () => console.log(data));
+ * const ready = reach(2, () => console.log(data));
  *
  * readFile('./a.txt', 'utf8', (_err, data) => {
  *     data['a'] = data;
@@ -37,12 +38,17 @@ import { isUintSafe } from '@curong/types';
  * 该函数是柯里化函数，柯里化函数就是把一个大函数拆分成很多的具体的功能的小函数。
  * 高阶函数中包含柯里化，柯理化的好处是可以保留参数，它非常像 `bind` 方法。
  */
-export default function reach<T>(count: number, callback: () => T): () => any {
+export default function reach<R, A extends unknown[]>(
+    count: number,
+    callback: (...args: A) => R,
+    ...args: A
+): () => any {
     if (!isUintSafe(count)) {
         throw new TypeError('[reach]: count 不是大于或等于0的安全整数');
     }
 
-    const reachCall = () => (--count <= 0 ? callback() : reachCall);
+    const reachCall = () =>
+        --count <= 0 ? callback.apply(callback, args) : reachCall;
 
     return reachCall;
 }
