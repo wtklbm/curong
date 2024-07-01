3fcccca3a199d9f3a2a9fa86f38564acc425433e
wtklbm <wtklbm@gmail.com>
2024-07-01 12:25:25 +0800

    🧪 test: 更改文件名

---
+++ packages/function/__tests__/currying.test.ts
@@ -0,0 +1,16 @@
+import { currying } from '../src';
+
+describe('@curong/function/currying', () => {
+    test('测试1', () => {
+        const c = currying((a: string, b: string, c: string) => a + b + c);
+
+        expect(c('1')('2')('3')).toBe('123');
+        expect(c('1', '2', '3')).toBe('123');
+    });
+
+    test('测试2', () => {
+        const c = currying(() => 10);
+
+        expect(c()).toBe(10);
+    });
+});
