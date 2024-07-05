88543bec964728b711c5190f79746b26a7f618e5
wtklbm <wtklbm@gmail.com>
2024-07-05 23:09:21 +0800

    ✨ feat: 添加 `getCrypto` 方法

---
+++ packages/number/src/constants/getCrypto.ts
@@ -0,0 +1,23 @@
+import { isBrowser, isNodejs } from '@curong/types';
+
+/** 获取 `Crypto` 对象 */
+export default function getCrypto(): Crypto {
+    let crypto: Crypto;
+
+    if (isBrowser()) {
+        crypto =
+            window.crypto ??
+            (window as any).webkitCrypto ??
+            (window as any).mozCrypto ??
+            (window as any).oCrypto ??
+            (window as any).msCrypto;
+    } else if (isNodejs()) {
+        crypto = globalThis.crypto ?? require('crypto').webcrypto;
+    } else {
+        throw new ReferenceError(
+            '[getCrypto] 在浏览器或 Node.js 环境下没有检测到 crypto 对象'
+        );
+    }
+
+    return crypto;
+}
