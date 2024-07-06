1ec8c197b9b4ca0bd27514e503793218c925e581
wtklbm <wtklbm@gmail.com>
2024-07-06 18:00:54 +0800

    🧪 test: 添加 `select` 方法的测试

---
+++ packages/function/__tests__/select.test.ts
@@ -0,0 +1,199 @@
+import { select } from '../src';
+
+describe('@curong/function/select', () => {
+    test('测试1', async () => {
+        expect(await select(true, 1, 2)).toBe(1);
+        expect(await select(Promise.resolve(true), 1, 2)).toBe(1);
+        expect(await select(true, 1, Promise.resolve(2))).toBe(1);
+        expect(await select(Promise.resolve(true), 1, Promise.resolve(2))).toBe(
+            1
+        );
+        expect(await select(true, Promise.resolve(1), 2)).toBe(1);
+        expect(await select(true, Promise.resolve(1), Promise.resolve(2))).toBe(
+            1
+        );
+        expect(
+            await select(
+                Promise.resolve(true),
+                Promise.resolve(1),
+                Promise.resolve(2)
+            )
+        ).toBe(1);
+    });
+
+    test('测试2', async () => {
+        expect(await select(() => true, 1, 2)).toBe(1);
+        expect(await select(() => Promise.resolve(true), 1, 2)).toBe(1);
+        expect(await select(() => true, 1, Promise.resolve(2))).toBe(1);
+        expect(
+            await select(() => Promise.resolve(true), 1, Promise.resolve(2))
+        ).toBe(1);
+        expect(await select(() => true, Promise.resolve(1), 2)).toBe(1);
+        expect(
+            await select(() => true, Promise.resolve(1), Promise.resolve(2))
+        ).toBe(1);
+        expect(
+            await select(
+                () => Promise.resolve(true),
+                Promise.resolve(1),
+                Promise.resolve(2)
+            )
+        ).toBe(1);
+    });
+
+    test('测试3', async () => {
+        expect(
+            await select(
+                () => true,
+                () => 1,
+                2
+            )
+        ).toBe(1);
+        expect(
+            await select(
+                () => Promise.resolve(true),
+                () => 1,
+                2
+            )
+        ).toBe(1);
+        expect(
+            await select(
+                () => true,
+                1,
+                () => Promise.resolve(2)
+            )
+        ).toBe(1);
+        expect(
+            await select(
+                () => Promise.resolve(true),
+                () => 1,
+                Promise.resolve(2)
+            )
+        ).toBe(1);
+        expect(
+            await select(
+                () => true,
+                () => Promise.resolve(1),
+                2
+            )
+        ).toBe(1);
+        expect(
+            await select(
+                () => true,
+                () => Promise.resolve(1),
+                Promise.resolve(2)
+            )
+        ).toBe(1);
+        expect(
+            await select(
+                () => Promise.resolve(true),
+                () => Promise.resolve(1),
+                Promise.resolve(2)
+            )
+        ).toBe(1);
+    });
+
+    test('测试4', async () => {
+        expect(
+            await select(
+                () => true,
+                1,
+                () => 2
+            )
+        ).toBe(1);
+        expect(
+            await select(
+                () => Promise.resolve(true),
+                1,
+                () => 2
+            )
+        ).toBe(1);
+        expect(
+            await select(
+                () => true,
+                1,
+                () => Promise.resolve(2)
+            )
+        ).toBe(1);
+        expect(
+            await select(
+                () => Promise.resolve(true),
+                1,
+                () => Promise.resolve(2)
+            )
+        ).toBe(1);
+        expect(
+            await select(
+                () => true,
+                Promise.resolve(1),
+                () => 2
+            )
+        ).toBe(1);
+        expect(
+            await select(
+                () => true,
+                Promise.resolve(1),
+                () => Promise.resolve(2)
+            )
+        ).toBe(1);
+        expect(
+            await select(
+                () => Promise.resolve(true),
+                Promise.resolve(1),
+                () => Promise.resolve(2)
+            )
+        ).toBe(1);
+    });
+
+    test('测试5', async () => {
+        expect(
+            await select(
+                () => true,
+                () => 1,
+                () => 2
+            )
+        ).toBe(1);
+        expect(
+            await select(
+                () => Promise.resolve(true),
+                () => 1,
+                () => 2
+            )
+        ).toBe(1);
+        expect(
+            await select(
+                () => true,
+                () => 1,
+                () => Promise.resolve(2)
+            )
+        ).toBe(1);
+        expect(
+            await select(
+                () => Promise.resolve(true),
+                () => 1,
+                () => Promise.resolve(2)
+            )
+        ).toBe(1);
+        expect(
+            await select(
+                () => true,
+                () => Promise.resolve(1),
+                () => 2
+            )
+        ).toBe(1);
+        expect(
+            await select(
+                () => true,
+                () => Promise.resolve(1),
+                () => Promise.resolve(2)
+            )
+        ).toBe(1);
+        expect(
+            await select(
+                () => Promise.resolve(true),
+                () => Promise.resolve(1),
+                () => Promise.resolve(2)
+            )
+        ).toBe(1);
+    });
+});
