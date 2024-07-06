0cb03fa8773996027db14f1a9c730c97dc8d6fc4
wtklbm <wtklbm@gmail.com>
2024-07-06 23:33:48 +0800

    🎈 perf: 更改选项名称

--- packages/number/src/rangeSafe.ts
+++ packages/number/src/rangeSafe.ts
@@ -5,8 +5,8 @@ import randomSafe from './randomSafe';
 /**
  * 生成一个从开始位置到结束位置的安全的随机整数
  *
- * @param min 开始时的数字(包含)。无论传递的值是否为整数，都会向上取整为整数
- * @param max 结束时的数字(包含)。无论传递的值是否为整数，都会向下取整为整数
+ * @param start 开始时的数字(包含)。无论传递的值是否为整数，都会向上取整为整数
+ * @param end 结束时的数字(包含)。无论传递的值是否为整数，都会向下取整为整数
  * @returns 返回生成的安全的随机数
  * @example
  *
@@ -15,14 +15,14 @@ import randomSafe from './randomSafe';
  * console.log(n); // 5
  * ```
  */
-export default function rangeSafe(min: number, max: number): number {
-    if (!isIntSafe(min)) {
-        throw new TypeError(`[range] min 必须是一个安全的整数: ${min}`);
+export default function rangeSafe(start: number, end: number): number {
+    if (!isIntSafe(start)) {
+        throw new TypeError(`[range] start 必须是一个安全的整数: ${start}`);
     }
 
-    if (!isIntSafe(max)) {
-        throw new TypeError(`[range] max 必须是一个安全的整数: ${max}`);
+    if (!isIntSafe(end)) {
+        throw new TypeError(`[range] end 必须是一个安全的整数: ${end}`);
     }
 
-    return Math.floor(randomSafe() * (max - min + 1)) + min;
+    return Math.floor(randomSafe() * (end - start + 1)) + start;
 }
