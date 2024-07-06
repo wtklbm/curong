dacca900f33c6a466be8d49a0c65d5cfd3b107db
wtklbm <wtklbm@gmail.com>
2024-07-06 15:41:22 +0800

    ✨ feat: 添加 `range` 方法

--- packages/number/src/range.ts
+++ packages/number/src/range.ts
@@ -1,9 +1,13 @@
+import { isIntSafe } from '@curong/types';
+
+import random from './random';
+
 /**
- * 生成一个从开始位置到结束位置的随机数
+ * 生成一个从开始位置到结束位置的不安全的随机整数
  *
- * @param min 开始时的数字(包含)
- * @param max 结束时的数字(包含)
- * @returns 返回生成的随机数
+ * @param min 开始时的数字(包含)。无论传递的值是否为整数，都会向上取整为整数
+ * @param max 结束时的数字(包含)。无论传递的值是否为整数，都会向下取整为整数
+ * @returns 返回生成的不安全的随机数
  * @example
  *
  * ```javascript
@@ -12,8 +16,13 @@
  * ```
  */
 export default function range(min: number, max: number): number {
-    min = Math.ceil(min);
-    max = Math.floor(max);
+    if (!isIntSafe(min)) {
+        throw new TypeError(`[range] min 必须是一个安全的整数: ${min}`);
+    }
 
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+    if (!isIntSafe(max)) {
+        throw new TypeError(`[range] max 必须是一个安全的整数: ${max}`);
+    }
+
+    return Math.floor(random() * (max - min + 1)) + min;
 }
