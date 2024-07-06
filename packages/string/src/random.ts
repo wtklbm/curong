5a182537ad43da8c4bfbb654d50428e1e5509473
wtklbm <wtklbm@gmail.com>
2024-07-06 15:43:14 +0800

    🦄 refactor: 简化调用

--- packages/string/src/random.ts
+++ packages/string/src/random.ts
@@ -1,5 +1,10 @@
-import { format } from '@curong/term';
-import { isBrowser, isNodejs } from '@curong/types';
+import getCrypto from '../../number/src/constants/getCrypto';
+
+// `getRandomValues` 方法所支持的最大长度，超过此长度就会报错
+const MAX_LENGTH = 65536;
+const crypto = getCrypto();
+const creator = (length: number) =>
+    crypto.getRandomValues(new Uint8Array(length)).join('');
 
 /**
  * 生成安全的随机数字符串
@@ -20,46 +25,8 @@ import { isBrowser, isNodejs } from '@curong/types';
  * // 为了实现安全的存储和计算，可以使用 `BigInt` 进行转换 (`BigInt(n)`)
  * console.log(n);
  * ```
- *
- * @note
- *
- * ### 伪随机数生成器
- *
- * 由于真随机数来自于计算机外部，获得的代价往往较高，
- * 而在计算机的应用场景中我们也并不总是要求随机数都有真随机那么高的不确定性，
- * 所以这就衍生出了对伪随机的需求。设计良好的算法产生的随机序列能够以假乱真，
- * 大部分计算机程序和语言中的随机函数，都是伪随机数生成器，它们都是由确定的算法，
- * 通过一个**种子** (比如：时间)，来产生**看起来随机**的结果。
- * 产生伪随机数的算法被叫做**伪随机数生成器**。
- *
- * JS 中的 `Math.random()` 是一个伪随机数生成器函数，它是不安全的。为了尽可能的保证安全，
- * 就需要 `Crypto` 对象，并调用其 `getRandomValues` 方法来获得一个相对安全的随机数。
  */
 export default function random(length: number = 15): string {
-    let crypto: Crypto;
-
-    if (isBrowser()) {
-        crypto =
-            window.crypto ??
-            (window as any).webkitCrypto ??
-            (window as any).mozCrypto ??
-            (window as any).oCrypto ??
-            (window as any).msCrypto;
-    } else if (isNodejs()) {
-        crypto = globalThis.crypto ?? require('crypto').webcrypto;
-    } else {
-        throw format({
-            name: 'random',
-            message: '在浏览器或 Node.js 环境下没有检测到 crypto 对象'
-        });
-    }
-
-    const creator = (length: number) =>
-        crypto.getRandomValues(new Uint8Array(length)).join('');
-
-    // `getRandomValues` 方法所支持的最大长度，超过此长度就会报错
-    const MAX_LENGTH = 65536;
-
     return (
         length < MAX_LENGTH
             ? creator(length)
