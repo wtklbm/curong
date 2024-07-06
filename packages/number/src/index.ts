0367adf415ec5e2999160eab62e217686abe1e6b
wtklbm <wtklbm@gmail.com>
2024-07-06 15:41:54 +0800

    ✨ feat: 添加方法

--- packages/number/src/index.ts
+++ packages/number/src/index.ts
@@ -1,5 +1,8 @@
 export { default as inRange } from './inRange';
+export { default as random } from './random';
+export { default as randomSafe } from './randomSafe';
 export { default as range } from './range';
+export { default as rangeSafe } from './rangeSafe';
 export { default as toFiniteNumber } from './toFiniteNumber';
 export { default as toLocaleString } from './toLocaleString';
 
