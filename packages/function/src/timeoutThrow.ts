22a6afd03ec9c4e9d0312648b94b4d58d9f2f25d
wtklbm <wtklbm@gmail.com>
2024-07-06 22:44:41 +0800

    🦄 refactor: 使用已有方法

--- packages/function/src/timeoutThrow.ts
+++ packages/function/src/timeoutThrow.ts
@@ -1,21 +1,15 @@
-/** 超时时错误 */
-export class TimeoutThrowError extends Error {
-    constructor(message: string) {
-        super(message);
-        this.name = 'TimeoutError';
-    }
-}
+import setTimeout from './setTimeout';
 
 /**
  * 执行一个函数，并获取函数的返回值。如果函数的执行时间超过 `duration` 时，就会抛出异常
  *
  * @param duration 一个以毫秒为单位的超时时间
- *  - 如果 `duration` 为小于或等于 `0` 的数字，则表示定时器应尽快执行
- *  - 如果 `duration` 为一个大于 `0` 的数字，则表示至少应等待 `duration` 毫秒后执行
+ *  - 如果 `duration` 为小于或等于 `0` 的数字，则会尽快抛出异常
+ *  - 如果 `duration` 为一个大于 `0` 的数字，则表示至少应等待 `duration` 毫秒后抛出异常
  * @param fn 要执行的同步或异步函数
  * @param args 要传递给函数的参数
  * @returns 如果函数执行的时间没有超时，则返回函数的返回值，否则返回一个超时错误
- * @throw 如果方法已超时，则会抛出 `TimeoutThrowError` 异常
+ * @throw 如果方法已超时，则会抛出 `EvalError` 异常
  * @example
  *
  * ```typescript
@@ -24,23 +18,21 @@ export class TimeoutThrowError extends Error {
  * console.log(ret); // 3
  * ```
  */
-export default async function timeoutThrow<
-    A extends readonly unknown[],
-    R = unknown
->(duration: number, fn: (...args: A) => R, ...args: A): Promise<R> {
+export default async function timeoutThrow<A extends unknown[], R = unknown>(
+    duration: number,
+    fn: (...args: A) => Promise<R> | R,
+    ...args: A
+): Promise<R> {
     return Promise.race([
-        Promise.resolve(fn(...args)),
-        new Promise((_, reject) => {
-            let timer: any = setTimeout(() => {
-                clearTimeout(timer);
-                timer = null;
-
+        Promise.resolve(fn.apply(fn, args)),
+        new Promise<R>((_, reject) => {
+            setTimeout(() => {
                 reject(
-                    new TimeoutThrowError(
+                    new EvalError(
                         `[timeoutThrow]: 该方法执行的时间已超时: "${duration}ms"`
                     )
                 );
             }, duration);
-        }) as Promise<R>
+        })
     ]);
 }
