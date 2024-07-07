ab7c01e3e0579bc18e95e8aadfb896145c1fc447
wtklbm <wtklbm@gmail.com>
2024-07-08 01:49:52 +0800

    📃 docs: 更新文档

--- packages/function/src/select.ts
+++ packages/function/src/select.ts
@@ -6,6 +6,7 @@ import fCall from './constants/fCall';
  * @param condition 一个布尔值条件，可以是布尔值或返回布尔值的同步函数、异步函数、`Promise`
  * @param doIf 条件为真时要返回的值，或者要执行的同步函数、异步函数、`Promise` 或普通值
  * @param doElse 条件为假时要返回的后备值，或者要执行的同步函数、异步函数、`Promise` 或普通值
+ * @param args 传递给 `condition`、`doIf` 和 `doElse` 的参数
  * @returns 返回指定条件的值
  * @example
  *
