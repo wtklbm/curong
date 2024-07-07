ded298831f4be0e4cdcb31e1317f34a9555a177d
wtklbm <wtklbm@gmail.com>
2024-07-07 23:34:57 +0800

    📃 docs: 更新文档

--- packages/function/src/sleepAsync.ts
+++ packages/function/src/sleepAsync.ts
@@ -4,8 +4,6 @@ import setTimeout from './setTimeout';
  * 异步阻塞一段时间
  *
  * @param duration 要阻塞多长时间，以毫秒为单位
- *  - 如果 `duration` 为小于或等于 `0` 的数字，则表示定时器应尽快执行
- *  - 如果 `duration` 为一个大于 `0` 的数字，则表示至少应等待 `duration` 毫秒后执行
  * @example
  *
  * ```typescript
