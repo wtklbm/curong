22bc67d2b2a31785c5eaca7ea94b3ffec20d3510
wtklbm <wtklbm@gmail.com>
2024-07-08 01:04:25 +0800

    📃 docs: 更新文档

--- packages/function/src/timeoutOr.ts
+++ packages/function/src/timeoutOr.ts
@@ -5,9 +5,7 @@ import setTimeout from './setTimeout';
 /**
  * 执行一个函数，并获取函数的返回值，如果函数的执行时间超过 `duration` 时，就执行回调函数
  *
- * @param duration 一个以毫秒为单位的超时时间
- *  - 如果 `duration` 为小于或等于 `0` 的数字，则表示定时器应尽快执行
- *  - 如果 `duration` 为一个大于 `0` 的数字，则表示至少应等待 `duration` 毫秒后执行
+ * @param duration 一个以毫秒为单位的超时时间，它是一个大于或等于 `0` 的整数
  * @param callable 一个 `Promise`、同步函数或异步函数
  * @param callback 当超时时，要返回的一个值、`Promise` 或一个可执行的回调函数
  * @returns 如果函数执行的时间没有超时，则返回该结果，否则返回回调函数的结果
