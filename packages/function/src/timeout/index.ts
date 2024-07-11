20db808cec57e93401122cf194aa24771857a3bf
wtklbm <wtklbm@gmail.com>
2024-07-11 23:38:03 +0800

    ✨ feat: 添加 `timeoutDurationResolve` 方法

---
+++ packages/function/src/timeout/index.ts
@@ -0,0 +1,4 @@
+export { default as setTimeout } from './setTimeout';
+export { default as timeoutDurationResolve } from './timeoutDurationResolve';
+export { default as timeoutLog } from './timeoutLog';
+export { default as timeoutOr } from './timeoutOr';
