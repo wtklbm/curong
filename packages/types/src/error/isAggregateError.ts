afab9ec26a859c977643f484669d86c66c5e6e9b
wtklbm <wtklbm@gmail.com>
2024-07-11 18:22:29 +0800

    ✨ feat: 添加 `isAggregateError` 方法

---
+++ packages/types/src/error/isAggregateError.ts
@@ -0,0 +1,17 @@
+import isAnyError from './isAnyError';
+
+/**
+ * 是不是一个 `AggregateError` 对象
+ *
+ * @param value 要验证的值
+ * @returns 是则返回 `true`，否则为 `false`
+ * @note
+ *
+ * - `AggregateError` 对象代表了包装了多个错误对象的单个错误对象
+ * - `AggregateError` 是 `Error` 的子类
+ */
+export default function isAggregateError(value: unknown): value is EvalError {
+    return (
+        isAnyError(value) && Object.getPrototypeOf(value).name === 'AggregateError'
+    );
+}
