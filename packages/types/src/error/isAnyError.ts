a80ac32aead2085dddc62f5fc2d67e43a8eb6667
wtklbm <wtklbm@gmail.com>
2024-07-11 18:25:44 +0800

    📃 docs: 更新文档

--- packages/types/src/error/isAnyError.ts
+++ packages/types/src/error/isAnyError.ts
@@ -7,15 +7,27 @@ import getTagEqual from '../type/getTagEqual';
  * @returns 是则返回 `true`，否则为 `false`
  * @note
  *
- * 验证以下错误对象:
+ * ### 验证以下错误对象
  *
+ * - `AggregateError`
  * - `Error`
  * - `EvalError`
  * - `RangeError`
  * - `ReferenceError`
  * - `SyntaxError`
  * - `TypeError`
+ * - `URIError`
  * - 一切 `Object.prototype.toString.call(value) === '[object Error]'` 的对象
+ *
+ * ### 一个 `Error` 对象包含以下属性
+ *
+ * - `name`: 错误的类型名称
+ * - `message`: 错误的消息
+ * - `stack`: 当前堆栈的跟踪记录
+ * - `cause` (可选): 导致错误的原因
+ * - `fileName` (可选): 引发此错误的文件的路径
+ * - `lineNumber` (可选): 引发此错误的代码所在的文件的行号
+ * - `columnNumber` (可选): 引发此错误的代码在文件中所在行的列号
  */
 export default function isAnyError<T extends Error = Error>(
     value: unknown
