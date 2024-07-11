9fdf12328dd0e437bfec9a4dfcd0fbb0ebff823b
wtklbm <wtklbm@gmail.com>
2024-07-11 15:26:31 +0800

    🦄 refactor: 移动到新路径

---
+++ packages/function/src/timeout/__tests__/timeoutLog.test.ts
@@ -0,0 +1,36 @@
+import { timeoutLog } from '..';
+
+describe('@curong/function/timeoutLog', () => {
+    const f = (f: (...args: unknown[]) => void, message: string) => {
+        const consoleSpy = jest.spyOn(console, 'log');
+        f();
+        expect(consoleSpy).toHaveBeenCalledWith(message);
+    };
+
+    test('测试1', async () => {
+        expect(() => timeoutLog(NaN)).toThrow();
+        expect(() => timeoutLog(Infinity)).toThrow();
+        expect(() => timeoutLog(-Infinity)).toThrow();
+        expect(() => timeoutLog(-1.5)).toThrow();
+        expect(() => timeoutLog(-10)).toThrow();
+        expect(() => timeoutLog(Number.MAX_VALUE)).toThrow();
+        expect(() => timeoutLog(Number.MAX_SAFE_INTEGER)).toThrow();
+        expect(() => timeoutLog(2 ** 31)).toThrow();
+        expect(() => timeoutLog(1.5)).toThrow();
+    });
+    test('测试2', async () => {
+        f(() => timeoutLog(2 ** 31 - 1), '等待 24 天 20:31:23.647');
+        f(() => timeoutLog(0), '等待 0ms');
+        f(() => timeoutLog(1), '等待 1ms');
+        f(() => timeoutLog(999), '等待 999ms');
+        f(() => timeoutLog(1000), '等待 00:00:01');
+        f(() => timeoutLog(1000 * 59), '等待 00:00:59');
+        f(() => timeoutLog(1000 * 60), '等待 00:01:00');
+        f(() => timeoutLog(1000 * 60 * 59), '等待 00:59:00');
+        f(() => timeoutLog(1000 * 60 * 60), '等待 01:00:00');
+        f(() => timeoutLog(1000 * 60 * 59 * 23), '等待 22:37:00');
+        f(() => timeoutLog(1000 * 60 * 60 * 23), '等待 23:00:00');
+        f(() => timeoutLog(1000 * 60 * 59 * 24), '等待 23:36:00');
+        f(() => timeoutLog(1000 * 60 * 60 * 24), '等待 1 天 00:00:00');
+    });
+});
---
+++ packages/function/src/timeout/timeoutLog.ts
@@ -0,0 +1,41 @@
+import { isUint } from '@curong/types';
+
+const padZero = (num: number | string) => num.toString().padStart(2, '0');
+const initTime = new Date('2000-01-01 00:00:00').getTime();
+
+/**
+ * 将超时时间打印为一个可读的字符串格式。包含天、小时、分钟和秒
+ *
+ * @param duration 以毫秒为单位的超时时间
+ */
+export default function timeoutLog(duration: number) {
+    // 浏览器内部将延迟存储为 `32` 位有符号整数 (一位用于符号位，数字部分为 `2^31-1`)
+    // 当使用大于 `2147483647` 毫秒（约 `24.8` 天）的延迟时，这会导致整数溢出
+    if (!isUint(duration) || duration > 2147483647) {
+        throw new TypeError(
+            `[setTimeout] 超时时间应大于或等于 0 且不要超过 2147483647 毫秒 (约 24.8 天)`
+        );
+    }
+
+    const date = new Date(initTime + duration);
+    let formatString: string;
+    let millisecond = date.getMilliseconds().toString();
+
+    if (duration < 1e3) {
+        formatString = `${millisecond}ms`;
+    } else {
+        const seconds = padZero(date.getSeconds());
+        const minutes = padZero(date.getMinutes());
+        const hours = padZero(date.getHours());
+        millisecond =
+            millisecond === '0' ? '' : `.${millisecond.padStart(3, '0')}`;
+
+        if (duration < 864e5) {
+            formatString = `${hours}:${minutes}:${seconds}${millisecond}`;
+        } else {
+            formatString = `${date.getDate() - 1} 天 ${hours}:${minutes}:${seconds}${millisecond}`;
+        }
+    }
+
+    console.log(`等待 ${formatString}`);
+}
