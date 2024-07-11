7eb5ea6701fb8609c6ab873485715207d8ac323e
wtklbm <wtklbm@gmail.com>
2024-07-11 18:30:10 +0800

    ✨ feat: 添加 `assertAggregateError` 方法

---
+++ packages/asserts/src/error/assertAggregateError.ts
@@ -0,0 +1,21 @@
+import { isAggregateError } from '@curong/types';
+
+import typeGuard from '../constants/typeGuard';
+
+/**
+ * 是不是一个 `AggregateError` 对象
+ *
+ * @param value 要验证的值
+ * @param variableName 该值的变量名
+ * @throws 如果不是则会抛出类型异常
+ * @note
+ *
+ * - `AggregateError` 对象代表了包装了多个错误对象的单个错误对象
+ * - `AggregateError` 是 `Error` 的子类
+ */
+export default function assertAggregateError(
+    value: unknown,
+    variableName: string
+): asserts value is TypeError {
+    return typeGuard(value, variableName, isAggregateError);
+}
