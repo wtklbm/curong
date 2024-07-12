36a39dadfa2045f4c7095c61ed2ed6c14b3018a4
wtklbm <wtklbm@gmail.com>
2024-07-12 13:04:01 +0800

    📃 docs: 更新文档

--- packages/function/src/constants/pWarper.ts
+++ packages/function/src/constants/pWarper.ts
@@ -2,6 +2,13 @@ import { isPromise } from '@curong/types';
 
 import fCall from './fCall';
 
+/**
+ * 判断一个值的类型，如果是 `Promise` 就直接返回，如果是函数，就将函数执行，如果是值就直接返回该值
+ *
+ * @param value 要判断的值
+ * @param args 要传递给函数的参数
+ * @returns 返回一个包装的 `Promise`
+ */
 export default function pWarper<A extends unknown[], R = any>(
     value: any,
     args?: A
