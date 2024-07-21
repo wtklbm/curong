edb2da11b848676519bbe90f8666f333a3b4a2f3
wtklbm <wtklbm@gmail.com>
2024-07-21 10:20:00 +0800

    ✨ feat: 增加 `delayReturn` 方法

--- packages/function/src/delay/index.ts
+++ packages/function/src/delay/index.ts
@@ -1,3 +1,4 @@
 export { default as delay } from './delay';
+export { default as delayReturn } from './delayReturn';
 export { default as delayRun } from './delayRun';
 export { default as delaySync } from './delaySync';
