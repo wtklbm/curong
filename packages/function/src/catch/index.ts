d316601c72b4cdbd1d6af456e52d640b830b9ed3
wtklbm <wtklbm@gmail.com>
2024-08-03 14:12:50 +0800

    🦄 refactor: 更名为 `ifThrow`

--- packages/function/src/catch/index.ts
+++ packages/function/src/catch/index.ts
@@ -1,3 +1,3 @@
-export { default as catchOr } from './catchOr';
+export { default as ifThrow } from './ifThrow';
 export { default as noCatch } from './noCatch';
 export { default as tryCatch } from './tryCatch';
