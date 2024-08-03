8011f0d543afd14f44abfc028185d18a503ae453
wtklbm <wtklbm@gmail.com>
2024-08-03 13:53:43 +0800

    ✨ feat: 添加新方法

---
+++ packages/function/src/chain/index.ts
@@ -0,0 +1,6 @@
+export { default as after } from './after';
+export { default as applyAfter } from './applyAfter';
+export { default as applyBefore } from './applyBefore';
+export { default as before } from './before';
+export { default as withAfter } from './withAfter';
+export { default as withBefore } from './withBefore';
