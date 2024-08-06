fecbfcb367f8d92ac82a6656f45480f2300bff83
wtklbm <wtklbm@gmail.com>
2024-08-07 01:08:16 +0800

    🦄 refactor: 移动文件

--- packages/function/src/index.ts
+++ packages/function/src/index.ts
@@ -3,11 +3,11 @@ export { default as collectItems } from './collectItems';
 export { default as currying } from './currying';
 export { default as padding } from './padding';
 export { default as parallel } from './parallel';
-export { default as select } from './select';
 
 export * from './catch';
 export * from './chain';
 export * from './delay';
 export * from './limit';
+export * from './select';
 export * from './timeout';
 export * from './types';
