dacffe10e449645113d5005ca8be8a8204a216b0
wtklbm <wtklbm@gmail.com>
2024-08-08 14:28:03 +0800

    ✨ feat: 添加 `isArraySparse` 方法

--- packages/types/src/array/index.ts
+++ packages/types/src/array/index.ts
@@ -3,4 +3,5 @@ export { default as isArrayFilled } from './isArrayFilled';
 export { default as isArrayIndex } from './isArrayIndex';
 export { default as isArrayLike } from './isArrayLike';
 export { default as isArrayLikeFilled } from './isArrayLikeFilled';
+export { default as isArraySparse } from './isArraySparse';
 export { default as isArrayTwoDimensional } from './isArrayTwoDimensional';
