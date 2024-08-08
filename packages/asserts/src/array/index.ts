a0fe18b83f5674007b371fc83fb0fab3fb1658c4
wtklbm <wtklbm@gmail.com>
2024-08-08 14:33:03 +0800

    ✨ feat: 添加 `assertArraySparse` 方法

--- packages/asserts/src/array/index.ts
+++ packages/asserts/src/array/index.ts
@@ -3,4 +3,5 @@ export { default as assertArrayFilled } from './assertArrayFilled';
 export { default as assertArrayIndex } from './assertArrayIndex';
 export { default as assertArrayLike } from './assertArrayLike';
 export { default as assertArrayLikeFilled } from './assertArrayLikeFilled';
+export { default as assertArraySparse } from './assertArraySparse';
 export { default as assertArrayTwoDimensional } from './assertArrayTwoDimensional';
