fb3d09fe134fda46c6907aa4b80f9d2eb80ff7ff
wtklbm <wtklbm@gmail.com>
2024-08-08 14:27:58 +0800

    ✨ feat: 添加 `isArraySparse` 方法

---
+++ packages/types/src/array/isArraySparse.ts
@@ -0,0 +1,32 @@
+import isArray from './isArray';
+
+/**
+ * 是不是一个稀疏数组
+ *
+ * 稀疏数组是指数组中存在未定义的元素（即某些索引没有自己的属性）。
+ *
+ * @param value 要检查的值，可以是任何类型。
+ * @returns 如果 `value` 是稀疏数组，则返回 `true`；否则返回 `false`。
+ * @example
+ *
+ * ```typescript
+ * const arr1 = [1, 2, , 4]; // 稀疏数组
+ * const arr2 = [1, 2, 3, 4]; // 非稀疏数组
+ *
+ * console.log(isArraySparse(arr1)); // true
+ * console.log(isArraySparse(arr2)); // false
+ * ```
+ */
+export default function isArraySparse<T>(value: unknown): value is Array<T> {
+    if (!isArray(value)) {
+        return false;
+    }
+
+    for (let i = 0, l = value.length; i < l; i++) {
+        if (!value.hasOwnProperty(i)) {
+            return true;
+        }
+    }
+
+    return false;
+}
