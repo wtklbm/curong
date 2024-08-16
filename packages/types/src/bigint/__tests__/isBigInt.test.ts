3b38d0cfe69926ff12487d23d75d029db44aad86
wtklbm <wtklbm@gmail.com>
2024-08-16 15:15:25 +0800

    🧪 test: 添加新的测试

--- packages/types/src/bigint/__tests__/isBigInt.test.ts
+++ packages/types/src/bigint/__tests__/isBigInt.test.ts
@@ -8,5 +8,8 @@ describe('@curong/types/isBigInt', () => {
     test('测试2', () => {
         // @ts-ignore
         expect(isBigInt(1n)).toBe(true);
+        expect(isBigInt(BigInt(1n))).toBe(true);
+        expect(isBigInt(Object(1n))).toBe(true);
+        expect(isBigInt(new Object(1n))).toBe(true);
     });
 });
