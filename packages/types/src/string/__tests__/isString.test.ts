14cca2d42be7ba170dfe7a3d8b63963489fea7e1
wtklbm <wtklbm@gmail.com>
2024-08-16 15:16:43 +0800

    🧪 test: 添加新的测试

--- packages/types/src/string/__tests__/isString.test.ts
+++ packages/types/src/string/__tests__/isString.test.ts
@@ -7,7 +7,10 @@ describe('@curong/types/isString', () => {
 
     test('测试2', () => {
         expect(isString('')).toBe(true);
+        expect(isString(String(''))).toBe(true);
         expect(isString(new String())).toBe(true);
+        expect(isString(Object(''))).toBe(true);
+        expect(isString(new Object(''))).toBe(true);
     });
 
     test('测试3', () => {
