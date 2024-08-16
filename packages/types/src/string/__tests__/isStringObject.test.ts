f8371a3af0e847f74f727ebcbda60289158c370f
wtklbm <wtklbm@gmail.com>
2024-08-16 15:16:49 +0800

    🧪 test: 添加新的测试

--- packages/types/src/string/__tests__/isStringObject.test.ts
+++ packages/types/src/string/__tests__/isStringObject.test.ts
@@ -7,5 +7,7 @@ describe('@curong/types/isStringObject', () => {
 
     test('测试2', () => {
         expect(isStringObject(new String())).toBe(true);
+        expect(isStringObject(Object(''))).toBe(true);
+        expect(isStringObject(new Object(''))).toBe(true);
     });
 });
