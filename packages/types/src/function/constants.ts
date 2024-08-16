f8ec88bbabe8a1f9f690bd391117915e67e8ec60
wtklbm <wtklbm@gmail.com>
2024-08-16 15:17:32 +0800

    ✨ feat: 添加 `isNativeFunctionToString`

--- packages/types/src/function/constants.ts
+++ packages/types/src/function/constants.ts
@@ -3,3 +3,8 @@ export const asyncPattern = /Async/;
 
 /** 函数原型上的 `toString` 方法 */
 export const functionToString = Function.prototype.toString;
+
+/** 是否为原生的 `Function.prototype.toString` */
+export const isNativeFunctionToString =
+    functionToString.call(functionToString) ===
+    'function toString() { [native code] }';
