db7f285d542026c210265776c5c5a6c6de338c8d
wtklbm <wtklbm@gmail.com>
2024-08-16 15:15:21 +0800

    🧪 test: 添加新的测试

--- packages/number/__tests__/stringify.test.ts
+++ packages/number/__tests__/stringify.test.ts
@@ -1,39 +1,33 @@
 import { stringify } from '../src';
 
 describe('@curong/number/stringify', () => {
-    test('0 应该转换为 "0"', () => {
+    test('测试1', () => {
         expect(stringify(0)).toBe('0');
-    });
-
-    test('-0 应该转换为 "-0"', () => {
         expect(stringify(-0)).toBe('-0');
-    });
-
-    test('正无穷大应该转换为 "Infinity"', () => {
-        expect(stringify(Number.POSITIVE_INFINITY)).toBe('Infinity');
-    });
-
-    test('负无穷大应该转换为 "-Infinity"', () => {
-        expect(stringify(Number.NEGATIVE_INFINITY)).toBe('-Infinity');
-    });
-
-    test('NaN 应该转换为 "NaN"', () => {
-        expect(stringify(NaN)).toBe('NaN');
-    });
-
-    test('普通数字 42 应该转换为 "42"', () => {
         expect(stringify(42)).toBe('42');
-    });
-
-    test('负数 -42 应该转换为 "-42"', () => {
         expect(stringify(-42)).toBe('-42');
-    });
-
-    test('小数 3.14 应该转换为 "3.14"', () => {
         expect(stringify(3.14)).toBe('3.14');
+        expect(stringify(-3.14)).toBe('-3.14');
     });
 
-    test('负小数 -3.14 应该转换为 "-3.14"', () => {
-        expect(stringify(-3.14)).toBe('-3.14');
+    test('测试2', () => {
+        expect(stringify(Number.EPSILON)).toBe('2.220446049250313e-16')
+        expect(stringify(-Number.EPSILON)).toBe('-2.220446049250313e-16')
+
+        expect(stringify(Number.MAX_SAFE_INTEGER)).toBe('9007199254740991')
+        expect(stringify(-Number.MAX_SAFE_INTEGER)).toBe('-9007199254740991')
+
+        expect(stringify(Number.MAX_VALUE)).toBe('1.7976931348623157e+308')
+        expect(stringify(-Number.MAX_VALUE)).toBe('-1.7976931348623157e+308')
+
+        expect(stringify(Number.MIN_SAFE_INTEGER)).toBe('-9007199254740991')
+        expect(stringify(-Number.MIN_SAFE_INTEGER)).toBe('9007199254740991')
+
+        expect(stringify(Number.MIN_VALUE)).toBe('5e-324')
+        expect(stringify(-Number.MIN_VALUE)).toBe('-5e-324')
+
+        expect(stringify(Number.POSITIVE_INFINITY)).toBe('Infinity');
+        expect(stringify(Number.NEGATIVE_INFINITY)).toBe('-Infinity');
+        expect(stringify(NaN)).toBe('NaN');
     });
 });
