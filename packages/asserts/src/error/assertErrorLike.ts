9a625afe75eec82eb184cea548702fcce2397bc1
wtklbm <wtklbm@gmail.com>
2024-08-16 16:54:42 +0800

    📃 docs: 更新文档

--- packages/asserts/src/error/assertErrorLike.ts
+++ packages/asserts/src/error/assertErrorLike.ts
@@ -5,7 +5,8 @@ import typeGuard from '../constants/typeGuard';
 /**
  * 是不是一个类错误对象
  *
- * 该函数会判断传入的值是否是一个对象，并且该对象是否具有 `name`、`message` 和 `stack` 属性
+ * 该函数会判断传入的值是否是一个对象，并且该对象是否具有 `name`、`message` 和 `stack` 属性。
+ * `message` 的值可以为空，`name` 和 `stack` 的值不能为空。
  *
  * @param value 要验证的值
  * @param variableName 该值的变量名
