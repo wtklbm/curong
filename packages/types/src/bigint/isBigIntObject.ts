7abeb08cb3e4e6f5cf70afc6dd705e72a802a021
wtklbm <wtklbm@gmail.com>
2024-08-16 15:17:03 +0800

    📃 docs: 更新文档

--- packages/types/src/bigint/isBigIntObject.ts
+++ packages/types/src/bigint/isBigIntObject.ts
@@ -2,7 +2,7 @@ import getTagEqual from '../type/getTagEqual';
 import typeofEqual from '../type/typeofEqual';
 
 /**
- * 是不是一个被包装后的 `BigInt` 对象，即 `Object(0n)`
+ * 是不是一个被包装后的 `BigInt` 对象，即 `Object()`
  *
  * @param value 要验证的值
  * @returns 是则返回 `true`，否则为 `false`
