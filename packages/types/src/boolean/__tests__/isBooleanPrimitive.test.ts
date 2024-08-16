d1b67f73348e5d20b685ad7b3c3613cbf1cb47e8
wtklbm <wtklbm@gmail.com>
2024-08-16 15:15:51 +0800

    🧪 test: 添加新的测试

--- packages/types/src/boolean/__tests__/isBooleanPrimitive.test.ts
+++ packages/types/src/boolean/__tests__/isBooleanPrimitive.test.ts
@@ -3,6 +3,8 @@ import { isBooleanPrimitive } from '..';
 describe('@curong/types/isBooleanPrimitive', () => {
     test('测试1', () => {
         expect(isBooleanPrimitive(new Boolean(false))).toBe(false);
+        expect(isBooleanPrimitive(Object(false))).toBe(false);
+        expect(isBooleanPrimitive(new Object(false))).toBe(false);
     });
 
     test('测试2', () => {
