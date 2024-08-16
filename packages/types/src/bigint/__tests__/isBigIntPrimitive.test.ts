c83179fa31e57fe55f7f1f0b900d535a64032f94
wtklbm <wtklbm@gmail.com>
2024-08-16 15:15:37 +0800

    🧪 test: 添加新的测试

--- packages/types/src/bigint/__tests__/isBigIntPrimitive.test.ts
+++ packages/types/src/bigint/__tests__/isBigIntPrimitive.test.ts
@@ -4,11 +4,13 @@ describe('@curong/types/isBigIntPrimitive', () => {
     test('测试1', () => {
         expect(isBigIntPrimitive(0)).toBe(false);
         expect(isBigIntPrimitive(1)).toBe(false);
+        expect(isBigIntPrimitive(Object(1n))).toBe(false);
+        expect(isBigIntPrimitive(new Object(1n))).toBe(false);
     });
 
     test('测试2', () => {
         expect(isBigIntPrimitive(-1n)).toBe(true);
         expect(isBigIntPrimitive(0n)).toBe(true);
-        expect(isBigIntPrimitive(Object(1n))).toBe(false);
+        expect(isBigIntPrimitive(BigInt(1n))).toBe(true);
     });
 });
