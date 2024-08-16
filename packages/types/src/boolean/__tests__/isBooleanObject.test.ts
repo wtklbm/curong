7060e316dd9f02c794c6d5a1d30181aad3f4f47a
wtklbm <wtklbm@gmail.com>
2024-08-16 15:15:45 +0800

    🧪 test: 添加新的测试

--- packages/types/src/boolean/__tests__/isBooleanObject.test.ts
+++ packages/types/src/boolean/__tests__/isBooleanObject.test.ts
@@ -6,6 +6,9 @@ describe('@curong/types/isBooleanObject', () => {
     });
 
     test('测试2', () => {
+        expect(isBooleanObject(Boolean(false))).toBe(false);
         expect(isBooleanObject(new Boolean(false))).toBe(true);
+        expect(isBooleanObject(Object(false))).toBe(true);
+        expect(isBooleanObject(new Object(false))).toBe(true);
     });
 });
