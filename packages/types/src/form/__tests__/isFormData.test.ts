e2ced218466dc7bcab510c5a4d79c7025524d679
wtklbm <wtklbm@gmail.com>
2024-08-16 15:16:14 +0800

    🧪 test: 添加新的测试

--- packages/types/src/form/__tests__/isFormData.test.ts
+++ packages/types/src/form/__tests__/isFormData.test.ts
@@ -9,4 +9,14 @@ describe('@curong/types/isFormData', () => {
         expect(isFormData({})).toBe(false);
         expect(isFormData(new FormData())).toBe(true);
     });
+
+    test('测试2', () => {
+        const formData = new FormData();
+        formData.append('username', 'JohnDoe');
+        const fileInput: any = document.querySelector('input[type="file"]');
+        const file = fileInput?.files[0];
+        formData.append('profilePicture', file);
+
+        expect(isFormData(formData)).toBe(true);
+    });
 });
