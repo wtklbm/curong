23de9616c06575bded24c171d5c7489d9f83f9e5
wtklbm <wtklbm@gmail.com>
2024-08-16 15:16:38 +0800

    🧪 test: 添加新的测试

--- packages/types/src/regexp/__tests__/isRegExp.test.ts
+++ packages/types/src/regexp/__tests__/isRegExp.test.ts
@@ -3,6 +3,7 @@ import { isRegExp } from '..';
 describe('@curong/types/isRegExp', () => {
     test('测试1', () => {
         expect(isRegExp(/\d/)).toBe(true);
+        expect(isRegExp(new RegExp('\\d'))).toBe(true);
     });
 
     test('测试2', () => {
