0d8aac18c3d70f10479aa562df20e6f07e78d25a
wtklbm <wtklbm@gmail.com>
2024-08-17 23:01:52 +0800

    ✨ feat: 添加新的类型

--- packages/util/src/copy.ts
+++ packages/util/src/copy.ts
@@ -1,10 +1,12 @@
 import {
     getTag,
+    isBigIntPrimitive,
     isBuffer,
     isFunction,
     isNull,
     isPrimitive,
-    isSymbol
+    isSymbol,
+    isSymbolPrimitive
 } from '@curong/types';
 
 import allAttrs from './allAttrs';
@@ -12,15 +14,31 @@ import lackAttrs from './lackAttrs';
 
 /** 拷贝基本类型的值 */
 const copyBaseType = (value: any) => {
-    return new (Object.getPrototypeOf(value).constructor)(value);
+    return new (Object.getPrototypeOf(value).constructor)(value.valueOf());
 };
 
 /** 拷贝 `ArrayBuffer` */
 const copyArrayBuffer = (value: any) => {
     const newArrayBuffer = new value.constructor(value.byteLength);
-    new Int16Array(newArrayBuffer).set(new Int16Array(value));
+    new Uint8Array(newArrayBuffer).set(new Uint8Array(value));
 
     return newArrayBuffer;
+};
+
+const copyGenerator = <T>(gen: Generator<T>): (() => Generator<T>) => {
+    const state: T[] = [];
+
+    for (let value of gen) {
+        state.push(value);
+    }
+
+    function* newGenerator(): Generator<T> {
+        for (let value of state) {
+            yield value;
+        }
+    }
+
+    return newGenerator;
 };
 
 const copyAttrs = <T extends object>(
@@ -44,10 +62,14 @@ const copyAttrs = <T extends object>(
                 set = copyFunction(descriptor.set, hash);
             }
 
-            if (get || set) {
-                Object.defineProperty(memo, key, { ...descriptor, get, set });
-                return memo;
-            }
+            Object.defineProperty(memo, key, {
+                ...descriptor,
+                ...(get || set
+                    ? { get, set }
+                    : { value: recursiveCopy(src[key], hash) })
+            });
+
+            return memo;
         }
 
         memo[key] = recursiveCopy(src[key], hash);
@@ -58,56 +80,83 @@ const copyAttrs = <T extends object>(
 
 /** 根据 `toStringTag` 拷贝类型 */
 const copyByTag = <T extends object>(value: any, weak: WeakMap<T, T>) => {
-    switch (getTag(value)) {
-        case 'BigInt':
-            return BigInt(value);
+    const typeTag = getTag(value);
 
-        case 'RegExp':
+    switch (typeTag) {
+        case 'Null':
+        case 'Undefined':
+            return value;
+
+        case 'BigInt':
+            return isBigIntPrimitive(value)
+                ? BigInt(value)
+                : Object(BigInt(value));
+
+        case 'Boolean':
+        case 'Number':
+        case 'String':
+            return isPrimitive(value) ? value : copyBaseType(value);
+
+        case 'Symbol':
+            return isSymbolPrimitive(value)
+                ? Symbol(value.description)
+                : Object(Symbol(value.description));
+
+        case 'RegExp': {
             const reg = new RegExp(value);
             reg.lastIndex = (value as RegExp).lastIndex;
             return reg;
+        }
 
         case 'BigInt64Array':
         case 'BigUint64Array':
             return new value.constructor(value);
 
-        case 'DataView':
+        case 'DataView': {
             const buffer = copyArrayBuffer(value.buffer);
             const { byteOffset, byteLength } = value as DataView;
             return new DataView(buffer, byteOffset, byteLength);
+        }
 
         case 'Blob':
-        case 'File':
+            return new Blob([value], { type: value.type });
+
+        case 'File': {
             const { lastModified, type } = value as File;
             return new File([value], value.name, { lastModified, type });
+        }
 
-        case 'FormData':
+        case 'FormData': {
             const formData = new value.constructor();
             for (const [k, v] of value) {
                 formData.append(k, v);
             }
             return formData;
+        }
 
         // `String` 为 `string` 的包装的形式
-        case 'Boolean':
         case 'Date':
         case 'FileReader':
-        case 'Map':
-        case 'Number':
         case 'Set':
-        case 'String':
+        case 'Map':
+        case 'URL':
+        case 'URLSearchParams':
             return copyBaseType(value);
 
+        case 'Generator':
+            return copyGenerator(value);
+
         case 'ArrayBuffer':
-        case 'Float32Array':
-        case 'Float64Array':
-        case 'Int16Array':
-        case 'Int32Array':
+        case 'SharedArrayBuffer':
         case 'Int8Array':
-        case 'Uint16Array':
-        case 'Uint32Array':
         case 'Uint8Array':
         case 'Uint8ClampedArray':
+        case 'Int16Array':
+        case 'Uint16Array':
+        case 'Int32Array':
+        case 'Uint32Array':
+        case 'Float32Array':
+        case 'Float64Array':
             return copyArrayBuffer(value);
 
         case 'Arguments':
@@ -117,29 +166,37 @@ const copyByTag = <T extends object>(value: any, weak: WeakMap<T, T>) => {
         case 'Readable':
         case 'Stream':
         case 'Transform':
-        case 'Writable':
+        case 'Writable': {
             const proto = Object.getPrototypeOf(value);
             const instance = isNull(proto)
                 ? Object.create(null)
                 : new proto.constructor();
             weak.set(value, instance);
             return copyAttrs(value, instance, weak);
+        }
 
+        case 'DOMException':
         case 'Error':
         case 'EvalError':
         case 'RangeError':
         case 'ReferenceError':
         case 'SyntaxError':
         case 'TypeError':
-            const ins = new (Object.getPrototypeOf(value).constructor)();
+        case 'URIError': {
+            const ins =
+                value instanceof AggregateError
+                    ? new AggregateError(value.errors.map(copy), value.message)
+                    : new (Object.getPrototypeOf(value).constructor)();
+
             weak.set(value, ins);
             const o = copyAttrs(value, ins, weak);
             o.stack = value.stack; // 堆栈也要一样
             return o;
+        }
 
         default:
             throw new EvalError(
-                '[copy]: 无法进行深度拷贝，对象中包含不支持类型'
+                `[copy]: 无法进行深度拷贝，${typeTag} 为不支持的类型`
             );
     }
 };
@@ -154,12 +211,23 @@ const copyFunction = (value: any, weak: any) => {
     let source: string;
     let m: any;
 
+    // 如果是 `ECMAScript` 原生函数
+    if (
+        // `function () { [native code] }` 中包含 `[native code]`
+        src.includes('[native code]') &&
+        // 不是经过 `.bind()` 之后的函数
+        !(name.startsWith('bound ') && !value.hasOwnProperty('prototype'))
+    ) {
+        return value;
+    }
+
     // 区分 `getter`、`setter`、箭头函数和普通函数，生成不同的函数源代码
     if ((m = /^(get|set) (\S+)$/.exec(name))) {
         const [, flag, name] = m;
         source = getDescriptorSource(src, name, flag);
-    } else if (src.startsWith('(')) {
-        source = `const ${name} = ${src}; return ${name};`;
+    } else if (/^(:?async ?\* ?)?\[/.test(src)) {
+        // 处理类似于 `[Symbol.iterator]() {}` || `async *[Symbol.asyncIterator]() {}` 的情况
+        source = `const __$$INNER$$__ = { ${src} }; return __$$INNER$$__${name};`;
     } else {
         source = `return ${src};`;
     }
@@ -249,12 +317,16 @@ function recursiveCopy<T extends object>(
  *   - `Uint32Array`
  *   - `Uint8Array`
  *   - `Uint8ClampedArray`
+ *   - `DOMException`
  *   - `Error`
  *   - `EvalError`
  *   - `RangeError`
  *   - `ReferenceError`
  *   - `SyntaxError`
  *   - `TypeError`
+ *   - `URIError`
+ *   - `URL`
+ *   - `URLSearchParams`
  *   - `Duplex`
  *   - `Readable`
  *   - `Stream`
@@ -264,11 +336,15 @@ function recursiveCopy<T extends object>(
  * - 其他的一些普通的对象，或者原型为 `null` 的对象
  *
  *
- * ### 不支持的类型
+ * ### 已知的不支持类型
  *
- *   1. `Promise` 是不支持的类型，它没有相关的 `toString` 方法。
- *   2. `WeakSet` 和 `WeakMap` 是不支持的类型，因为他们都不支持迭代器模式。
- *   3. 自定义数据类型是不支持的类型，目前还没有办法将它们一一实现。
+ * - 自定义数据类型: 还没有办法将它们一一实现
+ * - `ECMAScript` 原生函数: 源代码不可见
+ * - `Promise`: 没有相关的 `toString` 方法
+ * - `WeakSet` 和 `WeakMap`: 不支持迭代器模式，无法迭代
+ * - `Event`
+ * - `CustomEvent`
+ * - `DataTransfer`
  *
  * # 常见的拷贝方法
  *
@@ -298,6 +374,6 @@ function recursiveCopy<T extends object>(
  * console.log(ret === obj); // false
  * ```
  */
-export default function copy<T extends any>(value: T): T {
+export default function copy<T>(value: T): T {
     return recursiveCopy(value);
 }
