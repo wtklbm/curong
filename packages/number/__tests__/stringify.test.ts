cc02e2c506c15e5d57e99acc92a041423f6f86f8
wtklbm <wtklbm@gmail.com>
2024-08-17 23:19:34 +0800

    🌈 style: 格式化代码

--- packages/number/__tests__/stringify.test.ts
+++ packages/number/__tests__/stringify.test.ts
@@ -11,20 +11,20 @@ describe('@curong/number/stringify', () => {
     });
 
     test('测试2', () => {
-        expect(stringify(Number.EPSILON)).toBe('2.220446049250313e-16')
-        expect(stringify(-Number.EPSILON)).toBe('-2.220446049250313e-16')
+        expect(stringify(Number.EPSILON)).toBe('2.220446049250313e-16');
+        expect(stringify(-Number.EPSILON)).toBe('-2.220446049250313e-16');
 
-        expect(stringify(Number.MAX_SAFE_INTEGER)).toBe('9007199254740991')
-        expect(stringify(-Number.MAX_SAFE_INTEGER)).toBe('-9007199254740991')
+        expect(stringify(Number.MAX_SAFE_INTEGER)).toBe('9007199254740991');
+        expect(stringify(-Number.MAX_SAFE_INTEGER)).toBe('-9007199254740991');
 
-        expect(stringify(Number.MAX_VALUE)).toBe('1.7976931348623157e+308')
-        expect(stringify(-Number.MAX_VALUE)).toBe('-1.7976931348623157e+308')
+        expect(stringify(Number.MAX_VALUE)).toBe('1.7976931348623157e+308');
+        expect(stringify(-Number.MAX_VALUE)).toBe('-1.7976931348623157e+308');
 
-        expect(stringify(Number.MIN_SAFE_INTEGER)).toBe('-9007199254740991')
-        expect(stringify(-Number.MIN_SAFE_INTEGER)).toBe('9007199254740991')
+        expect(stringify(Number.MIN_SAFE_INTEGER)).toBe('-9007199254740991');
+        expect(stringify(-Number.MIN_SAFE_INTEGER)).toBe('9007199254740991');
 
-        expect(stringify(Number.MIN_VALUE)).toBe('5e-324')
-        expect(stringify(-Number.MIN_VALUE)).toBe('-5e-324')
+        expect(stringify(Number.MIN_VALUE)).toBe('5e-324');
+        expect(stringify(-Number.MIN_VALUE)).toBe('-5e-324');
 
         expect(stringify(Number.POSITIVE_INFINITY)).toBe('Infinity');
         expect(stringify(Number.NEGATIVE_INFINITY)).toBe('-Infinity');
