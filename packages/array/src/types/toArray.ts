8417e6f6872651e79e58b39c4f879c0c1a2c931a
wtklbm <wtklbm@gmail.com>
2024-08-22 19:25:20 +0800

    ✨ feat: 添加 `toArray` 方法

---
+++ packages/array/src/types/toArray.ts
@@ -0,0 +1,7 @@
+export type ToArrayOptions = {
+    /** 当值为 `null` 时是否可以转换为 `[null]`。默认为 `false` */
+    allowNull?: boolean;
+
+    /** 当值为 `undefined` 时是否可以转换为 `[undefined]`，默认为 `false` */
+    allowUndefined?: boolean;
+};
