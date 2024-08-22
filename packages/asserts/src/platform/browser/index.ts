b86bcd1a9566cde0cc2af8283616d5f0f13e78b9
wtklbm <wtklbm@gmail.com>
2024-08-22 16:29:45 +0800

    🦄 refactor: 移动文件位置

---
+++ packages/asserts/src/platform/browser/index.ts
@@ -0,0 +1,1 @@
+export { default as assertBrowser } from './assertBrowser';
--- packages/asserts/src/platform/index.ts
+++ packages/asserts/src/platform/index.ts
@@ -1,4 +1,5 @@
-export { default as assertBrowser } from './assertBrowser';
 export { default as assertDeno } from './assertDeno';
 export { default as assertNodejs } from './assertNodejs';
 export { default as assertWebWorker } from './assertWebWorker';
+
+export * from './browser';
