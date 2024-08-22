be6eb9f2410d7f0432b1c369f23570b54bcafe18
wtklbm <wtklbm@gmail.com>
2024-08-22 16:13:25 +0800

    🎈 perf: 将选项变为可选

--- packages/number/src/toLocaleString.ts
+++ packages/number/src/toLocaleString.ts
@@ -7,31 +7,29 @@ import type { ToLocaleStringOptions } from './types';
  *
  * @param value 要格式化的数字
  * @param options 格式化选项
- *  - `separator`: 分隔符。默认为 `,`
+ *  - `delimiter`: 分隔符。默认为 `,`
  *  - `capacity`: 进制的间隔数。默认为 `3` (千进制)
  * @returns 返回格式化后的字符串
  * @throws
- *
  *  - 如果 `value` 不是一个有限数，则会抛出类型错误异常
  *  - 如果 `capacity` 不是一个无符号整数，则会抛出类型错误异常
- *
  * @example
  *
  * ```typescript
  * toLocaleString(123456789); // 123,456,789
- * toLocaleString(123456789, { separator: '_', capacity: 4 }); // 1_2345_6789
+ * toLocaleString(123456789, { delimiter: '_', capacity: 4 }); // 1_2345_6789
  * ```
  */
 export default function toLocaleString(
-    value: number,
-    options: ToLocaleStringOptions
+    value: number | string,
+    options?: ToLocaleStringOptions
 ): string {
-    if (!isFinite(value)) {
+    if (!isFinite(+value)) {
         throw new TypeError(`[toLocaleString] value 必须是一个有效的数字`);
     }
 
     const numStr = value.toString();
-    const { separator, capacity } = { separator: ',', capacity: 3, ...options };
+    const { delimiter, capacity } = { delimiter: ',', capacity: 3, ...options };
 
     if (!isUintFilled(capacity)) {
         throw new TypeError(
@@ -42,7 +40,7 @@ export default function toLocaleString(
     const [integerPart, decimalPart] = numStr.split('.');
 
     const regex = new RegExp(`\\B(?=(\\d{${capacity}})+(?!\\d))`, 'g');
-    const integerWithCommas = integerPart.replace(regex, separator);
+    const integerWithCommas = integerPart.replace(regex, delimiter);
 
     return decimalPart
         ? `${integerWithCommas}.${decimalPart}`
