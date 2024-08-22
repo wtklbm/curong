2a4ba34c2647e5306c9919ff65ec99d8414d83fc
wtklbm <wtklbm@gmail.com>
2024-08-22 17:51:51 +0800

    ✨ feat: 添加 `trimAll` 方法

--- packages/string/src/trim/index.ts
+++ packages/string/src/trim/index.ts
@@ -1,4 +1,5 @@
 export { default as trim } from './trim';
+export { default as trimAll } from './trimAll';
 export { default as trimEnd } from './trimEnd';
 export { default as trimStart } from './trimStart';
 
