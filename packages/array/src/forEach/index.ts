396f602499817b1f2c2427f20f5004be9511edf6
wtklbm <wtklbm@gmail.com>
2024-08-23 15:01:15 +0800

    ✨ feat: 添加新的方法

---
+++ packages/array/src/forEach/index.ts
@@ -0,0 +1,6 @@
+export { default as each } from './each';
+export { default as eachAsync } from './eachAsync';
+export { default as eachParallel } from './eachParallel';
+export { default as eachRight } from './eachRight';
+export { default as eachRightAsync } from './eachRightAsync';
+export { default as eachRightParallel } from './eachRightParallel';
