50990dc7f91cf61b8ae0c3bfb3668abb27b5b493
wtklbm <wtklbm@gmail.com>
2024-08-24 11:35:20 +0800

    🧪 test: 添加 `splitByStep` 方法的测试

---
+++ packages/array/__tests__/splitByStep.test.ts
@@ -0,0 +1,87 @@
+import { splitByStep } from '../src';
+
+describe('@curong/array/splitByStep', () => {
+    test('测试1', () => {
+        const v: any = [];
+
+        expect(() => splitByStep(v, 0)).toThrow();
+        expect(() => splitByStep(v, -1)).toThrow();
+        expect(() => splitByStep(v, -1.2)).toThrow();
+
+        expect(splitByStep(v, 1)).toEqual([[]]);
+        expect(splitByStep(v, 2)).toEqual([[]]);
+        expect(splitByStep(v, 30)).toEqual([[]]);
+    });
+
+    test('测试2', () => {
+        const v: any = [0];
+
+        expect(splitByStep(v, 1)).toEqual([[0]]);
+        expect(splitByStep(v, 2)).toEqual([[0]]);
+        expect(splitByStep(v, 30)).toEqual([[0]]);
+    });
+
+    test('测试3', () => {
+        const v = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
+
+        expect(splitByStep(v, 1)).toEqual([
+            [1],
+            [2],
+            [3],
+            [4],
+            [5],
+            [6],
+            [7],
+            [8],
+            [9],
+            [10],
+            [11]
+        ]);
+    });
+
+    test('测试4', () => {
+        const v = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
+
+        expect(splitByStep(v, 2)).toEqual([
+            [1, 2],
+            [3, 4],
+            [5, 6],
+            [7, 8],
+            [9, 10],
+            [11]
+        ]);
+    });
+
+    test('测试5', () => {
+        const v = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
+
+        expect(splitByStep(v, 5)).toEqual([
+            [1, 2, 3, 4, 5],
+            [6, 7, 8, 9, 10],
+            [11]
+        ]);
+    });
+
+    test('测试6', () => {
+        const v = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
+
+        expect(splitByStep(v, 10)).toEqual([
+            [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+            [11]
+        ]);
+    });
+
+    test('测试7', () => {
+        const v = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
+
+        expect(() => splitByStep(v, 1.5)).toThrow();
+    });
+
+    test('测试8', () => {
+        const v = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
+
+        expect(splitByStep(v, 11)).toEqual([
+            [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
+        ]);
+    });
+});
