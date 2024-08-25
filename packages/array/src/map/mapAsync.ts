13a7abf7e140b686f3c792830a106d7eef63fefd
wtklbm <wtklbm@gmail.com>
2024-08-25 18:47:01 +0800

    ✨ feat: 添加 `mapAsync` 方法

---
+++ packages/array/src/map/mapAsync.ts
@@ -0,0 +1,29 @@
+/**
+ * 异步串行遍历数组并对每个元素依次执行回调函数
+ *
+ * @param value 要遍历的数组
+ * @param callback 对每个元素执行的异步串行回调函数
+ * @returns 返回一个 `Promise`，解析为包含处理结果的新数组
+ * @example
+ *
+ * ```typescript
+* const arr = [1, 2, 3, 4];
+*
+* await mapAsync(arr, async (value, index) => {
+*     console.log(`索引 ${index} 的值是 ${value}`);
+*     return `${value}`.toLowerCase();
+* }); // [ '1', '2', '3', '4' ]
+ * ```
+ */
+export default async function mapAsync<T, R>(
+    value: T[],
+    callback: (value: T, index: number, array: T[]) => Promise<R>,
+): Promise<R[]> {
+    const result = [];
+
+    for (let index = 0; index < value.length; index++) {
+        result.push(await callback(value[index], index, value));
+    }
+
+    return result;
+}
