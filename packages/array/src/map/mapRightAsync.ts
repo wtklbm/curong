3a0a67ac8e110566cdfade70dd3ecfe54ea118df
wtklbm <wtklbm@gmail.com>
2024-08-25 19:01:07 +0800

    🦄 refactor: 更新类型声明

--- packages/array/src/map/mapRightAsync.ts
+++ packages/array/src/map/mapRightAsync.ts
@@ -7,17 +7,17 @@
  * @example
  *
  * ```typescript
-* const arr = [1, 2, 3, 4];
-*
-* await mapRightAsync(arr, async (value, index) => {
-*     console.log(`索引 ${index} 的值是 ${value}`);
-*     return `${value}`.toLowerCase();
-* }); // [ '4', '3', '2', '1' ]
-* ```
-*/
+ * const arr = [1, 2, 3, 4];
+ *
+ * await mapRightAsync(arr, async (value, index) => {
+ *     console.log(`索引 ${index} 的值是 ${value}`);
+ *     return `${value}`.toLowerCase();
+ * }); // [ '4', '3', '2', '1' ]
+ * ```
+ */
 export default async function mapRightAsync<T, R>(
     value: T[],
-    callback: (value: T, index: number, array: T[]) => Promise<R>
+    callback: (value: T, index: number, array: T[]) => Promise<R> | R
 ): Promise<R[]> {
     const result: R[] = [];
 
