8803c5dca589a473b7f17d3c724db1554608b95c
wtklbm <wtklbm@gmail.com>
2024-08-25 18:49:29 +0800

    ✨ feat: 添加 `map` 方法

---
+++ packages/array/src/map/index.ts
@@ -0,0 +1,5 @@
+export { default as mapAsync } from './mapAsync';
+export { default as mapParallel } from './mapParallel';
+export { default as mapRight } from './mapRight';
+export { default as mapRightAsync } from './mapRightAsync';
+export { default as mapRightParallel } from './mapRightParallel';
