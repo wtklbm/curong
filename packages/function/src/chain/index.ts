ed5d72cbfea7035d40cc26a494b0cc35c938be78
wtklbm <wtklbm@gmail.com>
2024-08-28 01:02:19 +0800

    ✨ feat: 添加 `compose` 方法

--- packages/function/src/chain/index.ts
+++ packages/function/src/chain/index.ts
@@ -2,5 +2,6 @@ export { default as after } from './after';
 export { default as applyAfter } from './applyAfter';
 export { default as applyBefore } from './applyBefore';
 export { default as before } from './before';
+export { default as compose } from './compose';
 export { default as withAfter } from './withAfter';
 export { default as withBefore } from './withBefore';
