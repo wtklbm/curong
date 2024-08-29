15b541782bc58f28f7f37f445fcf07c16db6aff3
wtklbm <wtklbm@gmail.com>
2024-08-30 01:29:49 +0800

    ✨ feat: 添加 `nextTick` 方法

--- packages/function/src/timeout/index.ts
+++ packages/function/src/timeout/index.ts
@@ -1,5 +1,6 @@
 export { default as clearTimeoutAll } from './clearTimeoutAll';
+export { default as nextTick } from './nextTick';
 export { default as setTimeout } from './setTimeout';
-export { default as timeoutMsResolve } from './timeoutMsResolve';
 export { default as timeoutLog } from './timeoutLog';
+export { default as timeoutMsResolve } from './timeoutMsResolve';
 export { default as timeoutOr } from './timeoutOr';
