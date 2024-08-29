59671d0c7de4353df519a1640d4b872544b47d90
wtklbm <wtklbm@gmail.com>
2024-08-29 20:05:05 +0800

    🧪 test: 添加新的测试

--- packages/types/src/property/__tests__/hasOwnProperty.test.ts
+++ packages/types/src/property/__tests__/hasOwnProperty.test.ts
@@ -1,3 +1,5 @@
+// @ts-nocheck
+
 import { hasOwnProperty } from '..';
 
 describe('@curong/types/hasOwnProperty', () => {
@@ -87,5 +89,53 @@ describe('@curong/types/hasOwnProperty', () => {
         expect(hasOwnProperty(c, 'p')).toBe(true);
         expect(hasOwnProperty(c, 'c')).toBe(true);
         expect(hasOwnProperty(c, 'toString')).toBe(false);
+    });
+
+    test('测试5', () => {
+        const example = {};
+        expect(hasOwnProperty(example, 'prop')).toBe(false);
+
+        example.prop = 'exists';
+        expect(hasOwnProperty(example, 'prop')).toBe(true);
+        example.prop = null;
+        expect(hasOwnProperty(example, 'prop')).toBe(true);
+
+        example.prop = undefined;
+        expect(hasOwnProperty(example, 'prop')).toBe(true);
+    });
+
+    test('测试6', () => {
+        const example = {};
+        example.prop = 'exists';
+
+        expect(hasOwnProperty(example, 'prop')).toBe(true);
+        expect(hasOwnProperty(example, 'toString')).toBe(false);
+        expect(hasOwnProperty(example, 'hasOwnProperty')).toBe(false);
+
+        // `in` 运算符对目标对象的直接属性或继承属性均会返回 true：
+        expect('prop' in example).toBe(true);
+        expect('toString' in example).toBe(true);
+        expect('hasOwnProperty' in example).toBe(true);
+    });
+
+    test('测试7', () => {
+        const fruits = ['Apple', 'Banana', 'Watermelon', 'Orange'];
+        expect(hasOwnProperty(fruits, 3)).toBe(true);
+        expect(hasOwnProperty(fruits, 4)).toBe(false);
+    });
+
+    test('测试8', () => {
+        const foo = {
+            hasOwnProperty() {
+                return false;
+            },
+            bar: 'The dragons be out of office'
+        };
+
+        expect(hasOwnProperty(foo, 'bar')).toBe(true);
+
+        const foo2 = Object.create(null);
+        foo2.prop = 'exists';
+        expect(hasOwnProperty(foo2, 'prop')).toBe(true);
     });
 });
