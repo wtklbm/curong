757fa634ed7c436604f87aa3bda359b9fd6f6122
wtklbm <wtklbm@gmail.com>
2024-08-30 01:42:55 +0800

    📃 docs: 更新 `singletonClass` 的文档

--- packages/function/src/class/singletonClass.ts
+++ packages/function/src/class/singletonClass.ts
@@ -1,10 +1,10 @@
 import type { Class } from '@curong/types';
 
-export const CLASS_SINGLETON_KEY = Symbol.for('[[SINGLETON]]');
-
-export type Singleton<T extends Class> = T & {
+export type SingletonClass<T extends Class> = T & {
     [CLASS_SINGLETON_KEY]: InstanceType<T>;
 };
+
+const CLASS_SINGLETON_KEY = Symbol.for('[[SINGLETON]]');
 
 /**
  * 创建一个类的单例模式代理，确保该类在应用中只能被实例化一次
@@ -13,16 +13,14 @@ export type Singleton<T extends Class> = T & {
  *
  * @param classTarget 需要应用单例模式的类
  * @returns 返回该类的单例代理，当该类被实例化时，将确保只返回同一个实例
- *
- * @template T 类的类型，要求是一个构造函数
- *
  * @example
+ *
  * ```typescript
  * class MyClass {
  *   constructor(public value: number) {}
  * }
  *
- * const SingletonMyClass = singleton(MyClass);
+ * const SingletonMyClass = singletonClass(MyClass);
  *
  * const instance1 = new SingletonMyClass(1);
  * const instance2 = new SingletonMyClass(2);
@@ -33,7 +31,7 @@ export type Singleton<T extends Class> = T & {
  */
 export default function singletonClass<T extends Class>(classTarget: T) {
     return new Proxy(classTarget, {
-        construct(target: Singleton<T>, argumentsList, newTarget) {
+        construct(target: SingletonClass<T>, argumentsList, newTarget) {
             if (target.prototype !== newTarget.prototype) {
                 return Reflect.construct(target, argumentsList, newTarget);
             }
