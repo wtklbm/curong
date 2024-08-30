92423f46c165967c2f7355911f5b6a31f2a52da5
wtklbm <wtklbm@gmail.com>
2024-08-30 16:26:48 +0800

    ✨ feat: 添加 `retry` 方法的类型

---
+++ packages/function/src/catch/types/retry.ts
@@ -0,0 +1,42 @@
+export interface RetryOptions<T = any> {
+    /**
+     * 重试失败任务之前等待的时间（以毫秒为单位）。默认为 `0`，即不等待
+     *
+     * 该选项可以传递一个数字，表示等待固定的毫秒数。
+     * 也可以传递一个对象，表示生成从 `start` (包含) 到 `end` (包含) 之间的随机毫秒数。
+     */
+    retryWait?:
+        | number
+        /** 在每次重试时，都生成一个新的随机等待毫秒数 */
+        | {
+              /** 起始数字 (包含) */
+              start: number;
+
+              /** 结束数字 (包含) */
+              end: number;
+          };
+
+    /**
+     * 处理任务执行过程中发生的错误的函数
+     *
+     * 如果没有指定该函数，则当执行任务发生错误时，会立即抛出错误。
+     *
+     * @param error 发生的错误
+     * @returns 该方法可以返回一个值，该值可以作为该任务的最终值存储在结果数组中。
+     * 也就是说，如果当前任务报错，就可以返回一个值作为该任务的默认值。
+     *  - 如果设置了 `maxRetry` 选项，则经过多次重试后，才会将这个默认值设置为该任务的最终值
+     *  - 如果设置了 `onProgressRetry` 且返回 `true`，则会立即将这个默认值设置为该任务的最终值
+     */
+    onError?: <E extends Error>(error: E) => T | undefined;
+
+    /**
+     * 每次任务失败并重试时调用的回调函数
+     *
+     * @param error 发生的错误
+     * @param attempts 这是第几次重试
+     * @returns 如果该函数返回 `true`，则不再进行重试
+     *  - 如果同时传递了 `onError` 且该函数有返回值，则返回该值
+     *  - 如果没有传递 `onError`，则返回 `undefined`
+     */
+    onProgressRetry?: (error: Error, attempts: number) => boolean | undefined;
+}
