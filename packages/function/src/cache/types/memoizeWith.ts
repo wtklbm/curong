68669379caa8c3a6686cb55fd774eff91266187c
wtklbm <wtklbm@gmail.com>
2024-08-30 23:16:50 +0800

    ✨ feat: 更新类型声明

--- packages/function/src/cache/types/memoizeWith.ts
+++ packages/function/src/cache/types/memoizeWith.ts
@@ -1,4 +1,10 @@
 export type MemoizeWithReturn<F extends (...args: unknown[]) => unknown, K> = {
+    /** 缓存对象 */
+    cache: Map<K, {value: ReturnType<F>, expiration: number}>;
+
+    /** 清空所有已缓存的值 */
     clear(): void;
-    cache: Map<K, ReturnType<F>>;
+
+    /** 清空已缓存的键的值 */
+    delete(...args: Parameters<F>): void;
 } & F;
