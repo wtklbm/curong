05aec9226d61e2b1272b50ba723dfcfbaaebc555
wtklbm <wtklbm@gmail.com>
2024-08-30 16:26:29 +0800

    ✨ feat: 添加 `retry` 方法

--- packages/function/src/catch/index.ts
+++ packages/function/src/catch/index.ts
@@ -1,3 +1,4 @@
 export { default as ifThrow } from './ifThrow';
 export { default as noCatch } from './noCatch';
+export { default as retry } from './retry';
 export { default as tryCatch } from './tryCatch';
