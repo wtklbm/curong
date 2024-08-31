d8d456d8b74d5b86e34451980f3cf978f66b910c
wtklbm <wtklbm@gmail.com>
2024-08-31 15:38:52 +0800

    📃 docs: 更新文档

--- packages/types/src/variable/isVarName.ts
+++ packages/types/src/variable/isVarName.ts
@@ -4,6 +4,7 @@ import isString from '../string/isString';
  * 是不是可以把值当做 `JavaScript` 的变量名
  *
  * @param value 想要当做变量名的值
+ * @note 该方法使用了 `new Function`，如果用户禁用了 [eval](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/eval)，则导致代码异常
  * @returns 是则返回 `true`，否则为 `false`
  */
 export default function isVarName(value: unknown): value is string {
