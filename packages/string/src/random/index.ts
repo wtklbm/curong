5d3de1e8bfbfcfd83152d6b00f33ae5d76062079
wtklbm <wtklbm@gmail.com>
2024-09-04 00:13:53 +0800

    ✨ feat: 添加新的方法

--- packages/string/src/random/index.ts
+++ packages/string/src/random/index.ts
@@ -1,1 +1,5 @@
+export { default as randomBoolean } from './randomBoolean';
+export { default as randomHexColor } from './randomHexColor';
+export { default as randomLower } from './randomLower';
 export { default as randomNumber } from './randomNumber';
+export { default as randomUpper } from './randomUpper';
