20e1c0c4a507c82dbf01d4d9805c9623a14cab68
wtklbm <wtklbm@gmail.com>
2024-10-02 23:47:14 +0800

    ✨ feat: 添加 `assertBrowserExtension` 方法

---
+++ packages/asserts/src/platform/browser/assertBrowserExtension.ts
@@ -0,0 +1,16 @@
+import { isBrowserExtension } from '@curong/types';
+
+/**
+ * 当前的执行环境是不是浏览器扩展
+ *
+ * @param value 要验证的值
+ * @param variableName 该值的变量名
+ * @throws 如果不是则会抛出类型异常
+ */
+export default function assertBrowserExtension() {
+    if (!isBrowserExtension()) {
+        throw new TypeError(
+            '[assertBrowserExtension] 当前的执行环境不是浏览器扩展'
+        );
+    }
+}
