416e188776daf36db79e2f59d17a73c9c2a69701
wtklbm <wtklbm@gmail.com>
2024-10-06 10:26:51 +0800

    🎈 perf: 使用新语法重写代码

--- packages/types/src/platform/browser/getUserAgent.ts
+++ packages/types/src/platform/browser/getUserAgent.ts
@@ -1,19 +1,9 @@
-import isBrowser from './isBrowser';
-
 /**
  * 获取 `userAgent` 字符串
  *
  * @param ua 要使用的后备值
- * @returns 如果是浏览器且 `window.navigator.userAgent` 存在则返回该值，否则返回后备值
+ * @returns 如果 `navigator.userAgent` 存在则返回该值，否则返回后备值
  */
-function getUserAgent(ua: string = '') {
-    try {
-        return isBrowser() ? window.navigator.userAgent : ua;
-    } catch {}
-
-    return ua;
+export default function getUserAgent(ua: string = '') {
+    return globalThis.navigator?.userAgent ?? ua;
 }
-
-const userAgent = getUserAgent();
-
-export default userAgent;
