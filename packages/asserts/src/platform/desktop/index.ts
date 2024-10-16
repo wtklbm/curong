ef098df34586b6e563baa50d1e91572f0e93e559
wtklbm <wtklbm@gmail.com>
2024-10-17 00:01:25 +0800

    ✨ feat: 添加新的方法

---
+++ packages/asserts/src/platform/desktop/index.ts
@@ -0,0 +1,4 @@
+export { default as assertElectron } from './assertElectron';
+export { default as assertLinux } from './assertLinux';
+export { default as assertMacOS } from './assertMacOS';
+export { default as assertWindows } from './assertWindows';
