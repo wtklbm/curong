d69262dee092aa0b1e9e4603c5f0120bcb2469ff
wtklbm <wtklbm@gmail.com>
2024-10-16 23:02:37 +0800

    🦄 refactor: 更新逻辑

--- packages/types/src/platform/webWorker/isWebWorker.ts
+++ packages/types/src/platform/webWorker/isWebWorker.ts
@@ -1,13 +1,16 @@
+declare const WorkerGlobalScope: Function | undefined;
+
 /**
  * 当前的执行环境是不是 `Web Worker`
  *
  * @returns 是则返回 `true`，否则为 `false`
+ * @see [MDN](https://developer.mozilla.org/en-US/docs/Web/API/WorkerGlobalScope)
  */
 export default function isWebWorker(): boolean {
     return (
+        typeof WorkerGlobalScope === 'function' &&
         typeof self === 'object' &&
         self !== null &&
-        self.constructor &&
-        self.constructor.name === 'DedicatedWorkerGlobalScope'
+        self instanceof WorkerGlobalScope
     );
 }
