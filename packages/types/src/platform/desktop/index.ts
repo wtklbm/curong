2a171aee7d70b025ac259c3376c7f873611e8154
wtklbm <wtklbm@gmail.com>
2024-10-16 23:05:23 +0800

    ✨ feat: 添加新的方法

---
+++ packages/types/src/platform/desktop/index.ts
@@ -0,0 +1,4 @@
+export { default as isElectron } from './isElectron';
+export { default as isLinux } from './isLinux';
+export { default as isMacOS } from './isMacOS';
+export { default as isWindows } from './isWindows';
