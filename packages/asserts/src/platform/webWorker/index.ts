253b5d8eac6f70fb521fd4cb9c1811d0fe479fdf
wtklbm <wtklbm@gmail.com>
2024-10-17 00:01:34 +0800

    ✨ feat: 添加新的方法

---
+++ packages/asserts/src/platform/webWorker/index.ts
@@ -0,0 +1,4 @@
+export { default as assertDedicatedWorker } from './assertDedicatedWorker';
+export { default as assertServiceWorker } from './assertServiceWorker';
+export { default as assertSharedWorker } from './assertSharedWorker';
+export { default as assertWebWorker } from './assertWebWorker';
