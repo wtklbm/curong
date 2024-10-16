87841bbde790fe2670f85bd8c06cb8734db0c50d
wtklbm <wtklbm@gmail.com>
2024-10-16 23:05:16 +0800

    ✨ feat: 添加 `isElectron` 方法

---
+++ packages/types/src/platform/desktop/isElectron.ts
@@ -0,0 +1,13 @@
+import isNodejsProcess from '../command/isNodejsProcess';
+
+/**
+ * 当前的执行环境是不是 `Electron`
+ *
+ * @returns 是则返回 `true`，否则为 `false`
+ */
+export default function isElectron(): boolean {
+    return (
+        isNodejsProcess() &&
+        typeof globalThis.process.versions?.electron === 'string'
+    );
+}
