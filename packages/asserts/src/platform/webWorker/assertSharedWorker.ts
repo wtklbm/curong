b769c3a130778cc5c1e4378e6d6ad6c7c0cd46a1
wtklbm <wtklbm@gmail.com>
2024-10-16 23:59:54 +0800

    ✨ feat: 添加 `assertSharedWorker` 方法

---
+++ packages/asserts/src/platform/webWorker/assertSharedWorker.ts
@@ -0,0 +1,12 @@
+import { isSharedWorker } from '@curong/types';
+
+/**
+ * 当前的执行环境是不是 `SharedWorker`
+ *
+ * @throws 如果不是则会抛出类型异常
+ */
+export default function assertSharedWorker() {
+    if (!isSharedWorker()) {
+        throw new TypeError('[assertSharedWorker] 当前的执行环境不是 SharedWorker');
+    }
+}
