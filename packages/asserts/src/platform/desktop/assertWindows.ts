52fa434542389261ff9325b2a5ae5abcb288764c
wtklbm <wtklbm@gmail.com>
2024-10-16 23:58:53 +0800

    ✨ feat: 添加 `assertWindows` 方法

---
+++ packages/asserts/src/platform/desktop/assertWindows.ts
@@ -0,0 +1,12 @@
+import { isWindows } from '@curong/types';
+
+/**
+ * 当前的执行环境是不是 `Windows`
+ *
+ * @throws 如果不是则会抛出类型异常
+ */
+export default function assertWindows() {
+    if (!isWindows()) {
+        throw new TypeError('[assertWindows] 当前的执行环境不是 Windows');
+    }
+}
