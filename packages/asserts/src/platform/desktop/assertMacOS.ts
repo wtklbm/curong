dbc4c47f8e796359c83ba7aea8abb0d0ccb6f3dc
wtklbm <wtklbm@gmail.com>
2024-10-16 23:58:41 +0800

    ✨ feat: 添加 `assertMacOS` 方法

---
+++ packages/asserts/src/platform/desktop/assertMacOS.ts
@@ -0,0 +1,12 @@
+import { isMacOS } from '@curong/types';
+
+/**
+ * 当前的执行环境是不是 `macOS`
+ *
+ * @throws 如果不是则会抛出类型异常
+ */
+export default function assertMacOS() {
+    if (!isMacOS()) {
+        throw new TypeError('[assertMacOS] 当前的执行环境不是 macOS');
+    }
+}
