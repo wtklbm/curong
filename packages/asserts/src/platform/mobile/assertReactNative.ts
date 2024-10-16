ea0819f082aad9505bdc08794d8249c21cd6dc19
wtklbm <wtklbm@gmail.com>
2024-10-16 23:59:27 +0800

    ✨ feat: 添加 `assertReactNative` 方法

---
+++ packages/asserts/src/platform/mobile/assertReactNative.ts
@@ -0,0 +1,12 @@
+import { isReactNative } from '@curong/types';
+
+/**
+ * 当前的执行环境是不是 `React Native`
+ *
+ * @throws 如果不是则会抛出类型异常
+ */
+export default function assertReactNative() {
+    if (!isReactNative()) {
+        throw new TypeError('[assertReactNative] 当前的执行环境不是 React Native');
+    }
+}
