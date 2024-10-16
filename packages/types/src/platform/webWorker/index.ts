7a8b2651714d1f23431dfe8537c182a5d42f733d
wtklbm <wtklbm@gmail.com>
2024-10-16 23:03:27 +0800

    ✨ feat: 添加新的方法

---
+++ packages/types/src/platform/webWorker/index.ts
@@ -0,0 +1,4 @@
+export { default as isDedicatedWorker } from './isDedicatedWorker';
+export { default as isServiceWorker } from './isServiceWorker';
+export { default as isSharedWorker } from './isSharedWorker';
+export { default as isWebWorker } from './isWebWorker';
