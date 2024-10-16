16714d1970390a9a7c1d68a1a205581616c30a49
wtklbm <wtklbm@gmail.com>
2024-10-16 23:04:35 +0800

    ✨ feat: 添加 `isWindows` 方法

---
+++ packages/types/src/platform/desktop/isWindows.ts
@@ -0,0 +1,16 @@
+import isNodejsProcess from '../command/isNodejsProcess';
+import isNavigator from '../constants/isNavigator';
+
+/**
+ * 当前的执行环境是不是 `Windows`
+ *
+ * @returns 是则返回 `true`，否则为 `false`
+ */
+export default function isWindows(): boolean {
+    return (
+        (isNodejsProcess() && globalThis.process.platform === 'win32') ||
+        (isNavigator() && // @ts-ignore
+            (globalThis.navigator.userAgentData?.platform === 'Windows' ||
+                globalThis.navigator.platform === 'Win32 '))
+    );
+}
