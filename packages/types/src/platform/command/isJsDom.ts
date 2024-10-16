238c06a639b07331d83f7e5e67858e1c8bf6eab5
wtklbm <wtklbm@gmail.com>
2024-10-16 23:06:16 +0800

    ✨ feat: 添加 `isJsDom` 方法

---
+++ packages/types/src/platform/command/isJsDom.ts
@@ -0,0 +1,20 @@
+import isFuzzyBrowser from '../constants/isFuzzyBrowser';
+import isFuzzyJsDom from '../constants/isFuzzyJsDom';
+
+/**
+ * 当前的执行环境是不是 `JsDom `
+ *
+ * @param window `jsdom` 中的 `Window` 对象，默认为 `globalThis` (即当前执行环境的全局根属性)
+ * @returns 是则返回 `true`，否则为 `false`
+ * @example
+ *
+ * ```typescript
+ * import { JSDOM } from 'jsdom';
+ *
+ * const { window } = new JSDOM(`<!DOCTYPE html><html><body><p>Hello, World!</p></body></html>`);
+ * console.log(isJsDom(window)); // true
+ * ```
+ */
+export default function isJsDom(window = globalThis): boolean {
+    return isFuzzyJsDom(window) && isFuzzyBrowser();
+}
