ce5c32fad9922724a018ab959cee131f222f9d71
wtklbm <wtklbm@gmail.com>
2024-10-16 23:06:52 +0800

    ✨ feat: 添加新的方法

---
+++ packages/types/src/platform/command/index.ts
@@ -0,0 +1,5 @@
+export { default as isBun } from './isBun';
+export { default as isDeno } from './isDeno';
+export { default as isJsDom } from './isJsDom';
+export { default as isNodejs } from './isNodejs';
+export { default as isNodejsProcess } from './isNodejsProcess';
