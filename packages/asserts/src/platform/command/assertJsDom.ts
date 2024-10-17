09aa5903e59075dace9ea1fb1348ca9c9555c6e4
wtklbm <wtklbm@gmail.com>
2024-10-17 10:07:52 +0800

    ✨ feat: 添加参数

--- packages/asserts/src/platform/command/assertJsDom.ts
+++ packages/asserts/src/platform/command/assertJsDom.ts
@@ -3,10 +3,11 @@ import { isJsDom } from '@curong/types';
 /**
  * 当前的执行环境是不是 `JsDom`
  *
+ * @param window `jsdom` 中的 `Window` 对象，默认为 `globalThis.window` (即当前执行环境的全局根属性)
  * @throws 如果不是则会抛出类型异常
  */
-export default function assertJsDom() {
-    if (!isJsDom()) {
+export default function assertJsDom(window = globalThis.window) {
+    if (!isJsDom(window)) {
         throw new TypeError('[assertJsDom] 当前的执行环境不是 JsDom');
     }
 }
