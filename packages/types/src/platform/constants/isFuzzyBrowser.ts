8d9df44bc038d7d7b25d3e5bd420f1969fc750e0
wtklbm <wtklbm@gmail.com>
2024-10-17 10:24:36 +0800

    🎈 perf: 更新判断逻辑

--- packages/types/src/platform/constants/isFuzzyBrowser.ts
+++ packages/types/src/platform/constants/isFuzzyBrowser.ts
@@ -1,16 +1,16 @@
 import isDocument from '../../element/isDocument';
 import isWindow from '../../element/isWindow';
+import isFunction from '../../function/isFunction';
 import getTagEqual from '../../type/getTagEqual';
 
 export default function isFuzzyBrowser(): boolean {
     return (
-        typeof window === 'object' &&
-        isWindow(window) &&
-        isDocument(window.document) &&
-        getTagEqual(window.history, 'History') &&
-        getTagEqual(window.navigator, 'Navigator') &&
-        typeof HTMLImageElement === 'function' &&
-        typeof HTMLCanvasElement === 'function' &&
-        typeof HTMLVideoElement === 'function'
+        isWindow(globalThis) &&
+        isDocument(globalThis.document) &&
+        getTagEqual(globalThis.history, 'History') &&
+        getTagEqual(globalThis.navigator, 'Navigator') &&
+        isFunction(globalThis.HTMLImageElement) &&
+        isFunction(globalThis.HTMLCanvasElement) &&
+        isFunction(globalThis.HTMLVideoElement)
     );
 }
