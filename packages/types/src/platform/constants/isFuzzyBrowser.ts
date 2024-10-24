bfcf16c41ea7cdc71275d3f42090fe16bed36086
wtklbm <wtklbm@gmail.com>
2024-10-24 22:39:47 +0800

    🌈 style: 删除不通用的判断

--- packages/types/src/platform/constants/isFuzzyBrowser.ts
+++ packages/types/src/platform/constants/isFuzzyBrowser.ts
@@ -1,4 +1,3 @@
-import isDocument from '../../element/isDocument';
 import isWindow from '../../element/isWindow';
 import isFunction from '../../function/isFunction';
 import getTagEqual from '../../type/getTagEqual';
@@ -6,7 +5,6 @@ import getTagEqual from '../../type/getTagEqual';
 export default function isFuzzyBrowser(): boolean {
     return (
         isWindow(globalThis) &&
-        isDocument(globalThis.document) &&
         getTagEqual(globalThis.history, 'History') &&
         getTagEqual(globalThis.navigator, 'Navigator') &&
         isFunction(globalThis.HTMLImageElement) &&
