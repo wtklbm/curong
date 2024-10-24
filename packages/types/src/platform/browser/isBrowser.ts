279e69a126d725b678179dd1dfea2dec304b27cb
wtklbm <wtklbm@gmail.com>
2024-10-24 22:36:57 +0800

    🎈 perf: 添加判断 `Document` 的逻辑

--- packages/types/src/platform/browser/isBrowser.ts
+++ packages/types/src/platform/browser/isBrowser.ts
@@ -1,3 +1,5 @@
+import getTagEqual from '../../type/getTagEqual';
+
 import isFuzzyBrowser from '../constants/isFuzzyBrowser';
 import isFuzzyJsDom from '../constants/isFuzzyJsDom';
 
@@ -7,5 +9,10 @@ import isFuzzyJsDom from '../constants/isFuzzyJsDom';
  * @returns 是则返回 `true`，否则为 `false`
  */
 export default function isBrowser(): boolean {
-    return !isFuzzyJsDom() && isFuzzyBrowser();
+    return (
+        !isFuzzyJsDom() &&
+        isFuzzyBrowser() &&
+        // 浏览器是 `HTMLDocument`，`jsdom` 是 `Document`
+        getTagEqual(globalThis.document, 'HTMLDocument')
+    );
 }
