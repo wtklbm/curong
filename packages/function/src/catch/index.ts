51ad26a3b32b3b165e6f18348b4f6e00c82fce5d
wtklbm <wtklbm@gmail.com>
2024-10-26 00:10:06 +0800

    ✨ feat: 添加丢失的类型声明

--- packages/function/src/catch/index.ts
+++ packages/function/src/catch/index.ts
@@ -2,3 +2,5 @@ export { default as ifThrow } from './ifThrow';
 export { default as noCatch } from './noCatch';
 export { default as retry } from './retry';
 export { default as tryCatch } from './tryCatch';
+
+export * from './types';
