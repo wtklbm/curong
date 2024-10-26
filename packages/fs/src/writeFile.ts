41070fcae482669422ba2a33dee9b9d0efe17013
wtklbm <wtklbm@gmail.com>
2024-10-27 01:16:53 +0800

    🐞 fix: 处理空数组和空对象

--- packages/fs/src/writeFile.ts
+++ packages/fs/src/writeFile.ts
@@ -2,7 +2,7 @@ import { promises } from 'fs';
 import { dirname } from 'path';
 
 import { format } from '@curong/term';
-import { isArrayFilled, isObjectFilled } from '@curong/types';
+import { isArray, isObject } from '@curong/types';
 
 import mkdir from './mkdir';
 import type { WriteFileOptions } from './types';
@@ -13,18 +13,16 @@ import type { WriteFileOptions } from './types';
  * @param filePath 文件的路径 `绝对路径`
  * @param data 要写入的数据，对象默认换为JSON
  * @param options 配置选项
- *
- * - `encoding` 文件的编码，默认为 `utf8`
- * - `flag` 读写文件标识符，默认为 `w+`
- * - `isFormat` 是否把对象和数组进行序列化，序列化之后会将数据转换为 `JSON` 格式，默认为 `true`
- * - `isMkdir` 当目录不存在时，是否自动创建目录，然后在从该目录下写文件, 默认为 `true`
- * - `mode` 权限，默认为 `0o755`
- *
+ *  - `encoding` 文件的编码，默认为 `utf8`
+ *  - `flag` 读写文件标识符，默认为 `w+`
+ *  - `isFormat` 是否把对象和数组进行序列化，序列化之后会将数据转换为 `JSON` 格式，默认为 `true`
+ *  - `isMkdir` 当目录不存在时，是否自动创建目录，然后在从该目录下写文件, 默认为 `true`
+ *  - `mode` 权限，默认为 `0o755`
  * @throws
- *
- * - 如果将 `data` 转换为 `JSON` 时失败，则会抛出异常
- * - 如果创建文件夹失败，则会抛出异常
- * - 如果写入文件失败，则会抛出异常
+ *  - 如果将 `data` 转换为 `JSON` 时失败，则会抛出异常
+ *  - 如果创建文件夹失败，则会抛出异常
+ *  - 如果写入文件失败，则会抛出异常
+ * @note 如果要频繁的操作同一个文件，请使用 `Writer` 类
  */
 export default async function writeFile(
     filePath: string,
@@ -40,7 +38,7 @@ export default async function writeFile(
         ...options
     };
 
-    if (options.isFormat && (isObjectFilled(data) || isArrayFilled(data))) {
+    if (options.isFormat && (isObject(data) || isArray(data))) {
         try {
             data = JSON.stringify(data);
         } catch (error) {
