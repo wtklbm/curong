b2de158e9250a6d66fbc038257a3fdd63bd47363
wtklbm <wtklbm@gmail.com>
2024-10-27 00:15:02 +0800

    ✨ feat: 更改参数类型

--- packages/fs/src/isSymbolicLink.ts
+++ packages/fs/src/isSymbolicLink.ts
@@ -1,15 +1,14 @@
-import { promises } from 'fs';
+import type { PathLike } from 'fs';
+import { lstat } from 'fs/promises';
 
 /**
  * 是不是一个符号链接
  *
- * @param {string} pathString 要验证的路径的字符串
+ * @param path 要验证的路径的字符串、URL、Buffer
  * @returns 如果是符号链接则返回 `true`，否则为 `false`
  */
-export default async function isSymbolicLink(
-    pathString: string
-): Promise<boolean> {
-    return await promises.lstat(pathString).then(
+export default async function isSymbolicLink(path: PathLike): Promise<boolean> {
+    return await lstat(path).then(
         stat => stat.isSymbolicLink(),
         () => false
     );
