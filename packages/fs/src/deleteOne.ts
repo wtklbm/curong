af79becc40e1268dec708d9f50f8b5998814df95
wtklbm <wtklbm@gmail.com>
2024-10-27 00:13:46 +0800

    ✨ feat: 更改参数类型

--- packages/fs/src/deleteOne.ts
+++ packages/fs/src/deleteOne.ts
@@ -1,3 +1,4 @@
+import type { PathLike } from 'fs';
 import { rmdir, unlink } from 'fs/promises';
 
 import { format } from '@curong/term';
@@ -8,23 +9,23 @@ import isFile from './isFile';
 /**
  * 删除一个文件或一个文件夹
  *
- * @param pathString 要删除的文件或文件夹路径
+ * @param path 要删除的文件或文件夹路径
  * @throws
  *
- * - 如果 `pathString` 不是一个有效路径，则会抛出异常
+ * - 如果 `path` 不是一个有效路径，则会抛出异常
  */
-export default async function deleteOne(pathString: string): Promise<void> {
-    if (await isDir(pathString)) {
-        return await rmdir(pathString, { recursive: true });
+export default async function deleteOne(path: PathLike): Promise<void> {
+    if (await isDir(path)) {
+        return await rmdir(path, { recursive: true });
     }
 
-    if (await isFile(pathString)) {
-        return await unlink(pathString);
+    if (await isFile(path)) {
+        return await unlink(path);
     }
 
     throw format({
         name: 'deleteOne',
-        message: 'pathString不是一个有效路径',
-        data: { pathString }
+        message: 'path 不是一个有效路径',
+        data: { path }
     });
 }
