5942405eda246794d7f7f4555ec3710c20aefacb
wtklbm <wtklbm@gmail.com>
2024-10-27 00:15:49 +0800

    ✨ feat: 更改参数类型

--- packages/fs/src/symbolicLink.ts
+++ packages/fs/src/symbolicLink.ts
@@ -1,13 +1,14 @@
+import type { PathLike } from 'fs';
 import { readlink } from 'fs/promises';
 
 /**
  * 获取符号链接所指向的原始地址
  *
- * @param pathString 符号链接地址
+ * @param path 符号链接字符串、URL、Buffer
  * @returns 如果找到符号链接，则返回符号链接的源路径，否则返回 `null`
  */
 export default async function symbolicLink(
-    pathString: string
+    path: PathLike
 ): Promise<string | null> {
-    return await readlink(pathString).catch(() => null);
+    return await readlink(path).catch(() => null);
 }
