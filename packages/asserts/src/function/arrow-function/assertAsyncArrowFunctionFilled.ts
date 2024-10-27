60a69b2eb71b9ffca242b1f40d38080b5df2f4b3
wtklbm <wtklbm@gmail.com>
2024-10-27 20:17:51 +0800

    ✨ feat: 添加 `assertAsyncArrowFunctionFilled` 方法

---
+++ packages/asserts/src/function/arrow-function/assertAsyncArrowFunctionFilled.ts
@@ -0,0 +1,17 @@
+import { isAsyncArrowFunctionFilled, type AsyncFunction } from '@curong/types';
+
+import typeGuard from '../../constants/typeGuard';
+
+/**
+ * 是不是一个参数个数大于 `0` 的异步箭头函数
+ *
+ * @param value 要验证的值
+ * @param variableName 该值的变量名
+ * @throws 如果不是则会抛出类型异常
+ */
+export default function assertAsyncArrowFunctionFilled<
+    R = unknown,
+    A extends unknown[] = unknown[]
+>(value: unknown, variableName: string): asserts value is AsyncFunction<R, A> {
+    return typeGuard(value, variableName, isAsyncArrowFunctionFilled);
+}
