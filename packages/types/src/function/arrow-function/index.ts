cdc38d328d2252aaff6b64aad5e1a6b65eb7f037
wtklbm <wtklbm@gmail.com>
2024-10-27 19:45:36 +0800

    ✨ feat: 添加箭头函数相关方法

---
+++ packages/types/src/function/arrow-function/index.ts
@@ -0,0 +1,7 @@
+export { default as isArrowFunction } from './isArrowFunction';
+export { default as isArrowFunctionArray } from './isArrowFunctionArray';
+export { default as isArrowFunctionFilled } from './isArrowFunctionFilled';
+export { default as isAsyncArrowFunction } from './isAsyncArrowFunction';
+export { default as isAsyncArrowFunctionFilled } from './isAsyncArrowFunctionFilled';
+export { default as isSyncArrowFunction } from './isSyncArrowFunction';
+export { default as isSyncArrowFunctionFilled } from './isSyncArrowFunctionFilled';
