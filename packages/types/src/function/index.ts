a234341d78f8e460a71a8b848e555c25675dda53
wtklbm <wtklbm@gmail.com>
2024-10-27 19:45:39 +0800

    ✨ feat: 添加箭头函数相关方法

--- packages/types/src/function/index.ts
+++ packages/types/src/function/index.ts
@@ -1,5 +1,6 @@
 export { default as isArguments } from './isArguments';
 export { default as isClass } from './isClass';
 
+export * from './arrow-function';
 export * from './function';
 export * from './types';
