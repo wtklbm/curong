6180a3b3991dbee85f2f02b8594ba35e53243244
wtklbm <wtklbm@gmail.com>
2024-10-28 15:17:41 +0800

    ✨ feat: 添加新的方法

--- packages/asserts/src/function/index.ts
+++ packages/asserts/src/function/index.ts
@@ -1,4 +1,5 @@
 export { default as assertArguments } from './assertArguments';
 export { default as assertClass } from './assertClass';
 
+export * from './arrow-function';
 export * from './function';
