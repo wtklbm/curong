0dbc6189a04f51968274edebb82ab2ec6f23d529
wtklbm <wtklbm@gmail.com>
2024-10-28 15:17:48 +0800

    ✨ feat: 添加新的方法

---
+++ packages/asserts/src/function/arrow-function/index.ts
@@ -0,0 +1,7 @@
+export { default as assertArrowFunction } from './assertArrowFunction';
+export { default as assertArrowFunctionArray } from './assertArrowFunctionArray';
+export { default as assertArrowFunctionFilled } from './assertArrowFunctionFilled';
+export { default as assertAsyncArrowFunction } from './assertAsyncArrowFunction';
+export { default as assertAsyncArrowFunctionFilled } from './assertAsyncArrowFunctionFilled';
+export { default as assertSyncArrowFunction } from './assertSyncArrowFunction';
+export { default as assertSyncArrowFunctionFilled } from './assertSyncArrowFunctionFilled';
