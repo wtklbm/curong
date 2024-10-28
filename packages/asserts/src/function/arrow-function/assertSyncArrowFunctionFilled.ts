2feb2414a448813a9509609c1574d4206495c46e
wtklbm <wtklbm@gmail.com>
2024-10-28 15:17:28 +0800

    ✨ feat: 添加 `assertSyncArrowFunctionFilled` 方法

---
+++ packages/asserts/src/function/arrow-function/assertSyncArrowFunctionFilled.ts
@@ -0,0 +1,17 @@
+import { isSyncArrowFunctionFilled, type Function } from '@curong/types';
+
+import typeGuard from '../../constants/typeGuard';
+
+/**
+ * 是不是一个参数个数大于 `0` 的同步箭头函数
+ *
+ * @param value 要验证的值
+ * @param variableName 该值的变量名
+ * @throws 如果不是则会抛出类型异常
+ */
+export default function assertSyncArrowFunctionFilled<
+    R = unknown,
+    A extends unknown[] = unknown[]
+>(value: unknown, variableName: string): asserts value is Function<R, A> {
+    return typeGuard(value, variableName, isSyncArrowFunctionFilled);
+}
