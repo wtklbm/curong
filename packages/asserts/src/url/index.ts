c40a83a16bed792f44fcfff6ec0c1cacdc5b39ab
wtklbm <wtklbm@gmail.com>
2024-10-30 15:14:20 +0800

    ✨ feat: 添加 `assertLocalURLString` 方法

--- packages/asserts/src/url/index.ts
+++ packages/asserts/src/url/index.ts
@@ -1,3 +1,4 @@
+export { default as assertLocalURLString } from './assertLocalURLString';
 export { default as assertURL } from './assertURL';
 export { default as assertURLSearchParams } from './assertURLSearchParams';
 export { default as assertURLString } from './assertURLString';
