2816667997535083f56167c3b93d6b6e5bafe87c
wtklbm <wtklbm@gmail.com>
2024-10-30 15:41:58 +0800

    ✨ feat: 添加 `isSafeURLString` 方法

--- packages/types/src/url/index.ts
+++ packages/types/src/url/index.ts
@@ -1,4 +1,5 @@
 export { default as isLocalURLString } from './isLocalURLString';
 export { default as isURL } from './isURL';
+export { default as isSafeURLString } from './isSafeURLString';
 export { default as isURLSearchParams } from './isURLSearchParams';
 export { default as isURLString } from './isURLString';
