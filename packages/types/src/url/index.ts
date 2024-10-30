e957f94efbb5d6ea5be9fd2e92e57aff41a43e10
wtklbm <wtklbm@gmail.com>
2024-10-30 15:12:37 +0800

    ✨ feat: 添加 `isLocalURLString` 方法

--- packages/types/src/url/index.ts
+++ packages/types/src/url/index.ts
@@ -1,3 +1,4 @@
+export { default as isLocalURLString } from './isLocalURLString';
 export { default as isURL } from './isURL';
 export { default as isURLSearchParams } from './isURLSearchParams';
 export { default as isURLString } from './isURLString';
