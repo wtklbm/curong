ead9d77a0e3fbec4319dde290796d09e99772841
wtklbm <wtklbm@gmail.com>
2024-10-30 15:16:20 +0800

    ✨ feat: 更新正则表达式字符串

--- packages/regexp/src/source/katakana.ts
+++ packages/regexp/src/source/katakana.ts
@@ -9,10 +9,10 @@
  *
  * ```typescript
  *  // ES 2018
- *  const reg = /[\p{Katakana}]/u;
+ *  const reg = /\p{Script_Extensions=Katakana}/u;
  * ```
  */
 const katakana =
-    '[\u30A0-\u30FF\u30FD-\u30FF\u31F0-\u31FF\u32D0-\u32FE\u3300-\u3357\uFF66-\uFF6F\uFF71-\uFF9D]';
+    '(?:[\u0305\u0323\u3001-\u3003\u3008-\u3011\u3013-\u301F\u3030-\u3035\u3037\u303C\u303D\u3099-\u309C\u30A0-\u30FF\u31F0-\u31FF\u32D0-\u32FE\u3300-\u3357\uFE45\uFE46\uFF61-\uFF9F]|\uD82B[\uDFF0-\uDFF3\uDFF5-\uDFFB\uDFFD\uDFFE]|\uD82C[\uDC00\uDD20-\uDD22\uDD55\uDD64-\uDD67])';
 
 export default katakana;
