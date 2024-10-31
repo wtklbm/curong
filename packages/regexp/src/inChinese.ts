2fea2dd11562883038d3a31c56296d299314dbb7
wtklbm <wtklbm@gmail.com>
2024-11-01 01:44:45 +0800

    🎈 perf: 使用缓存

--- packages/regexp/src/inChinese.ts
+++ packages/regexp/src/inChinese.ts
@@ -1,10 +1,13 @@
 import { hiragana, katakana, unifiedIdeograph } from './source';
 
+let _: RegExp;
+let $: RegExp;
+
 /**
  * 是否包含中文汉字
  *
  * @param str 要验证的字符串
- * @param vhk 是否在该字符串中不能包含日文(平假名和片假名)，默认为 `false`
+ * @param vhk 是否在该字符串中不能包含日文 (平假名和片假名)，默认为 `false`
  * @returns 是则返回 `true`，否则为 `false`
  * @example
  *
@@ -14,9 +17,12 @@ import { hiragana, katakana, unifiedIdeograph } from './source';
  * ```
  */
 export default function inChinese(str: string, vhk: boolean = false): boolean {
-    if (vhk && new RegExp(`(${hiragana}|${katakana})+`).test(str)) {
+    if (
+        vhk &&
+        (_ ?? (_ = new RegExp(`${hiragana}|${katakana}`))).test(str)
+    ) {
         return false;
     }
 
-    return new RegExp(`(${unifiedIdeograph})+`).test(str);
+    return ($ ?? ($ = new RegExp(unifiedIdeograph))).test(str);
 }
