34179a6b1d6fbdfbed90597f46d47ab876b6b5a7
wtklbm <wtklbm@gmail.com>
2024-10-31 21:42:05 +0800

    📃 docs: 更新文档

--- packages/regexp/src/source/blank.ts
+++ packages/regexp/src/source/blank.ts
@@ -6,7 +6,7 @@
  *
  * ```typescript
  *  // ES 2018
- *  const reg = /\p{Z}/u;
+ *  const reg = /\p{Z}/u; // 等价于 `/\p{Separator}/u`
  * ```
  */
 const blank = '[\u0020\xA0\u1680\u2000-\u200A\u2028\u2029\u202F\u205F\u3000]';
