8856354ed3cd18e1795071f5cc890adeb3ff9c81
wtklbm <wtklbm@gmail.com>
2024-11-01 00:56:02 +0800

    📃 docs: 更新文档

--- packages/regexp/src/source/korean.ts
+++ packages/regexp/src/source/korean.ts
@@ -7,14 +7,6 @@
  *  - [韩文字母](http://www.unicode.org/charts/PDF/U1100.pdf)
  *  - [韩文兼容字母](http://www.unicode.org/charts/PDF/U3130.pdf)
  *
- * @note 该正则使用 `xregexp` 自动生成
- *
- * ```typescript
- *  const XRegExp = require('xregexp');
- *  const reg = new XRegExp('\\p{Hangul}', 'u');
- *  console.log(reg);
- * ```
- *
  * @example ```` 验证韩文的正则
  *
  * ```typescript
