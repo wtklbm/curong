5f7348a54d66fa4880945d89da6c82a6441e3c19
wtklbm <wtklbm@gmail.com>
2024-11-01 00:55:56 +0800

    📃 docs: 更新文档

--- packages/regexp/src/source/ascii.ts
+++ packages/regexp/src/source/ascii.ts
@@ -1,4 +1,10 @@
-/** 验证 `ASCII` 的正则字符串 */
+/**
+ * 验证 `ASCII` 的正则字符串
+ *
+ * 常见用法:
+ * - `/\p{ASCII}/u`: 匹配 `U+0000` 到 `U+007F` 的 ASCII 字符，包括 `\n`
+ * - `/\p{Non_ASCII_Unicode}/u`: 匹配非 ASCII 字符 `U+0080` 到 `U+10FFFF` 减去 `U+D800` 到 `U+DFFF`
+ */
 const ascii = '[\u0000-\u007F]';
 
 export default ascii;
