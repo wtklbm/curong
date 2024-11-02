1ebff552a1a1ad658d49bab6ad9a24904117c298
wtklbm <wtklbm@gmail.com>
2024-11-02 22:06:08 +0800

    📃 docs: 更新文档

--- packages/regexp/src/source/ascii.ts
+++ packages/regexp/src/source/ascii.ts
@@ -2,8 +2,10 @@
  * 验证 `ASCII` 的正则字符串
  *
  * 常见用法:
- * - `/\p{ASCII}/u`: 匹配 `U+0000` 到 `U+007F` 的 ASCII 字符，包括 `\n`
- * - `/\p{Non_ASCII_Unicode}/u`: 匹配非 ASCII 字符 `U+0080` 到 `U+10FFFF` 减去 `U+D800` 到 `U+DFFF`
+ *  - `/\p{ASCII}/u`: 匹配 `U+0000` 到 `U+007F` 的 ASCII 字符，包括 `\n`
+ *  - `/\p{Non_ASCII_Unicode}/u`: 匹配非 ASCII 字符 `U+0080` 到 `U+10FFFF` 减去 `U+D800` 到 `U+DFFF`
+ *
+ * @note 该正则是自动生成的。原始代码: [`/\p{ASCII}/u`](https://mothereff.in/regexpu#input=/\p{ASCII}/u)
  */
 const ascii = '[\u0000-\u007F]';
 
