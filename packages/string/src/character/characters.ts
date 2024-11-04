67f43d14f9a556aec0f3c108291d7ff71f0db092
wtklbm <wtklbm@gmail.com>
2024-11-04 21:23:45 +0800

    ✨ feat: 添加新的字符，并更新文档

--- packages/string/src/character/characters.ts
+++ packages/string/src/character/characters.ts
@@ -1,7 +1,17 @@
-/** 零宽字符 */
+/**
+ * 零宽字符
+ *
+ * @note
+ * - 除了 `\uFEFF`、`\u2060`、`\u180E`、`\u200B-\u200D` 字符外，为什么添加其他字符已经记不清了
+ * - [`/\p{Join_Control}/u`](https://mothereff.in/regexpu#input=/\p{Join_Control}/u)：`\u200C`、`\u200D`
+ * - [`/\p{Bidi_Control}/u`](https://mothereff.in/regexpu#input=/\p{Bidi_Control}/u): `\u061C`、`\u200E`、`\u200F`、`\u202A-\u202E`、`\u2066-\u2069`
+ */
 const ZERO_WIDTH_CHARS = {
     /** 零宽不折行空格，即 `BOM` */
     ZWNBSP: '\uFEFF',
+    /** 蒙古语元音分隔符 */
+    MVS: '\u180E',
+
     /** ​零宽间隔 */
     ZWSP: '\u200B',
     /** 零宽无连接符 */
@@ -54,13 +64,29 @@ const ZERO_WIDTH_CHARS = {
     /** 民族数字形状 */
     NADS: '\u206E',
     /** 名义数字形状 */
-    NODS: '\u206F'
+    NODS: '\u206F',
+
+    /** 阿拉伯字母标记 */
+    ALM: '\u061C'
 } as const;
 
-/** 看起来像空格的字符 */
+/**
+ * 看起来像空格的字符
+ *
+ * @note
+ *  - [`/\p{White_Space}/u`](https://mothereff.in/regexpu#input=/\p{White_Space}/u)
+ *    - `\t-\r`、`\u0085`、`\u00A0`、`\u1680`、`\u2000-\u200A`、`\u2028`、`\u2029`、`\u202F`、`\u205F`、`\u3000`
+ *  - [`/\p{Space_Separator}/u`](https://mothereff.in/regexpu#input=/\p{Space_Separator}/u)
+ *    - `\u00A0`、`\u1680`、`\u2000-\u200A`、`\u202F`、`\u205F`、`\u3000`
+ *  - [`/\p{Pattern_White_Space}/u`](https://mothereff.in/regexpu#input=/\p{Pattern_White_Space}/u)
+ *    - `\t-\r`、`\u0085`、`\u200E`、`\u200F、`\u2028`、`\u2029`
+ *  - 是否需要判断其他字符: `\u303F`、`\u2420`、`\u2422`、`\u2423`
+ */
 const LIKE_SPACE_CHARS = {
-    /** 不换行空格 (无中断空格) */
+    /** 不换行空格(无中断空格) */
     NBSP: '\u00A0',
+    // /** [欧甘语空格标记](https://en.wikipedia.org/wiki/Ogham_(Unicode_block))空格 */
+    // OSM: '\u1680',
     /** 窄的无中断空格 */
     NNBSP: '\u202F',
     /** 中型数学空间 */
@@ -92,7 +118,13 @@ const LIKE_SPACE_CHARS = {
     HSP: '\u200A'
 } as const;
 
-/** 控制符(包含换行、回车、制表符等) */
+/**
+ * 控制符 (包含换行、回车、制表符等)
+ *
+ * @note
+ *  - [`/\p{Control}/u`](https://mothereff.in/regexpu#input=/\p{Control}/u)
+ *  - [`/\p{Pattern_White_Space}/u`](https://mothereff.in/regexpu#input=/\p{Pattern_White_Space}/u)
+ */
 const CONTROL_CHARS = {
     //# 以下字符包含在 `/\p{Control}/u`
 
@@ -113,12 +145,15 @@ const CONTROL_CHARS = {
 
     //# 以下字符包含在：`/\p{Pattern_White_Space}/u`
 
+    /** 文本从左到右排版 */
+    LRM: '\u200E',
+    /** 文本从右到左排版 */
+    RLM: '\u200F',
     /** 行分隔符，用于分隔文本行而不引入换行符 */
     LSEP: '\u2028',
     /** 段落分隔符，用于指示段落的结束 */
     PSEP: '\u2029'
 } as const;
-
 
 export const controlRegSource = `[${Object.values(CONTROL_CHARS).join('')}]`;
 export const zeroWidthRegSource = `[${Object.values(ZERO_WIDTH_CHARS).join('')}]`;
