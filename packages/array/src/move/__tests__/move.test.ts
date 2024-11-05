289882bc2772c1096e3a1d7e0363b913a517b3c2
wtklbm <wtklbm@gmail.com>
2024-11-05 23:29:06 +0800

    🧪 test: 添加新的测试

--- packages/array/src/move/__tests__/move.test.ts
+++ packages/array/src/move/__tests__/move.test.ts
@@ -37,4 +37,44 @@ describe('@curong/string/move', () => {
         expect(move(arr2, 1, 1)).toEqual([]);
         expect(move([], 0, 1)).toEqual([]);
     });
+
+    test('测试5', () => {
+        const arr = [1, 2, 3, 4, 5];
+        expect(move(arr, 3, 0)).toEqual([4, 1, 2, 3, 5]);
+        expect(move(arr, -1, 0)).toEqual([5, 1, 2, 3, 4]);
+        expect(move(arr, 1, -2)).toEqual([1, 3, 4, 2, 5]);
+        expect(move(arr, -3, -4)).toEqual([1, 3, 2, 4, 5]);
+    });
+
+    test('测试6', () => {
+        const array = [1, 2, 3, 4];
+        const result = move(array, 1, 3);
+        expect(result).toEqual([1, 3, 4, 2]);
+    });
+
+    test('测试7', () => {
+        const array = [1, 2, 3, 4];
+        const result = move(array, -3, 1);
+        expect(result).toEqual([1, 2, 3, 4]);
+    });
+
+    test('测试8', () => {
+        const array = [1, 2, 3, 4];
+        const result = move(array, 2, 2);
+        expect(result).toEqual([1, 2, 3, 4]);
+    });
+
+    test('测试9', () => {
+        expect(move([1, 2, 3, 4], 1, 3)).toEqual([1, 3, 4, 2]);
+        expect(() => move([1, 2, 3, 4], 1, 4)).toThrow();
+        expect(() => move([1, 2, 3, 4], 1, 5)).toThrow();
+
+        expect(move([1, 2, 3, 4], -4, -1)).toEqual([2, 3, 4, 1]);
+        expect(() => move([1, 2, 3, 4], -5, -1)).toThrow();
+        expect(() => move([1, 2, 3, 4], -6, -1)).toThrow();
+    });
+
+    test('测试10', () => {
+        expect(move([1], 0, 0)).toEqual([1]);
+    });
 });
