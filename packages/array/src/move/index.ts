be09bd79777d529db59f9115c184346cb8673a61
wtklbm <wtklbm@gmail.com>
2024-11-05 22:18:58 +0800

    ✨ feat: 添加 `move` 相关方法

---
+++ packages/array/src/move/index.ts
@@ -0,0 +1,5 @@
+export { default as move } from './move';
+export { default as moveDown } from './moveDown';
+export { default as moveToBottom } from './moveToBottom';
+export { default as moveToTop } from './moveToTop';
+export { default as moveUp } from './moveUp';
