bb5597004c6c388bfb48ec28e0b162a575f1bfd9
wtklbm <wtklbm@gmail.com>
2024-11-06 22:53:54 +0800

    ✨ feat: 添加 `assertTaro` 方法

---
+++ packages/asserts/src/platform/mobile/assertTaro.ts
@@ -0,0 +1,12 @@
+import { isTaro } from '@curong/types';
+
+/**
+ * 当前的执行环境是不是 `Taro`
+ *
+ * @throws 如果不是则会抛出类型异常
+ */
+export default function assertTaro() {
+    if (!isTaro()) {
+        throw new TypeError('[assertTaro] 当前的执行环境不是 Taro');
+    }
+}
