88195c90c579d0386fc8b562180b535523888234
wtklbm <wtklbm@gmail.com>
2024-11-06 22:52:46 +0800

    ✨ feat: 添加 `isTaro` 方法

--- packages/types/src/platform/mobile/index.ts
+++ packages/types/src/platform/mobile/index.ts
@@ -1,3 +1,4 @@
 export { default as isAndroid } from './isAndroid';
 export { default as isIOS } from './isIOS';
 export { default as isReactNative } from './isReactNative';
+export { default as isTaro } from './isTaro';
