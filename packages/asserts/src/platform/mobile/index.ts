c910545737362d4534ea7dd44e50a858071747b7
wtklbm <wtklbm@gmail.com>
2024-11-07 13:26:56 +0800

    ✨ feat: 添加新的方法

--- packages/asserts/src/platform/mobile/index.ts
+++ packages/asserts/src/platform/mobile/index.ts
@@ -1,3 +1,6 @@
 export { default as assertAndroid } from './assertAndroid';
 export { default as assertIOS } from './assertIOS';
+export { default as assertMiniProgram } from './assertMiniProgram';
+export { default as assertMiniProgramWebView } from './assertMiniProgramWebView';
 export { default as assertReactNative } from './assertReactNative';
+export { default as assertWechatBrowser } from './assertWechatBrowser';
