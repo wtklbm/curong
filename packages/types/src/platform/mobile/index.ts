cfa785c3fa69f2ff0bf6ba325de8a0666d3d4dcb
wtklbm <wtklbm@gmail.com>
2024-11-07 13:25:32 +0800

    ✨ feat: 添加新的方法

--- packages/types/src/platform/mobile/index.ts
+++ packages/types/src/platform/mobile/index.ts
@@ -1,4 +1,9 @@
 export { default as isAndroid } from './isAndroid';
 export { default as isIOS } from './isIOS';
+export { default as isMiniProgram } from './isMiniProgram';
+export { default as isMiniProgramWebView } from './isMiniProgramWebView';
 export { default as isReactNative } from './isReactNative';
 export { default as isTaro } from './isTaro';
+export { default as isWechatBrowser } from './isWechatBrowser';
+
+export * from './types';
