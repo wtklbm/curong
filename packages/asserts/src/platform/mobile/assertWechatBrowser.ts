4a39130c54a7cececd29edcacc8aa324884d333d
wtklbm <wtklbm@gmail.com>
2024-11-07 13:26:50 +0800

    ✨ feat: 添加 `assertWechatBrowser` 方法

---
+++ packages/asserts/src/platform/mobile/assertWechatBrowser.ts
@@ -0,0 +1,14 @@
+import { isWechatBrowser } from '@curong/types';
+
+/**
+ * 当前的执行环境是不是微信的内置浏览器
+ *
+ * @throws 如果不是则会抛出类型异常
+ */
+export default function assertWechatBrowser() {
+    if (!isWechatBrowser()) {
+        throw new TypeError(
+            '[assertWechatBrowser] 当前的执行环境不是微信的内置浏览器'
+        );
+    }
+}
