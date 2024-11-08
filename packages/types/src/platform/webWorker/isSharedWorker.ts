1cab2b01426f886c228c65eaadbaed6e1211a26e
wtklbm <wtklbm@gmail.com>
2024-11-08 21:01:09 +0800

    📃 docs: 更新文档

--- packages/types/src/platform/webWorker/isSharedWorker.ts
+++ packages/types/src/platform/webWorker/isSharedWorker.ts
@@ -5,6 +5,7 @@ declare const SharedWorkerGlobalScope: Function | undefined;
  *
  * @returns 是则返回 `true`，否则为 `false`
  * @see [MDN](https://developer.mozilla.org/en-US/docs/Web/API/SharedWorkerGlobalScope)
+ * @note 该方法使用了 `globalThis`，有关更多信息，请参见 `isGlobalThis` 方法
  */
 export default function isSharedWorker(): boolean {
     return (
