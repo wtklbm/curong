467ea0b92a8561f12acc64ad355676a8dc613fad
wtklbm <wtklbm@gmail.com>
2024-11-08 20:58:26 +0800

    📃 docs: 更新文档

--- packages/types/src/platform/browser/isBrowser.ts
+++ packages/types/src/platform/browser/isBrowser.ts
@@ -7,6 +7,7 @@ import isFuzzyJsDom from '../constants/isFuzzyJsDom';
  * 当前的执行环境是不是浏览器
  *
  * @returns 是则返回 `true`，否则为 `false`
+ * @note 该方法使用了 `globalThis`，有关更多信息，请参见 `isGlobalThis` 方法
  */
 export default function isBrowser(): boolean {
     return (
