ba58d4bc03742ba68b911cd52e07eaaea0108194
wtklbm <wtklbm@gmail.com>
2024-11-08 20:58:21 +0800

    📃 docs: 更新文档

--- packages/asserts/src/platform/command/assertNodejsProcess.ts
+++ packages/asserts/src/platform/command/assertNodejsProcess.ts
@@ -5,6 +5,7 @@ import { isNodejsProcess } from '@curong/types';
  *
  * @param process `process` 对象，默认为 `globalThis.process`
  * @throws 如果不是则会抛出类型异常
+ * @note 该方法使用了 `globalThis`，有关更多信息，请参见 `@curong/types/isGlobalThis` 方法
  */
 export default function assertNodejsProcess(process = globalThis.process) {
     if (!isNodejsProcess(process)) {
