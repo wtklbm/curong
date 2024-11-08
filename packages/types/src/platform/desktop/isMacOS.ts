3fc14f3c08495898757728b2601a21fae36a9891
wtklbm <wtklbm@gmail.com>
2024-11-08 20:59:42 +0800

    📃 docs: 更新文档

--- packages/types/src/platform/desktop/isMacOS.ts
+++ packages/types/src/platform/desktop/isMacOS.ts
@@ -5,6 +5,7 @@ import isNavigator from '../constants/isNavigator';
  * 当前的执行环境是不是 `macOS`
  *
  * @returns 是则返回 `true`，否则为 `false`
+ * @note 该方法使用了 `globalThis`，有关更多信息，请参见 `isGlobalThis` 方法
  */
 export default function isMacOS(): boolean {
     // NOTE 也可以判断 `globalThis.os.type()` 是否为 `Darwin`
