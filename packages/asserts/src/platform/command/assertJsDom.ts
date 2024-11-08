bd4cbc976c4a76f911ae1d2627a5768b7def0ecd
wtklbm <wtklbm@gmail.com>
2024-11-08 20:58:15 +0800

    📃 docs: 更新文档

--- packages/asserts/src/platform/command/assertJsDom.ts
+++ packages/asserts/src/platform/command/assertJsDom.ts
@@ -5,6 +5,7 @@ import { isJsDom } from '@curong/types';
  *
  * @param window `jsdom` 中的 `Window` 对象，默认为 `globalThis.window` (即当前执行环境的全局根属性)
  * @throws 如果不是则会抛出类型异常
+ * @note 该方法使用了 `globalThis`，有关更多信息，请参见 `@curong/types/isGlobalThis` 方法
  */
 export default function assertJsDom(window = globalThis.window) {
     if (!isJsDom(window)) {
