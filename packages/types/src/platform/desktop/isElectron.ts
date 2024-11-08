2ab62745286c3e4a781748e1e0aa6518084f81b3
wtklbm <wtklbm@gmail.com>
2024-11-08 20:59:31 +0800

    📃 docs: 更新文档

--- packages/types/src/platform/desktop/isElectron.ts
+++ packages/types/src/platform/desktop/isElectron.ts
@@ -6,6 +6,7 @@ import isNodejsProcess from '../command/isNodejsProcess';
  * 当前的执行环境是不是 `Electron`
  *
  * @returns 是则返回 `true`，否则为 `false`
+ * @note 该方法使用了 `globalThis`，有关更多信息，请参见 `isGlobalThis` 方法
  */
 export default function isElectron(): boolean {
     return isNodejsProcess() && isString(globalThis.process.versions?.electron);
