ffb1be89e6824e8d70b4393b23c350abbda233ae
wtklbm <wtklbm@gmail.com>
2024-11-08 20:59:16 +0800

    📃 docs: 更新文档

--- packages/types/src/platform/constants/getUserAgent.ts
+++ packages/types/src/platform/constants/getUserAgent.ts
@@ -1,9 +1,12 @@
+import isNavigator from './isNavigator';
+
 /**
  * 获取 `userAgent` 字符串
  *
  * @param ua 要使用的后备值
  * @returns 如果 `navigator.userAgent` 存在则返回该值，否则返回后备值
+ * @note 该方法使用了 `globalThis`，有关更多信息，请参见 `isGlobalThis` 方法
  */
 export default function getUserAgent(ua: string = '') {
-    return globalThis.navigator?.userAgent ?? ua;
+    return isNavigator() ? ( globalThis.navigator.userAgent ?? ua) : ua;
 }
