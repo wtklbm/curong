1e903ad08cd8262a6efda7c525bbb70fec026017
wtklbm <wtklbm@gmail.com>
2024-11-08 21:00:47 +0800

    📃 docs: 更新文档

--- packages/types/src/platform/webWorker/isDedicatedWorker.ts
+++ packages/types/src/platform/webWorker/isDedicatedWorker.ts
@@ -5,6 +5,7 @@ declare const DedicatedWorkerGlobalScope: Function | undefined;
  *
  * @returns 是则返回 `true`，否则为 `false`
  * @see [MDN](https://developer.mozilla.org/en-US/docs/Web/API/DedicatedWorkerGlobalScope)
+ * @note 该方法使用了 `globalThis`，有关更多信息，请参见 `isGlobalThis` 方法
  */
 export default function isDedicatedWorker(): boolean {
     return (
