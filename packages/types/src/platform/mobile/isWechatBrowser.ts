a3261fe6a5734a7e13786b14a93b171262f319e1
wtklbm <wtklbm@gmail.com>
2024-11-08 21:00:39 +0800

    📃 docs: 更新文档

--- packages/types/src/platform/mobile/isWechatBrowser.ts
+++ packages/types/src/platform/mobile/isWechatBrowser.ts
@@ -1,8 +1,11 @@
+import isNavigator from '../constants/isNavigator';
+
 /**
  * 当前的执行环境是不是微信的内置浏览器
  *
  * @returns 是则返回 `true`，否则为 `false`
+ * @note 该方法使用了 `globalThis`，有关更多信息，请参见 `isGlobalThis` 方法
  */
 export default function isWechatBrowser() {
-    return /MicroMessenger/i.test(globalThis.navigator?.userAgent)
+    return isNavigator() && /MicroMessenger/i.test(globalThis.navigator.userAgent);
 }
