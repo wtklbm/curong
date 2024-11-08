0b53cbb9887198a8dccfc2f2747d903c93f2ce55
wtklbm <wtklbm@gmail.com>
2024-11-08 20:58:59 +0800

    📃 docs: 更新文档

--- packages/types/src/platform/command/isNodejs.ts
+++ packages/types/src/platform/command/isNodejs.ts
@@ -9,6 +9,7 @@ import isNodejsProcess from './isNodejsProcess';
  * 当前的执行环境是不是 `Node.js`
  *
  * @returns 是则返回 `true`，否则为 `false`
+ * @note 该方法使用了 `globalThis`，有关更多信息，请参见 `isGlobalThis` 方法
  */
 export default function isNodejs(): boolean {
     return (
