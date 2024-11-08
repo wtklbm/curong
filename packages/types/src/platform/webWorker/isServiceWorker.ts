90aa7d6bb6dd88c9a58e0191f3626fb750851a62
wtklbm <wtklbm@gmail.com>
2024-11-08 21:00:54 +0800

    📃 docs: 更新文档

--- packages/types/src/platform/webWorker/isServiceWorker.ts
+++ packages/types/src/platform/webWorker/isServiceWorker.ts
@@ -5,6 +5,7 @@ declare const ServiceWorkerGlobalScope: Function | undefined;
  *
  * @returns 是则返回 `true`，否则为 `false`
  * @see [MDN](https://developer.mozilla.org/en-US/docs/Web/API/ServiceWorkerGlobalScope)
+ * @note 该方法使用了 `globalThis`，有关更多信息，请参见 `isGlobalThis` 方法
  */
 export default function isServiceWorker(): boolean {
     return (
