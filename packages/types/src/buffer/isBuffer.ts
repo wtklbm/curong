cccb4a243cdd239d0edd8fa9a7c95c4fcf6aa7a2
wtklbm <wtklbm@gmail.com>
2024-11-08 14:02:50 +0800

    🦄 refactor: 更新实现与文档

--- packages/types/src/buffer/isBuffer.ts
+++ packages/types/src/buffer/isBuffer.ts
@@ -1,32 +1,20 @@
-const r = (value: any) => value.constructor.isBuffer(value);
-
 /**
  * 是不是一个 `Buffer`
  *
  * @param value 要验证的值
  * @returns 是则返回 `true`，否则为 `false`
- * @source
- *
- * ```typescript
- * Buffer.isBuffer = function isBuffer(b) {
- *   return b instanceof Buffer;
- * };
- * ```
- *
  * @note
- *
  * `Buffer` 类是 JavaScript `Uint8Array` 类的子类，并使用涵盖额外用例的方法对其进行扩展。
  * `Node.js` API 在支持 `Buffer` 的地方也接受纯 `Uint8Array`。
  *  在进行类型判断时，应该始终使用 `isBuffer` 方法。而不是使用 `isUint8Array`。
  */
 export default function isBuffer(value: unknown): value is Buffer {
-    try {
-        return (
-            r(value) ||
-            ((value as Buffer).readFloatLE &&
-                r((value as Buffer).subarray(0, 1)))
-        );
-    } catch {}
-
-    return false;
+    // 在判断时，也可以调用 `value.constructor.isBuffer(value)`
+    // 也可以调用 `value.constructor.isBuffer(value.subarray(0, 1))`
+    // 如果写简单点，就是调用 `Buffer.isBuffer` 方法
+    return (
+        typeof Buffer === 'function' &&
+        typeof Buffer.isBuffer === 'function' &&
+        Buffer.isBuffer(value)
+    );
 }
