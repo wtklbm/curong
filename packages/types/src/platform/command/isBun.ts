36434e22daf85e9bd790a2ea3ff17783370a41ce
wtklbm <wtklbm@gmail.com>
2024-11-08 20:58:38 +0800

    📃 docs: 更新文档

--- packages/types/src/platform/command/isBun.ts
+++ packages/types/src/platform/command/isBun.ts
@@ -9,6 +9,7 @@ declare global {
  * 当前的执行环境是不是 `Bun`
  *
  * @returns 是则返回 `true`，否则为 `false`
+ * @note 该方法使用了 `globalThis`，有关更多信息，请参见 `isGlobalThis` 方法
  */
 export default function isBun(): boolean {
     return (
