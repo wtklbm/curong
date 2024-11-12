e7bec511295991811de74d6afcfc61a6ccbc8a28
wtklbm <wtklbm@gmail.com>
2024-11-12 16:41:03 +0800

    🧪 test: 添加 `isBase64` 方法的测试

---
+++ packages/regexp/src/crypto/__tests__/isBase64.test.ts
@@ -0,0 +1,64 @@
+import { isBase64 } from '..';
+
+describe('@curong/regexp/isBase64', () => {
+    test('测试1', () => {
+        for (const value of [
+            '12345',
+            '',
+            'Vml2YW11cyBmZXJtZtesting123',
+            'Zg=',
+            'Z===',
+            'Zm=8',
+            '=m9vYg==',
+            'Zm9vYmFy===='
+        ]) {
+            expect(isBase64(value)).toBe(false);
+        }
+
+        for (const value of [
+            ' AA',
+            '\tAA',
+            '\rAA',
+            '123=',
+            '',
+            '\nAA',
+            'This+isa/bad+base64Url==',
+            '0K3RgtC+INC30LDQutC+0LTQuNGA0L7QstCw0L3QvdCw0Y8g0YHRgtGA0L7QutCw'
+        ]) {
+            expect(isBase64(value, true)).toBe(false);
+        }
+    });
+
+    test('测试2', () => {
+        for (const value of [
+            'Zg==',
+            'Zm8=',
+            'Zm9v',
+            'Zm9vYg==',
+            'Zm9vYmE=',
+            'Zm9vYmFy',
+            'TG9yZW0gaXBzdW0gZG9sb3Igc2l0IGFtZXQsIGNvbnNlY3RldHVyIGFkaXBpc2NpbmcgZWxpdC4=',
+            'Vml2YW11cyBmZXJtZW50dW0gc2VtcGVyIHBvcnRhLg==',
+            'U3VzcGVuZGlzc2UgbGVjdHVzIGxlbw==',
+            'MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAuMPNS1Ufof9EW/M98FNw' +
+                'UAKrwflsqVxaxQjBQnHQmiI7Vac40t8x7pIb8gLGV6wL7sBTJiPovJ0V7y7oc0Ye' +
+                'rhKh0Rm4skP2z/jHwwZICgGzBvA0rH8xlhUiTvcwDCJ0kc+fh35hNt8srZQM4619' +
+                'FTgB66Xmp4EtVyhpQV+t02g6NzK72oZI0vnAvqhpkxLeLiMCyrI416wHm5Tkukhx' +
+                'QmcL2a6hNOyu0ixX/x2kSFXApEnVrJ+/IxGyfyw8kf4N2IZpW5nEP847lpfj0SZZ' +
+                'Fwrd1mnfnDbYohX2zRptLy2ZUn06Qo9pkG5ntvFEPo9bfZeULtjYzIl6K8gJ2uGZ' +
+                'HQIDAQAB'
+        ]) {
+            expect(isBase64(value)).toBe(true);
+        }
+
+        for (const value of [
+            'bGFkaWVzIGFuZCBnZW50bGVtZW4sIHdlIGFyZSBmbG9hdGluZyBpbiBzcGFjZQ',
+            '1234',
+            'bXVtLW5ldmVyLXByb3Vk',
+            'PDw_Pz8-Pg',
+            'VGhpcyBpcyBhbiBlbmNvZGVkIHN0cmluZw'
+        ]) {
+            expect(isBase64(value, true)).toBe(true);
+        }
+    });
+});
