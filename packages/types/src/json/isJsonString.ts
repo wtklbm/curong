4d05aee1972eef3b73315a9d6cc22f2aacce3e77
wtklbm <wtklbm@gmail.com>
2024-11-12 16:42:14 +0800

    ✨ feat: 添加 `isJsonString` 方法

---
+++ packages/types/src/json/isJsonString.ts
@@ -0,0 +1,20 @@
+import isStringFilled from '../string/isStringFilled';
+
+/**
+ * 是不是一个 JSON 字符串
+ *
+ * @param value 要验证的值
+ * @returns 是则返回 `true`，否则为 `false`。空字符串为 `false`
+ */
+export default function isJsonString(value: unknown): boolean {
+    if (!isStringFilled(value)) {
+        return false;
+    }
+
+    try {
+        JSON.parse(value);
+        return true;
+    } catch {
+        return false;
+    }
+}
