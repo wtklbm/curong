9b4fdbe313c6ea21304e4fee5b44ab6e6e5beaaf
wtklbm <wtklbm@gmail.com>
2024-11-13 13:20:44 +0800

    🦄 refactor: 删除 `try...catch`

--- packages/types/src/array/isArray.ts
+++ packages/types/src/array/isArray.ts
@@ -5,16 +5,6 @@ import getTagEqual from '../type/getTagEqual';
  *
  * @param value 要验证的值
  * @returns 是则返回 `true`，否则为 `false`
- * @polyfill
- *
- * ```typescript
- * if (!Array.isArray) {
- *   Array.isArray = function(arg) {
- *     return Object.prototype.toString.call(arg) === '[object Array]';
- *   };
- * }
- * ```
- *
  * @note
  *
  * 该方法将首先尝试调用 `Array.isArray`，如果调用失败，则尝试通过 `Object.prototype.toString.call` 获取标签，判断是否等于 `[object Array]`。
@@ -22,9 +12,5 @@ import getTagEqual from '../type/getTagEqual';
 export default function isArray<T extends unknown[]>(
     value: unknown
 ): value is T {
-    try {
-        return Array.isArray(value);
-    } catch {}
-
-    return getTagEqual(value, 'Array');
+    return Array.isArray?.(value) || getTagEqual(value, 'Array');
 }
