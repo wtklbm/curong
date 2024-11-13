c5d34ae2881a84fa956a6809fabd54413a3d2506
wtklbm <wtklbm@gmail.com>
2024-11-13 15:49:54 +0800

    🦄 refactor: 删除 `try...catch`

--- packages/types/src/property/isPrototypeOf.ts
+++ packages/types/src/property/isPrototypeOf.ts
@@ -1,3 +1,5 @@
+import isNullOrUndefined from '../nullable/isNullOrUndefined';
+
 /**
  * 判断一个对象是否存在于另一个对象的原型链中
  *
@@ -5,9 +7,5 @@
  * @param v 要检查其原型链的另一个对象
  */
 export default function isPrototypeOf<T>(o: T, v: PropertyKey) {
-    try {
-        return Object.prototype.isPrototypeOf.call(o, v);
-    } catch {}
-
-    return false;
+    return !isNullOrUndefined(o) && Object.prototype.isPrototypeOf.call(o, v);
 }
