58f241bbc549e8ff43ad81067dbe445223990da1
wtklbm <wtklbm@gmail.com>
2024-11-13 15:54:30 +0800

    🦄 refactor: 更新逻辑

--- packages/util/src/allAttrs.ts
+++ packages/util/src/allAttrs.ts
@@ -20,7 +20,7 @@
 export default function allAttrs(value: any): PropertyKey[] {
     try {
         return Reflect.ownKeys(value);
-    } catch {}
-
-    return [];
+    } catch {
+        return [];
+    }
 }
