4ec84376cfd9906963007a1bc729a6ad428b9064
wtklbm <wtklbm@gmail.com>
2024-11-14 17:40:27 +0800

    ✨ feat: 添加 `parseBytesSize` 方法

--- packages/string/src/bytes/index.ts
+++ packages/string/src/bytes/index.ts
@@ -1,4 +1,7 @@
 export { default as bytesArray } from './bytesArray';
 export { default as bytesLength } from './bytesLength';
 export { default as formatBytesSize } from './formatBytesSize';
+export { default as parseBytesSize } from './parseBytesSize';
 export { default as splitByBytes } from './splitByBytes';
+
+export * from './types';
