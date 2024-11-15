2f5022a83c82d47372dcda49f65bf27df82bb439
wtklbm <wtklbm@gmail.com>
2024-11-15 13:15:15 +0800

    ✨ feat: 添加 `trimIndent` 方法

--- packages/string/src/indent/index.ts
+++ packages/string/src/indent/index.ts
@@ -1,4 +1,5 @@
 export { default as findMinIndent } from './findMinIndent';
 export { default as padIndent } from './padIndent';
+export { default as trimIndent } from './trimIndent';
 
 export * from './types';
