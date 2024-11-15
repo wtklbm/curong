822bec9cf85067da1237930350320df6bc36e68f
wtklbm <wtklbm@gmail.com>
2024-11-15 21:37:41 +0800

    📃 docs: 更新文档

--- packages/function/src/select/select.ts
+++ packages/function/src/select/select.ts
@@ -8,11 +8,23 @@ import executeToPromise from '../promise/executeToPromise';
  * @param doElse 条件为假时要返回的后备值，或者要执行的同步函数、异步函数、`Promise` 或普通值
  * @param args 传递给 `condition`、`doIf` 和 `doElse` 的参数
  * @returns 返回指定条件的值
- * @example
+ * @example ````
+ *
+ * ### 常规用法
  *
  * ```typescript
  * const ret = select(true, Promise.resolve(1), 2);
  * console.log(ret); // 1
+ * ```
+ *
+ * ### 区分开发环境和生成环境
+ *
+ * ```typescript
+ * const devOrProd = async (devValue: unknown, prodValue: unknown) => {
+ *     return await select(/dev|development/.test(process.env.NODE_ENV), devValue, prodValue);
+ * };
+ *
+ * devOrProd(1, 2);
  * ```
  */
 export default async function select<A extends unknown[], T>(
