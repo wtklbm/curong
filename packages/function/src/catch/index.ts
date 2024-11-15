56af3147afa7acf002313fe82be7b495efe36b00
wtklbm <wtklbm@gmail.com>
2024-11-16 01:26:06 +0800

    ✨ feat: 添加 `retryWithCondition` 方法

--- packages/function/src/catch/index.ts
+++ packages/function/src/catch/index.ts
@@ -1,6 +1,7 @@
 export { default as ifThrow } from './ifThrow';
 export { default as noCatch } from './noCatch';
 export { default as retry } from './retry';
+export { default as retryWithCondition } from './retryWithCondition';
 export { default as tryCatch } from './tryCatch';
 
 export * from './types';
