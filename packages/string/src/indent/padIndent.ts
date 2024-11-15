4dfb0b4174abaff2b9463c6c4bb4d7ab8a465bd6
wtklbm <wtklbm@gmail.com>
2024-11-15 12:54:07 +0800

    ✨ feat: 添加 `padIndent` 方法

---
+++ packages/string/src/indent/padIndent.ts
@@ -0,0 +1,34 @@
+import type { PadIndentOptions } from './types';
+
+/**
+ * 对字符串的每一行进行缩进
+ *
+ * @param value 要填充的字符串
+ * @param options 配置选项
+ *  - `indent`: 当缩进时要填充的内容，默认为 ` `
+ *  - `count`: 重复几次填充的内容，默认为 `1`
+ *  - `emptyLines`: 是否将空行也包含在内，默认为 `true`
+ * @returns 返回处理的字符串
+ * @example
+ *
+ * ```typescript
+ * const ret = padIndent('description.\n\nthis is a test.', {
+ *     count: 4,
+ *     emptyLines: false
+ * });
+ * console.log(ret); //  '    description.\n\n    this is a test.'
+ * ```
+ */
+export default function padIndent(value: string, options?: PadIndentOptions) {
+    const { indent, count, emptyLines } = {
+        indent: ' ',
+        count: 1,
+        emptyLines: true,
+        ...options
+    };
+
+    return value.replace(
+        emptyLines ? /^/gm : /^(?!^$)/gm,
+        indent.repeat(count)
+    );
+}
