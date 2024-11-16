8863da41306024a705e178cc3ffb8873a76c9bac
wtklbm <wtklbm@gmail.com>
2024-11-16 14:50:23 +0800

    🧪 test: 添加新的测试

--- packages/object/src/cascade/__tests__/cascade.test.ts
+++ packages/object/src/cascade/__tests__/cascade.test.ts
@@ -36,4 +36,32 @@ describe('@curong/object/cascade', () => {
         // @ts-ignore
         expect(cascade(o, 'key[1].value', null, false)).toBe(undefined);
     });
+
+    test('测试4', () => {
+        let obj = {
+            foo: {
+                bar: ['baz', 'bux'],
+                fux: 5,
+                '00N40000002S5U0': 1,
+                N40000002S5U0: 2,
+                'FE43-D880-21AE': 3
+            }
+        };
+
+        const getter = (k: string) => cascade(obj, k);
+        expect(getter('foo.fux')).toBe(5);
+        expect(getter('foo.bar')).toEqual(['baz', 'bux']);
+        expect(getter('foo.bar[1]')).toBe('bux');
+        expect(getter('["foo"]["bar"][1]')).toBe('bux');
+
+        expect(getter('foo.gih.df[0]')).toBeUndefined();
+        expect(getter('["fr"]["bzr"][1]')).toBeUndefined();
+
+        expect(getter('foo["00N40000002S5U0"]')).toBe(1);
+        expect(getter('foo.00N40000002S5U0')).toBe(1);
+        expect(getter('foo["N40000002S5U0"]')).toBe(2);
+        expect(getter('foo.N40000002S5U0')).toBe(2);
+        expect(getter('foo["FE43-D880-21AE"]')).toBe(3);
+        expect(getter('foo.FE43-D880-21AE')).toBe(3);
+    });
 });
