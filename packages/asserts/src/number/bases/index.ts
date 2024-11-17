0abd3454faf4a7f4bbf54bbbed3a47072cdaf466
wtklbm <wtklbm@gmail.com>
2024-11-17 16:20:42 +0800

    ✨ feat: 添加新的方法

---
+++ packages/asserts/src/number/bases/index.ts
@@ -0,0 +1,3 @@
+export { default as assertBinaryString } from './assertBinaryString';
+export { default as assertHexString } from './assertHexString';
+export { default as assertOctalString } from './assertOctalString';
