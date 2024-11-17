4493e45e90bb404b481684e5b197f585f7db7cc2
wtklbm <wtklbm@gmail.com>
2024-11-17 23:30:37 +0800

    ✨ feat: 添加 `assertTaro` 方法

--- packages/asserts/src/platform/mobile/index.ts
+++ packages/asserts/src/platform/mobile/index.ts
@@ -3,4 +3,5 @@ export { default as assertIOS } from './assertIOS';
 export { default as assertMiniProgram } from './assertMiniProgram';
 export { default as assertMiniProgramWebView } from './assertMiniProgramWebView';
 export { default as assertReactNative } from './assertReactNative';
+export { default as assertTaro } from './assertTaro';
 export { default as assertWechatBrowser } from './assertWechatBrowser';
