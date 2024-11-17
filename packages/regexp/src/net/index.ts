e3f130fff4ab56ec35b938b24b5cb1cc04010a16
wtklbm <wtklbm@gmail.com>
2024-11-17 16:37:14 +0800

    ✨ feat: 添加 `isPort` 方法

--- packages/regexp/src/net/index.ts
+++ packages/regexp/src/net/index.ts
@@ -1,3 +1,4 @@
 export { default as isIP } from './isIP';
 export { default as isIPv4 } from './isIPv4';
 export { default as isIPv6 } from './isIPv6';
+export { default as isPort } from './isPort';
