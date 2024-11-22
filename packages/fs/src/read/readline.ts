bb3e73afcc6c5927af39763a78c466bd7a1c19db
wtklbm <wtklbm@gmail.com>
2024-11-22 23:43:45 +0800

    🦄 refactor: 更新逻辑

--- packages/fs/src/read/readline.ts
+++ packages/fs/src/read/readline.ts
@@ -1,4 +1,4 @@
-import { isBuffer, isFunctionFilled, isString } from '@curong/types';
+import { isBuffer, isFunction, isString } from '@curong/types';
 
 import type { ReadlineCallback } from './types';
 
@@ -8,51 +8,66 @@ import type { ReadlineCallback } from './types';
  * @param chunk 要读取的文本内容
  * @param encoding 转换 `Buffer` 用到的编码
  * @param callback 回调函数
- * @throws
- *
- * - 如果 `chunk` 不是 `Buffer` 或 `string`，则会抛出异常
+ * @throws 如果 `chunk` 不是 `Buffer` 或 `string`，则会抛出异常
+ * @note
+ *  - 在调用 `readline()` 之前，请勿使用 `access()` 检查文件的可访问性。
+ *   这样做会引入竞态条件，因为其他进程可能会在两次调用之间更改文件的状态。
+ *   相反，用户代码应该直接 `read` 文件并处理文件不可访问时引发的错误。
  */
+export default function readline(
+    chunk: string | Buffer,
+    encoding?: BufferEncoding
+): string[];
+
+export default function readline(
+    chunk: string | Buffer,
+    callback: ReadlineCallback
+): null;
+
+export default function readline(
+    chunk: string | Buffer,
+    encoding: BufferEncoding,
+    callback: ReadlineCallback
+): null;
+
 export default function readline(
     chunk: string | Buffer,
     encoding?: BufferEncoding | undefined | null | ReadlineCallback,
     callback?: ReadlineCallback
-): Array<string> | null {
-    const contents: Array<string> = [];
+): string[] | null {
+    const contents: string[] = [];
     const buffer: unknown[] = [];
+
+    if (!isFunction(callback)) {
+        if (isFunction(encoding)) {
+            callback = encoding as ReadlineCallback;
+            encoding = 'utf8';
+        } else {
+            callback = (value, _done) => contents.push(value);
+            encoding = encoding ?? 'utf8';
+        }
+    }
+
+    // 根据 `chunk` 的类型来处理换行
     let toString: (data: any) => string;
-    let flag: boolean;
 
-    do {
-        if ((flag = !isFunctionFilled(callback))) {
-            if (isFunctionFilled(encoding)) {
-                callback = encoding as ReadlineCallback;
-                encoding = 'utf8';
-            } else {
-                callback = (value, _done) => contents.push(value);
-                encoding = encoding ?? 'utf8';
-            }
-        }
-
-        // 根据 `chunk` 的类型来处理换行
-        if (isString(chunk)) {
-            toString = data => data.join('');
-            break;
-        }
-
-        if (isBuffer(chunk)) {
-            toString = data =>
-                Buffer.from(data).toString(encoding as BufferEncoding);
-            break;
-        }
-
-        throw new TypeError(`[readline] 数据必须是 Buffer 或 string`);
-    } while (false);
+    if (isString(chunk)) {
+        toString = data => data.join('');
+    } else if (isBuffer(chunk)) {
+        toString = data => {
+            return Buffer.from(data).toString(encoding as BufferEncoding);
+        };
+    } else {
+        throw new TypeError('chunk 必须是 Buffer 或 string', {
+            cause: { function: 'readline', chunk, encoding, callback }
+        });
+    }
 
     for (let i = 0, len = chunk.length; i < len; i++) {
         const byte: string | number = chunk[i];
 
         switch (byte) {
-            // '\r'
+            // 回车 (Carriage Return)，即 `\r`
             case '\u000D':
                 callback(toString(buffer), false);
                 const next = chunk[i + 1];
@@ -60,7 +75,7 @@ export default function readline(
                 buffer.length = 0;
                 break;
 
-            // '\n'
+            // 行结束 (End of Line)，即 `\n`
             case '\u000A':
                 callback(toString(buffer), false);
                 buffer.length = 0;
@@ -72,11 +87,7 @@ export default function readline(
         }
     }
 
-    callback(buffer.length ? toString(buffer) : '', true);
+    callback(toString(buffer), true);
 
-    if (flag) {
-        return contents;
-    }
-
-    return null;
+    return contents.length ? contents : null;
 }
