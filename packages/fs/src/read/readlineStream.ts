bc7cd1e653ac90737d857bb8697e1e1255d7aad3
wtklbm <wtklbm@gmail.com>
2024-11-22 17:56:24 +0800

    🦄 refactor: 更新逻辑和返回值

--- packages/fs/src/read/readlineStream.ts
+++ packages/fs/src/read/readlineStream.ts
@@ -1,10 +1,7 @@
 import { createReadStream, type PathLike } from 'fs';
 import { createInterface } from 'readline';
 
-import { format } from '@curong/term';
-import { isFunctionFilled } from '@curong/types';
-
-import isFile from '../is/isFile';
+import { isFunction } from '@curong/types';
 
 import type { ReadlineStreamCallback } from './types';
 
@@ -14,36 +11,40 @@ import type { ReadlineStreamCallback } from './types';
  * @param path 文件的路径字符串、URL、Buffer
  * @param encoding 文件编码，默认为 `utf8`
  * @param callback 回调函数
- *
- * 回调函数参数:
- *  - `input` 每一行的数据
- *
  * @returns
- *
- * - 如果传递了回调函数，那么文件读完后该函数会返回 `true` 表示读取完毕
- * - 如果没有传递回调函数，那么文件读完后该函数会返回一个包含每一行数据的字符串数组
- *
- * @throws
- *
- * - 如果 `path` 不是文件路径，则会抛出异常
+ *  - 如果传递了回调函数，那么文件读完后该函数会返回 `null` 表示读取完毕
+ *  - 如果没有传递回调函数，那么文件读完后该函数会返回一个包含每一行数据的字符串数组
+ * @throws 如果文件读取失败，则会抛出异常
+ * @note
+ *  - 在调用 `readlineStream()` 之前，请勿使用 `access()` 检查文件的可访问性。
+ *   这样做会引入竞态条件，因为其他进程可能会在两次调用之间更改文件的状态。
+ *   相反，用户代码应该直接 `read` 文件并处理文件不可访问时引发的错误。
  */
+export default async function readlineStream(
+    path: PathLike,
+    encoding?: BufferEncoding
+): Promise<string[]>;
+
+export default async function readlineStream(
+    path: PathLike,
+    callback: ReadlineStreamCallback
+): Promise<null>;
+
+export default async function readlineStream(
+    path: PathLike,
+    encoding: BufferEncoding,
+    callback: ReadlineStreamCallback
+): Promise<null>;
+
 export default async function readlineStream(
     path: PathLike,
     encoding?: BufferEncoding | ReadlineStreamCallback,
     callback?: ReadlineStreamCallback
-): Promise<string[] | boolean> {
-    if (!(await isFile(path))) {
-        throw format({
-            name: 'readlineStream',
-            message: 'path 不是一个文件的路径，该文件不存在',
-            data: { path }
-        });
-    }
-
+): Promise<string[] | null> {
     const ret: string[] = [];
 
-    if (!isFunctionFilled(callback)) {
-        if (isFunctionFilled(encoding)) {
+    if (!isFunction(callback)) {
+        if (isFunction(encoding)) {
             callback = encoding;
             encoding = 'utf8';
         } else {
@@ -52,7 +53,7 @@ export default async function readlineStream(
         }
     }
 
-    return new Promise(resolve => {
+    return new Promise((resolve, reject) => {
         const stream = createInterface({
             input: createReadStream(path, {
                 encoding: encoding as BufferEncoding,
@@ -64,15 +65,21 @@ export default async function readlineStream(
         });
 
         stream.on('line', callback as ReadlineStreamCallback);
-
+        stream.on('error', error => {
+            const e = new Error('文件读取失败', {
+                cause: {
+                    function: 'readlineStream',
+                    error,
+                    path,
+                    encoding,
+                    callback: callback.toString()
+                }
+            });
+            reject(e);
+        });
         stream.once('close', () => {
             stream.removeAllListeners();
-
-            if (ret.length > 0) {
-                return resolve(ret);
-            }
-
-            return resolve(true);
+            return resolve(ret.length ? ret : null);
         });
     });
 }
