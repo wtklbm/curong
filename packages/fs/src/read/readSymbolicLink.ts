5d85b1f516385c3e4fa1999102c0fecdcca4641c
wtklbm <wtklbm@gmail.com>
2024-11-22 18:37:37 +0800

    📃 docs: 更新文档

--- packages/fs/src/read/readSymbolicLink.ts
+++ packages/fs/src/read/readSymbolicLink.ts
@@ -6,6 +6,10 @@ import { readlink } from 'fs/promises';
  *
  * @param path 符号链接字符串、URL、Buffer
  * @returns 如果找到符号链接，则返回符号链接的源路径，否则返回 `null`
+ * @note
+ *  - 在调用 `readSymbolicLink()` 之前，请勿使用 `access()` 检查文件的可访问性。
+ *   这样做会引入竞态条件，因为其他进程可能会在两次调用之间更改文件的状态。
+ *   相反，用户代码应该直接 `read` 文件并处理文件不可访问时引发的错误。
  */
 export default async function readSymbolicLink(
     path: PathLike
