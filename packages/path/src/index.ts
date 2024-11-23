d08cef35a343eadab250e13da6f6d37d548c188b
wtklbm <wtklbm@gmail.com>
2024-11-23 22:21:05 +0800

    ✨ feat: 添加 `toAbsolute` 方法

--- packages/path/src/index.ts
+++ packages/path/src/index.ts
@@ -1,5 +1,6 @@
 export { default as isWindowsExtendedLengthPath } from './isWindowsExtendedLengthPath';
 export { default as isWindowsPath } from './isWindowsPath';
 export { default as normalize } from './normalize';
+export { default as toAbsolute } from './toAbsolute';
 export { default as unifiedSeparator } from './unifiedSeparator';
 export { default as windowsPathSource } from './windowsPathSource';
