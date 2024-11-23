ec62be7968247aeec9f884952b2aa5dea6182376
wtklbm <wtklbm@gmail.com>
2024-11-23 18:20:44 +0800

    🦄 refactor: 优先删除文件

--- packages/fs/src/remove/rm.ts
+++ packages/fs/src/remove/rm.ts
@@ -13,12 +13,12 @@ import isFile from '../is/isFile';
  * - 如果 `path` 不是一个有效路径，则会抛出异常
  */
 export default async function rm(path: PathLike): Promise<void> {
-    if (await isDir(path)) {
-        return await rmdir(path, { recursive: true });
-    }
-
     if (await isFile(path)) {
         return await unlink(path);
+    }
+
+    if (await isDir(path)) {
+        return await rmdir(path, { recursive: true });
     }
 
     throw new TypeError('[rm] path 不是一个文件或文件夹', {
