dc31e3134735766dba35a163bb7e6ce167f91594
wtklbm <wtklbm@gmail.com>
2024-11-23 18:57:58 +0800

    ✨ feat: 添加 `isFileOrDir` 方法

--- packages/fs/src/is/index.ts
+++ packages/fs/src/is/index.ts
@@ -1,4 +1,5 @@
 export { default as exists } from './exists';
 export { default as isDir } from './isDir';
 export { default as isFile } from './isFile';
+export { default as isFileOrDir } from './isFileOrDir';
 export { default as isSymbolicLink } from './isSymbolicLink';
