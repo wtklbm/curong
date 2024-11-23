82c4167a52d95104c29103632367af237cee33b5
wtklbm <wtklbm@gmail.com>
2024-11-24 00:40:24 +0800

    ✨ feat: 添加 `assertBooleanString` 方法

---
+++ packages/asserts/src/boolean/assertBooleanString.ts
@@ -0,0 +1,17 @@
+import { isBooleanString } from '@curong/types';
+
+import typeGuard from '../constants/typeGuard';
+
+/**
+ * 是不是一个布尔值字符串，即字符串 `true` 或 `false`
+ *
+ * @param value 要验证的值
+ * @param variableName 该值的变量名
+ * @throws 如果不是则会抛出类型异常
+ */
+export default function assertBooleanString(
+    value: unknown,
+    variableName: string
+): asserts value is 'true' | 'false' {
+    return typeGuard(value, variableName, isBooleanString);
+}
