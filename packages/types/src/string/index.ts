3ec4adeae160999d749bb62be97ac8e7a99e29b5
wtklbm <wtklbm@gmail.com>
2024-11-24 22:34:39 +0800

    ✨ feat: 添加 `isToStringAble` 方法

--- packages/types/src/string/index.ts
+++ packages/types/src/string/index.ts
@@ -5,3 +5,6 @@ export { default as isStringFilled } from './isStringFilled';
 export { default as isStringObject } from './isStringObject';
 export { default as isStringPrimitive } from './isStringPrimitive';
 export { default as isStringTrim } from './isStringTrim';
+export { default as isToStringAble } from './isToStringAble';
+
+export * from './types';
