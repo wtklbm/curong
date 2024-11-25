f5f0bfec269e2a5755f4b488b7bee9755be15e3f
wtklbm <wtklbm@gmail.com>
2024-11-25 21:45:25 +0800

    ✨ feat: 添加新的方法

--- packages/types/src/nullable/function/index.ts
+++ packages/types/src/nullable/function/index.ts
@@ -1,4 +1,8 @@
 export { default as falsyOr } from './falsyOr';
 export { default as nullableOr } from './nullableOr';
+export { default as nullableToNull } from './nullableToNull';
+export { default as nullableToUndefined } from './nullableToUndefined';
 export { default as nullOr } from './nullOr';
+export { default as nullToUndefined } from './nullToUndefined';
 export { default as undefinedOr } from './undefinedOr';
+export { default as undefinedToNull } from './undefinedToNull';
