316a176ea052f8ee6f5ef18e7e779b1806cb79fd
wtklbm <wtklbm@gmail.com>
2024-11-25 21:52:02 +0800

    📃 docs: 更新文档

--- packages/number/src/toFiniteNumber.ts
+++ packages/number/src/toFiniteNumber.ts
@@ -7,19 +7,18 @@ import { isFinite } from '@curong/types';
  * @param defaultValue 默认值，如果无法转换为有限数值时使用
  * @returns 有限数值或默认值
  * @example
- *
  * ```typescript
  * // 返回 42，因为字符串 "42" 可以转换为有限数值
- * const result1 = toFiniteNumber("42", 0);
+ * console.log(toFiniteNumber("42", 0));
  *
  * // 返回 0，因为字符串 "foo" 无法转换为有限数值，使用了默认值 0
- * const result2 = toFiniteNumber("foo", 0);
+ * console.log(toFiniteNumber("foo", 0));
  *
  * // 返回 3.14，因为字符串 "3.14" 可以转换为有限数值
- * const result3 = toFiniteNumber("3.14", 0);
+ * console.log(toFiniteNumber("3.14", 0));
  *
  * // 返回 0，因为 undefined 无法转换为有限数值，使用了默认值 0
- * const result4 = toFiniteNumber(undefined, 0);
+ * console.log(toFiniteNumber(undefined, 0));
  * ```
  */
 export default function toFiniteNumber(
