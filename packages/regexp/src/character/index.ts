3d4845d41d83d4715e6a5bad4bc9f07b9daea45f
wtklbm <wtklbm@gmail.com>
2024-11-25 23:41:38 +0800

    ✨ feat: 添加新的方法

---
+++ packages/regexp/src/character/index.ts
@@ -0,0 +1,4 @@
+export { default as isCharacter } from './isCharacter';
+export { default as isDigit } from './isDigit';
+export { default as isLowerCase } from './isLowerCase';
+export { default as isUpperCase } from './isUpperCase';
