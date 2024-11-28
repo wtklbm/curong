1632636ffba9ff3e5fcc630c720dbcee898949b4
wtklbm <wtklbm@gmail.com>
2024-11-28 21:51:40 +0800

    ✨ feat: 添加 `isASCII` 方法

--- packages/regexp/src/character/index.ts
+++ packages/regexp/src/character/index.ts
@@ -1,3 +1,4 @@
+export { default as isASCII } from './isASCII';
 export { default as isCharacter } from './isCharacter';
 export { default as isDigit } from './isDigit';
 export { default as isLowerCase } from './isLowerCase';
