faf22a026d630771443a8fdb6bac6fc992f3568a
wtklbm <wtklbm@gmail.com>
2024-11-28 21:54:45 +0800

    ✨ feat: 添加 `isStrict` 选项

--- packages/types/src/number/bases/isOctalString.ts
+++ packages/types/src/number/bases/isOctalString.ts
@@ -1,11 +1,20 @@
 import isStringFilled from '../../string/isStringFilled';
 
+import type { Octal } from './types';
+
 /**
  * 是不是一个八进制表示形式的数字字符串，例如 `0o12`
  *
  * @param value 要验证的值
+ * @param isStrict 是否以 `0x` 前缀开头，默认为 `true`
  * @returns 是则返回 `true`，否则为 `false`
  */
-export default function isOctalString(value: unknown): value is string {
-    return isStringFilled(value) && /^0o[0-7]+$/.test(value);
+export default function isOctalString(
+    value: unknown,
+    isStrict = true
+): value is Octal {
+    return (
+        isStringFilled(value) &&
+        new RegExp(`^${isStrict ? '0o' : ''}[0-7]+$`).test(value)
+    );
 }
