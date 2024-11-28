64c2cf742f4e8e3863809b9237732a3ef93ac609
wtklbm <wtklbm@gmail.com>
2024-11-28 17:20:18 +0800

    ✨ feat: 增加一个方法重载

--- packages/term/src/printError.ts
+++ packages/term/src/printError.ts
@@ -1,11 +1,35 @@
+import { isError } from '@curong/types';
+
+import formatFromError from './formatFromError';
 import print from './print';
 import type { ForMatInfo } from './types';
+
+/**
+ * 在终端中打印一个 `Error` 对象
+ *
+ * @param error 要打印的 `Error` 对象
+ * @example
+ * ```typescript
+ * printError(new Error);
+ * ```
+ */
+export default function printError(error: Error): void;
 
 /**
  * 在终端打印一段错误消息
  *
  * @param message 要打印的消息列表
+ * @example
+ * ```typescript
+ * printError('抛出了一个错误，请处理');
+ * ```
  */
-export default function printError(...message: ForMatInfo['message'][]): void {
-    return print('error', ...message);
+export default function printError(...message: ForMatInfo['message'][]): void;
+
+export default function printError(...data: any): void {
+    if (isError(data[0])) {
+        return console.log(formatFromError(data[0]));
+    }
+
+    return print('error', ...data);
 }
