3f09a612a92a69016ce35ec9a23ef93656053be5
wtklbm <wtklbm@gmail.com>
2024-11-28 21:54:13 +0800

    ✨ feat: 添加 `isStrict` 选项

--- packages/types/src/number/bases/isBinaryString.ts
+++ packages/types/src/number/bases/isBinaryString.ts
@@ -1,11 +1,20 @@
 import isStringFilled from '../../string/isStringFilled';
 
+import type { Binary } from './types';
+
 /**
  * 是不是一个二进制表示形式的数字字符串，例如 `0b1010`
  *
  * @param value 要验证的值
+ * @param isStrict 是否以 `0x` 前缀开头，默认为 `true`
  * @returns 是则返回 `true`，否则为 `false`
  */
-export default function isBinaryString(value: unknown): value is string {
-    return isStringFilled(value) && /^0b[10]+$/.test(value);
+export default function isBinaryString(
+    value: unknown,
+    isStrict = true
+): value is Binary {
+    return (
+        isStringFilled(value) &&
+        new RegExp(`^${isStrict ? '0b' : ''}[10]+$`).test(value)
+    );
 }
