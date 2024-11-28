987c49a6f363bb7d375bdf38e0359d87c2fe77c5
wtklbm <wtklbm@gmail.com>
2024-11-28 16:57:40 +0800

    📃 docs: 更新类型文档

--- packages/term/src/types/format.ts
+++ packages/term/src/types/format.ts
@@ -25,19 +25,26 @@ export type ForMatInfo = {
     /** 要打印的数据 */
     data?: any;
 
-    /** 是否显示堆栈记录(除了 `type` 为 `error` 外) */
-    stack?: boolean;
+    /**
+     * 是否显示堆栈记录(除了 `type` 为 `error` 外)
+     *
+     * - 如果为 `true`，则显示一个新创建的调用栈
+     * - 如果为 `false`，则不显示调用栈
+     * - 如果为 `undefined` 且错误的类型为 `error`，则会自动显示一个新创建的调用栈
+     * - 如果为字符串，则将其视为调用栈字符串，并进行展示
+     */
+    stack?: boolean | string;
 
-    /** 是否显示日期 */
+    /** 是否显示日期，默认为 `true` */
     date?: boolean;
 };
 
 export type ForMatOptions = InspectOptions & {
     display?: {
-        /** 是否显示标题 */
+        /** 是否显示标题，默认为 `true` */
         title?: boolean;
 
-        /** 是否显示标题下面的分割线 */
+        /** 是否显示标题下面的分割线，默认为 `true` */
         dividers?: boolean;
     };
 };
