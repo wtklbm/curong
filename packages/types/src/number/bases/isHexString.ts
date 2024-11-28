de85784b24f05c042d43f0f563928f529e4f1c16
wtklbm <wtklbm@gmail.com>
2024-11-28 21:54:35 +0800

    ✨ feat: 添加 `isStrict` 选项

--- packages/types/src/number/bases/isHexString.ts
+++ packages/types/src/number/bases/isHexString.ts
@@ -1,11 +1,20 @@
 import isStringFilled from '../../string/isStringFilled';
 
+import type { Hex } from './types';
+
 /**
  * 是不是一个十六进制表示形式的数字字符串，例如 `0xA`
  *
  * @param value 要验证的值
+ * @param isStrict 是否以 `0x` 前缀开头，默认为 `true`
  * @returns 是则返回 `true`，否则为 `false`
  */
-export default function isHexString(value: unknown): value is string {
-    return isStringFilled(value) && /^0x[0-9a-fA-F]+$/.test(value);
+export default function isHexString(
+    value: unknown,
+    isStrict = true
+): value is Hex {
+    return (
+        isStringFilled(value) &&
+        new RegExp(`^${isStrict ? '0x' : ''}[0-9a-fA-F]+$`).test(value)
+    );
 }
