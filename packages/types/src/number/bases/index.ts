f7939bcc5d5ab0dc9d887e060ef872a44c4bb4e1
wtklbm <wtklbm@gmail.com>
2024-11-28 21:53:49 +0800

    ✨ feat: 添加新的类型

--- packages/types/src/number/bases/index.ts
+++ packages/types/src/number/bases/index.ts
@@ -1,3 +1,5 @@
 export { default as isBinaryString } from './isBinaryString';
 export { default as isHexString } from './isHexString';
 export { default as isOctalString } from './isOctalString';
+
+export * from './types';
