af936e2835cae2d11dc276987e01365442369084
wtklbm <wtklbm@gmail.com>
2024-11-29 15:25:04 +0800

    ✨ feat: 添加异常防止原型污染

--- packages/string/src/parseJson.ts
+++ packages/string/src/parseJson.ts
@@ -11,9 +11,10 @@ type Json =
  * @param reviver 一个转换结果的函数。这个函数被对象的每个成员调用。
  *   如果成员包含嵌套对象，则嵌套对象先于父对象进行转换。
  * @returns 返回 `JavaScript` 对象
- * @throw 如果转换失败，则会抛出异常
+ * @throw
+ *  - 如果转换失败，则会抛出异常
+ *  - 如果要解析的内容中包含 `__proto__` 和 `constructor` 属性，则会抛出异常
  * @example
- *
  * ```typescript
  * const s = '{"value":"","number":0,"bool":false}';
  * const ret = await parseJson(s);
@@ -22,11 +23,25 @@ type Json =
  */
 export default function parseJson<T = Json>(
     value: string,
-    reviver?: (this: any, key: string, value: any) => any
+    reviver?: (key: string, value: any) => any
 ): Promise<T> {
     return new Promise(resolve => {
         try {
-            resolve(JSON.parse(value, reviver));
+            const ignoreKeysReg = /^(?:__proto__|constructor)$/;
+
+            resolve(
+                JSON.parse(value, (key: string, value: any) => {
+                    // 剥离原型和构造函数属性以防止原型污染
+                    if (ignoreKeysReg.test(key)) {
+                        throw new Error(
+                            '[parseJson] 在解析的 JSON 中，不可以包含 "__proto__" 和 "constructor" 属性，必须要防止原型污染',
+                            { cause: { value, reviver } }
+                        );
+                    }
+
+                    return reviver ? reviver(key, value) : value;
+                })
+            );
         } catch (error) {
             throw new Error('[parseJson] 解析 JSON 对象失败', {
                 cause: { value, reviver, error }
