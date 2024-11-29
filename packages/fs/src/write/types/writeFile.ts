45e013e6431b26cd7b60b8c9dbc66f97a4e9f074
wtklbm <wtklbm@gmail.com>
2024-11-29 18:58:40 +0800

    🌈 style: 删除 `isFormat` 选项

--- packages/fs/src/write/types/writeFile.ts
+++ packages/fs/src/write/types/writeFile.ts
@@ -2,9 +2,6 @@ export type WriteFileOptions = {
     /** 当目录不存在时，是否自动创建目录，然后在从该目录下写文件, 默认为 `true` */
     isMkdir?: boolean;
 
-    /** 是否把对象和数组进行序列化，序列化之后会将数据转换为 `JSON` 格式，默认为 `true` */
-    isFormat?: boolean;
-
     /** 文件的编码，默认为 `utf8` */
     encoding?: BufferEncoding;
 
