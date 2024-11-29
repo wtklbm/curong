6c50aac7f3424694134b05dd6b5ba50b409789f8
wtklbm <wtklbm@gmail.com>
2024-11-30 00:19:07 +0800

    🦄 refactor: 重写逻辑

--- packages/fs/src/remove/rm.ts
+++ packages/fs/src/remove/rm.ts
@@ -1,29 +1,30 @@
-import type { PathLike } from 'fs';
-import { rmdir, unlink } from 'fs/promises';
-
-import isDir from '../is/isDir';
-import isFile from '../is/isFile';
+import type { PathLike, RmOptions } from 'fs';
+import { rm as originRm } from 'fs/promises';
 
 /**
  * 删除一个文件或一个文件夹
  *
  * @param path 要删除的文件或文件夹路径
- * @throws
- *
- * - 如果 `path` 不是一个有效路径，则会抛出异常
+ * @param options 配置选项
+ *  - `force`: 当 `true` 时，如果 `path` 不存在，异常将被忽略。默认为 `false`
+ *  - `maxRetries`: 如果遇到 `EBUSY`、`EMFILE`、`ENFILE`、`ENOTEMPTY` 或 `EPERM` 错误，
+ *    Node.js 将重试该操作，每次尝试时线性退避等待时间延长 `retryDelay` 毫秒。
+ *    该选项代表重试次数。如果 `recursive` 选项不是 `true`，则忽略此选项。
+ *    默认为 `0`
+ *  - `recursive`: 是否递归删除目录。默认为 `true`。在递归模式下，操作失败时会重试
+ *  - `retryDelay`: 重试之间等待的时间 (以毫秒为单位)
+ *     如果 `recursive` 选项不是 `true`，则忽略此选项。
+ * @throws 如果删除失败，则会抛出异常
  */
-export default async function rm(path: PathLike): Promise<void> {
-    if (await isFile(path)) {
-        return await unlink(path).catch(error => {
-            throw new Error('[rm] 删除文件失败', { cause: { path, error } });
-        });
-    }
+export default async function rm(
+    path: PathLike,
+    options?: RmOptions
+): Promise<void> {
+    options = { recursive: true, ...options };
 
-    if (await isDir(path)) {
-        return await rmdir(path, { recursive: true }).catch(error => {
-            throw new Error('[rm] 删除文件夹失败', { cause: { path, error } });
+    return await originRm(path, options).catch(error => {
+        throw new TypeError('[rm] 无法删除文件或目录', {
+            cause: { path, options, error }
         });
-    }
-
-    throw new TypeError('[rm] path 不是一个文件或文件夹', { cause: { path } });
+    });
 }
