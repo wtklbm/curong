a50eab24601fd93693565fee121112dd1afb2b02
wtklbm <wtklbm@gmail.com>
2024-11-30 23:15:08 +0800

    ✨ feat: 添加 `setTimeoutDynamic` 方法

--- packages/function/src/timeout/index.ts
+++ packages/function/src/timeout/index.ts
@@ -1,6 +1,9 @@
 export { default as clearTimeoutAll } from './clearTimeoutAll';
 export { default as nextTick } from './nextTick';
 export { default as setTimeout } from './setTimeout';
+export { default as setTimeoutDynamic } from './setTimeoutDynamic';
 export { default as timeoutLog } from './timeoutLog';
 export { default as timeoutMsResolve } from './timeoutMsResolve';
 export { default as timeoutOr } from './timeoutOr';
+
+export * from './types';
