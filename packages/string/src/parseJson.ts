c0597eae553c74dab66d7093cbc3d8bf9c53904a
wtklbm <wtklbm@gmail.com>
2024-11-30 15:27:30 +0800

    🦄 refactor: 代码回退

--- packages/string/src/parseJson.ts
+++ packages/string/src/parseJson.ts
@@ -7,9 +7,7 @@ import type { Json } from '@curong/types';
  * @param reviver 一个转换结果的函数。这个函数被对象的每个成员调用。
  *   如果成员包含嵌套对象，则嵌套对象先于父对象进行转换。
  * @returns 返回 `JavaScript` 对象
- * @throw
- *  - 如果转换失败，则会抛出异常
- *  - 如果要解析的内容中包含 `__proto__` 和 `constructor` 属性，则会抛出异常
+ * @throw 如果转换失败，则会抛出异常
  * @example
  * ```typescript
  * const s = '{"value":"","number":0,"bool":false}';
@@ -23,21 +21,7 @@ export default function parseJson<T = Json>(
 ): Promise<T> {
     return new Promise(resolve => {
         try {
-            const ignoreKeysReg = /^(?:__proto__|constructor)$/;
-
-            resolve(
-                JSON.parse(value, (key: string, value: any) => {
-                    // 剥离原型和构造函数属性以防止原型污染
-                    if (ignoreKeysReg.test(key)) {
-                        throw new Error(
-                            '[parseJson] 在解析的 JSON 中，不可以包含 "__proto__" 和 "constructor" 属性，必须要防止原型污染',
-                            { cause: { value, reviver } }
-                        );
-                    }
-
-                    return reviver ? reviver(key, value) : value;
-                })
-            );
+            resolve(JSON.parse(value, reviver));
         } catch (error) {
             throw new Error('[parseJson] 解析 JSON 对象失败', {
                 cause: { value, reviver, error }
