bd9be47e0039ab6aa2d105ebfa1c47dd1d5a5fca
wtklbm <wtklbm@gmail.com>
2024-12-03 19:36:51 +0800

    🌈 style: 移动 `currying` 到目录中

---
+++ packages/function/src/currying/index.ts
@@ -0,0 +1,1 @@
+export { default as currying } from './currying';
--- packages/function/src/index.ts
+++ packages/function/src/index.ts
@@ -1,10 +1,9 @@
-export { default as currying } from './currying';
-
 export * from './cache';
 export * from './catch';
 export * from './chain';
 export * from './class';
 export * from './control';
+export * from './currying';
 export * from './delay';
 export * from './limit';
 export * from './promise';
