cd8f4ff2a02682ae23128fad706b0af2d6d9b334
wtklbm <wtklbm@gmail.com>
2024-12-03 21:07:04 +0800

    📃 docs: 更新文档

--- packages/string/src/random/randomHexColor.ts
+++ packages/string/src/random/randomHexColor.ts
@@ -6,9 +6,10 @@ import randomUint8 from './constants/randomUint8';
  * 该颜色值的格式为 `#RRGGBB`，其中 `RR`、`GG` 和 `BB` 是从 `00` 到 `FF` 的随机十六进制数。
  *
  * @param isSafe 是否生成安全的颜色值，默认为 `false`
+ *  - `true`: 使用 `crypto.getRandomValues` 方法来生成随机数，运行速度慢，安全
+ *  - `false`: 使用 `Math.random` 方法来生成随机数，运行速度快，不安全
  * @returns 返回生成的十六进制颜色字符串
  * @example
- *
  * ```typescript
  * console.log(randomHexColor()); // #A3E12F
  * console.log(randomHexColor(true)); // #09F166
