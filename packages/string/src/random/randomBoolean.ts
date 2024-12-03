9f73a7a4b2d7534d66c5eeda2274ec309b198575
wtklbm <wtklbm@gmail.com>
2024-12-03 21:06:57 +0800

    📃 docs: 更新文档

--- packages/string/src/random/randomBoolean.ts
+++ packages/string/src/random/randomBoolean.ts
@@ -4,9 +4,10 @@ import randomUint8 from './constants/randomUint8';
  * 生成一个随机的布尔值
  *
  * @param isSafe 是否生成安全的布尔值，默认为 `false`
+ *  - `true`: 使用 `crypto.getRandomValues` 方法来生成随机数，运行速度慢，安全
+ *  - `false`: 使用 `Math.random` 方法来生成随机数，运行速度快，不安全
  * @returns 返回 `true` 或 `false`
  * @example
- *
  * ```typescript
  * console.log(randomBoolean()); // true
  * console.log(randomBoolean(true)); // false
