014621c62a89a425a206dfd7c4d666e457e9cf22
wtklbm <wtklbm@gmail.com>
2024-12-03 19:39:38 +0800

    ✨ feat: 添加 `noop` 方法

--- packages/function/src/index.ts
+++ packages/function/src/index.ts
@@ -5,6 +5,7 @@ export * from './class';
 export * from './control';
 export * from './currying';
 export * from './delay';
+export * from './helper';
 export * from './limit';
 export * from './promise';
 export * from './select';
