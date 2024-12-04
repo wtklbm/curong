436d6aa89fb8989367cf40011f144a82c7e018ed
wtklbm <wtklbm@gmail.com>
2024-12-05 00:42:29 +0800

    ✨ feat: 添加 `uniqueBy` 方法

--- packages/array/src/unique/index.ts
+++ packages/array/src/unique/index.ts
@@ -1,3 +1,4 @@
 export { default as isUnique } from './isUnique';
 export { default as uniq } from './uniq';
+export { default as uniqueBy } from './uniqueBy';
 export { default as uniqueByKey } from './uniqueByKey';
