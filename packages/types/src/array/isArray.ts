e8b52b4bb2501a33acdb1b6532aa4ecd2c5b7c87
wtklbm <wtklbm@gmail.com>
2024-12-05 16:06:51 +0800

    🎈 perf: 更换判断方法

--- packages/types/src/array/isArray.ts
+++ packages/types/src/array/isArray.ts
@@ -1,3 +1,5 @@
+import getTagEqual from '../type/getTagEqual';
+
 /**
  * 是不是一个数组
  *
@@ -8,6 +10,6 @@
  * 该方法将首先尝试调用 `Array.isArray`，如果调用失败，则尝试通过 `Object.prototype.toString.call` 获取标签，判断是否等于 `[object Array]`。
  */
 export default function isArray<T>(value: unknown): value is T[] {
-    // 也可以判断 `getTagEqual(value, 'Array')`
-    return Array.isArray(value);
+    // 也可以判断 `Array.isArray?.(value)`
+    return getTagEqual(value, 'Array');
 }
