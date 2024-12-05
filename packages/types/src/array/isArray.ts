b8f20d49e18aac5dda4000dcdcef8f3aa8c323bd
wtklbm <wtklbm@gmail.com>
2024-12-05 15:04:33 +0800

    🦄 refactor: 删除多余判断

--- packages/types/src/array/isArray.ts
+++ packages/types/src/array/isArray.ts
@@ -1,5 +1,3 @@
-import getTagEqual from '../type/getTagEqual';
-
 /**
  * 是不是一个数组
  *
@@ -9,8 +7,7 @@ import getTagEqual from '../type/getTagEqual';
  *
  * 该方法将首先尝试调用 `Array.isArray`，如果调用失败，则尝试通过 `Object.prototype.toString.call` 获取标签，判断是否等于 `[object Array]`。
  */
-export default function isArray<T extends unknown[]>(
-    value: unknown
-): value is T {
-    return Array.isArray?.(value) || getTagEqual(value, 'Array');
+export default function isArray<T>(value: unknown): value is T[] {
+    // 也可以判断 `getTagEqual(value, 'Array')`
+    return Array.isArray(value);
 }
