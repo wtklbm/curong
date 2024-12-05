8cbdafbcbb0cda1a3053d06c2f1e55583117868f
wtklbm <wtklbm@gmail.com>
2024-12-05 16:08:07 +0800

    🎈 perf: 更换参数类型

--- packages/array/src/convert/flat.ts
+++ packages/array/src/convert/flat.ts
@@ -3,17 +3,17 @@ import { isArray } from '@curong/types';
 /**
  * 展平一个多维数组
  *
- * @param value 多维数组
- * @returns 返回一个一位数组
+ * @param args 一个或多个数组
+ * @returns 返回一个一维数组
  * @example
  * ```typescript
  * const ret = flat([1, [[2], 3], 4]);
  * console.log(ret); // [ 1, 2, 3, 4 ]
  * ```
  */
-export default function flat<T>(value: T[]): T[] {
-    return value.reduce<T[]>(
-        (a, b) => a.concat(isArray(b) ? (flat(b) as T[]) : b),
+export default function flat<T>(...args: (T | T[])[]): T[] {
+    return args.reduce<T[]>(
+        (a, b) => a.concat(isArray(b) ? flat(...b) : b),
         []
     );
 }
