705507857cac9c898110b79204c8a017213947a3
wtklbm <wtklbm@gmail.com>
2024-12-10 21:38:36 +0800

    📃 docs: 更新文档

--- packages/number/src/toInt.ts
+++ packages/number/src/toInt.ts
@@ -7,7 +7,7 @@ import { isString, isZero } from '@curong/types';
  * @returns 转换后的整数值
  * @throws
  *  - 如果 `value` 为空字符串，则会抛出类型异常
- *  - 如果 `value` 转换为数字后为 `NaN` 或正负 `Infinity`，则会抛出类型异常\
+ *  - 如果 `value` 转换为数字后为 `NaN` 或正负 `Infinity`，则会抛出类型异常
  * @note 最简便的转换为整数的方法为 `value >> 0`
  * @example
  * ```typescript
